43e3f2466397ccd4cf860c53e7179cd06bd1e93f
Forster Groux <groux.forster@gmail.com>
2020-12-23 22:07:45 +0100

    basic cleanup

---
+++ front-end/src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
