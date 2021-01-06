8ea5c72a28cbe520a9e46e4bb10e4d2ac880d8cd
Forster Groux <groux.forster@gmail.com>
2021-01-06 18:53:59 +0100

    add basic navbar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <div id="nav">
-    <h1>Ma Cantine Egalim</h1>
-    <Welcome msg="Tout savoir sur la loi Egalim"/>
+  <Navbar/>
 
-    <router-link to="/about">About</router-link>
-  </div>
+  <router-link to="/About">About</router-link>
+
   <router-view/>
 </template>
 
 <script>
 // @ is an alias to /src
-import Welcome from '@/components/Welcome.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   components: {
-    Welcome
+    Navbar
   }
 }
 </script>
@@ -26,18 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,19 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import About from '@/views/About.vue';
 
 const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About,
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})
+});
 
-export default router
+export default router;
