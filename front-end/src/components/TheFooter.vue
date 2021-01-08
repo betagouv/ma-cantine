45ca50faa92fb42f1fe8538b9da1ab1246dd0020
Forster Groux <groux.forster@gmail.com>
2021-01-08 18:31:26 +0100

    add footer

---
+++ front-end/src/components/TheFooter.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="footer">
+    <div id="logos">
+      <img src="@/assets/Marianne.png" id="Marianne">
+      <img src="@/assets/beta-gouv-logo.jpg" id="Marianne">
+    </div>
+    <div id="footer-columns">
+      <div class="footer-column">
+        <div class="footer-column-title">Ma Cantine</div>
+        <a class="navbar-item" href="https://beta.gouv.fr/startups/egalim.html" target="_blank">
+          Qui sommes nous ? <i class="fas fa-external-link-alt"></i>
+        </a>
+        <a class="navbar-item" href="https://beta.gouv.fr/approche/" target="_blank">
+          Les start-ups d'état <i class="fas fa-external-link-alt"></i>
+        </a>
+        <a class="navbar-item" href="mailto:contact@egalim.beta.gouv.fr">Contactez-nous !</a>
+      </div>
+      <div class="footer-column">
+        <div class="footer-column-title">Aidez-nous à améliorer cet outils</div>
+        <a class="navbar-item" href="https://github.com/betagouv/ma-cantine" target="_blank">
+          Contribuer sur Github <i class="fas fa-external-link-alt"></i>
+        </a>
+        <a class="navbar-item" href="https://github.com/betagouv/ma-cantine/issues" target="_blank">
+          Envoyer une suggestion <i class="fas fa-external-link-alt"></i>
+        </a>
+      </div>
+      <div class="footer-column">
+        <div class="footer-column-title">En collaboration avec</div>
+        <a class="navbar-item" href="https://agriculture.gouv.fr/thematique-generale/alimentation" target="_blank">
+          DGAL (Ministère de l'agriculture) <i class="fas fa-external-link-alt"></i>
+        </a>
+        <a class="navbar-item" href="https://beta.gouv.fr/" target="_blank">
+          beta.gouv.fr <i class="fas fa-external-link-alt"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+}
+</script>
+
+<style scoped>
+  #footer {
+    display: flex;
+    padding: 50px;
+    border-top: 1px solid #F7B48E;
+  }
+
+  #logos {
+    width: 240px;
+    min-height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  #footer-columns {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .footer-column {
+    display: flex;
+    flex-direction: column;
+    font-size: 14px;
+    color: #414141;
+    text-align: left;
+  }
+
+  .footer-column-title {
+    font-weight: bold;
+    line-height: 24px;
+    margin-bottom: 20px;
+  }
+
+  .footer-column a {
+    text-decoration: none;
+    line-height: 30px;
+    color: #414141;
+  }
+
+  .footer-column a:hover {
+    text-decoration: underline;
+  }
+</style>
