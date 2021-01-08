21ad2f9a0e50ac548053bf18f562b83dd545417a
Helen Root <helenroot1@gmail.com>
2021-01-08 16:23:00 +0100

    Ajouter Raleway typographie

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -20,8 +20,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Raleway, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
