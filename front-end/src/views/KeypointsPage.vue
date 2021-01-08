aed7c7749cae9c0a3af5e3e0fe32cd2a26b8615a
Helen Root <helenroot1@gmail.com>
2021-01-08 12:34:19 +0100

    Further tag styling and alternating alignment improvements

--- front-end/src/views/KeypointsPage.vue
+++ front-end/src/views/KeypointsPage.vue
@@ -12,16 +12,20 @@
       <img src="@/assets/lighthouse.svg" id="lighthouse">
     </div>
     <div id="points">
-      <div v-for="point in keyPoints" :key="point.id">
+      <div v-for="point in keyPoints" :key="point.id" :id="point.id">
         <h2>{{point.title}}</h2>
         <div class="tags" v-if="point.tags">
-          <p v-for="tag in point.tags" :key="tag">{{tags.find(element => element.id === tag).title}}</p>
+          <p v-for="tag in point.tags" :key="tag" :style="tags.find(element => element.id === tag).style">
+            {{tags.find(element => element.id === tag).title}}
+          </p>
         </div>
         <p class="deadline" v-if="point.deadline">{{point.deadline}}</p>
         <div v-for="subPoint in point.subPoints" :key="subPoint.id">
           <h3>{{subPoint.title}}</h3>
           <div class="tags" v-if="subPoint.tags">
-            <p v-for="tag in subPoint.tags" :key="tag">{{tags.find(element => element.id === tag).title}}</p>
+            <p v-for="spTag in subPoint.tags" :key="spTag" :style="tags.find(element => element.id === spTag).style">
+              {{tags.find(element => element.id === spTag).title}}
+            </p>
           </div>
           <p class="deadline" v-if="subPoint.deadline">{{subPoint.deadline}}</p>
           <p class="description">{{subPoint.description}}</p>
@@ -72,6 +76,41 @@ h1 {
   color: rgba(64,64,64,0.87);
 }
 
+/* measures styling */
+#points {
+  padding: 2em;
+}
+
+.tags {
+  display: flex;
+}
+
+.tags > p {
+  height: 20px;
+  padding: 3px 5px;
+  left: 0px;
+  top: 0px;
+  border-radius: 50px;
+  color: #FFF;
+}
+
+.deadline::before {
+  content: "📅 ";
+}
+
+/* alternating alignment of measures content left and right */
+#qualite-durable, #contre-gaspillage {
+  text-align: left;
+}
+
+#information {
+  text-align: right;
+}
+
+#information .tags {
+  justify-content: flex-end;
+}
+
 </style>
 
 <script>
@@ -83,7 +122,8 @@ export default {
           id: "qualite-durable",
           title: "🍎 Au moins 50% de produits de qualité et durables dont 20% de bio",
           tags: [
-            "scolaire"
+            "scolaire",
+            "administration"
           ],
           deadline: "1er janvier 2022",
           subPoints: [
@@ -125,19 +165,33 @@ export default {
               id: "affichage",
               title: "Expérimentation de l’affichage de la nature des produits (facultatif)",
               tags: [
-                "scolaire"
+                "scolaire",
+                "administration"
               ],
               deadline: "Du 14 avril 2019 au 31 octobre 2021",
               description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
           ]
         },
+        {
+          id: "contre-gaspillage",
+          title: "🙌🏻 Lutte contre le gaspillage alimentaire et dons alimentaires"
+        }
       ],
       tags: [
         {
           id: "scolaire",
           title: "Scolaire",
-          color: "#EB5C2E"
+          style: {
+            "background-color": "#EB5C2E"
+          }
+        },
+        {
+          id: "administration",
+          title: "Administration",
+          style: {
+            "background-color": "rgba(255,82,82,0.28)"
+          }
         }
       ]
     }
