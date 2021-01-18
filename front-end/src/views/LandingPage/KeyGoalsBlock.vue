a0b8e91b0e5d402d81221a92b3660405b0bde270
Forster Groux <groux.forster@gmail.com>
2021-01-18 16:48:03 +0100

    move key goals block into new component

---
+++ front-end/src/views/LandingPage/KeyGoalsBlock.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="key-goals">
+    <h2>Les objectifs de la loi EGAlim</h2>
+    <div id="key-goals-list">
+      <div class="key-goal">
+        <img src="@/assets/broco.png" alt="">
+        <p>Promouvoir une alimentation saine et respectueuse de l'environnement.</p>
+      </div>
+      <div class="key-goal">
+        <img src="@/assets/convive.png" alt="">
+        <p>Réduire les inégalités d'accès à une alimentation durable et de qualité.</p>
+      </div>
+      <div class="key-goal">
+        <img src="@/assets/checklist.png" alt="">
+        <p>Assurer la juste rémunération des agriculteurs du secteur agricole.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  h2 {
+    font-weight: bold;
+    font-size: 37px;
+    line-height: 52px;
+    color: #EB5B25;
+  }
+
+  #key-goals {
+    margin-top: 50px;
+    background-color: #FDEFE9;
+    padding: 30px;
+  }
+
+  #key-goals-list {
+    display: flex;
+    justify-content: space-evenly;
+    margin: 30px 0;
+  }
+
+  .key-goal {
+    width: 250px;
+    font-size: 20px;
+    line-height: 20px;
+    color: #333333;
+  }
+
+  .key-goal > img {
+    width: 60px;
+    margin-bottom: 20px;
+  }
+</style>
