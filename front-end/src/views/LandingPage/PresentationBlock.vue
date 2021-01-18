6fa24084af84b8c98e01f93360dd4bf00ffd0525
Forster Groux <groux.forster@gmail.com>
2021-01-18 16:38:55 +0100

    move presentation block into new component

---
+++ front-end/src/views/LandingPage/PresentationBlock.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="presentation">
+    <div class="presentation-content">
+      <h1>Approvisionner ma cantine en denrées de qualité</h1>
+      <p>
+        La loi EGAlim vise à apporter une alimentation saine, de qualité et plus durable dans nos assiettes.
+      </p>
+      <p>
+        Vous êtes en charge de l'approvisionnement d'une restauration collective ?
+      </p>
+      <a :href="`${publicPath}Diagnostic approvisionnement (ma-cantine-alpha) v0.1.ods`" download class="presentation-diagnostic">
+        Télécharger notre calculateur
+      </a>
+    </div>
+    <img src="@/assets/toque.svg" class="presentation-right-image">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        publicPath: process.env.BASE_URL,
+      };
+    },
+  }
+</script>
+
+<style scoped>
+  #presentation {
+    display: flex;
+    justify-content: space-around;
+    padding: 20px;
+  }
+
+  .presentation-content {
+    text-align: left;
+    max-width: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-size: 20px;
+    color: rgba(0, 0, 0, 0.60);
+  }
+
+  h1 {
+    font-weight: bold;
+    font-size: 37px;
+    line-height: 52px;
+    color: #EB5B25;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  .presentation-diagnostic {
+    text-align: center;
+    max-width: 400px;
+    height: 50px;
+    line-height: 50px;
+    color: white;
+    background-color: #EB5B25;
+    border-radius: 50px;
+    font-weight: bold;
+    text-decoration: none;
+  }
+
+  .presentation-right-image {
+    width: 450px;
+    height: 400px;
+  }
+</style>
--- front-end/src/views/LandingPage/index.vue
+++ front-end/src/views/LandingPage/index.vue
@@ -1,19 +1,5 @@
 <template>
-  <div id="presentation">
-    <div class="presentation-content">
-      <h1>Approvisionner ma cantine en denrées de qualité</h1>
-      <p>
-        La loi EGAlim vise à apporter une alimentation saine, de qualité et plus durable dans nos assiettes.
-      </p>
-      <p>
-        Vous êtes en charge de l'approvisionnement d'une restauration collective ?
-      </p>
-      <a :href="`${publicPath}Diagnostic approvisionnement (ma-cantine-alpha) v0.1.ods`" download class="presentation-diagnostic">
-        Télécharger notre calculateur
-      </a>
-    </div>
-    <img src="@/assets/toque.svg" class="presentation-right-image">
-  </div>
+  <PresentationBlock/>
 
   <div id="main-goal">
     <h2>Les objectifs de la loi EGAlim</h2>
@@ -74,49 +60,28 @@
   </div>
 </template>
 
+<script>
+  import PresentationBlock from '@/views/LandingPage/PresentationBlock';
+  import keyMeasures from "@/data/key-measures.json";
+
+  export default {
+    components: {
+      PresentationBlock,
+    },
+    data() {
+      return {
+        keyMeasures,
+      };
+    },
+  }
+</script>
+
 <style scoped>
-#presentation {
-  display: flex;
-  justify-content: space-around;
-  padding: 20px;
-}
-
-.presentation-content {
-  text-align: left;
-  max-width: 600px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  font-size: 20px;
-  color: rgba(0, 0, 0, 0.60);
-}
-
-h1, h2 {
+h2 {
   font-weight: bold;
   font-size: 37px;
   line-height: 52px;
   color: #EB5B25;
-}
-
-p {
-  margin: 0;
-}
-
-.presentation-diagnostic {
-  text-align: center;
-  max-width: 400px;
-  height: 50px;
-  line-height: 50px;
-  color: white;
-  background-color: #EB5B25;
-  border-radius: 50px;
-  font-weight: bold;
-  text-decoration: none;
-}
-
-.presentation-right-image {
-  width: 450px;
-  height: 400px;
 }
 
 #main-goal {
@@ -271,16 +236,3 @@ p.discover-measure {
   display: none;
 }
 </style>
-
-<script>
-import keyMeasures from "@/data/key-measures.json";
-
-export default {
-  data() {
-    return {
-      keyMeasures,
-      publicPath: process.env.BASE_URL,
-    };
-  },
-};
-</script>
