50686ede7717514a6c31055cd477fdaf3c3dc3f8
Forster Groux <groux.forster@gmail.com>
2021-01-18 17:00:41 +0100

    move goal block into new component

--- front-end/src/views/LandingPage/index.vue
+++ front-end/src/views/LandingPage/index.vue
@@ -3,19 +3,7 @@
 
   <KeyGoalsBlock/>
 
-  <div id="goal">
-    <div class="goal-information">
-      La restauration collective représente <b>3 milliards de repas par an !</b>
-      Ces repas sont un levier de transition agricole et alimentaire fort.
-    </div>
-    <h3>Les enjeux pour la restauration collective</h3>
-    <div class="goal-list">
-      <ul>La loi EGAlim est issue des États Généraux de l'Alimentation (EGA). Elle permet :
-        <li>✔️ <b>l'équilibre des relations</b> commerciales dans le secteur agricole.</li>
-        <li>✔️ une entrée en vigueur progressive d'<b>une alimentation saine et durable</b></li>
-      </ul>
-    </div>
-  </div>
+  <GoalBlock/>
 
   <div class="divider"></div>
 
@@ -47,12 +35,14 @@
 <script>
   import PresentationBlock from '@/views/LandingPage/PresentationBlock';
   import KeyGoalsBlock from '@/views/LandingPage/KeyGoalsBlock';
+  import GoalBlock from '@/views/LandingPage/GoalBlock';
   import keyMeasures from "@/data/key-measures.json";
 
   export default {
     components: {
       PresentationBlock,
       KeyGoalsBlock,
+      GoalBlock,
     },
     data() {
       return {
@@ -63,38 +53,6 @@
 </script>
 
 <style scoped>
-#goal {
-  margin: 70px auto;
-  max-width: 1200px;
-}
-
-h3 {
-  font-weight: bold;
-  font-size: 37px;
-  line-height: 52px;
-  color: #748852;
-}
-
-.goal-information {
-  width: 850px;
-  padding: 24px;
-  border-radius: 20px;
-  margin: auto;
-  text-align: left;
-  color: #748852;
-  background-color: rgba(185, 195, 168, 0.3);
-  font-size: 20px;
-}
-
-.goal-list {
-  width: 850px;
-  margin: auto;
-  margin-top: 30px;
-  text-align: left;
-  font-size: 20px;
-  line-height: 35px;
-}
-
 .divider {
   height: 50px;
   background-image: url('../../assets/divider-section.png');
