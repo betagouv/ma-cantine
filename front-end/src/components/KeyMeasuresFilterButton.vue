31bc06bbb509289a84f2e7ac23bfb6b982af62a0
Helen Root <helenroot1@gmail.com>
2021-01-19 12:02:41 +0100

    Add borders to the filter buttons on hover

--- front-end/src/components/KeyMeasuresFilterButton.vue
+++ front-end/src/components/KeyMeasuresFilterButton.vue
@@ -5,10 +5,12 @@
 <style scoped>
 .filter-button {
   border: none;
+  border: 2px solid #fff;
   background-color: #fff;
   cursor: pointer;
   margin: 0.2em 0em;
   padding: 0.5em 1em;
+  border-radius: 42px;
   font-weight: bold;
   font-size: 20px;
   /* Dark 1 */
@@ -18,9 +20,12 @@
 .filter-button.active {
   /* Green 3 */
   background: #F1F3EE;
-  border-radius: 42px;
   /* Green 1 */
   color: #748852;
+}
+
+.filter-button:hover {
+  border-color: #F1F3EE;
 }
 </style>
 
