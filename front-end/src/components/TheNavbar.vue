b857c7f696cbba1787ca08eb17a551d4ab207fa6
Forster Groux <groux.forster@gmail.com>
2021-01-21 14:14:56 +0100

    make navbar responsive

--- front-end/src/components/TheNavbar.vue
+++ front-end/src/components/TheNavbar.vue
@@ -7,8 +7,8 @@
         <div id="slogan">Pour une alimentation saine, de qualité et plus durable dans nos assiettes grâce à EGAlim</div>
       </div>
     </router-link>
-    <router-link :to="{ name: 'KeyMeasuresPage' }" class="navbar-item">Mesures-phares</router-link>
-    <a class="navbar-item" href="https://beta.gouv.fr/startups/egalim.html" target="_blank">
+    <router-link :to="{ name: 'KeyMeasuresPage' }" class="navbar-item key-measures-link">Mesures-phares</router-link>
+    <a class="navbar-item about" href="https://beta.gouv.fr/startups/egalim.html" target="_blank">
       À propos <i class="fas fa-external-link-alt"></i>
     </a>
   </div>
@@ -22,72 +22,96 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
-  text-decoration: none;
-}
+  a {
+    text-decoration: none;
+  }
 
-#navbar {
-  top: 0;
-  z-index: 1000;
-  position: fixed;
-  width: 96%;
-  padding: 10px 2%;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid #E1E1E1;
-  background-color: white;
-}
+  #navbar {
+    top: 0;
+    z-index: 1000;
+    position: fixed;
+    width: 96%;
+    padding: 10px 2%;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #E1E1E1;
+    background-color: white;
+  }
 
-#goto-landing {
-  display: flex;
-}
+  #goto-landing {
+    display: flex;
+  }
 
-#Marianne {
-  height: 75px;
-}
+  #Marianne {
+    height: 75px;
+  }
 
-#brand {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  margin-left: 50px;
-  text-align: left;
-  max-width: 300px;
-  text-decoration: none;
-}
+  #brand {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-left: 50px;
+    text-align: left;
+    max-width: 300px;
+    text-decoration: none;
+  }
 
-#logo {
-  font-style: italic;
-  font-weight: 200;
-  font-size: 25px;
-  line-height: 29px;
-  color: rgba(64, 64, 64, 0.85);
-}
+  #logo {
+    white-space: nowrap;
+    font-style: italic;
+    font-weight: 200;
+    font-size: 25px;
+    line-height: 29px;
+    color: rgba(64, 64, 64, 0.85);
+  }
 
-#ma-cantine {
-  font-weight: bold;
-  color: #EB5B25;
-}
+  #ma-cantine {
+    font-weight: bold;
+    color: #EB5B25;
+  }
 
-#slogan {
-  font-size: 11px;
-  font-weight: 600;
-  line-height: 15px;
-  color: rgba(0,0,0,0.83);
-}
+  #slogan {
+    font-size: 11px;
+    font-weight: 600;
+    line-height: 15px;
+    color: rgba(0,0,0,0.83);
+  }
 
-.navbar-item {
-  margin: auto 0;
-  border-radius: 25px;
-  padding: 15px;
-  font-size: 17px;
-  font-weight: bold;
-  line-height: 20px;
-  color: rgba(64, 64, 64, 0.85);
-}
+  .navbar-item {
+    margin: auto 0;
+    border-radius: 25px;
+    padding: 15px;
+    font-size: 17px;
+    font-weight: bold;
+    line-height: 20px;
+    color: rgba(64, 64, 64, 0.85);
+  }
 
-.navbar-item:hover {
-  color: white;
-  background-color: #F7B48E;
-}
+  .navbar-item:hover {
+    color: white;
+    background-color: #F7B48E;
+  }
+
+  @media (max-width: 800px) {
+    #Marianne, .about {
+      display: none;
+    }
+
+    #brand {
+      margin-left: 0;
+    }
+  }
+
+  @media (max-width: 480px) {
+    #navbar {
+      flex-direction: column;
+      position: relative;
+    }
+
+    .key-measures-link {
+      text-align: left;
+      margin-top: 30px;
+      padding: 3px;
+    }
+  }
 </style>
