fc91c9231e410a9b8021ba0cf02da45aef393eab
Helen Root <helenroot1@gmail.com>
2021-01-22 15:37:03 +0100

    Add back class deleted from conflict resolution

--- front-end/src/components/TheNavbar.vue
+++ front-end/src/components/TheNavbar.vue
@@ -8,7 +8,7 @@
       </div>
     </router-link>
     <router-link :to="{ name: 'KeyMeasuresPage' }" class="navbar-item key-measures-link">Mesures phares</router-link>
-    <a class="navbar-item" href="https://beta.gouv.fr/startups/egalim.html" target="_blank">
+    <a class="navbar-item about" href="https://beta.gouv.fr/startups/egalim.html" target="_blank">
       À propos <i class="fas fa-external-link-alt"></i>
     </a>
   </div>
