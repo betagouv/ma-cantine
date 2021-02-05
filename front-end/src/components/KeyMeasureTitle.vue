9f64096ec2d9025d80dc2f3dfe894e6039dab1d0
Helen Root <helenroot1@gmail.com>
2021-02-05 10:51:07 +0100

    Refactor key measures title rendering to share across views

---
+++ front-end/src/components/KeyMeasureTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <span><i :class="'fas '+measure.faIcon" v-if="measure.faIcon"></i>
+   {{measure.title}}</span>
+</template>
+
+<script>
+  export default {
+    name: "KeyMeasureTitle",
+    props: {
+      measure: Object
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .fa-apple-alt, .fa-hands, .fa-wind {
+    color: $orange;
+  }
+  
+  .fa-bullhorn, .fa-leaf {
+    color: $green;
+  }
+</style>
