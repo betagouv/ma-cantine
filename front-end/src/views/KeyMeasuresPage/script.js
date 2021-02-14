60f455edc752deb02e778817a123a3e57a0d6a4b
Forster Groux <groux.forster@gmail.com>
2021-02-14 21:14:30 +0100

    move InfoCard component

--- front-end/src/views/KeyMeasuresPage/script.js
+++ front-end/src/views/KeyMeasuresPage/script.js
@@ -1,14 +1,14 @@
-import InfoCard from '@/views/KeyMeasuresPage/InfoCard'
-import KeyMeasureTitle from '@/components/KeyMeasureTitle'
 import KeyMeasureDescription from '@/components/KeyMeasureDescription'
+import KeyMeasureInfoCard from '@/components/KeyMeasureInfoCard'
+import KeyMeasureTitle from '@/components/KeyMeasureTitle'
 import keyMeasures from '@/data/key-measures.json'
 import tags from '@/data/sector-tags.json'
 
 export default {
   components: {
-    InfoCard,
+    KeyMeasureDescription,
+    KeyMeasureInfoCard,
     KeyMeasureTitle,
-    KeyMeasureDescription
   },
   data() {
     return {
