bc716a456c83aba36e5cbc231329396ba7f2e5fb
Forster Groux <groux.forster@gmail.com>
2021-02-14 21:45:43 +0100

    move KeyMeasuresPage content into children view

---
+++ front-end/src/views/KeyMeasuresPage/KeyMeasuresHome.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="key-measures">
+    <h1>Les 5 mesures phares de la loi EGAlim</h1>
+    <div id="measures" v-for="measure in keyMeasures" :key="measure.id">
+      <KeyMeasure :measure="measure"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import KeyMeasure from '@/components/KeyMeasure'
+  import keyMeasures from '@/data/key-measures.json'
+
+  export default {
+    components: {
+      KeyMeasure,
+    },
+    data() {
+      return {
+        keyMeasures,
+      };
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  #key-measures {
+    text-align: center;
+    padding: 1em 1em;
+
+    h1 {
+      font-weight: bold;
+      font-size: 48px;
+      color: $green;
+      margin: 1em 0em;
+    }
+
+    #measures {
+      text-align: left;
+      display: flex;
+      overflow: hidden;
+      align-items: center;
+      max-width: 1170px;
+      margin: auto;
+    }
+  }
+
+  @media (max-width: 480px) {
+    #key-measures {
+      text-align: center;
+      padding: 1em 0.5em;
+    }
+  }
+</style>
