bb5a4de708a192957ce2afe6d526a3a6a80617bb
Helen Root <helenroot1@gmail.com>
2021-02-15 15:18:03 +0100

    Create page with summary of the diagnostic results

---
+++ front-end/src/data/KeyMeasures.js
@@ -0,0 +1,12 @@
+import keyMeasures from "@/data/key-measures.json"
+
+let statuses = localStorage.getItem('statuses') || "{}";
+statuses = JSON.parse(statuses);
+
+keyMeasures.forEach(measure => {
+  measure.subMeasures.forEach(subMeasure => {
+    subMeasure.status = statuses[subMeasure.id];
+  });
+});
+
+export default keyMeasures;
