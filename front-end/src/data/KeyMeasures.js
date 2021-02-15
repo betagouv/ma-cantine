ceda952fd4daa9cd6f51c4e3515785877e52990f
Helen Root <helenroot1@gmail.com>
2021-02-15 18:55:30 +0100

    Add simple score for each measure

--- front-end/src/data/KeyMeasures.js
+++ front-end/src/data/KeyMeasures.js
@@ -1,12 +1,20 @@
-import keyMeasures from "@/data/key-measures.json"
+import keyMeasures from "@/data/key-measures.json";
 
 let statuses = localStorage.getItem('statuses') || "{}";
 statuses = JSON.parse(statuses);
 
 keyMeasures.forEach(measure => {
+  let score = 0;
   measure.subMeasures.forEach(subMeasure => {
     subMeasure.status = statuses[subMeasure.id];
+    if(subMeasure.status === 'done') {
+      score += 1;
+    } else if(subMeasure.status === 'planned') {
+      score += 0.5;
+    }
   });
+  measure.statusMaxScore = measure.subMeasures.length;
+  measure.statusScore = score;
 });
 
 export default keyMeasures;
