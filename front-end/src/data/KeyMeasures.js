132e26fcd6bbf0529e83a5ab2732230378ca1199
Helen Root <helenroot1@gmail.com>
2021-02-16 12:53:19 +0100

    Add measure score guage

--- front-end/src/data/KeyMeasures.js
+++ front-end/src/data/KeyMeasures.js
@@ -4,17 +4,9 @@ let statuses = localStorage.getItem('statuses') || "{}";
 statuses = JSON.parse(statuses);
 
 keyMeasures.forEach(measure => {
-  let score = 0;
   measure.subMeasures.forEach(subMeasure => {
     subMeasure.status = statuses[subMeasure.id];
-    if(subMeasure.status === 'done') {
-      score += 1;
-    } else if(subMeasure.status === 'planned') {
-      score += 0.5;
-    }
   });
-  measure.statusMaxScore = measure.subMeasures.length;
-  measure.statusScore = score;
 });
 
 export default keyMeasures;
