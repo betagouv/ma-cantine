1a2798d49e62cd20e63fda6b509e1532dc9a7585
Helen Root <helenroot1@gmail.com>
2021-02-16 15:44:22 +0100

    Put all measure status management logic in one place

--- front-end/src/data/KeyMeasures.js
+++ front-end/src/data/KeyMeasures.js
@@ -1,5 +1,17 @@
 import keyMeasures from "@/data/key-measures.json";
 
+function saveStatusesLocally() {
+  let statuses = {};
+  keyMeasures.forEach((measure) => {
+    measure.subMeasures.forEach((subMeasure) => {
+      if(subMeasure.status) {
+        statuses[subMeasure.id] = subMeasure.status;
+      }
+    });
+  });
+  localStorage.setItem('statuses', JSON.stringify(statuses));
+}
+
 let statuses = localStorage.getItem('statuses') || "{}";
 statuses = JSON.parse(statuses);
 
@@ -9,4 +21,7 @@ keyMeasures.forEach(measure => {
   });
 });
 
-export default keyMeasures;
+export {
+  keyMeasures,
+  saveStatusesLocally
+};
