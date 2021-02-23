af0f19e57c323f62c61e26f72d64d7a2f40ea8db
Helen Root <helenroot1@gmail.com>
2021-02-23 10:19:30 +0100

    Input validation and number parsing

--- front-end/src/views/PosterFormPage.vue
+++ front-end/src/views/PosterFormPage.vue
@@ -5,28 +5,67 @@
       <h2>À propos de votre cantine</h2>
       <p>
         <label for="profession">Je suis </label> <input id="profession" v-model="form.profession" class="field" placeholder="chef.fe">
-        dans <label for="school">la cantine du </label> <input id="school" v-model="form.school" class="field" placeholder="nom de l'école">
+        dans <label for="school">la cantine du </label> <input id="school" v-model="form.school" class="field" placeholder="nom de l'école" required="required">
         dans <label for="commune">le commune de </label> <input id="commune" v-model="form.commune" class="field" placeholder="Plouër-sur-Rance">.
       </p>
       <p>
         <label for="servings">Nous servons </label>
-        <input id="servings" v-model="form.servings" class="number-field" inputmode="numeric" pattern="[0-9]*" placeholder="200">
+        <input id="servings"
+          v-model="form.servings"
+          class="number-field"
+          inputmode="numeric"
+          pattern="[0-9]*"
+          placeholder="200"
+          required="required"
+        >
         enfants par jour.
       </p>
       <h2>À propos de vos achats</h2>
       <p>
         <label for="total">Sur l'année de 2020, les achats répresent </label>
-        <input id="total" v-model="form.total" class="number-field" placeholder="1500.00" aria-describedby="euros">
+        <input id="total"
+          v-model="form.total"
+          class="number-field"
+          inputmode="decimal"
+          placeholder="1500,00"
+          :pattern="pattern"
+          aria-describedby="euros"
+          required="required"
+        >
         <span id="euros">euros HT</span>.
       </p>
       <p>
         Sur ce total,
-         <input id="bio" v-model="form.bio" class="number-field" placeholder="300.00" aria-describedby="euros">
-          euros HT correspondaient à des <label for="bio">produits bio</label>,
-        <input id="quality" v-model="form.quality" class="number-field" placeholder="200.00" aria-describedby="euros">
-         euros HT correspondaient à des <label for="quality">produits de qualité et durables (sauf bio)</label>
-         et <input id="equitable" v-model="form.equitable" class="number-field" placeholder="100.00" aria-describedby="euros">
-         euros HT correspondaient à des <label for="equitable">produits du commerce équitable</label>.
+        <input id="bio"
+          v-model="form.bio"
+          class="number-field"
+          inputmode="decimal"
+          placeholder="300,00"
+          :pattern="pattern"
+          aria-describedby="euros"
+          required="required"
+        >
+        euros HT correspondaient à des <label for="bio">produits bio</label>,
+        <input id="quality"
+          v-model="form.quality"
+          class="number-field"
+          inputmode="decimal"
+          placeholder="200,00"
+          :pattern="pattern"
+          aria-describedby="euros"
+          required="required"
+        >
+        euros HT correspondaient à des <label for="quality">produits de qualité et durables (sauf bio)</label> et 
+        <input id="equitable"
+          v-model="form.equitable"
+          class="number-field"
+          inputmode="decimal"
+          placeholder="100,00"
+          :pattern="pattern"
+          aria-describedby="euros"
+          required="required"
+        >
+        euros HT correspondaient à des <label for="equitable">produits du commerce équitable</label>.
       </p>
       <input type="submit" id="submit" value="Générer mon affichage" @click.prevent="submit">
     </form>
@@ -37,11 +76,16 @@
   export default {
     data() {
       return {
-        form: {}
+        form: {},
+        pattern: "[0-9]*(,[0-9]{2})?"
       };
     },
     methods: {
       submit() {
+        this.form["servingsNumber"] = parseInt(this.form["servings"], 10);
+        ["total", "bio", "quality", "equitable"].forEach(key => {
+          this.form[key + "Number"] = parseFloat(this.form[key].replace(',', '.'))
+        });
         console.log(this.form);
       }
     }
