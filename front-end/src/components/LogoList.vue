af4885627045d0951d508920a32dc189cbea4329
Helen Root <helenroot1@gmail.com>
2021-03-11 11:54:40 +0100

    Refactor measure descriptions

--- front-end/src/components/LogoList.vue
+++ front-end/src/components/LogoList.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="logos">
-    <img src="@/assets/logos/label-rouge.png" alt="logo Label Rouge"/>
-    <img src="@/assets/logos/Logo-AOC-AOP.png" alt="logo appellation d’origine"/>
-    <img src="@/assets/logos/IGP.png" alt="logo indication géographique"/>
-    <img src="@/assets/logos/STG.png" alt="logo Spécialité traditionnelle garantie"/>
-    <img src="@/assets/logos/hve.png" alt="logo Haute Valeur Environnementale"/>
-    <img src="@/assets/logos/logo_label-peche-durable.png" alt="logo écolabel pêche durable"/>
-    <img src="@/assets/logos/rup.png" alt="logo Région Ultrapériphérique"/>
-    <img src="@/assets/logos/logo_bio_eurofeuille.png" alt="logo Agriculture Biologique"/>
+    <img
+      v-for="label in labels"
+      :key="label.title"
+      :src="require(`@/assets/quality-labels/${label.src}`)"
+      :alt="label.title"
+      :title="label.title"
+    />
+    <LogoBio/>
   </div>
 </template>
+
+<script>
+  import labels from '@/data/quality-labels.json';
+  import LogoBio from '@/components/LogoBio';
+
+  export default {
+    components: {
+      LogoBio
+    },
+    data() {
+      return {
+        labels
+      }
+    }
+  }
+</script>
 
 <style scoped lang="scss">
   #logos {
