ecf674955c73a936f85c2b0371e1bb4697893886
Helen Root <helenroot1@gmail.com>
2021-03-11 14:48:09 +0100

    Use dynamic component pattern for additional description rendering

--- front-end/src/components/KeyMeasureDescription.vue
+++ front-end/src/components/KeyMeasureDescription.vue
@@ -2,9 +2,7 @@
   <div class="description-container">
     <p class="description" v-if="measure.htmlDescription" v-html="measure.htmlDescription"></p>
     <p class="description" v-else-if="measure.description">{{measure.description}}</p>
-    <KeyMeasureDescriptionQuality v-if="measure.id === 'qualite-des-produits'"/>
-    <KeyMeasureDescriptionLabels v-if="measure.id === 'cinquante'" class="description"/>
-    <LogoBio id="bio-logo" v-if="measure.id === 'vingt'"/>
+    <component :is="measure.descriptionComponent" v-if="measure.descriptionComponent" class="description"/>
   </div>
 </template>
 
@@ -40,10 +38,5 @@
     font-weight: 400;
     line-height: 1.5em;
     white-space: pre-wrap;
-  }
-
-  #bio-logo {
-    height: 2.9em;
-    margin-top: 1em;
   }
 </style>
