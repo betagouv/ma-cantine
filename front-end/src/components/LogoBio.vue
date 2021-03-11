cff5093ce9a9287ae4fbce497c5d4a98fc6c22c8
Helen Root <helenroot1@gmail.com>
2021-03-11 14:48:09 +0100

    Use dynamic component pattern for additional description rendering

--- front-end/src/components/LogoBio.vue
+++ front-end/src/components/LogoBio.vue
@@ -1,7 +1,15 @@
 <template>
   <img
+    id="bio-logo"
     src="@/assets/quality-labels/logo_bio_eurofeuille.png"
     alt="Logo Agriculture Biologique"
     title="Logo Agriculture Biologique"
   >
 </template>
+
+<style scoped lang="scss">
+  #bio-logo {
+    height: 2.9em;
+    margin-top: 1em;
+  }
+</style>
