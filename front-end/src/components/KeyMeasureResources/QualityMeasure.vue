99c52b46edf28009d8aa64d1aca759656ccae309
Helen Root <helenroot1@gmail.com>
2021-03-16 17:22:31 +0100

    Add resources to measure description pages

---
+++ front-end/src/components/KeyMeasureResources/QualityMeasure.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <a href="Diagnostic approvisionnement (ma-cantine-alpha) v0.2.ods" class="resource calculator" download>
+      <div class="link-icon"><i class="fas fa-calculator"></i></div>
+      <p>
+        Si vous avez besoin d'aide pour calculer votre part de bios et de produits labélisés,
+        vous pouvez tester notre calculateur sous format excel.
+      </p>
+      <div class="link-icon"><i class="fas fa-file-download"></i></div>
+    </a>
+    <a href="https://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit" class="resource" target="_blank">
+      <div class="link-icon"><i class="fas fa-seedling"></i></div>
+      <p>
+        Trouver des catégories de produits en recherchant par labels, siqo et/ou régions
+        avec le moteur de recherche de l'INAO.
+      </p>
+      <div class="link-icon"><i class="fas fa-external-link-alt"></i></div>
+    </a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import '@/styles/key-measure-resource.scss';
+
+  .resource.calculator {
+    background: $light-blue;
+    border-color: $blue;
+
+    .fa-calculator {
+      color: $blue;
+    }
+
+    .fa-file-download {
+      color: $blue;
+    }
+
+    p {
+      color: $blue;
+    }
+  }
+</style>
---
+++ front-end/src/components/KeyMeasureResources/SaveLeftovers.vue
@@ -0,0 +1,14 @@
+<template>
+  <a href="Convention de dons type - restauration collective.pdf" class="resource" download>
+    <div class="link-icon"><i class="fas fa-clipboard"></i></div>
+    <p>
+      Vous voulez éviter le gaspillage ? Télécharger ce modèle type de convention
+      aux associations auxquelles faire des dons.
+    </p>
+    <div class="link-icon"><i class="fas fa-file-download"></i></div>
+  </a>
+</template>
+
+<style scoped lang="scss">
+  @import '@/styles/key-measure-resource.scss';
+</style>
