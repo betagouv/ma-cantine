fc373a7ae96459d82fa6051de1eb49cff47e071e
Helen Root <helenroot1@gmail.com>
2021-03-22 10:49:38 +0100

    Use parent resource component for consistent file structure

---
+++ front-end/src/components/KeyMeasureResource/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <component :is="baseComponent"/>
+</template>
+
+<script>
+  import InformDiners from "./InformDiners";
+  import QualityMeasure from "./QualityMeasure";
+  import WasteMeasure from "./WasteMeasure";
+
+  export default {
+    props: {
+      baseComponent: String
+    },
+    components: {
+      InformDiners,
+      QualityMeasure,
+      WasteMeasure
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .resource, /deep/ .resource {
+    display: flex;
+    width: 90%;
+    margin: auto;
+    margin-top: 3em;
+    align-items: center;
+    justify-content: space-between;
+    background: $light-yellow;
+    border: 4px solid $yellow;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+
+  /deep/ .resource-icon {
+    font-size: 2em;
+    padding: 0.5em;
+    color: $orange;
+  }
+
+  /deep/ .fa-seedling {
+    color: $green;
+  }
+
+  /deep/ .resource-prompt {
+    text-align: left;
+    color: $dark-orange;
+    width: 100%;
+  }
+</style>
