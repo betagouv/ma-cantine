21e0c0be5c44f8f96b31eb847fba1e28d45bdfa6
Helen Root <helenroot1@gmail.com>
2021-03-30 11:15:54 +0200

    Corriger le lien de téléchargement de convention de dons

--- front-end/src/components/KeyMeasureResource/WasteMeasure.vue
+++ front-end/src/components/KeyMeasureResource/WasteMeasure.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="Convention de dons type - restauration collective.pdf" class="resource" download>
+  <a href="/Convention de dons type - restauration collective.pdf" class="resource" download>
     <div class="resource-icon"><i class="fas fa-clipboard"></i></div>
     <p class="resource-prompt">
       Vous voulez éviter le gaspillage ? Télécharger ce modèle type de convention
