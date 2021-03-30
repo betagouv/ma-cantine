3e5aa3be85475b9b6d2f079b944cb17ae6f172c0
Helen Root <helenroot1@gmail.com>
2021-03-30 09:37:51 +0200

    Add in missed api files

--- api/index.js
+++ api/index.js
@@ -1,24 +1,23 @@
 'use strict';
-
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const betaTester = require('./application/beta-tester.js');
 
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost'
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-
-        return 'Hello World!';
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'] // an array of origins or 'ignore'
+      }
     }
   });
 
+  betaTester.register(server);
+
   await server.start();
-  console.log('Server running on %s', server.info.uri);
+    console.log('Server running on %s', server.info.uri);
   };
 
   process.on('unhandledRejection', (err) => {
