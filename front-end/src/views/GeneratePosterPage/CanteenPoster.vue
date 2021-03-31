40615ac4076132a156d7e074b783b7186978c723
Jenn S <jennifer.stephan@edu.escpeurope.eu>
2021-03-31 11:55:36 +0200

    Update CanteenPoster.vue
    correction typos ds une phrase

--- front-end/src/views/GeneratePosterPage/CanteenPoster.vue
+++ front-end/src/views/GeneratePosterPage/CanteenPoster.vue
@@ -17,7 +17,7 @@
       <h2>Pourquoi je vois cette affiche ?</h2>
       <p>
         À partir du 1er janvier 2020, les gestionnaires de restaurant collectif doivent informer les convives une fois par an de la part des produits de qualité et durables entrant dans la composition des repas servis
-        ainsi des démarches entreprises pour développer des produits issus du commerce équitable. Ces informations sont fonctions de la valeur totale des achats réalisés sur année.
+        ainsi des démarches entreprises pour développer des produits issus du commerce équitable. Ces informations sont fonction de la valeur totale des achats réalisés sur l'année.
       </p>
       <p>
         <b>Un bon moyen de savoir ce qu'il y a dans votre assiette et d'en discuter avec le personnel de votre restaurant !</b>
