5fa51a590a770bc55152b8526319311441e1f037
forster <groux.forster@gmail.com>
2021-03-31 20:48:49 +0200

    refactor inputs

--- front-end/src/components/KeyMeasureDiagnostic/DiversificationMeasure.vue
+++ front-end/src/components/KeyMeasureDiagnostic/DiversificationMeasure.vue
@@ -1,59 +1,39 @@
 <template>
-  <CheckboxUnique
+  <BaseCheckbox
     v-model="diagnostic.hasMadeDiversificationPlan"
     :label="'J’ai mis en place un plan pluriannuel de diversification des protéines'"
     :inputId="'plan-made'"
   />
 
-  <fieldset class="form-bloc">
-    <legend>J’ai mis en place un menu végétarien dans ma cantine :</legend>
+  <BaseRadios
+    v-model="diagnostic.vegetarianFrequency"
+    :legend="'J’ai mis en place un menu végétarien dans ma cantine :'"
+    :options="[
+      { inputId: 'less-than-once', value: 'lessThanOnce', label: 'Moins d\'une fois par semaine' },
+      { inputId: 'once', value: 'once', label: 'Une fois par semaine' },
+      { inputId: 'more-than-once', value: 'moreThanOnce', label: 'Plus d\'une fois par semaine' },
+    ]"
+  />
 
-    <div class="form-group">
-      <div class="form-item">
-        <input id="less-than-once" value="lessThanOnce" v-model="diagnostic.vegetarianFrequency" type="radio" >
-        <label for="less-than-once">Moins d'une fois par semaine</label>
-      </div>
-
-      <div class="form-item">
-        <input id="once" value="once" v-model="diagnostic.vegetarianFrequency" type="radio" >
-        <label for="once">1 fois par semaine</label>
-      </div>
-
-      <div class="form-item">
-        <input id="more-than-once" value="moreThanOnce" v-model="diagnostic.vegetarianFrequency" type="radio" >
-        <label for="more-than-once">Plus d'une fois par semaine</label>
-      </div>
-    </div>
-  </fieldset>
-
-  <fieldset class="form-bloc">
-    <legend>Le menu végétarien proposé est :</legend>
-
-    <div class="form-group">
-      <div class="form-item">
-        <input id="standalone" value="standalone" v-model="diagnostic.vegetarianMenuType" type="radio" >
-        <label for="standalone">Un menu végétarien unique</label>
-      </div>
-
-      <div class="form-item">
-        <input id="all-vegetarian" value="allVegetarian" v-model="diagnostic.vegetarianMenuType" type="radio" >
-        <label for="all-vegetarian">Plusieurs menus végétariens alternatifs</label>
-      </div>
-
-      <div class="form-item">
-        <input id="alternative" value="alternative" v-model="diagnostic.vegetarianMenuType" type="radio" >
-        <label for="alternative">Un menu végétarien alternatif à d'autres menus non-végétariens</label>
-      </div>
-    </div>
-  </fieldset>
+  <BaseRadios
+    v-model="diagnostic.vegetarianMenuType"
+    :legend="'Le menu végétarien proposé est :'"
+    :options="[
+      { inputId: 'standalone', value: 'standalone', label: 'Un menu végétarien unique' },
+      { inputId: 'all-vegetarian', value: 'allVegetarian', label: 'Plusieurs menus végétariens alternatifs' },
+      { inputId: 'alternative', value: 'alternative', label: 'Un menu végétarien alternatif à d\'autres menus non-végétariens' },
+    ]"
+  />
 </template>
 
 <script>
-import CheckboxUnique from '@/components/KeyMeasureDiagnostic/Inputs/CheckboxUnique';
+import BaseCheckbox from '@/components/KeyMeasureDiagnostic/Inputs/BaseCheckbox';
+import BaseRadios from '@/components/KeyMeasureDiagnostic/Inputs/BaseRadios';
 
   export default {
     components: {
-      CheckboxUnique
+      BaseCheckbox,
+      BaseRadios
     },
     props: {
       modelValue: Object,
@@ -65,49 +45,3 @@ import CheckboxUnique from '@/components/KeyMeasureDiagnostic/Inputs/CheckboxUni
     }
   }
 </script>
-
-<style scoped lang="scss">
-  fieldset {
-    border: none;
-    padding: 0;
-    margin: 10px 0;
-  }
-
-  .form-bloc {
-    margin: 20px 0;
-
-    legend {
-      text-align: left;
-      font-weight: bold;
-      padding: 0;
-    }
-
-    .form-group {
-      display: flex;
-      margin-top: 20px;
-      flex-wrap: wrap;
-    }
-
-    .form-item {
-      display: flex;
-      justify-content: space-between;
-      min-width: 150px;
-      width: 30%;
-      justify-content: left;
-      padding: 10px;
-      text-align: left;
-
-      label {
-        font-weight: normal;
-        padding-left: 5px;
-        max-width: 105px;
-        cursor: pointer;
-      }
-
-      input {
-        width: 30px;
-        height: 30px;
-      }
-    }
-  }
-</style>
--- front-end/src/components/KeyMeasureDiagnostic/QualityMeasure.vue
+++ front-end/src/components/KeyMeasureDiagnostic/QualityMeasure.vue
@@ -1,5 +1,5 @@
 <template>
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2019'].valueTotal"
     :label="'Je connais la valeur totale (en HT) de mes achats alimentaires en 2019'"
     :inputId="'value-total-2019'"
@@ -7,7 +7,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2019'].valueBio"
     :label="'Je connais la valeur (en HT) des achats alimentaires de produits bio sur 2019'"
     :inputId="'value-bio-2019'"
@@ -15,7 +15,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2019'].valueSustainable"
     :label="'Je connais la valeur (en HT) des achats alimentaires de produits durables (hors bio) sur 2019'"
     :inputId="'value-sustainable-2019'"
@@ -23,7 +23,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2019'].valueFairTrade"
     :label="'Je connais la valeur (en HT) des achats alimentaires de produits issus du commerce équitable sur 2019'"
     :inputId="'value-fair-trade-2019'"
@@ -31,7 +31,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2020'].valueTotal"
     :label="'Je connais la valeur totale (en HT) de mes achats alimentaires en 2020'"
     :inputId="'value-total-2020'"
@@ -39,7 +39,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2020'].valueBio"
     :label="'Je connais la valeur (en HT) des achats alimentaires de produits bio sur 2020'"
     :inputId="'value-bio-2020'"
@@ -47,7 +47,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2020'].valueSustainable"
     :label="'Je connais la valeur (en HT) des achats alimentaires de produits durables (hors bio) sur 2020'"
     :inputId="'value-sustainable-2020'"
@@ -55,7 +55,7 @@
     :min="0"
   />
 
-  <BasicInput
+  <BaseInput
     v-model="diagnostic['2020'].valueFairTrade"
     :label="'Je connais la valeur (en HT) des achats alimentaires de produits issus du commerce équitable sur 2020'"
     :inputId="'value-fair-trade-2020'"
@@ -65,11 +65,11 @@
 </template>
 
 <script>
-  import BasicInput from '@/components/KeyMeasureDiagnostic/Inputs/BasicInput';
+  import BaseInput from '@/components/KeyMeasureDiagnostic/Inputs/BaseInput';
 
   export default {
     components: {
-      BasicInput
+      BaseInput
     },
     props: {
       modelValue: Object,
