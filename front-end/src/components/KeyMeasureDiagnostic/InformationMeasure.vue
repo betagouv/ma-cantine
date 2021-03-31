66cb3313f8455beff30c072fe4d0561fa6901a53
forster <groux.forster@gmail.com>
2021-03-31 20:48:49 +0200

    refactor inputs

--- front-end/src/components/KeyMeasureDiagnostic/InformationMeasure.vue
+++ front-end/src/components/KeyMeasureDiagnostic/InformationMeasure.vue
@@ -1,38 +1,23 @@
 <template>
-  <fieldset class="form-communication-support">
-    <legend>Information sur la qualité des approvisionnements :</legend>
+  <BaseCheckboxes
+    v-model="diagnostic.communicationSupport"
+    :legend="'Information sur la qualité des approvisionnements :'"
+    :options="[
+      { inputId: 'email-communication', value: 'email', label: 'Envoi d\'e-mail aux convives ou à leurs représentants' },
+      { inputId: 'display-communication', value: 'display', label: 'Par affichage sur le lieu de restauration' },
+      { inputId: 'website-communication', value: 'website', label: 'Sur site internet ou intranet (mairie, cantine)' },
+      { inputId: 'other-communication', value: 'other', label: 'Autres moyens d\'affichage et de communication électronique' },
+    ]"
+  />
 
-    <div class="form-group">
-      <div class="form-item">
-        <input id="email-communication" value="email" v-model="diagnostic.communicationSupport" type="checkbox" >
-        <label for="email-communication"><p>Envoi d’e-mail aux convives ou à leurs représentants</p></label>
-      </div>
-
-      <div class="form-item">
-        <input id="display-communication" value="display" v-model="diagnostic.communicationSupport" type="checkbox" >
-        <label for="display-communication"><p>Par affichage sur le lieu de restauration</p></label>
-      </div>
-
-      <div class="form-item">
-        <input id="website-communication" value="website" v-model="diagnostic.communicationSupport" type="checkbox" >
-        <label for="website-communication"><p>Sur site internet ou intranet (mairie, cantine)</p></label>
-      </div>
-
-      <div class="form-item">
-        <input id="other-communication" value="other" v-model="diagnostic.communicationSupport" type="checkbox" >
-        <label for="other-communication"><p>Autres moyens d'affichage et de communication électronique</p></label>
-      </div>
-    </div>
-  </fieldset>
-
-  <BasicInput
+  <BaseInput
     v-model="diagnostic.communicationSupportLink"
     :label="'Lien vers le support de communication'"
     :inputId="'communication-link'"
     :type="'text'"
   />
 
-  <CheckboxUnique
+  <BaseCheckbox
     v-model="diagnostic.communicateOnFoodPlan"
     :label="'Je communique sur la mise en place d\'un plan alimentaire'"
     :inputId="'food-plan'"
@@ -40,13 +25,15 @@
 </template>
 
 <script>
-  import BasicInput from '@/components/KeyMeasureDiagnostic/Inputs/BasicInput';
-  import CheckboxUnique from '@/components/KeyMeasureDiagnostic/Inputs/CheckboxUnique';
+  import BaseInput from '@/components/KeyMeasureDiagnostic/Inputs/BaseInput';
+  import BaseCheckboxes from '@/components/KeyMeasureDiagnostic/Inputs/BaseCheckboxes';
+  import BaseCheckbox from '@/components/KeyMeasureDiagnostic/Inputs/BaseCheckbox';
 
   export default {
     components: {
-      BasicInput,
-      CheckboxUnique
+      BaseInput,
+      BaseCheckboxes,
+      BaseCheckbox
     },
     props: {
       modelValue: Object,
@@ -58,51 +45,3 @@
     }
   }
 </script>
-
-<style scoped lang="scss">
-  fieldset {
-    border: none;
-    padding: 0;
-    margin: 10px 0;
-  }
-
-  .form-communication-support {
-    legend {
-      text-align: left;
-      font-weight: bold;
-      padding: 0;
-    }
-
-    .form-group {
-      display: flex;
-      flex-direction: column;
-      margin-top: 20px;
-    }
-
-    .form-item {
-      display: flex;
-      justify-content: left;
-      padding: 5px 0;
-
-      label {
-        text-align: left;
-        font-weight: normal;
-        max-width: 85%;
-        padding-left: 5px;
-        cursor: pointer;
-        height: 30px;
-        display: table;
-
-        p {
-          display: table-cell;
-          vertical-align: middle;
-        }
-      }
-
-      input {
-        width: 30px;
-        height: 30px;
-      }
-    }
-  }
-</style>
--- front-end/src/components/KeyMeasureDiagnostic/NoPlasticMeasure.vue
+++ front-end/src/components/KeyMeasureDiagnostic/NoPlasticMeasure.vue
@@ -1,23 +1,23 @@
 <template>
-  <CheckboxUnique
+  <BaseCheckbox
     v-model="diagnostic.cookingFoodContainersSubstituted"
     :label="'Je n’utilise plus de contenants alimentaires de cuisson / de réchauffe en plastique'"
     :inputId="'cooking-food-container'"
   />
 
-  <CheckboxUnique
+  <BaseCheckbox
     v-model="diagnostic.serviceFoodContainersSubstituted"
     :label="'Je n’utilise plus de contenants de service en plastique'"
     :inputId="'service-food-container'"
   />
 
-  <CheckboxUnique
+  <BaseCheckbox
     v-model="diagnostic.waterBottlesSubstituted"
     :label="'Je ne mets plus à disposition des convives des bouteilles d’eau plate en plastique'"
     :inputId="'water-bottles'"
   />
 
-  <CheckboxUnique
+  <BaseCheckbox
     v-model="diagnostic.disposableUtensilsSubstituted"
     :label="'Je ne mets plus à disposition des convives des ustensiles à usage unique en matière plastique'"
     :inputId="'disposable-utensils'"
@@ -25,11 +25,11 @@
 </template>
 
 <script>
-  import CheckboxUnique from '@/components/KeyMeasureDiagnostic/Inputs/CheckboxUnique';
+  import BaseCheckbox from '@/components/KeyMeasureDiagnostic/Inputs/BaseCheckbox';
 
   export default {
     components: {
-      CheckboxUnique
+      BaseCheckbox
     },
     props: {
       modelValue: Object,
