eb3129193e2aa1a7e0912698fa72bb49d4843fa2
Helen Root <helenroot1@gmail.com>
2021-04-07 15:05:04 +0200

    Potential workaround to be able to spyOn node-fetch

--- api/application/beta-tester.js
+++ api/application/beta-tester.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const fetch = require('node-fetch');
+const FetchWrapper = require('../fetch');
 
 exports.register = async function(server) {
   server.route([{
@@ -42,7 +42,7 @@ async function createBetaTester(request, h) {
     })
   };
 
-  const response = await fetch("https://api.sendinblue.com/v3/smtp/email", requestOptions);
+  const response = await FetchWrapper.fetch("https://api.sendinblue.com/v3/smtp/email", requestOptions);
   const json = await response.json();
 
   return h.response({ message: json.message }).code(response.status);
