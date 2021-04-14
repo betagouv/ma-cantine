1ed65eedb5ea4de2ab29e3d615e062d6b70543aa
forster <groux.forster@gmail.com>
2021-04-14 19:45:18 +0200

    calculate percentage inside SummaryStatistics

--- front-end/src/components/CanteenDashboard.vue
+++ front-end/src/components/CanteenDashboard.vue
@@ -5,12 +5,12 @@
       <div class="statistics-by-year">
         <div>
           <h3>Sur l'année 2019 :</h3>
-          <SummaryStatistics :statistics="statistics['2019']"/>
+          <SummaryStatistics :qualityDiagnostic="diagnostics['qualite-des-produits']['2019']"/>
         </div>
         <div class="separator"></div>
         <div>
           <h3>Sur l'année 2020 :</h3>
-          <SummaryStatistics :statistics="statistics['2020']"/>
+          <SummaryStatistics :qualityDiagnostic="diagnostics['qualite-des-produits']['2020']"/>
         </div>
       </div>
       <KeyMeasureResource :baseComponent='qualityMeasure.baseComponent'/>
@@ -101,12 +101,6 @@
       diagnostics: Object,
     },
     data() {
-      const quality2019 = this.diagnostics['qualite-des-produits']['2019'];
-      const total2019 = quality2019.valueTotal;
-
-      const quality2020 = this.diagnostics['qualite-des-produits']['2020'];
-      const total2020 = quality2020.valueTotal;
-
       const vegetarianFrequency = this.diagnostics['diversification-des-menus'].vegetarianFrequency;
       const hasVegetarianMenu = vegetarianFrequency && vegetarianFrequency !== "less-than-once";
 
@@ -116,28 +110,12 @@
         diversificationMeasure: keyMeasures.find(measure => measure.id === 'diversification-des-menus'),
         noPlasticMeasure: keyMeasures.find(measure => measure.id === 'interdiction-du-plastique'),
         informationMeasure: keyMeasures.find(measure => measure.id === 'information-des-usagers'),
-        statistics: {
-          '2019': {
-            bio: getPercentage(quality2019.valueBio, total2019),
-            sustainable: getPercentage(quality2019.valueSustainable, total2019),
-            fairTrade: getPercentage(quality2019.valueFairTrade, total2019),
-          },
-          '2020': {
-            bio: getPercentage(quality2020.valueBio, total2020),
-            sustainable: getPercentage(quality2020.valueSustainable, total2020),
-            fairTrade: getPercentage(quality2020.valueFairTrade, total2020),
-          }
-        },
         vegetarianFrequency,
         hasVegetarianMenu,
         vegetarianMenuActionLabel: getVegetarianMenuActionLabel(hasVegetarianMenu, vegetarianFrequency),
       };
     },
   };
-
-  function getPercentage(partialValue, totalValue) {
-    return !!partialValue && !!totalValue ? Math.round((100 * partialValue) / totalValue) : '--';
-  }
 
   function getVegetarianMenuActionLabel(hasVegetarianMenu, vegetarianFrequency) {
     if (!hasVegetarianMenu) {
