c675d5be32146135b5468e9b1fd0644ac526ba8b
Helen Root <helenroot1@gmail.com>
2021-04-16 12:47:01 +0200

    Remove old comment

--- api/infrastructure/postgres-database.js
+++ api/infrastructure/postgres-database.js
@@ -4,7 +4,6 @@ const { Sequelize } = require('sequelize');
 // Jest sets NODE_ENV to test if it is undefined
 const DB_NAME = (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_NAME : process.env.DB_NAME;
 
-// TODO: how to connect to test database when running tests, but prod normally?
 exports.sequelize = new Sequelize(DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
