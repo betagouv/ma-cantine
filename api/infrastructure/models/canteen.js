4843fcd965bf00d8ccef67e470fddc469e00037a
Helen Root <helenroot1@gmail.com>
2021-04-16 12:35:29 +0200

    Reorganise files; remove unnecessary table drops

---
+++ api/infrastructure/models/canteen.js
@@ -0,0 +1,26 @@
+// For some reason, removing the unused Sequelize from here causes the tests to break
+const { Sequelize, DataTypes } = require('sequelize');
+const { sequelize } = require('../postgres-database');
+
+exports.Canteen = sequelize.define('Canteen', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  // TODO: validate department and sector input
+  department: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  sector: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
