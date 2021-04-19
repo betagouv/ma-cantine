e4e9523fc314e94318d38fe8bf7c61969aaa9537
Helen Root <helenroot1@gmail.com>
2021-04-19 10:51:59 +0200

    Use full URL for database

--- api/infrastructure/postgres-database.js
+++ api/infrastructure/postgres-database.js
@@ -2,11 +2,6 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Jest sets NODE_ENV to test if it is undefined
-const DB_NAME = (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_NAME : process.env.DB_NAME;
+const DATABASE_URL = (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 
-exports.sequelize = new Sequelize(DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: false // TODO: decide where logs should go
-});
+exports.sequelize = new Sequelize(DATABASE_URL);
