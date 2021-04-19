6305deac6d0dca36262d442ada69cc1eb676c3a5
Helen Root <helenroot1@gmail.com>
2021-04-19 16:37:44 +0200

    Improve running tests

--- api/infrastructure/postgres-database.js
+++ api/infrastructure/postgres-database.js
@@ -4,4 +4,6 @@ const { Sequelize } = require('sequelize');
 // Jest sets NODE_ENV to test if it is undefined
 const DATABASE_URL = (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 
-exports.sequelize = new Sequelize(DATABASE_URL);
+exports.sequelize = new Sequelize(DATABASE_URL, {
+  logging: process.env.NODE_ENV !== 'test' // don't log if running tests
+});
