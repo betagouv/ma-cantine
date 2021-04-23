937fe7f374a02011454b65ea7426c8f4eafe5acd
Helen Root <helenroot1@gmail.com>
2021-04-23 16:17:31 +0200

    Use Joi for request data validation

--- api/application/controllers/sign-up.js
+++ api/application/controllers/sign-up.js
@@ -1,17 +1,10 @@
 const { initiateMagicLinkLogin } = require("../../domain/usecases/login");
 const { createUserWithCanteen } = require("../../infrastructure/repositories/user");
 
-// TODO: fail with 400 for badly formatted request payload
 exports.signUp = async function(request, h) {
-  let code;
   const user = request.payload.user;
   const canteen = request.payload.canteen;
   const createdUser = await createUserWithCanteen(user, canteen);
-  if(createdUser) {
-    code = 201;
-    initiateMagicLinkLogin(createdUser.email);
-  } else {
-    code = 400;
-  }
-  return h.response().code(code);
+  initiateMagicLinkLogin(createdUser.email);
+  return h.response().code(201);
 };
