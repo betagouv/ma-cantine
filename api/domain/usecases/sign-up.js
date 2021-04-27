37b7560aea46e4f24617bc76968923db1b71c1b8
Helen Root <helenroot1@gmail.com>
2021-04-27 16:14:38 +0200

    Provide database user to send login link on sign up

--- api/domain/usecases/sign-up.js
+++ api/domain/usecases/sign-up.js
@@ -1,15 +1,7 @@
 const { sendLoginLink } = require("../services/authentication");
 const { createUserWithCanteen } = require("../../infrastructure/repositories/user");
-const { DuplicateUserError } = require("../../infrastructure/errors");
 
 exports.signUp = async function(user, canteen, loginUrl) {
-  try {
-    await createUserWithCanteen(user, canteen);
-  } catch(e) {
-    // send login link if user is duplicated
-    if(!(e instanceof DuplicateUserError)) {
-      throw e;
-    }
-  }
-  await sendLoginLink(user, loginUrl);
+  const userEntry = await createUserWithCanteen(user, canteen);
+  await sendLoginLink(userEntry, loginUrl);
 }
