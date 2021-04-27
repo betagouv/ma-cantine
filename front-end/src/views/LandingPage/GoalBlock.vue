49028e695f929e8de01b90df39668b3d020469a0
forster <groux.forster@gmail.com>
2021-04-27 17:54:52 +0200

    add presentation egalim video

--- front-end/src/views/LandingPage/GoalBlock.vue
+++ front-end/src/views/LandingPage/GoalBlock.vue
@@ -4,8 +4,34 @@
     <div class="goal-information">
       La restauration collective représente près de <b>4 milliards de repas servis par an</b> dans plus de 70 000 sites de restauration, des cantines scolaires, des hopitaux, des EPHAD, des entreprises, des administrations...  Agir sur la composition des assiettes est un levier fort pour améliorer l'impact sur notre santé et sur l'environnement tout en influançant positivement les filières d'agriculture biologique et de qualité. Les mesures en faveur d'une alimentation plus durable peuvent contribuer à modifier les comportements alimentaires notamment chez les plus jeunes.
     </div>
+    <div class="presentation-i-frame" v-show="iframeIsLoaded">
+      <iframe
+        src="https://www.youtube-nocookie.com/embed/kkQ3ttLFiD8"
+        title="Vidéo présentation d'Egalim"
+        @load="iframeLoad"
+        class="presentation-video"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        iframeIsLoaded: false,
+      };
+    },
+    methods: {
+      iframeLoad() {
+        this.iframeIsLoaded = true;
+      }
+    }
+  }
+</script>
 
 <style scoped lang="scss">
   #goal {
@@ -29,5 +55,19 @@
     background-color: $light-green;
     font-size: 20px;
   }
-  
+
+  .presentation-i-frame {
+    position: relative;
+    width: 90%;
+    padding-top: 63%;
+    margin: 50px auto;
+
+    .presentation-video {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
