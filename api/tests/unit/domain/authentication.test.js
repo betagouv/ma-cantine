f541cb4701d11a60eee5a46cacbc37a6153bc53a
Helen Root <helenroot1@gmail.com>
2021-05-04 11:07:30 +0200

    Isolate dependencies in unit tests

---
+++ api/tests/unit/domain/authentication.test.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { generateJwtForUser } = require("../../../domain/services/authentication");
+
+jest.mock('@hapi/jwt');
+const Jwt = require('@hapi/jwt');
+
+describe('Authentication service', () => {
+  it('uses user email and secret key to generate JWT', () => {
+    const mockedToken = "xxx.yyy.zzz";
+    Jwt.token.generate.mockReturnValue(mockedToken);
+    const email = "example@test.com";
+    const jwt = generateJwtForUser({
+      id: 5,
+      email
+    });
+    expect(Jwt.token.generate).toHaveBeenCalledWith({ email }, process.env.JWT_SECRET_KEY);
+    expect(jwt).toBe(mockedToken);
+  });
+});
