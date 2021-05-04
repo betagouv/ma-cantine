f75ba127905f439fcc37507f0a140ad55c04dd06
hfroot <9282816+hfroot@users.noreply.github.com>
2021-05-04 11:13:47 +0200

    Revert "Isolate dependencies in unit tests"
    This reverts commit 84c693cf440fc4ca765137548fd5f94c4b14fa67.

--- api/tests/unit/domain/complete-login.test.js
+++ api/tests/unit/domain/complete-login.test.js
@@ -1,24 +1,18 @@
+const Jwt = require('@hapi/jwt');
 const { completeLogin } = require('../../../domain/usecases/complete-login');
 const { NotFoundError } = require('../../../infrastructure/errors');
 
 jest.mock('../../../infrastructure/repositories/login-token');
 const { getUserForLoginToken } = require('../../../infrastructure/repositories/login-token');
 
-jest.mock('../../../domain/services/authentication');
-const { generateJwtForUser } = require('../../../domain/services/authentication');
-
 describe('Log in completion', () => {
 
   it('generates a JSON web token given a valid login token', async () => {
     const user = { email: "some@email.com" };
     getUserForLoginToken.mockReturnValue(user);
-
-    const testToken = 'xxx.yyy.zzz';
-    generateJwtForUser.mockReturnValue(testToken);
-
     const res = await completeLogin('somelogintoken');
-    expect(generateJwtForUser).toHaveBeenCalledWith(user);
-    expect(res).toStrictEqual({ jwt: testToken });
+    const decodedEmail = Jwt.token.decode(res.jwt).decoded.payload.email;
+    expect(decodedEmail).toBe(user.email);
   });
 
   it('throws an error given an invalid token', async () => {
