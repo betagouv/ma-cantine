858c6f76fd2dc541ea0262b198af5bb8d33dd46f
Helen Root <helenroot1@gmail.com>
2021-05-08 10:44:12 +0200

    Isolate login initiation and authentication service tests

--- api/tests/unit/domain/initiate-login.test.js
+++ api/tests/unit/domain/initiate-login.test.js
@@ -1,64 +1,40 @@
 const { initiateLogin } = require('../../../domain/usecases/initiate-login');
 
-jest.mock('../../../infrastructure/repositories/login-token');
-const { saveLoginTokenForUser } = require('../../../infrastructure/repositories/login-token');
-
 jest.mock('../../../infrastructure/repositories/user');
 const { getUserByEmail } = require('../../../infrastructure/repositories/user');
 const { NotFoundError } = require('../../../infrastructure/errors');
 
-jest.mock('../../../domain/services/mailer');
-const { sendTransactionalEmail } = require('../../../domain/services/mailer');
-
-const user = {
-  email: "test@email.com"
-};
-
-const URL_PREFIX = 'https://example.com/login?token=';
+jest.mock('../../../domain/services/authentication');
+const { sendLoginLink, sendSignUpLink } = require('../../../domain/services/authentication');
 
 describe('Log in initiation', () => {
-
-  beforeAll(() => {
-    saveLoginTokenForUser.mockResolvedValue(0);
-  });
-
-  // TODO: isolate dependencies properly
   it('generates, saves, and emails temp token given known email', async () => {
-    process.env.SENDINBLUE_TEMPLATE_LOGIN = 60;
-    getUserByEmail.mockReturnValue(user);
+    getUserByEmail.mockReturnValue({ email: "test@example.com" });
 
-    await initiateLogin(user.email, URL_PREFIX);
+    await initiateLogin("test@example.com", "https://example.com/");
 
-    expect(saveLoginTokenForUser).toHaveBeenCalledTimes(1);
-    const token = saveLoginTokenForUser.mock.calls[0][1]; // token is second argument
-    expect(token).toBeDefined();
-
-    expect(sendTransactionalEmail).toHaveBeenCalledWith([{ email:"test@email.com" }], 60, { LINK: 'https://example.com/login?token='+encodeURIComponent(token) });
+    expect(sendLoginLink).toHaveBeenCalledTimes(1);
+    expect(sendLoginLink).toHaveBeenCalledWith({ email: "test@example.com" }, "https://example.com/");
   });
 
   it('sends a sign up, not login, link given unknown email', async () => {
-    process.env.SENDINBLUE_TEMPLATE_SIGN_UP = 7;
     getUserByEmail.mockRejectedValue(new NotFoundError());
-    await initiateLogin('unknown@test.com', URL_PREFIX);
-    expect(saveLoginTokenForUser).not.toHaveBeenCalled();
 
-    expect(sendTransactionalEmail).toHaveBeenCalledWith([{ email:"unknown@test.com" }], 7);
+    await initiateLogin("test@example.com", "https://example.com/");
+
+    expect(sendSignUpLink).toHaveBeenCalledTimes(1);
+    expect(sendSignUpLink).toHaveBeenCalledWith("test@example.com");
+    expect(sendLoginLink).not.toHaveBeenCalled();
   });
 
-  it('generates unique tokens', async () => {
-    getUserByEmail.mockReturnValue(user);
-    await initiateLogin(user.email, URL_PREFIX);
-    await initiateLogin(user.email, URL_PREFIX);
-    const token1 = saveLoginTokenForUser.mock.calls[0][1];
-    const token2 = saveLoginTokenForUser.mock.calls[1][1];
-    expect(token1).not.toBe(token2);
+  it('passes up unknown error', async () => {
+    getUserByEmail.mockRejectedValue(new Error());
+    await expect(initiateLogin('email', 'url')).rejects.toThrow(Error);
   });
 
   afterEach(() => {
-    saveLoginTokenForUser.mockClear();
     getUserByEmail.mockClear();
-    sendTransactionalEmail.mockClear();
+    sendLoginLink.mockClear();
+    sendSignUpLink.mockClear();
   });
-
-  // TODO: check appropriate rate limiting on email sending
 });
