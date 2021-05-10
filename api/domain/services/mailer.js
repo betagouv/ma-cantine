7095721982dfa9d820fbbea15c9f9121c54b1c34
Helen Root <helenroot1@gmail.com>
2021-05-10 14:12:24 +0200

    Code review changes

--- api/domain/services/mailer.js
+++ api/domain/services/mailer.js
@@ -1,27 +1,27 @@
 const fetch = require('node-fetch');
 
-const sendEmail = function(to, subject, html) {
+const sendEmailBody = function(body) {
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json", "api-key": process.env.SENDINBLUE_API_KEY },
-    body: JSON.stringify({
-      sender: { email: process.env.SENDINBLUE_SENDER_EMAIL, name: process.env.SENDINBLUE_SENDER_NAME },
-      to,
-      replyTo: { email: process.env.SENDINBLUE_CONTACT_EMAIL },
-      subject,
-      htmlContent: html
-    })
+    body: body
   }
   return fetch("https://api.sendinblue.com/v3/smtp/email", requestOptions);
 };
 
+const sendEmail = function(to, subject, html) {
+  const body = JSON.stringify({
+    sender: { email: process.env.SENDINBLUE_SENDER_EMAIL, name: process.env.SENDINBLUE_SENDER_NAME },
+    to,
+    replyTo: { email: process.env.SENDINBLUE_CONTACT_EMAIL },
+    subject,
+    htmlContent: html
+  });
+  return sendEmailBody(body);
+};
+
 const sendTransactionalEmail = function(to, templateId, params) {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json", "api-key": process.env.SENDINBLUE_API_KEY },
-    body: JSON.stringify({ to, templateId, params })
-  }
-  return fetch("https://api.sendinblue.com/v3/smtp/email", requestOptions);
+  return sendEmailBody(JSON.stringify({ to, templateId, params }));
 };
 
 module.exports = {
