6c1c90fae324ee901eff9815dc947499140eddc7
forster <groux.forster@gmail.com>
2021-05-17 15:24:25 +0200

    improve call to action on dashboard

--- front-end/src/views/KeyMeasuresPage/DashboardPage.vue
+++ front-end/src/views/KeyMeasuresPage/DashboardPage.vue
@@ -2,11 +2,35 @@
   <div>
     <h1>Mon tableau de bord</h1>
 
-    <CanteenDashboard :diagnostics="diagnostics" :showResources="true"/>
+    <div v-if="$jwt.token" class="action-information">
+      <p>
+        Les données que vous avez renseignées précédemment ont été sauvegardées.
+        Vous pouvez les modifier en effectuant à nouveau
+        <router-link :to="{ name: 'DiagnosticPage' }">l'auto-diagnostic "savoir où j'en suis"</router-link>.
+        Votre tableau de bord sera mis à jour automatiquement.
+      </p>
+      <p>
+        Vous pouvez choisir de valoriser les démarches entreprises dans votre établissement
+        en décidant de rendre publique vos données. Les informations que vous avez
+        renseignées dans l'auto-diagnostic seront donc visible sur notre page
+        <router-link :to="{ name: 'CanteensHome' }">nos cantines</router-link>.
+        C'est une page que vous pouvez partager avec vos élu.e.s, convives, personnels...
+      </p>
 
-    <p v-if="jwt"><i class="fas fa-check-square"></i> Vos données ont été sauvegardées</p>
-    <router-link class="call-to-action" v-if="jwt" :to="{ name: 'CanteenInfo' }">Publier mes données</router-link>
-    <router-link class="call-to-action" v-if="!jwt" :to="{ name: 'SignUpPage' }">Sauvegarder mes données</router-link>
+      <router-link class="call-to-action" :to="{ name: 'CanteenInfo' }">Publier mes données</router-link>
+    </div>
+
+    <div v-else class="action-information">
+      <p>
+        Vous accédez actuellement à la plateforme en tant qu'utilisateur.trice invité.e.
+        Afin de conserver vos données et enregistrer votre tableau de bord, pouvoir le modifier, le partager...
+        Vous pouvez vous connecter ou créér un compte. Les données précédemment remplies seront sauvegardées automatiquement
+      </p>
+
+      <router-link class="call-to-action" :to="{ name: 'SignUpPage' }">Sauvegarder mes données</router-link>
+    </div>
+
+    <CanteenDashboard :diagnostics="diagnostics" :showResources="true"/>
   </div>
 </template>
 
@@ -18,11 +42,6 @@
       CanteenDashboard
     },
     props: ['diagnostics'],
-    data() {
-      return {
-        jwt: localStorage.getItem('jwt')
-      };
-    },
   };
 </script>
 
@@ -31,13 +50,17 @@
     font-weight: bold;
     font-size: 48px;
     color: $green;
-    margin: 1em 0em;
+  }
+
+  .action-information {
+    border: 1px solid $green;
+    border-radius: 10px;
+    padding: 15px;
   }
 
   .call-to-action {
     font-size: 1.2em;
     margin: auto;
-    margin-top: 2em;
     border: none;
     background: $orange;
     border-radius: 1em;
