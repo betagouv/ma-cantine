7ddcc507252b04583a7d715470e70c78906b2f42
Alejandro MG <alejandro@amguillen.dev>
2021-05-31 16:54:01 +0200

    Adds "not found" page

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card elevation="0" class="text-left">
+    <v-card-title class="text-h2 font-weight-black my-8">
+      Oups !
+    </v-card-title>
+    <v-card-subtitle class="text-h6 grey--text text--darken-4">
+      La page que vous recherchez semble introuvable
+    </v-card-subtitle>
+    <v-card-text>
+      <v-btn x-large color="primary" class="mt-4" :to="{ name: 'LandingPage' }">Revenir à l'accueil</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
