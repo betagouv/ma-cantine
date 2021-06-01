a1dd8e888e9140dd0878349bcc1dd27f07a1e686
Alejandro MG <alejandro@amguillen.dev>
2021-06-01 14:09:15 +0200

    Adds buildpacks, procfile, and pre_compile hook for Scalingo

---
+++ macantine/management/commands/buildnpm.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+from django.core.management.base import BaseCommand
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+class Command(BaseCommand):
+    help = 'Fetches npm dependencies and makes a prod build of the frontend application'
+
+    def handle(self, *args, **options):
+        print(BASE_DIR)
+
+        os.chdir(os.path.join(BASE_DIR, 'frontend'))
+        subprocess.run(['npm', 'install'])
+        subprocess.run(['npm', 'run', 'build'])
+        os.chdir(os.path.join(BASE_DIR))
