ebe670b614cc7eeae1857409543bbf60bab9e446
Helen Root <helenroot1@gmail.com>
2021-06-04 15:36:57 +0200

    Add fields for provisional years

--- frontend/src/components/KeyMeasureDiagnostic/QualityMeasure.vue
+++ frontend/src/components/KeyMeasureDiagnostic/QualityMeasure.vue
@@ -79,6 +79,86 @@
         v-model="latestDiagnostic.valueFairTradeHt"
       ></v-text-field>
     </div>
+
+    <div class="d-flex flex-column mt-10">
+      <p>En 2021, la valeur prévisionnelle (en HT) de mes achats alimentaires...</p>
+
+      <p class="body-2 mb-1 mt-2">...totale</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear1Diagnostic.valueTotalHt"
+      ></v-text-field>
+
+      <p class="body-2 mb-1 mt-4">...en produits bio</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear1Diagnostic.valueBioHt"
+      ></v-text-field>
+
+      <p class="body-2 mb-1 mt-4">...en autres produits de qualité et durables (hors bio)</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear1Diagnostic.valueSustainableHt"
+      ></v-text-field>
+
+      <p class="body-2 mb-1 mt-4">...en produits issus du commerce équitable</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear1Diagnostic.valueFairTradeHt"
+      ></v-text-field>
+    </div>
+
+    <div class="d-flex flex-column mt-10">
+      <p>En 2022, la valeur prévisionnelle (en HT) de mes achats alimentaires...</p>
+
+      <p class="body-2 mb-1 mt-2">...totale</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear2Diagnostic.valueTotalHt"
+      ></v-text-field>
+
+      <p class="body-2 mb-1 mt-4">...en produits bio</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear2Diagnostic.valueBioHt"
+      ></v-text-field>
+
+      <p class="body-2 mb-1 mt-4">...en autres produits de qualité et durables (hors bio)</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear2Diagnostic.valueSustainableHt"
+      ></v-text-field>
+
+      <p class="body-2 mb-1 mt-4">...en produits issus du commerce équitable</p>
+      <v-text-field
+        hide-details="auto"
+        type="number"
+        :rules="[validators.positiveNumber]"
+        solo
+        v-model="provisionalYear2Diagnostic.valueFairTradeHt"
+      ></v-text-field>
+    </div>
   </div>
 </template>
 
@@ -93,6 +173,8 @@ export default {
     return {
       latestDiagnostic: this.diagnostics.latest,
       previousDiagnostic: this.diagnostics.previous,
+      provisionalYear1Diagnostic: this.diagnostics.provisionalYear1,
+      provisionalYear2Diagnostic: this.diagnostics.provisionalYear2,
     }
   },
   computed: {
