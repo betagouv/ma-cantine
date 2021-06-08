74892ef95ae8fa7dc3ee47f09b0a0ea289dea879
Alejandro MG <alejandro@amguillen.dev>
2021-06-08 14:47:35 +0200

    Adds CGU page

---
+++ frontend/src/views/CGU.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="text-left">
+    <h1 class="font-weight-black my-6">
+      Conditions générales d'utilisation de Ma Cantine
+    </h1>
+    <p>
+      Les présentes conditions d’utilisation (CGU) sont mises en œuvre conformément à l’article L. 112-9 du code des
+      relations entre le public et l’administration. Elles s’imposent aux usagers.
+    </p>
+    <h2 class="my-6">
+      1 - Présentation
+    </h2>
+    <p>
+      Ma Cantine (ci-après dénommé « le Service ») est mis en œuvre par le ministère de l’Agriculture qui s’appuie sur
+      l’expertise de la Direction interministérielle du numérique dans le cadre d’une délégation de gestion.
+    </p>
+    <p>
+      Le Service est destiné à accompagner au mieux les acteurs de la restauration collective(ci-après abrégé « RC »)
+      dans leur offre aux consommateurs pour une alimentation de qualité, saine et durable.
+    </p>
+    <h2 class="my-6">
+      2 - Parcours usagers
+    </h2>
+    <p>
+      Le Service est une plateforme numérique permettant :
+    </p>
+    <ul class="mb-4">
+      <li>
+        une meilleure connaissance de la loi EGAlim via une description détaillée et pédagogique de ses mesures,
+      </li>
+      <li>
+        un aperçu de l'état des lieux des cantines ayant partagé leurs données pour connaître leur positionnement et
+        éventuellement fomenter des échanges entre elles et avec leurs convives, et
+      </li>
+      <li>
+        une option d'auto-diagnostic pour les acteurs de la restauration collective afin de s'auto-évaluer et accéder à
+        des ressources pour s'améliorer
+      </li>
+    </ul>
+    <p>
+      Un parcours « gestionnaire » où une personne de la restauration collective (ci-après nommé « Gestionnaire »)
+      remplit des diagnostics et - si elle le souhaite - partage sur la plateforme des informations sur sa cantine. Dans
+      ce parcours, la création d’un compte est obligatoire.
+    </p>
+    <p>
+      La création du compte peut être faite directement par l’équipe chargée du Service ou par l’usager. Le compte
+      nécessite un mot de passe et permet également l'utilisation d'un lien d’accès envoyé à l’adresse e-mail
+      renseignée. Un compte permet de créer et modifier les informations de la cantine du Gestionnaire.
+    </p>
+    <h2 class="my-6">
+      3 - Mentions légales
+    </h2>
+    <p>
+      Éditeur : ministère de l’Agriculture et de l’Alimentation.
+    </p>
+    <p>
+      Directeur de la publication : ministère de l’Agriculture et de l’Alimentation.
+    </p>
+    <p>
+      Prestataire d'hébergement : Clevercloud, 3 rue de l'Allier, 44000 Nantes, France.
+    </p>
+    <h2 class="my-6">
+      4 – Modalités d’utilisation du Service
+    </h2>
+    <p>
+      L'utilisation du Service est facultative et gratuite.
+    </p>
+    <p>
+      Le Service dépose des cookies de mesure d’audience (nombre de visites, pages consultées), respectant les
+      conditions d’exemption du consentement de l’internaute définies par la recommandation « Cookies » de la Commission
+      nationale informatique et libertés (CNIL); il utilise Matomo, un outil libre, paramétré pour ce faire. Cela
+      signifie, notamment, que ces cookies ne servent qu’à la production de statistiques anonymes et ne permettent pas
+      de suivre la navigation de l’internaute sur d’autres sites.
+    </p>
+    <p>
+      Le Service dépose des cookies de navigation à des fins strictement techniques, qui ne sont pas conservés.
+    </p>
+    <h2 class="my-6">
+      5 - Traitement des données
+    </h2>
+    <p>
+      Le Service ne collecte que les données strictement nécessaires à sa mise en œuvre.
+    </p>
+    <p>
+      Les catégories de données traitées sont les suivantes :
+    </p>
+    <ul class="mb-4">
+      <li>
+        les données obligatoires pour la création du compte : nom, prénom, nom d'utilisateur, adresse e-mail. L’équipe
+        chargé du Service est destinataire de ces données,
+      </li>
+      <li>
+        les données facultatives : l’ensemble des champs composant les diagnostics des établissements de restauration
+        collective. L’équipe chargée du Service se réserve néanmoins le droit de supprimer ou de proposer une
+        modification lorsque le contenu proposé ne répond à des critères minimaux de qualité. L’équipe chargée du
+        Service et les usagers sont destinataires de ces données.
+      </li>
+    </ul>
+    <p>
+      Les données sont conservées jusqu’à la fin de fonctionnement du Service.
+    </p>
+    <p>
+      Le ministère de l’Agriculture s'engage à prendre toutes précautions utiles pour préserver la sécurité des données
+      collectées auprès de l'usager, et notamment empêcher qu'elles soient déformées et endommagées ou que des tiers non
+      autorisés y aient accès.
+    </p>
+    <p>
+      Le ministère de l’agriculture garantit aux usagers du Service les droits d'accès, de rectification et d'opposition
+      prévus par la loi n° 78-17 du 6 janvier 1978 relative à l'informatique aux fichiers et aux libertés.
+    </p>
+    <p>
+      Ces droits s’exercent auprès de l’équipe du Service. (
+      <a href="mailto:contact@egalim.beta.gouv.fr">contact@egalim.beta.gouv.fr</a>
+      )
+    </p>
+    <p>
+      Les informations transmises par l'usager/utilisateur restent sous son contrôle. Il peut à tout moment les modifier
+      ou les supprimer. Il peut choisir de supprimer toutes les informations de son compte en supprimant son compte.
+      Seules sont conservées les informations permettant de répondre à d'éventuelles contestations et aux besoins
+      statistiques du service.
+    </p>
+    <h2 class="my-6">
+      6 - Engagements et responsabilité
+    </h2>
+    <p>
+      Le présent Service est mis en œuvre selon les dispositions du code des relations entre le public et
+      l’administration (CRPA).
+    </p>
+    <p>
+      Le Service a été développé sous licence MIT , le code est disponible à l’adresse suivante :
+      <a href="https://github.com/betagouv/ma-cantine/" target="_blank">https://github.com/betagouv/ma-cantine/</a>
+    </p>
+    <p>
+      Le Service est mis à disposition sans autres garanties expresses ou tacites que celles qui sont prévues par les
+      présentes.
+    </p>
+    <p>
+      Le Service est développé conformément à l’état de l’art. Toutefois, il n’est pas garanti qu’il soit exempt
+      d’anomalies ou d'erreurs. Le service est donc mis à disposition sans garantie sur sa disponibilité et ses
+      performances. A ce titre, le ministère de l’Agriculture ne peut être tenu responsable des pertes et/ou préjudices,
+      de quelque nature qu’ils soient, qui pourraient être causés à la suite d’un dysfonctionnement ou une
+      indisponibilité du service. De telles situations n'ouvriront droit à aucune compensation financière.
+    </p>
+    <p>
+      L’utilisateur s’engage à respecter les présentes CGU et la législation en vigueur. Il s’engage notamment à ne
+      fournir, dans le cadre de l'utilisation du Service, que des informations exactes, à jour et complètes.
+    </p>
+    <p>
+      Dans l'hypothèse où l'usager ne s'acquitte pas de ses engagements, le ministère de l’Agriculture se réserve le
+      droit de suspendre ou résilier le service pour cet usager sans préjudice des éventuelles actions en responsabilité
+      pénale et civile qui pourraient être engagées à son encontre.
+    </p>
+    <h2 class="my-6">
+      7 - Modification et évolution du Service
+    </h2>
+    <p>
+      Les termes des présentes conditions d’utilisation peuvent être modifiés ou complétés à tout moment, sans préavis,
+      en fonction des modifications apportées au Service, de l’évolution de la législation ou pour tout autre motif jugé
+      nécessaire. Ces modifications et mises à jour s’imposent à l’utilisateur qui doit, en conséquence, se référer
+      régulièrement à cette rubrique pour vérifier les conditions générales en vigueur.
+    </p>
+    <h2 class="my-6">
+      8 - Nous contacter
+    </h2>
+    <ul class="mb-4">
+      <li>
+        Difficultés techniques :
+        <a href="mailto:contact@egalim.beta.gouv.fr">contact@egalim.beta.gouv.fr.</a>
+      </li>
+      <li>
+        Problèmes liés à la procédure :
+        <a href="mailto:contact@egalim.beta.gouv.fr">contact@egalim.beta.gouv.fr.</a>
+      </li>
+      <li>
+        Droit d’accès CNIL :
+        <a href="mailto:contact@egalim.beta.gouv.fr">contact@egalim.beta.gouv.fr.</a>
+      </li>
+    </ul>
+  </div>
+</template>
