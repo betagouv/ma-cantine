bf01fe1167dd1e04f7e264a7ac4dd721b16fc5d6
Helen Root <helenroot1@gmail.com>
2021-06-08 17:55:41 +0200

    Remove excess question

--- frontend/src/components/KeyMeasureDiagnostic/InformationMeasure.vue
+++ frontend/src/components/KeyMeasureDiagnostic/InformationMeasure.vue
@@ -6,24 +6,6 @@
       label="J’informe mes convives sur la part de produits de qualité et durables, entrant dans la composition des repas servis, et sur les démarches d’acquisition de produits issus du commerce équitable"
     />
 
-    <p class="text-left mt-6 mb-2">
-      Les actions ci-dessous sont des exemples, vous pouvez indiquer si ce sont des actions que vous avez mises en place
-      ou préciser ou ajouter ou d’autres actions :
-    </p>
-    <v-checkbox
-      hide-details="auto"
-      class="ml-8"
-      v-model="diagnostic.communicationContents"
-      v-for="support in communicationContents"
-      :key="support.value"
-      :value="support.value"
-      :label="support.label"
-    />
-    <v-row align="center" class="ml-8 mt-2 mr-2">
-      <v-checkbox v-model="otherContentEnabled" hide-details class="shrink mt-0"></v-checkbox>
-      <v-text-field :disabled="!otherContentEnabled" label="Autre : donnez plus d'informations"></v-text-field>
-    </v-row>
-
     <p class="text-left mt-6 mb-2">Je fais cette information :</p>
     <v-radio-group v-model="diagnostic.communicationFrequency">
       <v-radio
@@ -35,7 +17,7 @@
       ></v-radio>
     </v-radio-group>
 
-    <p class="text-left mt-6 mb-2">J'informe sur la qualité des approvisionnements :</p>
+    <p class="text-left my-2">J'informe sur la qualité des approvisionnements :</p>
     <v-checkbox
       hide-details="auto"
       class="ml-8"
@@ -50,7 +32,6 @@
       <v-text-field :disabled="!otherSupportEnabled" label="Autre : donnez plus d'informations"></v-text-field>
     </v-row>
 
-    <p class="text-left mt-4"><u>Information sur la qualité nutritionnelle des repas</u></p>
     <v-checkbox
       hide-details="auto"
       v-model="diagnostic.communicatesOnFoodPlan"
