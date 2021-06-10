b9e4e85f814de34bdb9419290aa9d342c8f08b78
Alejandro MG <alejandro@amguillen.dev>
2021-06-10 18:30:08 +0200

    Adds first_name and last_name to user creation form

--- data/admin/user.py
+++ data/admin/user.py
@@ -47,7 +47,7 @@ class MaCanteenUserAdmin(UserAdmin):
             None,
             {
                 "classes": ("wide",),
-                "fields": ("username", "email", "password1", "password2"),
+                "fields": ("username", "email", "first_name", "last_name", "password1", "password2"),
             },
         ),
     )
