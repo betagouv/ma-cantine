4728cd2637dae2de85efd5128d2a145c4d7b4136
Helen Root <helenroot1@gmail.com>
2021-06-11 10:31:57 +0200

    Add guidance text to quality diagnostic

--- frontend/src/components/KeyMeasureDiagnostic/QualityMeasure.vue
+++ frontend/src/components/KeyMeasureDiagnostic/QualityMeasure.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="quality-measure" class="text-left">
+    <p class="text-center help">Si vous ne connaissez pas une valeur, laissez le champ vide.</p>
     <v-row>
       <v-spacer></v-spacer>
       <v-col cols="12" sm="5" class="my-8">
@@ -87,6 +88,13 @@
       </v-col>
       <v-spacer></v-spacer>
     </v-row>
+    <p class="text-center help mx-4">
+      Indiquez ci-après les montants estimés de produits durables et de qualité sur la base des actions mises en œuvre
+      ou prévues.
+      <br />
+      (ex : renouvellement de marchés, modification de contrats d’approvisionnements, rationalisation des consommations
+      évolution des grammages, etc.)
+    </p>
     <v-row>
       <v-spacer></v-spacer>
       <v-col cols="12" sm="5" class="my-8">
@@ -199,3 +207,10 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="scss">
+p.help {
+  color: $ma-cantine-grey;
+  font-size: 0.95em;
+}
+</style>
