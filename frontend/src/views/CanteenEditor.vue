733d235876993839448a0c356e51a43fb1865499
Helen Root <helenroot1@gmail.com>
2021-06-17 15:31:12 +0200

    Update and create canteens in database

--- frontend/src/views/CanteenEditor.vue
+++ frontend/src/views/CanteenEditor.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-left pb-10">
     <h1 class="font-weight-black text-h4 my-4">
-      {{ isNewCantine ? "Nouvelle cantine" : "Modifier ma cantine" }}
+      {{ isNewCanteen ? "Nouvelle cantine" : "Modifier ma cantine" }}
     </h1>
-    <div class="mb-8 mt-2" v-if="!isNewCantine">
+    <div class="mb-8 mt-2" v-if="!isNewCanteen">
       <v-chip small :color="canteen.dataIsPublic ? 'green lighten-4' : 'grey lighten-4'" label>
         {{ canteen.dataIsPublic ? "Publiée" : "Pas encore publiée" }}
       </v-chip>
@@ -11,11 +11,17 @@
         {{ canteen.dataIsPublic ? "Enlever la publication" : "Publier ma cantine" }}
       </v-btn>
     </div>
-    <v-form v-model="formIsValid" lazy-validation>
+    <v-form ref="form" v-model="formIsValid">
       <v-row>
         <v-col cols="12" md="8">
           <p class="body-2 my-2">Nom de la cantine</p>
-          <v-text-field hide-details="auto" :rules="[validators.notEmpty]" solo v-model="canteen.name"></v-text-field>
+          <v-text-field
+            hide-details="auto"
+            :rules="[validators.notEmpty]"
+            validate-on-blur
+            solo
+            v-model="canteen.name"
+          ></v-text-field>
         </v-col>
         <v-col cols="12" md="8">
           <p class="body-2 my-2">SIRET</p>
@@ -25,7 +31,13 @@
       <v-row>
         <v-col cols="12" md="4">
           <p class="body-2 my-2">Ville / commune</p>
-          <v-text-field hide-details="auto" :rules="[validators.notEmpty]" solo v-model="canteen.city"></v-text-field>
+          <v-text-field
+            hide-details="auto"
+            :rules="[validators.notEmpty]"
+            validate-on-blur
+            solo
+            v-model="canteen.city"
+          ></v-text-field>
         </v-col>
         <v-col cols="12" md="4">
           <p class="body-2 my-2">Département</p>
@@ -33,6 +45,7 @@
             solo
             v-model="canteen.department"
             :rules="[validators.notEmpty]"
+            validate-on-blur
             :items="departments"
             :item-text="(item) => `${item.departmentCode} - ${item.departmentName}`"
             item-value="departmentCode"
@@ -45,6 +58,7 @@
           <v-text-field
             hide-details="auto"
             :rules="[validators.greaterThanZero]"
+            validate-on-blur
             solo
             v-model="canteen.dailyMealCount"
           ></v-text-field>
@@ -83,10 +97,15 @@
 
     <v-sheet rounded color="grey lighten-4 pa-3" class="d-flex">
       <v-spacer></v-spacer>
-      <v-btn x-large color="primary">Valider</v-btn>
+      <v-btn large outlined color="primary" class="mr-4 align-self-center" :to="{ name: 'ManagementPage' }">
+        Annuler
+      </v-btn>
+      <v-btn x-large color="primary" @click="saveCanteen">
+        {{ isNewCanteen ? "Ajouter" : "Modifier" }}
+      </v-btn>
     </v-sheet>
 
-    <div v-if="!isNewCantine">
+    <div v-if="!isNewCanteen">
       <h2 class="font-weight-black text-h5 mt-10">
         Mes diagnostics pour cette cantine
       </h2>
@@ -143,15 +162,42 @@ export default {
     departments() {
       return departments
     },
-    isNewCantine() {
+    isNewCanteen() {
       return !this.canteenUrlComponent
     },
   },
   mounted() {
-    if (this.isNewCantine) return
+    if (this.isNewCanteen) return
     const canteen = this.$store.getters.getCanteenFromUrlComponent(this.canteenUrlComponent)
     if (canteen) this.canteen = JSON.parse(JSON.stringify(canteen))
     else this.$router.push({ name: "NewCanteen" })
+  },
+  methods: {
+    saveCanteen() {
+      this.$refs.form.validate()
+
+      if (!this.formIsValid) {
+        window.alert("Merci de vérifier les champs en rouge et réessayer")
+        return
+      }
+      const payload = {
+        name: this.canteen.name,
+        city: this.canteen.city,
+        department: this.canteen.department,
+        dailyMealCount: this.canteen.dailyMealCount,
+        sectors: this.canteen.sectors,
+        siret: this.canteen.siret,
+        managementType: this.canteen.managementType,
+      }
+      this.$store
+        .dispatch(this.isNewCanteen ? "createCanteen" : "updateCanteen", { id: this.canteen.id, payload })
+        .then(() => {
+          this.$router.push({ name: "ManagementPage", query: { operation: this.isNewCanteen ? "cree" : "modifiee" } })
+        })
+        .catch(() => {
+          alert("Une erreur s'est produite. Merci de réesayer plus tard.")
+        })
+    },
   },
 }
 </script>
