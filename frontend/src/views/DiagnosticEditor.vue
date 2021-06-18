3af4584e31f50167cef28c6d1391a5eb14fb6562
Alejandro MG <alejandro@amguillen.dev>
2021-06-18 11:51:35 +0200

    Manages existing diagnostics on canteen+year combination

--- frontend/src/views/DiagnosticEditor.vue
+++ frontend/src/views/DiagnosticEditor.vue
@@ -3,7 +3,7 @@
     <h1 class="font-weight-black text-h4 my-4">
       {{ isNewDiagnostic ? "Nouveaux diagnostic" : "Modifier mon diagnostic" }}
     </h1>
-    <div class="mb-8 mt-2" v-if="!isNewDiagnostic && canteen">
+    <div class="mb-4 mt-2" v-if="!isNewDiagnostic && canteen">
       <v-chip small :color="canteen.dataIsPublic ? 'green lighten-4' : 'grey lighten-4'" label>
         {{ canteen.dataIsPublic ? "Publiée" : "Pas encore publiée" }}
       </v-chip>
@@ -15,27 +15,43 @@
           <v-select
             solo
             ref="canteenSelect"
-            :value="canteen ? canteen.id : undefined"
-            :rules="[validators.notEmpty]"
-            :items="$store.state.userCanteens"
+            v-model="selectedCanteenId"
+            :rules="[validators.notEmpty, validators.diagnosticIsUnique]"
+            :items="userCanteens"
             item-text="name"
             item-value="id"
             hide-details="auto"
             placeholder="Choisissez votre cantine"
+            v-if="isNewDiagnostic"
           ></v-select>
+          <div v-else class="text-h6 font-weight-bold">{{ canteen.name }}</div>
         </v-col>
         <v-col cols="12" md="3">
           <p class="body-2 my-2">Année</p>
           <v-select
             solo
+            ref="yearSelect"
             v-model="diagnostic.year"
-            :rules="[validators.notEmpty]"
+            :rules="[validators.notEmpty, validators.diagnosticIsUnique]"
             :items="allowedYears"
             hide-details="auto"
             placeholder="Année du diagnostic"
+            v-if="isNewDiagnostic"
           ></v-select>
+          <div v-else class="text-h6 font-weight-bold">{{ diagnostic.year }}</div>
         </v-col>
-        <v-col cols="12" class="mb-8 mt-4">
+        <v-col v-if="!diagnosticIsUnique" cols="12" class="ma-0 text-body-2 red--text">
+          Un diagnostic pour cette cantine et cette année existe déjà.
+          <v-btn
+            small
+            text
+            class="text-decoration-underline text-body-2 mt-n1"
+            :to="{ name: 'DiagnosticModification', params: existingDiagnosticRouteParams }"
+          >
+            Modifier le diagnostic existant.
+          </v-btn>
+        </v-col>
+        <v-col cols="12" class="mb-8 mt-3">
           <v-divider></v-divider>
         </v-col>
       </v-row>
@@ -121,6 +137,7 @@ export default {
   data() {
     return {
       diagnostic: {},
+      selectedCanteenId: undefined,
     }
   },
   components: { InformationMeasure, WasteMeasure, DiversificationMeasure, NoPlasticMeasure },
@@ -141,8 +158,31 @@ export default {
       if (this.isNewDiagnostic) return null
       return this.$store.getters.getCanteenFromUrlComponent(this.canteenUrlComponent)
     },
+    userCanteens() {
+      return this.$store.state.userCanteens
+    },
     validators() {
-      return validators
+      return {
+        ...validators,
+        ...{ diagnosticIsUnique: this.diagnosticIsUnique },
+      }
+    },
+    diagnosticIsUnique() {
+      if (!this.isNewDiagnostic) return true
+      if (!this.selectedCanteenId || !this.diagnostic.year) return true
+
+      const existingDiagnostic = this.userCanteens
+        .find((x) => x.id === this.selectedCanteenId)
+        .diagnostics.some((x) => x.year === this.diagnostic.year)
+
+      return !existingDiagnostic
+    },
+    existingDiagnosticRouteParams() {
+      if (this.diagnosticIsUnique) return {}
+      const existingCanteen = this.userCanteens.find((x) => x.id === this.selectedCanteenId)
+      const canteenUrlComponent = this.$store.getters.getCanteenUrlComponent(existingCanteen)
+      const year = this.diagnostic.year
+      return { canteenUrlComponent, year }
     },
     allowedYears() {
       return [
@@ -165,7 +205,7 @@ export default {
       ]
     },
   },
-  mounted() {
+  beforeMount() {
     if (this.isNewDiagnostic) return
 
     if (!this.canteen) this.$router.push({ name: "Landing" })
