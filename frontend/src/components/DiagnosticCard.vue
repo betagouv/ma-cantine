e5069031c201d6d97307b15bc227fd3194caefae
Helen Root <helenroot1@gmail.com>
2021-06-18 11:48:03 +0200

    Improve diagnostic card display

--- frontend/src/components/DiagnosticCard.vue
+++ frontend/src/components/DiagnosticCard.vue
@@ -6,10 +6,12 @@
     <v-sheet color="grey lighten-4" width="150" class="d-flex flex-column justify-center px-4">
       <div class="text-h3 grey--text font-weight-black text-center">{{ diagnostic.year }}</div>
     </v-sheet>
-    <div>
+    <v-col cols="6">
       <v-card-title class="font-weight-bold">{{ canteen.name }}</v-card-title>
-      <v-card-text>Dernière modification {{ timeAgo(diagnostic.modificationDate, true) }}</v-card-text>
-    </div>
+      <v-card-subtitle>{{ dataStatus }}</v-card-subtitle>
+    </v-col>
+    <v-spacer></v-spacer>
+    <v-card-text class="align-self-center">Ouvert {{ timeAgo(diagnostic.creationDate, true) }}</v-card-text>
   </v-card>
 </template>
 
@@ -34,10 +36,17 @@ export default {
       return this.$store.getters.getCanteenUrlComponent(this.canteen)
     },
     dataStatus() {
-      return {
-        PUBLISHED: "Données complètées, sauvegardées, et publiées",
-        INCOMPLETE: "Données incomplètées",
-      }[this.diagnostic.status]
+      let status = "Données d'appro incomplètes"
+      if (this.canteen.isPublished) {
+        status = "Données publiées"
+      } else {
+        const approComplete = ["valueBioHt", "valueSustainableHt", "valueTotalHt"].every(
+          // sadly null >= 0 is true
+          (key) => this.diagnostic[key] > 0 || this.diagnostic[key] === 0
+        )
+        status = approComplete ? "Données complètées" : status
+      }
+      return status
     },
   },
   methods: {
