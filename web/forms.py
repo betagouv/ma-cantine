18f1cd5856d9fd65a3bc1767d6b4e4a023af8149
Alejandro MG <alejandro@amguillen.dev>
2021-06-30 16:13:20 +0200

    Registration view with city autocomplete

--- web/forms.py
+++ web/forms.py
@@ -1,11 +1,11 @@
 import re
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django import forms
 from django.utils.safestring import mark_safe
 from data.models import Canteen
 from data.models import Sector
-from data.department_choices import Department
 
 
 class RegisterForm(UserCreationForm):
@@ -17,8 +17,13 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField()
     canteen_name = forms.CharField(label="Nom de la cantine")
     siret = forms.CharField(label="SIRET", required=False)
-    city = forms.CharField(label="Ville/Commune")
-    department = forms.ChoiceField(label="Département", choices=[(None, '---')] + Department.choices)
+
+    autocomplete = forms.CharField(label="Ville / Région")
+    city = forms.CharField(widget=forms.HiddenInput())
+    department = forms.CharField(widget=forms.HiddenInput())
+    city_insee_code = forms.CharField(widget=forms.HiddenInput())
+    postal_code = forms.CharField(widget=forms.HiddenInput())
+
     sectors = forms.MultipleChoiceField(label="Secteurs d'activité", widget=forms.CheckboxSelectMultiple)
     daily_meal_count = forms.IntegerField(label="Nombre de repas moyen par jour")
     management_type = forms.ChoiceField(
@@ -31,22 +36,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:  
         model = get_user_model()
-        fields = (
-            "first_name",
-            "last_name",
-            "email",
-            "canteen_name",
-            "siret",
-            "city",
-            "department",
-            "daily_meal_count",
-            "sectors",
-            "management_type",
-            "username",
-            "password1",
-            "password2",
-            "cgu_approved",
-        )
+        fields = "__all__"
 
     def left_column_fields(self):
         field_names = [
@@ -57,30 +47,32 @@ class RegisterForm(UserCreationForm):
             "password1",
             "password2",
         ]
-        return [field for field in self if not field.is_hidden and field.name in field_names]
+        return [next(field for field in self if field.name == field_name) for field_name in field_names]
 
     def right_column_fields(self):
         field_names = [
             "canteen_name",
             "siret",
-            "city",
-            "department",
+            "autocomplete",
             "daily_meal_count",
             "sectors",
             "management_type",
         ]
-        return [field for field in self if not field.is_hidden and field.name in field_names]
+        return [next(field for field in self if field.name == field_name) for field_name in field_names]
+
+    def hidden_fields(self):
+        return [field for field in self if field.is_hidden]
 
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
         self.label_suffix = ""
         self.fields["first_name"].widget.attrs.update({"placeholder": "Agnès"})
         self.fields["last_name"].widget.attrs.update({"placeholder": "Dufresne"})
-        self.fields["username"].widget.attrs.update({"placeholder": "agnes.dufresne"})
+        self.fields["username"].widget.attrs.update({"placeholder": "agnes.dufresne", "autofocus": False})
         self.fields["email"].widget.attrs.update({"placeholder": "agnes.d@example.com"})
         self.fields["canteen_name"].widget.attrs.update({"placeholder": "Ma cantine"})
         self.fields["siret"].widget.attrs.update({"placeholder": "123 456 789 00001"})
-        self.fields["city"].widget.attrs.update({"placeholder": "Ville/Commune"})
+        self.fields["autocomplete"].widget.attrs.update({"placeholder": "Sélectionnez une ville "})
         self.fields["sectors"].widget.attrs.update({"class": "sector"})
         self.fields["sectors"].choices = Sector.choices()
         self.fields["management_type"].widget.attrs.update({"class": "management-type"})
@@ -90,6 +82,11 @@ class RegisterForm(UserCreationForm):
         self.fields["password2"].widget.attrs.update(
             {"placeholder": "Confirmez votre mot de passe"}
         )
+
+    def clean_autocomplete(self):
+        if not self.cleaned_data.get('city_insee_code'):
+            raise ValidationError("Veuillez choisir une ville de la liste déroulante")
+        return self.cleaned_data["autocomplete"]
 
     def clean_cgu_approved(self):
         if not self.cleaned_data.get("cgu_approved"):
@@ -123,6 +120,8 @@ class RegisterForm(UserCreationForm):
                 siret=self.cleaned_data.get("siret"),
                 city=self.cleaned_data.get("city"),
                 department=self.cleaned_data.get("department"),
+                city_insee_code = self.cleaned_data.get("city_insee_code"),
+                postal_code = self.cleaned_data.get("postal_code"),
                 daily_meal_count=self.cleaned_data.get("daily_meal_count"),
                 management_type=self.cleaned_data.get("management_type"),
             )
