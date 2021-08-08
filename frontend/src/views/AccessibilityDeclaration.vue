7251bd07b658b9a2f0cf47cde866030611036bb0
Helen Root <helenroot1@gmail.com>
2021-08-08 19:32:18 +0200

    Begin to fill out standard declaration

---
+++ frontend/src/views/AccessibilityDeclaration.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="text-left">
+    <h1 class="font-weight-black my-6">Déclaration d'accessibilité</h1>
+    <p>
+      ma cantine s’engage à rendre ses sites internet, intranet, extranet et ses progiciels accessibles (et ses
+      applications mobiles et mobilier urbain numérique) conformément à l’article 47 de la loi n°2005-102 du 11 février
+      2005.
+    </p>
+    <p>À cette fin, elle met en œuvre la stratégie et les actions suivantes :</p>
+    <ul>
+      <li>
+        Schéma pluriannuel de mise en accessibilité 2020-2022
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        Actions réalisées en 2019-2020
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        Plan d’actions 2020-2021
+        <span class="secondary">url</span>
+      </li>
+    </ul>
+    <p>
+      Cette déclaration d’accessibilité s’applique à
+      <span class="secondary">ma-cantine.beta.gouv.fr</span>
+      .
+    </p>
+    <h2 class="my-6">État de conformité</h2>
+    <p>
+      <span class="secondary">ma cantine, ma-cantine.beta.gouv.fr</span>
+      est non conforme avec le référentiel général d’amélioration de l’accessibilité (RGAA), version 4 en raison des
+      non-conformités et des dérogations énumérées ci-dessous.
+    </p>
+    <h3 class="my-4">Résultats des tests</h3>
+    <p>En l’absence d’audit de conformité il n’y a pas de résultats de tests.</p>
+    <h2 class="my-6">Contenus non accessibles</h2>
+    <h3 class="my-4">
+      <span class="secondary">Non-conformités</span>
+    </h3>
+    <ul>
+      <li>
+        La vidéo de présentation des enjeux de la restauration collective n’a pas de transcription. Pour toute question
+        sur cette démarche, vous pouvez nous contacter à
+        <a href="mailto:contact@egalim.beta.gouv.fr">contact@egalim.beta.gouv.fr</a>
+        .
+      </li>
+      <li>Le livret de la loi EGAlim concernant la restauration collective est donné en format PDF.</li>
+      <li>La page statistiques utilise des iframes, sans texte alternatif qui décrit les chartes.</li>
+      <li>Le chatbox n'est pas accessible par clavier.</li>
+    </ul>
+    <h2 class="my-6">Établissement de cette déclaration d’accessibilité</h2>
+    <p>
+      <!-- TODO: automate dates? -->
+      Cette déclaration a été établie le 08/08/2021. Elle a été mise à jour le 08/08/2021.
+    </p>
+    <h3 class="my-4">Technologies utilisées pour la réalisation de ma cantine, ma-cantine.beta.gouv.fr</h3>
+    <ul>
+      <li>HTML5</li>
+      <li>CSS</li>
+      <li>Javascript</li>
+      <li>Vue.js</li>
+      <li>Python</li>
+      <li>Django</li>
+    </ul>
+    <h3 class="my-4">Environnement de test</h3>
+    <p>
+      Les vérifications de restitution de contenus ont été réalisées sur la base de la combinaison fournie par la base
+      de référence du RGAA, avec les versions suivantes :
+    </p>
+    <ul>
+      <li>Firefox et NVDA</li>
+      <li>Safari et VoiceOver</li>
+      <li><span class="secondary">...</span></li>
+    </ul>
+    <h3 class="my-4">Outils pour évaluer l’accessibilité</h3>
+    <ul>
+      <li>ANDI</li>
+      <li>WAVE</li>
+      <li><span class="secondary">...</span></li>
+    </ul>
+    <h3 class="my-4">Pages du site ayant fait l’objet de la vérification de conformité</h3>
+    <ol>
+      <li>
+        page d’accueil
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        page contact
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        page mentions légales
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        page accessibilité
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        page plan du site
+        <span class="secondary">url</span>
+      </li>
+      <li>
+        page d’aide
+        <span class="secondary">url</span>
+      </li>
+      <li><span class="secondary">...</span></li>
+    </ol>
+    <h2 class="my-6">Retour d’information et contact</h2>
+    <p>
+      Si vous n’arrivez pas à accéder à un contenu ou à un service, vous pouvez contacter le responsable de
+      <span class="secondary">nom, url du site</span>
+      pour être orienté vers une alternative accessible ou obtenir le contenu sous une autre forme.
+    </p>
+    <ul>
+      <li>
+        Envoyer un message
+        <span class="secondary">url du formulaire en ligne</span>
+      </li>
+      <li>
+        Contacter
+        <span class="secondary">Nom de l’entité responsable du service en ligne</span>
+        <span class="secondary">url d’une page avec les coordonnées de l’entité</span>
+      </li>
+    </ul>
+    <h2 class="my-6">Voies de recours</h2>
+    <p>
+      Si vous constatez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site,
+      que vous nous le signalez et que vous ne parvenez pas à obtenir une réponse de notre part, vous êtes en droit de
+      faire parvenir vos doléances ou une demande de saisine au Défenseur des droits.
+    </p>
+    <p>Plusieurs moyens sont à votre disposition :</p>
+    <ul>
+      <li><a href="https://formulaire.defenseurdesdroits.fr">Écrire un message au Défenseur des droits</a></li>
+      <li>
+        <a href="https://www.defenseurdesdroits.fr/saisir/delegues">
+          Contacter le délégué du Défenseur des droits dans votre région
+        </a>
+      </li>
+      <li>
+        Envoyer un courrier par la poste (gratuit, ne pas mettre de timbre)
+        <br />
+        Défenseur des droits
+        <br />
+        Libre réponse 71120
+        <br />
+        75342 Paris CEDEX 07
+        <br />
+      </li>
+    </ul>
+  </div>
+</template>
