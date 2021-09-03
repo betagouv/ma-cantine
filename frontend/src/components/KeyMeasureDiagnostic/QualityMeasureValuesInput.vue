2e064c787e89296c843ba981d6f3b4e336b35ba5
Helen Root <helenroot1@gmail.com>
2021-09-03 11:27:25 +0200

    Front end validation for sum of appro values

--- frontend/src/components/KeyMeasureDiagnostic/QualityMeasureValuesInput.vue
+++ frontend/src/components/KeyMeasureDiagnostic/QualityMeasureValuesInput.vue
@@ -8,13 +8,22 @@
     <v-text-field
       hide-details="auto"
       type="number"
-      :rules="[validators.nonNegativeOrEmpty]"
+      :rules="[
+        validators.nonNegativeOrEmpty,
+        validators.gteSum(
+          [diagnostic.valueBioHt, diagnostic.valueSustainableHt, diagnostic.valueFairTradeHt],
+          totalErrorMessage
+        ),
+      ]"
       validate-on-blur
       solo
       placeholder="Je ne sais pas"
       v-model="diagnostic.valueTotalHt"
       :readonly="readonly"
       :disabled="readonly"
+      :messages="totalError ? [totalErrorMessage] : undefined"
+      :error="totalError"
+      @blur="totalError = false"
     ></v-text-field>
 
     <p class="body-2 mb-1 mt-4">...en produits bio</p>
@@ -28,6 +37,7 @@
       v-model="diagnostic.valueBioHt"
       :readonly="readonly"
       :disabled="readonly"
+      @blur="checkTotal"
     ></v-text-field>
 
     <p class="body-2 mb-1 mt-4">...en autres produits de qualité et durables (hors bio)</p>
@@ -41,6 +51,7 @@
       v-model="diagnostic.valueSustainableHt"
       :readonly="readonly"
       :disabled="readonly"
+      @blur="checkTotal"
     ></v-text-field>
 
     <p class="body-2 mb-1 mt-4">...en produits issus du commerce équitable</p>
@@ -54,6 +65,7 @@
       v-model="diagnostic.valueFairTradeHt"
       :readonly="readonly"
       :disabled="readonly"
+      @blur="checkTotal"
     ></v-text-field>
   </div>
 </template>
@@ -73,11 +85,22 @@ export default {
   data() {
     return {
       diagnostic: this.originalDiagnostic,
+      totalError: false,
+      totalErrorMessage: "Le totale ne peut pas être moins que le somme des valeurs suivantes",
     }
   },
   computed: {
     validators() {
       return validators
+    },
+  },
+  methods: {
+    checkTotal() {
+      const result = validators.gteSum(
+        [this.diagnostic.valueBioHt, this.diagnostic.valueSustainableHt, this.diagnostic.valueFairTradeHt],
+        this.totalErrorMessage
+      )(this.diagnostic.valueTotalHt)
+      this.totalError = result !== true
     },
   },
 }
