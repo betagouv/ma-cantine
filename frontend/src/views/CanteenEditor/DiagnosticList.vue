093a4a4c3c81b6615c35d54bd28d5134e844e385
Helen Root <helenroot1@gmail.com>
2021-09-28 11:56:57 +0200

    Provide more info about diagnostics if the user doesn't have any

--- frontend/src/views/CanteenEditor/DiagnosticList.vue
+++ frontend/src/views/CanteenEditor/DiagnosticList.vue
@@ -3,23 +3,50 @@
     <h1 class="font-weight-black text-h4 my-4">
       Diagnostics
     </h1>
-    <v-btn
-      text
-      color="primary"
-      class="mt-2 mb-8 ml-n4"
-      :to="{
-        name: 'NewDiagnosticForCanteen',
-        params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(originalCanteen) },
-      }"
-    >
-      <v-icon class="mr-2">mdi-plus</v-icon>
-      Ajouter un diagnostic
-    </v-btn>
-    <v-row>
-      <v-col cols="12" v-for="diagnostic in orderedDiagnostics" :key="`diagnostic-${diagnostic.id}`">
-        <DiagnosticCard :diagnostic="diagnostic" class="fill-height" />
-      </v-col>
-    </v-row>
+    <div v-if="orderedDiagnostics.length">
+      <v-btn
+        text
+        color="primary"
+        class="mt-2 mb-8 ml-n4"
+        :to="{
+          name: 'NewDiagnosticForCanteen',
+          params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(originalCanteen) },
+        }"
+      >
+        <v-icon class="mr-2">mdi-plus</v-icon>
+        Ajouter un diagnostic
+      </v-btn>
+      <v-row>
+        <v-col cols="12" v-for="diagnostic in orderedDiagnostics" :key="`diagnostic-${diagnostic.id}`">
+          <DiagnosticCard :diagnostic="diagnostic" class="fill-height" />
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else>
+      <p class="body-2">
+        Vous pouvez creer des diagnostics par année pour decouvrir les atouts ainsi que les points d'amelioration de
+        votre cantine en relation des mesures de la loi EGAlim. Après creation, vos diagnostics vous permettront à
+        publier vos données en repondant
+        <router-link
+          :to="{ name: 'KeyMeasurePage', params: { id: 'information-des-usagers' } }"
+          class="text-decoration-underline primary--text"
+        >
+          la mesure d'information
+        </router-link>
+        .
+      </p>
+      <v-btn
+        color="primary"
+        class="mt-4"
+        x-large
+        :to="{
+          name: 'NewDiagnosticForCanteen',
+          params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(originalCanteen) },
+        }"
+      >
+        Ajouter un diagnostic
+      </v-btn>
+    </div>
   </div>
 </template>
 
