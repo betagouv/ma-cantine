cde6fcfe9b769866b610c850f950f8367c2d5f4e
Helen Root <helenroot1@gmail.com>
2021-10-08 16:49:12 +0200

    Make value text bigger on publication cards

--- frontend/src/views/CanteensPage/PublishedCanteenCard.vue
+++ frontend/src/views/CanteensPage/PublishedCanteenCard.vue
@@ -7,27 +7,43 @@
     <v-card-title class="font-weight-black">
       {{ canteen.name }}
     </v-card-title>
-    <v-card-subtitle v-if="canteen.dailyMealCount || canteen.city">
-      <CanteenIndicators :canteen="canteen" :includeProductOrigin="true" />
+    <v-card-subtitle v-if="canteen.dailyMealCount || canteen.city" class="pb-2">
+      <CanteenIndicators :canteen="canteen" />
     </v-card-subtitle>
-    <v-card-text v-if="Object.keys(earnedBadges).length" class="grey--text text--darken-4 d-flex">
-      <v-img
-        max-width="25"
-        contain
-        :src="`/static/images/badge-${key}.svg`"
-        v-for="(badge, key) in earnedBadges"
-        :key="key"
-        class="mr-2"
-        :alt="badge.title"
-        :title="badge.title"
-      ></v-img>
+    <v-card-text v-if="hasBadges || hasPercentages" class="grey--text text--darken-4">
+      <v-row class="ma-0 pb-2" v-if="hasBadges">
+        <v-img
+          max-width="25"
+          contain
+          :src="`/static/images/badge-${key}.svg`"
+          v-for="(badge, key) in earnedBadges"
+          :key="key"
+          class="mr-2"
+          :alt="badge.title"
+          :title="badge.title"
+        ></v-img>
+      </v-row>
+      <v-row class="ma-0" v-if="hasPercentages">
+        <p class="ma-0 mr-4" v-if="bioPercent">
+          <span class="grey--text text-h5 font-weight-black text--darken-2 mr-1">{{ bioPercent }} %</span>
+          <span class="caption grey--text text--darken-2">
+            bio
+          </span>
+        </p>
+        <p class="ma-0" v-if="sustainablePercent">
+          <span class="grey--text text-h5 font-weight-black text--darken-2 mr-1">{{ sustainablePercent }} %</span>
+          <span class="caption grey--text text--darken-2">
+            durables et de qualité
+          </span>
+        </p>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import CanteenIndicators from "@/components/CanteenIndicators"
-import { lastYear, earnedBadges } from "@/utils"
+import { lastYear, getPercentage, isDiagnosticComplete, earnedBadges } from "@/utils"
 
 export default {
   name: "PublishedCanteenCard",
@@ -55,6 +71,24 @@ export default {
       } else {
         return {}
       }
+    },
+    hasBadges() {
+      return Object.keys(this.earnedBadges).length
+    },
+    bioPercent() {
+      return this.diagValuePercent("valueBioHt")
+    },
+    sustainablePercent() {
+      return this.diagValuePercent("valueSustainableHt")
+    },
+    hasPercentages() {
+      return this.bioPercent || this.sustainablePercent
+    },
+  },
+  methods: {
+    diagValuePercent(valueKey) {
+      if (!this.diagnostic || !isDiagnosticComplete(this.diagnostic)) return null
+      return getPercentage(this.diagnostic[valueKey], this.diagnostic.valueTotalHt)
     },
   },
 }
