5f576ef3f149a1141d80d977a803fb4c1697f59e
Helen Root <helenroot1@gmail.com>
2021-10-11 15:29:51 +0200

    Reinstate sector filter options with test

--- api/tests/test_canteens.py
+++ api/tests/test_canteens.py
@@ -458,8 +458,14 @@ class TestCanteenApi(APITestCase):
         self.assertIn("69", body.get("departments"))
 
     def test_pagination_sectors(self):
+        """
+        The pagination endpoint should return all sectors that are used by canteens, even when the data is filtered by another sector
+        """
         school = SectorFactory.create(name="School")
         enterprise = SectorFactory.create(name="Enterprise")
+        administration = SectorFactory.create(name="Administration")
+        # unused sectors shouldn't show up as an option
+        SectorFactory.create(name="Unused")
         CanteenFactory.create(
             publication_status="published", sectors=[school, enterprise], name="Shiso"
         )
@@ -470,17 +476,26 @@ class TestCanteenApi(APITestCase):
             publication_status="published", sectors=[school], name="Mochi"
         )
         CanteenFactory.create(
-            publication_status="published", sectors=[school], name="Umami"
+            publication_status="published", sectors=[administration], name="Umami"
         )
 
         url = f"{reverse('published_canteens')}"
         response = self.client.get(url)
         body = response.json()
 
-        # There are two unique sectors : school and enterprise
-        self.assertEqual(len(body.get("sectors")), 2)
+        self.assertEqual(len(body.get("sectors")), 3)
         self.assertIn(school.id, body.get("sectors"))
         self.assertIn(enterprise.id, body.get("sectors"))
+        self.assertIn(administration.id, body.get("sectors"))
+
+        url = f"{reverse('published_canteens')}?sectors={enterprise.id}"
+        response = self.client.get(url)
+        body = response.json()
+
+        self.assertEqual(len(body.get("sectors")), 3)
+        self.assertIn(school.id, body.get("sectors"))
+        self.assertIn(enterprise.id, body.get("sectors"))
+        self.assertIn(administration.id, body.get("sectors"))
 
     @authenticate
     def test_canteen_publication_fields_read_only(self):
