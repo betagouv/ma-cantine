0790eadc9898dc08786d140caaafe14f41a9339e
Helen Root <helenroot1@gmail.com>
2021-10-12 10:27:06 +0200

    Suppress button hover colour

--- frontend/src/views/CanteensPage/CanteensHome.vue
+++ frontend/src/views/CanteensPage/CanteensHome.vue
@@ -179,6 +179,8 @@
             icon
             @click="toggleOrderDirection"
             :title="`Resultats affichés en ordre ${orderDescending ? 'décroissant' : 'croissant'}`"
+            plain
+            :ripple="false"
           >
             <v-icon v-if="orderDescending">mdi-arrow-down</v-icon>
             <v-icon v-else>
