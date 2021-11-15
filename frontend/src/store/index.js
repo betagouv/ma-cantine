a5da2b7e2a92d0848a4c4a3854cbaf77745f9436
hfroot <9282816+hfroot@users.noreply.github.com>
2021-11-15 17:17:08 +0100

    Update frontend/src/store/index.js
    Co-authored-by: Alejandro M Guillén <alejandro@amguillen.dev>

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -432,7 +432,7 @@ export default new Vuex.Store({
       const title = "Oops !"
       const message =
         error instanceof AuthenticationError
-          ? "Votre session a expiré. Râfraichez la page, et vous re-connectez pour continuer."
+          ? "Votre session a expiré. Rechargez la page et reconnectez-vous pour continuer."
           : "Une erreur est survenue, vous pouvez réessayer plus tard ou nous contacter directement à contact@egalim.beta.gouv.fr"
       const status = "error"
       context.dispatch("notify", { title, message, status })
