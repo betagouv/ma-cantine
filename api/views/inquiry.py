118439ab638c8c9f84c207bbe0e5379b3b220f2f
Alejandro MG <alejandro@amguillen.dev>
2021-12-02 16:11:42 +0100

    Uses 400 bad request errors for missing mandatory fields

--- api/views/inquiry.py
+++ api/views/inquiry.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.conf import settings
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.exceptions import ValidationError
 import logging
 
 # not importing create_trello_card directly because that breaks patching for tests
@@ -26,6 +27,10 @@ class InquiryView(APIView):
 
             email = request.data.get("from")
             name = request.data.get("name")
+            message = request.data.get("message")
+            meta = request.data.get("meta") or {}
+
+            InquiryView._raise_for_mandatory_fields(email, message)
 
             title = f"{email} - {self.inquiry_types[inquiry_type]}"
 
@@ -34,19 +39,32 @@ class InquiryView(APIView):
                 title = f"({env.upper()}) {title}"
 
             body = f"Nom/Prénom\n---\n{name or 'Non renseigné'}"
-            body += f"\nMessage\n---\n{request.data.get('message')}"
+            body += f"\nMessage\n---\n{message}"
             body += "\nDétails\n---"
             body += f"\nAdresse : {email}"
-            for key, value in request.data.get("meta").items():
+            for key, value in meta.items():
                 body += f"\n{key} : {value}"
             utils.create_trello_card(title, body)
 
             return JsonResponse({}, status=status.HTTP_200_OK)
+        except ValidationError as e:
+            logger.error("Missing field from inquiry view")
+            logger.exception(e)
+            raise e
         except Exception as e:
             logger.error(f"Exception ocurred while handling inquiry. Title: {title}, Body:\n{body}")
             logger.exception(e)
-            print(e)
             return JsonResponse(
                 {"error": "An error has ocurred"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+    @staticmethod
+    def _raise_for_mandatory_fields(email, message):
+        missing_mandatory_fields = {}
+        if not email:
+            missing_mandatory_fields["from"] = "Merci d'indiquer une adresse email"
+        if not message:
+            missing_mandatory_fields["message"] = "Message manquant dans la requête"
+        if missing_mandatory_fields:
+            raise ValidationError(missing_mandatory_fields)
