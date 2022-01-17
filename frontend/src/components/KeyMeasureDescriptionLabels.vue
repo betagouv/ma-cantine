834c3a8297437bc9c3814d70036d5620cc396625
Alejandro MG <alejandro@amguillen.dev>
2022-01-17 12:15:59 +0100

    Line formatting

--- frontend/src/components/KeyMeasureDescriptionLabels.vue
+++ frontend/src/components/KeyMeasureDescriptionLabels.vue
@@ -15,7 +15,9 @@
         Produit acquis suivant des modalités prenant en compte les coûts imputés aux externalités environnementales
         liées au produit pendant son cycle de vie (production, transformation, conditionnement, transport, stockage,
         utilisation) - À ce jour, il n’existe pas de référentiel ni de méthodologie offciels afin d’effectuer une
-        sélection des produits alimentaires sur la base de ces coûts. Les acheteurs ayant recours à ce mode de sélection sont libres de définir les modalités qui leur semblent les plus pertinentes sous réserve qu'elles respectent les conditions de recours à une telle évaluation fixées par le code de la commande publique.
+        sélection des produits alimentaires sur la base de ces coûts. Les acheteurs ayant recours à ce mode de sélection
+        sont libres de définir les modalités qui leur semblent les plus pertinentes sous réserve qu'elles respectent les
+        conditions de recours à une telle évaluation fixées par le code de la commande publique.
       </li>
       <li>
         <v-icon color="green" small>mdi-checkbox-marked</v-icon>
