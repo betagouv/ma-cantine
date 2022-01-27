6197a9bfd1c1ce7745317720452cbde88efd0194
Jenn S <jennifer.stephan@edu.escpeurope.eu>
2022-01-27 16:04:05 +0100

    Update DiversificationMeasure.vue

--- frontend/src/components/KeyMeasureResource/DiversificationMeasure.vue
+++ frontend/src/components/KeyMeasureResource/DiversificationMeasure.vue
@@ -10,14 +10,14 @@
       ressources.
     </v-alert>
   </v-card>
-    <v-card
+  <v-card
     href="https://ma-cantine-1.gitbook.io/ma-cantine-egalim/diversification-des-sources-de-proteines-et-menus-vegetariens/untitled"
     class="text-decoration-none text-left mt-4"
     target="_blank"
     hover
   >
     <v-alert icon="mdi-carrot" class="text-body-2 mb-0" color="#61753f" prominent colored-border border="left">
-      Télécharger le guide de plan de diversification des protéines
+      Télécharger le guide de plan de diversification des protéines.
     </v-alert>
   </v-card>
 </template>
