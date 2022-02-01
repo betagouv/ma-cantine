201ade4fe40ec7aa683e65504205348e144d2271
Alejandro MG <alejandro@amguillen.dev>
2022-02-01 15:45:31 +0100

    Fixes double template tag

--- frontend/src/components/KeyMeasureResource/DiversificationMeasure.vue
+++ frontend/src/components/KeyMeasureResource/DiversificationMeasure.vue
@@ -1,25 +1,27 @@
 <template>
-  <v-card
-    href="https://ma-cantine-1.gitbook.io/ma-cantine-egalim/diversification-des-sources-de-proteines-et-menus-vegetariens/guide-pour-la-mise-en-place-du-menu-vegetarien-en-milieu-scolaire"
-    class="text-decoration-none text-left mt-4"
-    target="_blank"
-    hover
-  >
-    <v-alert icon="mdi-carrot" class="text-body-2 mb-0" color="#61753f" prominent colored-border border="left">
-      Vous pouvez accéder à un guide menu végétarien ou notre livret de recettes pour menus végétariens dans nos
-      ressources.
-    </v-alert>
-  </v-card>
-</template>
-<template>
-  <v-card
-    href="https://ma-cantine-1.gitbook.io/ma-cantine-egalim/diversification-des-sources-de-proteines-et-menus-vegetariens/untitled"
-    class="text-decoration-none text-left mt-4"
-    target="_blank"
-    hover
-  >
-    <v-alert icon="mdi-carrot" class="text-body-2 mb-0" color="#61753f" prominent colored-border border="left">
-      Télécharger le guide de plan de diversification des protéines.
-    </v-alert>
-  </v-card>
+  <div>
+    <v-card
+      href="https://ma-cantine-1.gitbook.io/ma-cantine-egalim/diversification-des-sources-de-proteines-et-menus-vegetariens/guide-pour-la-mise-en-place-du-menu-vegetarien-en-milieu-scolaire"
+      class="text-decoration-none text-left mt-4"
+      target="_blank"
+      rel="noopener"
+      hover
+    >
+      <v-alert icon="mdi-carrot" class="text-body-2 mb-0" color="#61753f" prominent colored-border border="left">
+        Vous pouvez accéder à un guide menu végétarien ou notre livret de recettes pour menus végétariens dans nos
+        ressources.
+      </v-alert>
+    </v-card>
+    <v-card
+      href="https://ma-cantine-1.gitbook.io/ma-cantine-egalim/diversification-des-sources-de-proteines-et-menus-vegetariens/untitled"
+      class="text-decoration-none text-left mt-4"
+      target="_blank"
+      rel="noopener"
+      hover
+    >
+      <v-alert icon="mdi-carrot" class="text-body-2 mb-0" color="#61753f" prominent colored-border border="left">
+        Télécharger le guide de plan de diversification des protéines.
+      </v-alert>
+    </v-card>
+  </div>
 </template>
