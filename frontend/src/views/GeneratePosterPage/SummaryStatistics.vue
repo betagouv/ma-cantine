1bfc878679908da559c71e75e8e7cba21500a1d7
Helen Root <helenroot1@gmail.com>
2022-02-15 15:53:22 +0100

    Make sure legend chart option is updated on change

--- frontend/src/views/GeneratePosterPage/SummaryStatistics.vue
+++ frontend/src/views/GeneratePosterPage/SummaryStatistics.vue
@@ -24,9 +24,21 @@ export default {
     width: Number,
     hideLegend: Boolean,
   },
-  data() {
-    return {
-      chartOptions: {
+  computed: {
+    dataPresent() {
+      return !!this.qualityDiagnostic.valueTotalHt
+    },
+    bioPercentage() {
+      return getPercentage(this.qualityDiagnostic.valueBioHt, this.qualityDiagnostic.valueTotalHt)
+    },
+    sustainablePercentage() {
+      return getPercentage(this.qualityDiagnostic.valueSustainableHt, this.qualityDiagnostic.valueTotalHt)
+    },
+    series() {
+      return [this.bioPercentage, this.sustainablePercentage, 100 - this.bioPercentage - this.sustainablePercentage]
+    },
+    chartOptions() {
+      return {
         plotOptions: {
           pie: {
             expandOnClick: false,
@@ -61,28 +73,14 @@ export default {
           position: "right",
         },
         dataLabels: {
-          formatter: function(value) {
+          formatter(value) {
             return value + "%"
           },
           dropShadow: {
             enabled: false,
           },
         },
-      },
-    }
-  },
-  computed: {
-    dataPresent() {
-      return !!this.qualityDiagnostic.valueTotalHt
-    },
-    bioPercentage() {
-      return getPercentage(this.qualityDiagnostic.valueBioHt, this.qualityDiagnostic.valueTotalHt)
-    },
-    sustainablePercentage() {
-      return getPercentage(this.qualityDiagnostic.valueSustainableHt, this.qualityDiagnostic.valueTotalHt)
-    },
-    series() {
-      return [this.bioPercentage, this.sustainablePercentage, 100 - this.bioPercentage - this.sustainablePercentage]
+      }
     },
   },
 }
