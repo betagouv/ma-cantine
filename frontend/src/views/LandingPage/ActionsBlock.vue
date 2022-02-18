7b7ccedb40dfdcb53bf7dc508b59524647c5ad86
Helen Root <helenroot1@gmail.com>
2022-02-18 12:08:33 +0100

    Colours, elevation, text size

--- frontend/src/views/LandingPage/ActionsBlock.vue
+++ frontend/src/views/LandingPage/ActionsBlock.vue
@@ -3,81 +3,99 @@
     <v-row class="text-left">
       <v-spacer></v-spacer>
       <v-col cols="12" md="4">
-        <v-card hover :to="{ name: 'PublicCanteenStatisticsPage' }" class="fill-height pa-4 d-flex flex-column">
-          <v-img
-            src="/static/images/elue.png"
-            v-if="$vuetify.breakpoint.smAndUp"
-            class="mx-auto rounded-0"
-            contain
-            max-height="100"
-          ></v-img>
-          <v-card-title>
-            <h2 class="text-h6 font-weight-bold">
-              Étudier l'évolution de mon territoire
-            </h2>
-          </v-card-title>
-          <v-card-text>
-            Savoir où en sont les établissements de ma région ou département
-          </v-card-text>
-          <v-spacer></v-spacer>
-          <v-card-actions class="px-4 pt-0">
-            <v-spacer></v-spacer>
-            <v-icon color="primary">mdi-arrow-right</v-icon>
-          </v-card-actions>
-        </v-card>
+        <v-hover>
+          <template v-slot:default="{ hover }">
+            <v-card
+              :elevation="hover ? 10 : 2"
+              :to="loggedUser ? { name: 'ManagementPage' } : undefined"
+              :href="!loggedUser ? '/s-identifier' : undefined"
+              class="fill-height pa-4 d-flex flex-column"
+            >
+              <v-img
+                src="/static/images/doodles/secondary/SprintingDoodle.png"
+                v-if="$vuetify.breakpoint.smAndUp"
+                class="mx-auto rounded-0"
+                contain
+                max-height="100"
+              ></v-img>
+              <v-card-title>
+                <h2 class="text-h6 font-weight-bold">
+                  Gérer ma cantine et mes objectifs
+                </h2>
+              </v-card-title>
+              <v-card-text>
+                Être outillé pour attendre les objectifs des lois EGAlim et Climat
+              </v-card-text>
+              <v-spacer></v-spacer>
+              <v-card-actions class="px-4 pt-0">
+                <v-spacer></v-spacer>
+                <v-icon color="secondary">mdi-arrow-right</v-icon>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
-
       <v-col cols="12" md="4">
-        <v-card
-          hover
-          :to="loggedUser ? { name: 'ManagementPage' } : undefined"
-          :href="!loggedUser ? '/s-identifier' : undefined"
-          class="fill-height pa-4 d-flex flex-column"
-        >
-          <v-img
-            src="/static/images/ChartDoodle.png"
-            v-if="$vuetify.breakpoint.smAndUp"
-            class="mx-auto rounded-0"
-            contain
-            max-height="100"
-          ></v-img>
-          <v-card-title>
-            <h2 class="text-h6 font-weight-bold">
-              Gérer ma cantine et mes objectifs
-            </h2>
-          </v-card-title>
-          <v-card-text>
-            Être outillé pour attendre les objectifs des lois EGAlim et Climat
-          </v-card-text>
-          <v-spacer></v-spacer>
-          <v-card-actions class="px-4 pt-0">
-            <v-spacer></v-spacer>
-            <v-icon color="primary">mdi-arrow-right</v-icon>
-          </v-card-actions>
-        </v-card>
+        <v-hover>
+          <template v-slot:default="{ hover }">
+            <v-card
+              :elevation="hover ? 10 : 2"
+              :to="{ name: 'CanteensHome' }"
+              class="fill-height pa-4 d-flex flex-column"
+            >
+              <v-img
+                src="/static/images/doodles/secondary/DogDoodle.png"
+                v-if="$vuetify.breakpoint.smAndUp"
+                class="mx-auto rounded-0"
+                contain
+                max-height="100"
+              ></v-img>
+              <v-card-title class="text-h6 font-weight-bold">
+                <h2 class="text-h6 font-weight-bold">En savoir plus sur la cantine que je fréquente</h2>
+              </v-card-title>
+              <v-card-text>
+                Connaître les initiatives de mon restaurant ou celui de mes enfants
+              </v-card-text>
+              <v-spacer></v-spacer>
+              <v-card-actions class="px-4 pt-0">
+                <v-spacer></v-spacer>
+                <v-icon color="secondary">mdi-arrow-right</v-icon>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
-
       <v-col cols="12" md="4">
-        <v-card hover :to="{ name: 'CanteensHome' }" class="fill-height pa-4 d-flex flex-column">
-          <v-img
-            src="/static/images/parent.png"
-            v-if="$vuetify.breakpoint.smAndUp"
-            class="mx-auto rounded-0"
-            contain
-            max-height="100"
-          ></v-img>
-          <v-card-title class="text-h6 font-weight-bold">
-            <h2 class="text-h6 font-weight-bold">En savoir plus sur la cantine que je fréquente</h2>
-          </v-card-title>
-          <v-card-text>
-            Connaître les initiatives de mon restaurant ou celui de mes enfants
-          </v-card-text>
-          <v-spacer></v-spacer>
-          <v-card-actions class="px-4 pt-0">
-            <v-spacer></v-spacer>
-            <v-icon color="primary">mdi-arrow-right</v-icon>
-          </v-card-actions>
-        </v-card>
+        <v-hover>
+          <template v-slot:default="{ hover }">
+            <v-card
+              :elevation="hover ? 10 : 2"
+              :to="{ name: 'PublicCanteenStatisticsPage' }"
+              class="fill-height pa-4 d-flex flex-column"
+            >
+              <v-img
+                src="/static/images/doodles/secondary/SitReadingDoodle.png"
+                v-if="$vuetify.breakpoint.smAndUp"
+                class="mx-auto rounded-0"
+                contain
+                max-height="100"
+              ></v-img>
+              <v-card-title>
+                <h2 class="text-h6 font-weight-bold">
+                  Étudier l'évolution de mon territoire
+                </h2>
+              </v-card-title>
+              <v-card-text>
+                Savoir où en sont les établissements de ma région ou département
+              </v-card-text>
+              <v-spacer></v-spacer>
+              <v-card-actions class="px-4 pt-0">
+                <v-spacer></v-spacer>
+                <v-icon color="secondary">mdi-arrow-right</v-icon>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
       <v-spacer></v-spacer>
     </v-row>
--- frontend/src/views/LandingPage/PresentationBlock.vue
+++ frontend/src/views/LandingPage/PresentationBlock.vue
@@ -3,15 +3,15 @@
     <v-card elevation="0" class="text-center text-md-right">
       <v-row v-if="$vuetify.breakpoint.smAndDown">
         <v-col cols="12">
-          <v-img max-height="150px" contain src="/static/images/SittingChef.png"></v-img>
+          <v-img max-height="150px" contain src="/static/images/doodles/primary/SittingChef.png"></v-img>
         </v-col>
       </v-row>
       <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="12" sm="9" md="6">
+        <v-col cols="12" md="7">
           <v-card-title>
             <v-spacer></v-spacer>
-            <h1 class="font-weight-black text-h4">
+            <h1 class="font-weight-black text-h3">
               Mieux manger
               <br v-if="$vuetify.breakpoint.mdAndUp" />
               de la crêche à l’EHPAD
@@ -33,11 +33,12 @@
             </p>
           </v-card-text>
         </v-col>
-        <v-col cols="12" sm="3" md="3" class="d-flex flex-column" v-if="$vuetify.breakpoint.mdAndUp">
+        <v-col cols="3" class="d-flex flex-column" v-if="$vuetify.breakpoint.mdAndUp">
           <v-spacer></v-spacer>
-          <v-img max-height="200px" contain src="/static/images/SittingChef.png"></v-img>
+          <v-img max-height="200px" contain src="/static/images/doodles/primary/SittingChef.png"></v-img>
           <v-spacer></v-spacer>
         </v-col>
+        <v-spacer v-if="$vuetify.breakpoint.mdAndUp"></v-spacer>
         <v-spacer></v-spacer>
       </v-row>
     </v-card>
