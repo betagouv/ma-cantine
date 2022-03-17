42cae7e88ae7f9ef25af3bd49a84b7ad386fce2a
Alejandro MG <alejandro@amguillen.dev>
2022-03-17 14:57:03 +0100

    Adds read-only purchases to admin

--- data/admin/__init__.py
+++ data/admin/__init__.py
@@ -8,6 +8,7 @@ from .managerinvitation import ManagerInvitation  # noqa
 from .teledeclaration import TeledeclarationAdmin  # noqa
 from .reservationexpe import ReservationExpeAdmin  # noqa
 from .message import MessageAdmin  # noqa
+from .purchase import PurchaseAdmin  # noqa
 
 from django.contrib import admin
 from django.contrib.auth.models import Group
