dd5ec03648fd231ff82642b56b818f50bb7cec37
Alejandro MG <alejandro@amguillen.dev>
2022-04-11 16:27:10 +0200

    Service API pour la génération d'une suggestion de nom d'utilisateur

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -2,6 +2,7 @@ import json
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from data.factories import UserFactory
 from .utils import authenticate
 
 
@@ -28,3 +29,34 @@ class TestLoggedUserApi(APITestCase):
         self.assertEqual(body.get("firstName"), authenticate.user.first_name)
         self.assertEqual(body.get("lastName"), authenticate.user.last_name)
         self.assertIn("id", body)
+
+    def test_username_generation(self):
+        response = self.client.post(reverse("username_suggestion"), {"first_name": "Anne", "last_name": "Iversaire"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        body = json.loads(response.content.decode())
+        self.assertIn("suggestion", body)
+        self.assertEqual(body["suggestion"], "anne_iversaire")
+
+    def test_username_generation_with_spaces(self):
+        response = self.client.post(
+            reverse("username_suggestion"), {"first_name": " Omar Khe ", "last_name": "   Ting"}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        body = json.loads(response.content.decode())
+        self.assertIn("suggestion", body)
+        self.assertEqual(body["suggestion"], "omar-khe_ting")
+
+    def test_with_email(self):
+        response = self.client.post(reverse("username_suggestion"), {"email": "tester_12@example.com"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        body = json.loads(response.content.decode())
+        self.assertIn("suggestion", body)
+        self.assertEqual(body["suggestion"], "tester_12")
+
+    def test_random_number(self):
+        UserFactory.create(username="anne_iversaire")
+        response = self.client.post(reverse("username_suggestion"), {"first_name": "Anne", "last_name": "Iversaire"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        body = json.loads(response.content.decode())
+        self.assertIn("suggestion", body)
+        self.assertTrue(body["suggestion"].startswith("anne_iversaire_"))
