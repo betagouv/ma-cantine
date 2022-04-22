7e9cbdea6c2cbc90c4dd252e84878983d8d4a05b
hfroot <9282816+hfroot@users.noreply.github.com>
2022-04-22 10:43:38 +0200

    Update frontend/src/components/GeneralContactForm.vue
    Co-authored-by: Alejandro M Guillén <alejandro@amguillen.dev>

--- frontend/src/components/GeneralContactForm.vue
+++ frontend/src/components/GeneralContactForm.vue
@@ -108,7 +108,7 @@ export default {
         functionalityQuestion: "fonctionnalité",
         bug: "bug",
         egalim: "loi",
-        demo: "requête de démo",
+        demo: "demande de démo",
         other: "autre",
       }
 
