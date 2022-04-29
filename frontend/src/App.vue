4eee44691135a461033e42db751e5786b028ce14
Helen Root <helenroot1@gmail.com>
2022-04-29 11:53:03 +0200

    Add user email if logged in

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -47,6 +47,11 @@ export default {
       this.$store.dispatch("removeLocalStorageDiagnostics")
     },
   },
+  mounted() {
+    if (this.$store.state.loggedUser) {
+      window.$crisp.push(["set", "user:email", [this.$store.state.loggedUser.email]])
+    }
+  },
 }
 </script>
 
