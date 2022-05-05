55b41db2e1ff21555f4210b25f955e8b337f0656
Jenn S <jennifer.stephan@edu.escpeurope.eu>
2022-05-05 14:14:26 +0200

    ajout du guide d'expé gaspi sur la mesure gaspi

--- frontend/src/components/KeyMeasureResource/WasteMeasure.vue
+++ frontend/src/components/KeyMeasureResource/WasteMeasure.vue
@@ -10,4 +10,14 @@
       dons.
     </v-alert>
   </v-card>
+  <v-card
+    href="/static/documents/Guide_pratique_XP_RESERVATION.pdf"
+    target="_blank"
+    class="text-decoration-none text-left mt-4"
+    hover
+  >
+    <v-alert icon="mdi-content-paste" class="text-body-2 mb-0" color="#3F62D5" prominent colored-border border="left">
+      Consulter le Guide d’accompagnement – décret d’expérimentation de solution de réservation de repas en restauration collective
+    </v-alert>
+  </v-card>
 </template>
