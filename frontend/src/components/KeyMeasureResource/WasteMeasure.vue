9f7fadcd2043efdbde47851f420ae529d7f19d14
Alejandro MG <alejandro@amguillen.dev>
2022-05-05 14:31:55 +0200

    Fix de souci avec les racines multiples du WasteMeasure.vue. Formatting Eslint.

--- frontend/src/components/KeyMeasureResource/WasteMeasure.vue
+++ frontend/src/components/KeyMeasureResource/WasteMeasure.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-card
-    href="/static/documents/Convention de dons type - restauration collective.docx"
-    target="_blank"
-    class="text-decoration-none text-left mt-4"
-    hover
-  >
-    <v-alert icon="mdi-content-paste" class="text-body-2 mb-0" color="#3F62D5" prominent colored-border border="left">
-      Vous voulez éviter le gaspillage ? Télécharger ce modèle type de convention aux associations auxquelles faire des
-      dons.
-    </v-alert>
-  </v-card>
-  <v-card
-    href="/static/documents/Guide_pratique_XP_RESERVATION.pdf"
-    target="_blank"
-    class="text-decoration-none text-left mt-4"
-    hover
-  >
-    <v-alert icon="mdi-content-paste" class="text-body-2 mb-0" color="#3F62D5" prominent colored-border border="left">
-      Consulter le Guide d’accompagnement – décret d’expérimentation de solution de réservation de repas en restauration collective
-    </v-alert>
-  </v-card>
+  <div>
+    <v-card
+      href="/static/documents/Convention de dons type - restauration collective.docx"
+      target="_blank"
+      class="text-decoration-none text-left mt-4"
+      hover
+    >
+      <v-alert icon="mdi-content-paste" class="text-body-2 mb-0" color="#3F62D5" prominent colored-border border="left">
+        Vous voulez éviter le gaspillage ? Télécharger ce modèle type de convention aux associations auxquelles faire
+        des dons.
+      </v-alert>
+    </v-card>
+    <v-card
+      href="/static/documents/Guide_pratique_XP_RESERVATION.pdf"
+      target="_blank"
+      class="text-decoration-none text-left mt-4"
+      hover
+    >
+      <v-alert icon="mdi-content-paste" class="text-body-2 mb-0" color="#3F62D5" prominent colored-border border="left">
+        Consulter le Guide d’accompagnement – décret d’expérimentation de solution de réservation de repas en
+        restauration collective
+      </v-alert>
+    </v-card>
+  </div>
 </template>
