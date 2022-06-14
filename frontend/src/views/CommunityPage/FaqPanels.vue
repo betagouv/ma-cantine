e4f2eb8596af1a90a2930182af8cefc0c9c07f5f
Alejandro MG <alejandro@amguillen.dev>
2022-06-14 16:08:47 +0200

    Ajout des guillemets

--- frontend/src/views/CommunityPage/FaqPanels.vue
+++ frontend/src/views/CommunityPage/FaqPanels.vue
@@ -33,17 +33,17 @@ export default {
     return {
       questions: [
         {
-          title: "L'inscription sur Ma Cantine est-elle obligatoire ?",
+          title: "L'inscription sur « ma cantine » est-elle obligatoire ?",
           answer:
             "L'inscription est fortement recommandée dès maintenant pour bénéficier d'un accompagnement à la mise en place des mesures réglementaires des lois EGAlim et Climat et résilience. Elle est aussi un moyen de répondre à vos obligations d'information de vos convives et de faire remonter les informations nécessaires pour le suivi de l'application de la loi (bilan statistique annuel).",
         },
         {
           title: "Qui est le responsable restauration, qui doit etre désigné sous ma cantine ?",
           answer:
-            "La structure/personne responsable légal du service de restauration : élu, responsable administratif de la collectivité D'autres personnes peuvent être associées à votre compte cantine : personne qui fait les commandes, celle qui reçoit les repas et met en chauffe, le prestataire peut aussi participer au renseignement des informations concernant votre cantine si vous lui donnez un accès à votre compte « cantine ». Il suffit d'indiquer son mail dans la partie « gestionnaire additionnel » de votre profil. Exemple : si la gestion des repas est assurée par un SIVU par le biais d'une cuisine centrale. Le SIVU doit créer un compte dans ma cantine pour inscrire sa cantine et déclarer le nombre de cantines sites (satellites) desservis et vous pouvez aussi créer votre cantine le satellite desservis par cette cuisine centrale.",
+            "La structure/personne responsable légal du service de restauration : élu, responsable administratif de la collectivité D'autres personnes peuvent être associées à votre compte cantine : personne qui fait les commandes, celle qui reçoit les repas et met en chauffe, le prestataire peut aussi participer au renseignement des informations concernant votre cantine si vous lui donnez un accès à votre compte « cantine ». Il suffit d'indiquer son mail dans la partie « gestionnaire additionnel » de votre profil. Exemple : si la gestion des repas est assurée par un SIVU par le biais d'une cuisine centrale. Le SIVU doit créer un compte dans « ma cantine » pour inscrire sa cantine et déclarer le nombre de cantines sites (satellites) desservis et vous pouvez aussi créer votre cantine le satellite desservis par cette cuisine centrale.",
         },
         {
-          title: "Nous sommes satellites, faut-il se référencer dans ma cantine ?",
+          title: "Nous sommes satellites, faut-il se référencer dans « ma cantine » ?",
           answer:
             "Oui. Il est également nécessaire de donner l'info (SIRET) sur sa cuisine centrale qui dessert la cantine.",
         },
@@ -55,7 +55,7 @@ export default {
         {
           title: "Je suis en gestion concédée/approvisionné par un restaurateur privé, suis-je concerné ?",
           answer:
-            "Vous êtes concerné et vous êtes encouragé à vous référencer sur ma cantine (création de compte et création de cantine). Vous pourrez demander à votre prestataire les informations nécessaires - notamment sur les achats.",
+            "Vous êtes concerné et vous êtes encouragé à vous référencer sur « ma cantine » (création de compte et création de cantine). Vous pourrez demander à votre prestataire les informations nécessaires - notamment sur les achats.",
         },
         {
           title:
