58840a0d7e935ab2f8c1ec210e349fd6da75623a
Helen Root <helenroot1@gmail.com>
2022-06-17 15:25:34 +0200

    Draft reuse dashboard for guiding managers

--- frontend/src/components/CanteenDashboard.vue
+++ frontend/src/components/CanteenDashboard.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
+  <div class="text-left">
     <v-row>
       <v-col cols="12">
         <v-card outlined class="mt-4 pa-4">
           <v-card-title>
-            <h2 class="font-weight-bold text-h6" id="appro-heading">
-              Données d'approvisionnement en produits de qualité et durables
+            <h2 class="font-weight-bold text-h6">
+              <KeyMeasureTitle class="flex-shrink-1" :measure="qualityMeasure" />
             </h2>
           </v-card-title>
           <v-card-text class="pb-0">
-            <p class="text-left grey--text text--darken-3">
+            <p class="grey--text text--darken-3">
               La loi EGAlim encadre la répartition des produits achetés pour la conception des repas. Les menus doivent
               comporter, au cours de l'année 2022, 50% de produits de qualité et durables dont 20% issus de
               l’agriculture biologique ou en conversion.
             </p>
             <MultiYearSummaryStatistics
-              :diagnostics="diagnostics"
+              :diagnostics="dashboardDiagnostics"
               headingId="appro-heading"
               height="260"
               :width="$vuetify.breakpoint.mdAndUp ? '650px' : '100%'"
@@ -25,10 +25,12 @@
         </v-card>
       </v-col>
 
-      <v-col cols="12" md="6">
+      <v-col cols="12" :md="singleColumn ? '12' : '6'">
         <v-card outlined class="mt-4 pa-4">
           <v-card-title class="font-weight-bold">
-            <h2 class="font-weight-bold text-h6">Initiatives contre le gaspillage alimentaire</h2>
+            <h2 class="font-weight-bold text-h6">
+              <KeyMeasureTitle class="flex-shrink-1" :measure="wasteMeasure" />
+            </h2>
           </v-card-title>
           <v-card-text>
             <div class="actions">
@@ -40,12 +42,12 @@
                 :isDone="latestDiagnostic.hasWastePlan"
                 label="Mise en place d'un plan d'actions contre le gaspillage"
               />
-              <ul class="specifics-actions text-left ml-4">
+              <ul class="specifics-actions ml-4">
                 <li v-for="action in latestDiagnostic.wasteActions" :key="action" class="my-1">
                   {{ wasteActions[action] }}
                 </li>
               </ul>
-              <ul class="specifics-actions text-left ml-4" v-if="latestDiagnostic.otherWasteAction">
+              <ul class="specifics-actions ml-4" v-if="latestDiagnostic.otherWasteAction">
                 <li class="my-1">
                   {{ latestDiagnostic.otherWasteAction }}
                 </li>
@@ -58,9 +60,12 @@
 
         <v-card outlined class="mt-4 pa-4">
           <v-card-title class="text-left">
-            <h2 class="font-weight-bold text-h6">Dans l'établissement, ont été supprimé l'usage des :</h2>
+            <h2 class="font-weight-bold text-h6">
+              <KeyMeasureTitle class="flex-shrink-1" :measure="noPlasticMeasure" />
+            </h2>
           </v-card-title>
           <v-card-text>
+            <p>Dans l'établissement, ont été supprimé l'usage des :</p>
             <div class="actions">
               <KeyMeasureAction
                 :isDone="latestDiagnostic.cookingPlasticSubstituted"
@@ -83,10 +88,12 @@
         </v-card>
       </v-col>
 
-      <v-col cols="12" md="6">
+      <v-col cols="12" :md="singleColumn ? '12' : '6'">
         <v-card outlined class="mt-4 pa-4">
           <v-card-title>
-            <h2 class="font-weight-bold text-h6">{{ diversificationMeasure.shortTitle }}</h2>
+            <h2 class="font-weight-bold text-h6">
+              <KeyMeasureTitle class="flex-shrink-1" :measure="diversificationMeasure" />
+            </h2>
           </v-card-title>
           <v-card-text>
             <div class="actions">
@@ -102,7 +109,9 @@
 
         <v-card outlined class="mt-4 pa-4">
           <v-card-title>
-            <h2 class="font-weight-bold text-h6">{{ informationMeasure.shortTitle }}</h2>
+            <h2 class="font-weight-bold text-h6">
+              <KeyMeasureTitle class="flex-shrink-1" :measure="informationMeasure" />
+            </h2>
           </v-card-title>
           <v-card-text>
             <div class="actions">
@@ -114,12 +123,12 @@
                 :isDone="latestDiagnostic.communicationSupports && latestDiagnostic.communicationSupports.length > 0"
                 label="Communication à disposition des convives sur la qualité des approvisionnements"
               />
-              <ul class="specifics-actions text-left ml-4">
+              <ul class="specifics-actions ml-4">
                 <li v-for="action in latestDiagnostic.communicationSupports" :key="action" class="my-1">
                   {{ communicationSupports[action] }}
                 </li>
               </ul>
-              <ul class="specifics-actions text-left ml-4" v-if="latestDiagnostic.otherCommunicationSupport">
+              <ul class="specifics-actions ml-4" v-if="latestDiagnostic.otherCommunicationSupport">
                 <li class="my-1">
                   {{ latestDiagnostic.otherCommunicationSupport }}
                 </li>
@@ -151,30 +160,31 @@ import wasteActions from "@/data/waste-actions.json"
 import communicationSupports from "@/data/communication-supports.json"
 import KeyMeasureResource from "@/components/KeyMeasureResource"
 import KeyMeasureAction from "@/components/KeyMeasureAction"
+import KeyMeasureTitle from "@/components/KeyMeasureTitle"
 import MultiYearSummaryStatistics from "./MultiYearSummaryStatistics"
+import { lastYear } from "@/utils"
 
 export default {
   components: {
     KeyMeasureResource,
     KeyMeasureAction,
+    KeyMeasureTitle,
     MultiYearSummaryStatistics,
   },
   props: {
-    diagnostics: Object,
+    diagnostics: {
+      Object,
+      required: false,
+    },
     showResources: Boolean,
     canteen: {
       type: Object,
       required: false,
     },
+    singleColumn: Boolean,
   },
   data() {
-    const latestDiagnostic = this.diagnostics.latest
-    const vegetarianFrequency = latestDiagnostic.vegetarianWeeklyRecurrence
-    const hasVegetarianMenu = vegetarianFrequency && vegetarianFrequency !== "LOW"
-
     return {
-      latestDiagnostic,
-      previousDiagnostic: this.diagnostics.previous,
       wasteActions,
       communicationSupports,
       qualityMeasure: keyMeasures.find((measure) => measure.id === "qualite-des-produits"),
@@ -182,27 +192,49 @@ export default {
       diversificationMeasure: keyMeasures.find((measure) => measure.id === "diversification-des-menus"),
       noPlasticMeasure: keyMeasures.find((measure) => measure.id === "interdiction-du-plastique"),
       informationMeasure: keyMeasures.find((measure) => measure.id === "information-des-usagers"),
-      vegetarianFrequency,
-      hasVegetarianMenu,
-      vegetarianMenuActionLabel: getVegetarianMenuActionLabel(hasVegetarianMenu, vegetarianFrequency),
     }
+  },
+  computed: {
+    dashboardDiagnostics() {
+      if (this.diagnostics) {
+        return this.diagnostics
+      }
+      const diagCount = this.canteen.diagnostics.length
+      if (!diagCount) {
+        return {} // TODO: what to do?
+      }
+      const lastYearDiag = this.canteen.diagnostics.find((d) => d.year === lastYear())
+      return {
+        latest: lastYearDiag || this.canteen.diagnostics[diagCount - 1],
+      }
+    },
+    latestDiagnostic() {
+      return this.dashboardDiagnostics.latest
+    },
+    previousDiagnostic() {
+      return this.dashboardDiagnostics.previous
+    },
+    vegetarianFrequency() {
+      return this.latestDiagnostic.vegetarianWeeklyRecurrence
+    },
+    hasVegetarianMenu() {
+      return this.vegetarianFrequency && this.vegetarianFrequency !== "LOW"
+    },
+    vegetarianMenuActionLabel() {
+      if (this.vegetarianFrequency === "MID") {
+        return "Mise en place d'un menu végétarien par semaine"
+      } else if (this.vegetarianFrequency === "HIGH") {
+        return "Mise en place d'un menu végétarien plusieurs fois par semaine"
+      } else if (this.vegetarianFrequency === "DAILY") {
+        return "Mise en place d'un menu végétarien de façon quotidienne"
+      }
+      return "Pas de menu végétarien hébdomadaire"
+    },
   },
   methods: {
     prepareHref(link) {
       return link.startsWith("http") ? link : "//" + link
     },
   },
-}
-
-function getVegetarianMenuActionLabel(hasVegetarianMenu, vegetarianFrequency) {
-  if (!hasVegetarianMenu) {
-    return "Pas de menu végétarien hébdomadaire"
-  } else if (vegetarianFrequency === "MID") {
-    return "Mise en place d'un menu végétarien par semaine"
-  } else if (vegetarianFrequency === "HIGH") {
-    return "Mise en place d'un menu végétarien plusieurs fois par semaine"
-  } else if (vegetarianFrequency === "DAILY") {
-    return "Mise en place d'un menu végétarien de façon quotidienne"
-  }
 }
 </script>
