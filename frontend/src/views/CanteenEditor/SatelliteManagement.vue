f3bf2d4b53a534a19d11447afde983d0c539ea5a
Alejandro MG <alejandro@amguillen.dev>
2022-07-05 12:53:09 +0200

    Use of v-data-table

--- frontend/src/views/CanteenEditor/SatelliteManagement.vue
+++ frontend/src/views/CanteenEditor/SatelliteManagement.vue
@@ -1,108 +1,110 @@
 <template>
   <div class="text-left">
     <h1 class="font-weight-black text-h4 my-4">
-      Les cantines satellites
+      Vos cantines satellites
     </h1>
     <p>
       Cette cuisine centrale fournit des repas
       {{ satelliteCanteensCount > 1 ? `à ${satelliteCanteensCount} cantines` : "à une cantine" }}.
+      <span v-if="!existingSatellites || existingSatellites.length === 0">
+        Vous n'avez ajouté aucune cantine satellite.
+      </span>
+      <span v-else-if="satelliteCanteensCount !== existingSatellites.length">
+        Vous en avez renseigné {{ existingSatellites.length }}.
+      </span>
     </p>
-    <!-- TODO: replace with condensed version of canteen pagination - limit to 10 at a time displayed -->
-    <!-- including info on location, nb repas, sectors -->
-    <div v-if="existingSatellites.length">
-      <ol>
-        <v-row v-for="satellite in existingSatellites" :key="satellite.id">
-          <v-col cols="12" md="9" class="pb-1 pb-md-3">
-            <li>{{ satellite.name }} (SIRET : {{ satellite.siret }})</li>
-          </v-col>
-          <v-col class="pt-0 pt-md-3">
-            <router-link
-              color="primary"
-              :to="{
-                name: 'CanteenModification',
-                params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(satellite) },
-              }"
-            >
-              Mettez-la à jour
-            </router-link>
-          </v-col>
-        </v-row>
-      </ol>
-    </div>
-    <h2 class="mt-10 mb-4">
-      Reliér vos cuisines satellites
+    <v-data-table
+      :options.sync="options"
+      :items="existingSatellites"
+      @click:row="onRowClick"
+      :headers="headers"
+      hide-default-footer
+    >
+      <template v-slot:[`item.edit`]="{}">
+        <a>
+          Mettre à jour
+        </a>
+      </template>
+    </v-data-table>
+
+    <v-divider class="my-8"></v-divider>
+    <h2 class="mb-4 text-h6 font-weight-bold">
+      Ajoutez une nouvelle cantine satellite
     </h2>
-    <v-form ref="form" v-model="formIsValid">
-      <v-row>
-        <v-col cols="12" md="8">
-          <label class="body-2" for="satellite-siret">SIRET</label>
-          <v-text-field
-            id="satellite-siret"
-            class="mt-2"
-            hide-details="auto"
-            validate-on-blur
-            solo
-            v-model="satellite.siret"
-            :rules="[validators.length(14), validators.luhn]"
-          ></v-text-field>
-          <p class="caption mt-1 ml-2">
-            Utilisez cet
-            <a href="https://annuaire-entreprises.data.gouv.fr/" target="_blank" rel="noopener">
-              outil de recherche pour trouver le SIRET
-              <v-icon color="primary" small>mdi-open-in-new</v-icon>
-            </a>
-            de votre cantine.
-          </p>
-        </v-col>
-        <v-col cols="12" md="4">
-          <label class="body-2" for="meal-count">Couverts par jour</label>
-          <v-text-field
-            id="meal-count"
-            class="mt-2"
-            hide-details="auto"
-            validate-on-blur
-            solo
-            v-model="satellite.dailyMealCount"
-            prepend-icon="mdi-silverware-fork-knife"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row class="mt-n4">
-        <v-col cols="12" md="5">
-          <label class="body-2" for="satellite-name">Nom</label>
-          <v-text-field
-            id="satellite-name"
-            class="mt-2"
-            hide-details="auto"
-            validate-on-blur
-            solo
-            v-model="satellite.name"
-            :rules="[validators.required]"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" md="5">
-          <label class="body-2" for="sectors">Secteurs d'activité</label>
-          <v-select
-            id="sectors"
-            class="mt-2"
-            multiple
-            :items="sectors"
-            solo
-            v-model="satellite.sectors"
-            item-text="name"
-            item-value="id"
-            hide-details
-          ></v-select>
-        </v-col>
-        <v-spacer></v-spacer>
-        <v-col class="align-self-end">
-          <v-btn @click="saveSatellite" color="primary darken-1" class="ml-4 mt-1" x-large>
-            Valider
-          </v-btn>
-        </v-col>
-        <!-- TODO: location -->
-      </v-row>
-    </v-form>
+    <v-card outlined>
+      <v-card-text>
+        <v-form ref="form" v-model="formIsValid">
+          <v-row>
+            <v-col cols="12" md="8">
+              <label class="body-2" for="satellite-siret">SIRET</label>
+              <v-text-field
+                id="satellite-siret"
+                class="mt-2"
+                hide-details="auto"
+                validate-on-blur
+                solo
+                v-model="satellite.siret"
+                :rules="[validators.length(14), validators.luhn]"
+              ></v-text-field>
+              <p class="caption mt-1 ml-2">
+                Utilisez cet
+                <a href="https://annuaire-entreprises.data.gouv.fr/" target="_blank" rel="noopener">
+                  outil de recherche pour trouver le SIRET
+                  <v-icon color="primary" small>mdi-open-in-new</v-icon>
+                </a>
+                de votre cantine.
+              </p>
+            </v-col>
+            <v-col cols="12" md="4">
+              <label class="body-2" for="meal-count">Couverts par jour</label>
+              <v-text-field
+                id="meal-count"
+                class="mt-2"
+                hide-details="auto"
+                validate-on-blur
+                solo
+                v-model="satellite.dailyMealCount"
+                prepend-icon="mdi-silverware-fork-knife"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mt-n4">
+            <v-col cols="12" md="5">
+              <label class="body-2" for="satellite-name">Nom de la cantine</label>
+              <v-text-field
+                id="satellite-name"
+                class="mt-2"
+                hide-details="auto"
+                validate-on-blur
+                solo
+                v-model="satellite.name"
+                :rules="[validators.required]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="5">
+              <label class="body-2" for="sectors">Secteurs d'activité</label>
+              <v-select
+                id="sectors"
+                class="mt-2"
+                multiple
+                :items="sectors"
+                solo
+                v-model="satellite.sectors"
+                item-text="name"
+                item-value="id"
+                hide-details
+              ></v-select>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col class="align-self-end">
+              <v-btn @click="saveSatellite" color="primary darken-1" class="mt-1" x-large>
+                Ajouter
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -121,6 +123,16 @@ export default {
       formIsValid: true,
       satelliteCanteensCount: this.originalCanteen.satelliteCanteensCount,
       existingSatellites: this.originalCanteen.satellites || [], // TODO: fetch satellites on this view
+      options: {
+        sortBy: [],
+        sortDesc: [],
+        page: 1,
+      },
+      headers: [
+        { text: "Nom", value: "name" },
+        { text: "SIRET", value: "siret" },
+        { text: "", value: "edit" },
+      ],
     }
   },
   computed: {
@@ -160,9 +172,24 @@ export default {
           this.$store.dispatch("notifyServerError", e)
         })
     },
+    onRowClick(satellite) {
+      this.$router.push({
+        name: "CanteenModification",
+        params: { canteenUrlComponent: this.$store.getters.getCanteenUrlComponent(satellite) },
+      })
+    },
   },
   created() {
     document.title = `Satellites - ${this.originalCanteen.name} - ${this.$store.state.pageTitleSuffix}`
   },
 }
 </script>
+
+<style scoped>
+.v-data-table {
+  border: solid 1px #ddd;
+}
+.v-data-table >>> tr {
+  cursor: pointer;
+}
+</style>
