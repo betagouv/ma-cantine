c10e7c3496292c2f477d74a1903248f76b01418c
Alejandro MG <alejandro@amguillen.dev>
2022-07-11 16:35:27 +0200

    Ajout de la section outils et ressources

--- frontend/src/views/LandingPage/ActionsBlock.vue
+++ frontend/src/views/LandingPage/ActionsBlock.vue
@@ -1,126 +1,110 @@
 <template>
-  <div>
-    <AuthDialog v-model="showAuthDialog" />
-    <v-row class="text-left">
-      <v-spacer></v-spacer>
-      <v-col cols="12" md="4">
-        <v-hover>
-          <template v-slot:default="{ hover }">
-            <v-card
-              :elevation="hover ? 6 : 2"
-              :to="loggedUser ? { name: 'ManagementPage' } : undefined"
-              @click="loggedUser ? undefined : openAuthDialog()"
-              class="fill-height pa-4 d-flex flex-column hover-transition"
-            >
-              <v-img
-                src="/static/images/doodles/secondary/SprintingDoodle.png"
-                v-if="$vuetify.breakpoint.smAndUp"
-                class="mx-auto rounded-0"
-                contain
-                max-height="100"
-              ></v-img>
-              <v-card-title>
-                <h2 class="text-h6 font-weight-bold">
-                  Gérer ma cantine pour atteindre mes objectifs
-                </h2>
-              </v-card-title>
-              <v-card-text>
-                Être outillé pour atteindre les objectifs des lois EGAlim et Climat
-              </v-card-text>
+  <v-row class="text-left">
+    <v-spacer></v-spacer>
+    <v-col cols="12" md="4">
+      <v-hover>
+        <template v-slot:default="{ hover }">
+          <v-card
+            :elevation="hover ? 6 : 2"
+            :to="loggedUser ? { name: 'ManagementPage' } : undefined"
+            :href="loggedUser ? undefined : '/s-identifier'"
+            class="fill-height pa-4 d-flex flex-column hover-transition"
+          >
+            <v-img
+              src="/static/images/doodles/secondary/SprintingDoodle.png"
+              v-if="$vuetify.breakpoint.smAndUp"
+              class="mx-auto rounded-0"
+              contain
+              max-height="100"
+            ></v-img>
+            <v-card-title>
+              <h2 class="text-h6 font-weight-bold">
+                Gérer ma cantine pour atteindre mes objectifs
+              </h2>
+            </v-card-title>
+            <v-card-text>
+              Être outillé pour atteindre les objectifs des lois EGAlim et Climat
+            </v-card-text>
+            <v-spacer></v-spacer>
+            <v-card-actions class="px-4 pt-0">
               <v-spacer></v-spacer>
-              <v-card-actions class="px-4 pt-0">
-                <v-spacer></v-spacer>
-                <v-icon color="primary">mdi-arrow-right</v-icon>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-hover>
-      </v-col>
-      <v-col cols="12" md="4" :style="{ 'border-right': $vuetify.breakpoint.mdAndUp ? 'dotted 4px #e5fbf0' : 'none' }">
-        <v-hover>
-          <template v-slot:default="{ hover }">
-            <v-card
-              :elevation="hover ? 6 : 2"
-              :to="{ name: 'PublicCanteenStatisticsPage' }"
-              class="fill-height pa-4 d-flex flex-column hover-transition"
-            >
-              <v-img
-                src="/static/images/doodles/secondary/SitReadingDoodle.png"
-                v-if="$vuetify.breakpoint.smAndUp"
-                class="mx-auto rounded-0"
-                contain
-                max-height="100"
-              ></v-img>
-              <v-card-title>
-                <h2 class="text-h6 font-weight-bold">
-                  Suivre l'évolution de mon territoire
-                </h2>
-              </v-card-title>
-              <v-card-text>
-                Savoir où en sont les établissements de ma région ou mon département
-              </v-card-text>
+              <v-icon color="primary">mdi-arrow-right</v-icon>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-hover>
+    </v-col>
+    <v-col cols="12" md="4" :style="{ 'border-right': $vuetify.breakpoint.mdAndUp ? 'dotted 4px #e5fbf0' : 'none' }">
+      <v-hover>
+        <template v-slot:default="{ hover }">
+          <v-card
+            :elevation="hover ? 6 : 2"
+            :to="{ name: 'PublicCanteenStatisticsPage' }"
+            class="fill-height pa-4 d-flex flex-column hover-transition"
+          >
+            <v-img
+              src="/static/images/doodles/secondary/SitReadingDoodle.png"
+              v-if="$vuetify.breakpoint.smAndUp"
+              class="mx-auto rounded-0"
+              contain
+              max-height="100"
+            ></v-img>
+            <v-card-title>
+              <h2 class="text-h6 font-weight-bold">
+                Suivre l'évolution de mon territoire
+              </h2>
+            </v-card-title>
+            <v-card-text>
+              Savoir où en sont les établissements de ma région ou mon département
+            </v-card-text>
+            <v-spacer></v-spacer>
+            <v-card-actions class="px-4 pt-0">
               <v-spacer></v-spacer>
-              <v-card-actions class="px-4 pt-0">
-                <v-spacer></v-spacer>
-                <v-icon color="primary">mdi-arrow-right</v-icon>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-hover>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-hover>
-          <template v-slot:default="{ hover }">
-            <v-card
-              :elevation="hover ? 6 : 2"
-              :to="{ name: 'CanteensHome' }"
-              class="fill-height pa-4 d-flex flex-column hover-transition"
-            >
-              <v-img
-                src="/static/images/doodles/secondary/DogDoodle.png"
-                v-if="$vuetify.breakpoint.smAndUp"
-                class="mx-auto rounded-0"
-                contain
-                max-height="100"
-              ></v-img>
-              <v-card-title class="text-h6 font-weight-bold">
-                <h2 class="text-h6 font-weight-bold">En savoir plus sur la cantine que je fréquente</h2>
-              </v-card-title>
-              <v-card-text>
-                Connaître les initiatives de mon restaurant ou celui de mes enfants
-              </v-card-text>
+              <v-icon color="primary">mdi-arrow-right</v-icon>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-hover>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-hover>
+        <template v-slot:default="{ hover }">
+          <v-card
+            :elevation="hover ? 6 : 2"
+            :to="{ name: 'CanteensHome' }"
+            class="fill-height pa-4 d-flex flex-column hover-transition"
+          >
+            <v-img
+              src="/static/images/doodles/secondary/DogDoodle.png"
+              v-if="$vuetify.breakpoint.smAndUp"
+              class="mx-auto rounded-0"
+              contain
+              max-height="100"
+            ></v-img>
+            <v-card-title class="text-h6 font-weight-bold">
+              <h2 class="text-h6 font-weight-bold">En savoir plus sur la cantine que je fréquente</h2>
+            </v-card-title>
+            <v-card-text>
+              Connaître les initiatives de mon restaurant ou celui de mes enfants
+            </v-card-text>
+            <v-spacer></v-spacer>
+            <v-card-actions class="px-4 pt-0">
               <v-spacer></v-spacer>
-              <v-card-actions class="px-4 pt-0">
-                <v-spacer></v-spacer>
-                <v-icon color="primary">mdi-arrow-right</v-icon>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-hover>
-      </v-col>
-      <v-spacer></v-spacer>
-    </v-row>
-  </div>
+              <v-icon color="primary">mdi-arrow-right</v-icon>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-hover>
+    </v-col>
+    <v-spacer></v-spacer>
+  </v-row>
 </template>
 
 <script>
-import AuthDialog from "@/views/LandingPage/AuthDialog"
-
 export default {
-  components: { AuthDialog },
-  data() {
-    return {
-      showAuthDialog: false,
-    }
-  },
   computed: {
     loggedUser() {
       return this.$store.state.loggedUser
-    },
-  },
-  methods: {
-    openAuthDialog() {
-      this.showAuthDialog = true
     },
   },
 }
