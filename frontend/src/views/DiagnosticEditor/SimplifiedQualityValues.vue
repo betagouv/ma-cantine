2b08fe5de13aa72a8092da47b94732ec1790a40d
Alejandro MG <alejandro@amguillen.dev>
2022-07-18 09:01:15 +0200

    Front-end form for simplified diagnostic

--- frontend/src/views/DiagnosticEditor/SimplifiedQualityValues.vue
+++ frontend/src/views/DiagnosticEditor/SimplifiedQualityValues.vue
@@ -1,113 +1,340 @@
 <template>
   <div>
-    <QualityMeasureValuesInput
-      :originalDiagnostic="diagnostic"
-      label="La valeur (en HT) de mes achats alimentaires..."
-      :readonly="hasActiveTeledeclaration"
-      :purchasesSummary="purchasesSummary"
+    <label :for="'total-' + diagnostic.year" class="body-2">
+      La valeur (en HT) de mes achats alimentaires total
+    </label>
+
+    <v-text-field
+      :id="'total-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueTotalHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="totalError"
+      :messages="totalError ? [totalErrorMessage] : undefined"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+    <PurchaseHint
+      v-if="displayPurchaseHints"
+      v-model="diagnostic.valueTotalHt"
+      @autofill="checkTotal"
+      purchaseType="totaux"
+      :amount="purchasesSummary.total"
     />
-    <fieldset class="d-flex flex-column mt-4">
-      <legend class="body-2 mb-2">
-        Les valeurs par label des produits de qualité et durables hors bio (facultatif)
-      </legend>
-      <v-row>
-        <v-col cols="12" md="9" class="pr-3">
-          <label class="caption mb-1 mt-2" for="label-rouge">Label Rouge</label>
-          <v-container class="d-flex pa-0 align-center">
-            <div style="min-width: 100px; width: 100px;">
-              <img src="/static/images/quality-labels/label-rouge.png" alt="" style="height: 2em;" />
-            </div>
-            <div class="flex-grow-1">
-              <v-text-field
-                id="label-rouge"
-                hide-details="auto"
-                type="number"
-                suffix="€ HT"
-                :rules="[validators.nonNegativeOrEmpty]"
-                validate-on-blur
-                solo
-                dense
-                v-model.number="diagnostic.valueLabelRouge"
-                :readonly="hasActiveTeledeclaration"
-                :disabled="hasActiveTeledeclaration"
-              ></v-text-field>
-              <PurchaseHint
-                v-if="displayPurchaseHints"
-                v-model="diagnostic.valueLabelRouge"
-                purchaseType="Label Rouge"
-                :amount="purchasesSummary.rouge"
-              />
-            </div>
-          </v-container>
-        </v-col>
-        <v-col cols="12" md="9" class="pr-3">
-          <label class="caption mb-1 mt-2" for="aoc-aop-igp">AOC / AOP / IGP</label>
-          <v-container class="d-flex pa-0 align-center">
-            <div style="min-width: 100px;">
-              <img src="/static/images/quality-labels/Logo-AOC-AOP.png" alt="" style="height: 2em;" />
-              <img src="/static/images/quality-labels/IGP.png" alt="" style="height: 2em;" class="mr-1" />
-            </div>
-            <div class="flex-grow-1">
-              <v-text-field
-                id="aoc-aop-igp"
-                hide-details="auto"
-                type="number"
-                suffix="€ HT"
-                :rules="[validators.nonNegativeOrEmpty]"
-                validate-on-blur
-                solo
-                dense
-                v-model.number="diagnostic.valueLabelAocIgp"
-                :readonly="hasActiveTeledeclaration"
-                :disabled="hasActiveTeledeclaration"
-              ></v-text-field>
-              <PurchaseHint
-                v-if="displayPurchaseHints"
-                v-model="diagnostic.valueLabelAocIgp"
-                purchaseType="AOC / AOP / IGP"
-                :amount="purchasesSummary.aocAopIgp"
-              />
-            </div>
-          </v-container>
-        </v-col>
-        <v-col cols="12" md="9" class="pr-3">
-          <label class="caption mb-1 mt-2" for="hve">Haute Valeur Environnementale</label>
-          <v-container class="d-flex pa-0 align-center">
-            <div style="min-width: 100px;">
-              <img src="/static/images/quality-labels/hve.png" alt="" style="height: 2em;" />
-            </div>
-            <div class="flex-grow-1">
-              <v-text-field
-                id="hve"
-                hide-details="auto"
-                type="number"
-                suffix="€ HT"
-                :rules="[validators.nonNegativeOrEmpty]"
-                validate-on-blur
-                solo
-                dense
-                v-model.number="diagnostic.valueLabelHve"
-                :readonly="hasActiveTeledeclaration"
-                :disabled="hasActiveTeledeclaration"
-              ></v-text-field>
-              <PurchaseHint
-                v-if="displayPurchaseHints"
-                v-model="diagnostic.valueLabelHve"
-                purchaseType="HVE"
-                :amount="purchasesSummary.hve"
-              />
-            </div>
-          </v-container>
-        </v-col>
-      </v-row>
-    </fieldset>
+
+    <!-- Bio -->
+    <v-divider class="my-4"></v-divider>
+    <div class="d-block d-sm-flex align-center">
+      <LogoBio style="max-height: 30px;" />
+      <label class="body-2 ml-4" :for="'bio-' + diagnostic.year">
+        La valeur (en HT) de mes achats Bio ou en conversion Bio
+      </label>
+    </div>
+    <v-text-field
+      :id="'bio-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueBioHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="totalError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+    <PurchaseHint
+      v-if="displayPurchaseHints"
+      v-model="diagnostic.valueBioHt"
+      @autofill="checkTotal"
+      purchaseType="bio"
+      :amount="purchasesSummary.bio"
+    />
+
+    <!-- SIQO -->
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <div v-for="label in siqoLabels" :key="label.title">
+          <img
+            :src="`/static/images/quality-labels/${label.src}`"
+            :alt="label.title"
+            :title="label.title"
+            style="max-height: 30px;"
+          />
+        </div>
+      </div>
+      <label class="body-2 ml-4" :for="'siqo-' + diagnostic.year">
+        La valeur (en HT) de mes achats SIQO (AOP/AOC, IGP, STG)
+      </label>
+    </div>
+    <v-text-field
+      :id="'siqo-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueSustainableHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="totalError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Performance Externalités -->
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <v-icon size="30" color="purple">
+          mdi-flower-tulip-outline
+        </v-icon>
+        <v-icon size="30" class="ml-2" color="green">
+          mdi-chart-line
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'ext-perf-' + diagnostic.year">
+        La valeur (en HT) de mes achats prenant en compte les coûts imputés aux externalités environnementales ou acquis
+        sur la base de leurs performances en matière environnementale
+      </label>
+    </div>
+    <v-text-field
+      :id="'ext-perf-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueExternalityPerformanceHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="totalError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Other EGAlim -->
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <div v-for="label in otherLabels" :key="label.title">
+          <img
+            :src="`/static/images/quality-labels/${label.src}`"
+            :alt="label.title"
+            :title="label.title"
+            style="max-height: 30px;"
+          />
+        </div>
+        <v-icon size="30" color="brown">
+          mdi-cow
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'other-' + diagnostic.year">
+        La valeur (en HT) des autres achats EGAlim
+      </label>
+    </div>
+    <v-text-field
+      :id="'other-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueEgalimOthersHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="totalError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Viande et volailles -->
+    <v-divider class="my-4"></v-divider>
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <v-icon size="30" color="brown">
+          mdi-food-steak
+        </v-icon>
+        <v-icon size="30" color="brown">
+          mdi-food-drumstick
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'meat-poultry-' + diagnostic.year">
+        La valeur (en HT) des mes achats en viandes et volailles fraiches ou surgelées total
+      </label>
+    </div>
+    <v-text-field
+      :id="'meat-poultry-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueMeatPoultryHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="meatPoultryError"
+      :messages="meatPoultryError ? [meatPoultryErrorMessage] : undefined"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Viande et volailles EGALIM -->
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <v-icon size="30" color="green">
+          mdi-check-circle
+        </v-icon>
+        <v-icon size="30" color="brown">
+          mdi-food-steak
+        </v-icon>
+        <v-icon size="30" color="brown">
+          mdi-food-drumstick
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'meat-poultry-egalim-' + diagnostic.year">
+        La valeur (en HT) des mes achats EGAlim en viandes et volailles fraiches ou surgelées
+      </label>
+    </div>
+    <v-text-field
+      :id="'meat-poultry-egalim-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueMeatPoultryEgalimHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="meatPoultryError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Viande et volailles provenance FRANCE -->
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <v-icon size="30" color="indigo">
+          mdi-hexagon-outline
+        </v-icon>
+        <v-icon size="30" color="brown">
+          mdi-food-steak
+        </v-icon>
+        <v-icon size="30" color="brown">
+          mdi-food-drumstick
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'meat-poultry-france-' + diagnostic.year">
+        La valeur (en HT) des mes achats provenance France en viandes et volailles fraiches ou surgelées
+      </label>
+    </div>
+    <v-text-field
+      :id="'meat-poultry-france-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueMeatPoultryFranceHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="meatPoultryError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Poissons -->
+    <v-divider class="my-4"></v-divider>
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <v-icon size="30" color="blue">
+          mdi-fish
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'fish-' + diagnostic.year">
+        La valeur (en HT) des mes achats en poissons et produits aquatiques total
+      </label>
+    </div>
+    <v-text-field
+      :id="'fish-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueFishHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="fishError"
+      :messages="fishError ? [fishErrorMessage] : undefined"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
+
+    <!-- Poissons EGALIM -->
+    <div class="d-block d-sm-flex align-center mt-8">
+      <div class="d-flex">
+        <v-icon size="30" color="green">
+          mdi-check-circle
+        </v-icon>
+        <v-icon size="30" color="blue">
+          mdi-fish
+        </v-icon>
+      </div>
+      <label class="body-2 ml-4" :for="'fish-egalim-' + diagnostic.year">
+        La valeur (en HT) des mes achats EGAlim en poissons et produits aquatiques
+      </label>
+    </div>
+    <v-text-field
+      :id="'fish-egalim-' + diagnostic.year"
+      hide-details="auto"
+      type="number"
+      :rules="[validators.nonNegativeOrEmpty]"
+      validate-on-blur
+      solo
+      placeholder="Je ne sais pas"
+      suffix="€ HT"
+      v-model.number="diagnostic.valueFishEgalimHt"
+      :readonly="readonly"
+      :disabled="readonly"
+      :error="fishError"
+      @blur="checkTotal"
+      class="mt-2"
+    ></v-text-field>
   </div>
 </template>
 
 <script>
-import QualityMeasureValuesInput from "@/components/KeyMeasureDiagnostic/QualityMeasureValuesInput"
 import PurchaseHint from "@/components/KeyMeasureDiagnostic/PurchaseHint"
+import labels from "@/data/quality-labels.json"
 import validators from "@/validators"
+import LogoBio from "@/components/LogoBio"
+
+const DEFAULT_TOTAL_ERROR = "Le totale doit être plus que la somme des valeurs par label"
+const DEFAULT_MEAT_POULTRY_ERROR = "La valeur totale doit être supérieure que celle des labels"
+const DEFAULT_FISH_ERROR = "La valeur totale doit être supérieure que la valeur EGAlim"
 
 export default {
   name: "SimplifiedQualityValues",
@@ -120,8 +347,32 @@ export default {
     },
   },
   components: {
-    QualityMeasureValuesInput,
     PurchaseHint,
+    LogoBio,
+  },
+  data() {
+    const siqoLogos = [
+      "Logo Label Rouge",
+      "Logo Appellation d'origine (AOC/AOP)",
+      "Logo indication géographique",
+      "Logo Spécialité traditionnelle garantie",
+    ]
+    const otherLogos = [
+      "Logo Haute Valeur Environnementale",
+      "Écolabel pêche durable",
+      "Logo Région Ultrapériphérique",
+      "Logo Commerce Équitable",
+    ]
+    return {
+      totalError: false,
+      totalErrorMessage: DEFAULT_TOTAL_ERROR,
+      meatPoultryError: false,
+      meatPoultryErrorMessage: DEFAULT_MEAT_POULTRY_ERROR,
+      fishError: false,
+      fishErrorMessage: DEFAULT_FISH_ERROR,
+      siqoLabels: labels.filter((x) => siqoLogos.includes(x.title)),
+      otherLabels: labels.filter((x) => otherLogos.includes(x.title)),
+    }
   },
   computed: {
     validators() {
@@ -135,6 +386,38 @@ export default {
     },
     hasActiveTeledeclaration() {
       return this.diagnostic.teledeclaration && this.diagnostic.teledeclaration.status === "SUBMITTED"
+    },
+  },
+  methods: {
+    checkTotal() {
+      const d = this.diagnostic
+      const sumEgalim = this.sumAllEgalim()
+      // Note that we do Math.max because some meat-poultry may be overlapped : both bio and provenance France
+      const sumMeatPoultry = Math.max(d.valueMeatPoultryEgalimHt, d.valueMeatPoultryFranceHt)
+      const sumFish = d.valueFishEgalimHt
+      const total = d.valueTotalHt
+      const totalMeatPoultry = d.valueMeatPoultryHt
+      const totalFish = d.valueFishHt
+
+      this.totalError = sumEgalim > total
+      this.totalErrorMessage = this.totalError ? `${DEFAULT_TOTAL_ERROR}, actuellement ${sumEgalim || 0} €` : ""
+
+      this.meatPoultryError = sumMeatPoultry > totalMeatPoultry
+      this.meatPoultryErrorMessage = this.meatPoultryError
+        ? `${DEFAULT_MEAT_POULTRY_ERROR}, actuellement ${sumMeatPoultry || 0} €`
+        : ""
+
+      this.fishError = sumFish > totalFish
+      this.fishErrorMessage = this.fishError ? `${DEFAULT_FISH_ERROR}, actuellement ${sumFish || 0} €` : ""
+    },
+    sumAllEgalim() {
+      const d = this.diagnostic
+      const egalimValues = [d.valueBioHt, d.valueSustainableHt, d.valueExternalityPerformanceHt, d.valueEgalimOthersHt]
+      let total = 0
+      egalimValues.forEach((val) => {
+        total += parseFloat(val) || 0
+      })
+      return total
     },
   },
 }
