723c457e1830c361e6a32ef11256655470aacc15
Alejandro MG <alejandro@amguillen.dev>
2022-08-01 16:39:20 +0200

    Shaped card proposal

---
+++ frontend/src/views/ManagementPage/TeledeclarationBanner.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card color="deep-purple lighten-4" elevation="1" shaped class="d-flex" max-width="750">
+    <div class="ma-4 ml-8 align-center">
+      <v-img max-width="100" contain src="/static/images/sitting-person-color.png"></v-img>
+    </div>
+    <div>
+      <v-card-title class="text-body-1 font-weight-bold">Participez à la campagne de télédéclaration 2022</v-card-title>
+      <v-card-text>
+        Du 18 juillet au 15 octobre, remplissez vos données d'appro pour l'année civile 2021 et compléter ainsi la
+        collecte de données nationale.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text>
+          <v-icon class="mr-2">mdi-arrow-right</v-icon>
+          En savoir plus
+        </v-btn>
+      </v-card-actions>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TeledeclarationBanner",
+}
+</script>
+
+<style scoped>
+.v-sheet--shaped {
+  border-radius: 26px 4px !important;
+}
+</style>
--- frontend/src/views/ManagementPage/index.vue
+++ frontend/src/views/ManagementPage/index.vue
@@ -2,13 +2,14 @@
   <div class="text-left">
     <div class="mt-4">
       <p class="my-2 text-body-1 font-weight-bold">
-        Bienvenue {{ loggedUser.firstName }}
+        Bienvenue dans votre espace, {{ loggedUser.firstName }}
         <v-btn text class="text-decoration-underline text-caption mb-1" :to="{ name: 'AccountEditor' }">
           <v-icon class="mr-1" small>mdi-pencil</v-icon>
           Modifier mon profil
         </v-btn>
       </p>
     </div>
+    <TeledeclarationBanner />
     <CompleteProfileField />
     <div class="mt-4">
       <h1 class="my-4 text-h5 font-weight-black">Mes cantines</h1>
@@ -27,10 +28,11 @@ import CanteensPagination from "./CanteensPagination.vue"
 import PageSatisfaction from "@/components/PageSatisfaction.vue"
 import UserTools from "./UserTools"
 import CompleteProfileField from "./CompleteProfileField"
+import TeledeclarationBanner from "./TeledeclarationBanner"
 import validators from "@/validators"
 
 export default {
-  components: { CanteensPagination, UserTools, CompleteProfileField, PageSatisfaction },
+  components: { CanteensPagination, UserTools, CompleteProfileField, PageSatisfaction, TeledeclarationBanner },
   data() {
     return {
       validators,
