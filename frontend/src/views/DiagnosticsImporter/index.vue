9261f78fb88f471ea8962c2a913e4f4dd8cbb391
Alejandro MG <alejandro@amguillen.dev>
2022-08-04 17:52:15 +0200

    Card sizing more appropriate for intermediate screens

--- frontend/src/views/DiagnosticsImporter/index.vue
+++ frontend/src/views/DiagnosticsImporter/index.vue
@@ -12,7 +12,7 @@
     </p>
     <v-row class="cta-group my-10 pa-2">
       <v-col cols="12"><h2>Choisir le niveau d'information disponible</h2></v-col>
-      <v-col cols="12" md="4" v-for="type in diagnosticTypes" :key="type.key">
+      <v-col cols="12" sm="4" v-for="type in diagnosticTypes" :key="type.key">
         <v-hover>
           <template v-slot:default="{ hover }">
             <v-card
@@ -20,9 +20,11 @@
               :to="{ name: 'DiagnosticImportPage', params: { importLevel: type.key } }"
               class="fill-height pa-4 d-flex flex-column hover-transition"
             >
-              <v-icon x-large class="pt-4 pb-1" color="primary">{{ type.icon }}</v-icon>
+              <v-icon :x-large="$vuetify.breakpoint.mdAndUp" class="pt-4 pb-1" color="primary">
+                {{ type.icon }}
+              </v-icon>
               <v-card-title>
-                <h2 class="text-h6 font-weight-bold">
+                <h2 class="text-body-2 text-md-h6 font-weight-bold">
                   {{ type.label }}
                 </h2>
               </v-card-title>
