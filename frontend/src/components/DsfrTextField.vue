328fe20cc5762f47de6e35efde0cc5c03abac799
Alejandro MG <alejandro@amguillen.dev>
2022-08-11 14:59:16 +0200

    Active/focus outline in dsfr text field

--- frontend/src/components/DsfrTextField.vue
+++ frontend/src/components/DsfrTextField.vue
@@ -43,6 +43,7 @@ export default {
   border-radius: 0.25rem 0.25rem 0 0;
   background-color: #eee !important;
   box-shadow: inset 0 -2px 0 0 #3a3a3a;
+  transition: none;
 }
 
 .v-text-field.error--text >>> .v-input__slot {
@@ -54,5 +55,13 @@ export default {
 .v-text-field.v-text-field--solo.v-input--dense >>> .v-input__prepend-outer,
 .v-text-field.v-text-field--solo.v-input--dense >>> .v-input__append-outer {
   margin-top: 15px !important;
+}
+
+.v-text-field >>> .v-input__slot:focus-within {
+  outline-style: solid;
+  outline-width: 2px;
+  outline-color: #0a76f6;
+  outline-offset: 2px;
+  border-radius: 2px 2px 0 0;
 }
 </style>
