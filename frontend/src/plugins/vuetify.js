7617566c01dc5897379de8c89203539476b7c01b
Alejandro MG <alejandro@amguillen.dev>
2022-08-16 17:13:35 +0200

    Adds remix icons as svgs onto Vuetify

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Vuetify from "vuetify/lib/framework"
 import theme from "@/theme"
 import { VBtn, VCard } from "vuetify/lib"
+import remixJson from "./remix.json"
 
 Vue.use(Vuetify)
 
@@ -15,5 +16,18 @@ export default new Vuetify({
   iconfont: "md",
   breakpoint: {
     mobileBreakpoint: "sm",
+  },
+  /*
+  remixJson contains a key-value pair of icon names and svg paths. These correspond to the icons
+  on https://www.systeme-de-design.gouv.fr/elements-d-interface/fondamentaux-techniques/icones.
+  To use them, instead of using the "mdi-" prefix, just append a dollar sign. For example, to use
+  icon "cloudy-2-fill" you can use "<v-icon>$cloudy-2-fill</v-icon>".
+
+  Note that not all Remix icons are included, only those chosen by the DSFR. MDI icons are still
+  available.
+  */
+  icons: {
+    iconfont: "mdi",
+    values: remixJson,
   },
 })
