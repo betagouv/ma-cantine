066792caf4e174e8329b5084a5a8f221881bbfcf
Alejandro MG <alejandro@amguillen.dev>
2022-08-29 14:59:46 +0200

    End of process log entry

--- macantine/tasks.py
+++ macantine/tasks.py
@@ -219,3 +219,5 @@ def fill_missing_geolocation_data():
             logger.exception(f"Geolocation Bot error: Timeout\n{e}")
         except Exception as e:
             logger.exception(f"Geolocation Bot error: Unexpected exception\n{e}")
+
+    logger.info(f"Geolocation Bot: Ended process for {candidate_canteens.count()} canteens")
