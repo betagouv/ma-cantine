3eee2660d22bb231c3332a49afedb452bee810c6
Alejandro MG <alejandro@amguillen.dev>
2022-09-05 09:39:38 +0200

    Fixes error on geolocation bot attempts

--- macantine/tasks.py
+++ macantine/tasks.py
@@ -172,10 +172,10 @@ def _get_candidate_canteens():
     candidate_canteens = (
         Canteen.objects.filter(Q(city=None) | Q(department=None))
         .filter(Q(postal_code__isnull=False) | Q(city_insee_code__isnull=False))
-        .filter(geolocation_bot_attempts__lt=5)
+        .filter(geolocation_bot_attempts__lt=10)
         .annotate(postal_code_len=Length("postal_code"))
         .annotate(city_insee_code_len=Length("city_insee_code"))
-        .filter(Q(postal_code_len=5) | Q(city_insee_code_len=10))
+        .filter(Q(postal_code_len=5) | Q(city_insee_code_len=5))
     )
     return candidate_canteens
 
