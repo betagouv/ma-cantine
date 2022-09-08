097f81ff1cde287b69a7dc5860ce4ce2e24f3e19
Alejandro MG <alejandro@amguillen.dev>
2022-09-08 11:17:58 +0200

    Partner serializer

---
+++ api/serializers/partner.py
@@ -0,0 +1,26 @@
+from drf_base64.fields import Base64ImageField
+from rest_framework import serializers
+from data.models import Partner
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+
+    types = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
+    image = Base64ImageField(required=False, allow_null=True)
+
+    class Meta:
+        model = Partner
+        fields = (
+            "id",
+            "creation_date",
+            "modification_date",
+            "name",
+            "short_description",
+            "long_description",
+            "image",
+            "types",
+            "departments",
+            "national",
+            "free",
+            "economic_model",
+        )
