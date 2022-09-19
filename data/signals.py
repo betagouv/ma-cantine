0c48b0f8d325d7247191d5db934b3d717141d153
Helen Root <helenroot1@gmail.com>
2022-09-19 16:43:04 +0200

    When checking if invitations exist for new user, use case insensitive email matching

--- data/signals.py
+++ data/signals.py
@@ -6,7 +6,7 @@ from .models import User, ManagerInvitation
 @receiver(post_save, sender=User)
 def update_canteen_managers(sender, instance, created, **kwargs):
     if created:
-        canteen_links = ManagerInvitation.objects.filter(email=instance.email)
+        canteen_links = ManagerInvitation.objects.filter(email__iexact=instance.email)
         for link in canteen_links:
             link.canteen.managers.add(instance)
             link.delete()
