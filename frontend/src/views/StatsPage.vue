5ac64851880b3802545ed037cfc460631e194d05
Jenn S <jennifer.stephan@edu.escpeurope.eu>
2022-10-13 20:10:31 +0200

    Update StatsPage.vue

--- frontend/src/views/StatsPage.vue
+++ frontend/src/views/StatsPage.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="text-left">
     <BreadcrumbsNav />
-    <h1 class="text-h4 font-weight-black black--text mb-6">Mesures d'impact, résultats 
-    et statistiques d'usage de ma-cantine</h1>
+    <h1 class="text-h4 font-weight-black black--text mb-6">Mesures d'impact</h1>
     <div>
       <p v-for="(section, sectionIdx) in sections" :key="`a-${sectionIdx}`">
         <a :href="`#${normalise(section.title)}`">{{ section.title }}</a>
