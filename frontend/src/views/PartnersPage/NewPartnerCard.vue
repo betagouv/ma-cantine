46e2675cd761184c5c3cc075459b6c6bb4bee3e9
Alejandro M Guillén <alejandro@amguillen.dev>
2022-10-24 14:07:42 +0200

    Update frontend/src/views/PartnersPage/NewPartnerCard.vue
    Co-authored-by: hfroot <9282816+hfroot@users.noreply.github.com>

--- frontend/src/views/PartnersPage/NewPartnerCard.vue
+++ frontend/src/views/PartnersPage/NewPartnerCard.vue
@@ -10,7 +10,7 @@
       Vous êtes un acteur d'accompagnement dans la restauration collective ? Contribuez à notre initiative de
       référencement !
     </v-card-text>
-    <v-btn class="mt-2" color="primary" href="https://startupdetat.typeform.com/to/JhhsMCYC" target="_blank">
+    <v-btn class="mt-2" color="primary" href="https://startupdetat.typeform.com/to/JhhsMCYC" target="_blank" rel="noopener">
       Me recenser
       <v-icon small class="ml-2">mdi-open-in-new</v-icon>
     </v-btn>
