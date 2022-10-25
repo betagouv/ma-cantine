d8de48a8325aedaf771b64c7c093880783aa99d3
Alejandro MG <alejandro@amguillen.dev>
2022-10-25 13:41:24 +0200

    Linter + changement de message concernant l'API

--- frontend/src/views/FaqPage/FaqPanels.vue
+++ frontend/src/views/FaqPage/FaqPanels.vue
@@ -111,32 +111,29 @@ export default {
             "Je suis gestionnaire d'un collège ou d'un lycée, mon établissement a été créé, suis-je concerné par l'inscription dans ma cantine ?",
           answer:
             "Les Départements et les Régions ont respectivement la compétence de la restauration collective pour les collèges et les lycées. Ils sont à ce titre sont responsables du référencement de leurs établissements sur « ma cantine ».  Une convention passée entre l'établissement et la collectivité territoriale de rattachement précise les modalités d'exercice de leurs compétences respectives. Elle comprend un volet relatif à la restauration scolaire, qui vise en particulier à répondre aux objectifs fixés par la loi EGAlim (article L. 421-23 du code de l'éducation). La cantine peut être créée par l'une et/ou l'autre des parties et gérée par les deux.",
-        },        
+        },
         {
-          title:
-            "Les produits Bleu-Blanc-Coeur (BBC) entrent-ils dans les 50 % de produits durables et de qualité ?",
+          title: "Les produits Bleu-Blanc-Coeur (BBC) entrent-ils dans les 50 % de produits durables et de qualité ?",
           answer:
             "A ce jour, pour les produits issus de la démarche Bleu-Blanc-Coeur ne bénéficiant pas d’un signe ou mention « officiel » listé par la loi EGAlim, il est possible de les comptabiliser, comme pour tout autre produit, dans les objectifs de la loi EGAlim à condition qu’ils soient sélectionnés à l’issue d’un processus de sélection ciblant « les produits acquis selon les modalités prenant en compte les coûts imputés aux externalités environnementales tout au long de son cycle de vie » ou « les produits dont l’acquisition a été fondée principalement sur la base de leurs performances en matière de protection de l’environnement et de développement des approvisionnements directs de produits de l’agriculture ». Le recours à ces modalités de sélection relève du libre choix et de la responsabilité de l’acheteur, dans le respect du code de la commande publique.",
         },
-       {
+        {
           title:
             "Quels sont les produits issus de la « pêche durable » qui entrent dans les 50 % de produits durables et de qualité ?",
           answer:
             "La catégorie citée par la loi EGAlim est « Produits issus de la pêche maritime bénéficiant de l’écolabel Pêche durable ». Le label MSC (porté par l’ONG internationale MSC) est différent de la certification par l’écolabel « Pêche Durable ». Dans le cas où un fournisseur propose des produits labellisés « MSC pêche durable » en lieu et place de produits labellisés « écolabel Pêche durable, l’acheteur, sous sa propre responsabilité et sur la base d’éléments de preuve apportés par le fournisseur, peut étudier l’équivalence, dans le respect du code de la commande publique, de produits labellisés avec la catégorie écolabel « Pêche Durable ».",
         },
-       {
-          title:
-            "Quid des produits locaux ? Sont-ils pris en compte dans les 50% de produits « EGAlim » ?",
+        {
+          title: "Quid des produits locaux ? Sont-ils pris en compte dans les 50% de produits « EGAlim » ?",
           answer:
             "Le code de la commande publique ne permet pas de faire mention directement de l’origine locale, ce qui serait contraire aux principes du droit de la concurrence. Ainsi, les produits « locaux » ne sont pas pris en compte dans l’objectif de 50 % de produits durables et de qualité, en tant que tels. Un produit local n’est comptabilisable que s’il entre dans une des catégories citées par la loi. Cependant, les guides d’achats, construits dans le cadre du Conseil National de la Restauration Collective, consultables sur « ma-cantine », présentent des stratégies d’achats permettant de travailler sur des approvisionnements durables et de qualité, dans le cadre d’une démarche territoriale (notamment dans le cadre des projets alimentaires territoriaux).",
         },
-       {
-          title:
-            "Les produits « circuit court » et « origine France » sont-ils comptabilisables au titre des 50 % ?",
+        {
+          title: "Les produits « circuit court » et « origine France » sont-ils comptabilisables au titre des 50 % ?",
           answer:
             "Pour un produit « origine France » ou acquis en « circuit court », il peut être comptabilisé s’il satisfait une des catégories définies dans la loi. Pour la catégorie « produit dont l’acquisition a été fondée principalement sur la base de leurs performances en matière de protection de l’environnement et de développement des approvisionnements directs de produits de l’agriculture », une expertise juridique est encore en cours, pour cadrer l’utilisation de ces critères de sélection. Le suivi des caractéristiques, produit « issu d’un circuit court » ou « d’origine française » est nécessaire pour l’établissement d’un bilan statistique annuel à remettre au Parlement chaque année. ",
         },
-       {
+        {
           title:
             "Quel intérêt pour une SRC (société de restauration collective) de saisir les informations des restaurants clients concernant les dispositions de la loi sur la plateforme « ma-cantine » ?",
           answer:
@@ -146,7 +143,7 @@ export default {
           title:
             "J'utilise un outil de suivi d'achats (type GPAO). Pourrais-je transmettre les données relatives aux achats automatiquement à partir de mon logiciel de suivi ?",
           answer:
-            "Une API "ma cantine" est disponible pour les développeurs afin de se pouvoir "s'interconnecter" à la plateforme. Une documentation est en cours d’élaboration mais c'est déjà opérationnel pour les éditeurs de logiciel par ex : https://ma-cantine.agriculture.gouv.fr/, en créant ou en modifiant un profil en cochant l'option "mode développeur".",
+            "Une API « ma cantine » est disponible pour les développeurs afin de se pouvoir s'interconnecter à la plateforme.  La documentation est accessible pour les éditeurs de logiciel, il suffit de créer ou modifier votre profil en cochant le mode développeur.",
         },
       ],
     }
