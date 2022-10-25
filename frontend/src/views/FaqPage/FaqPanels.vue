7d03181e9d8967a34862107a0cae95b3548a7a7f
Jenn S <jennifer.stephan@edu.escpeurope.eu>
2022-10-25 11:28:51 +0200

    ajout de nouvelles questions sur la FAQ

--- frontend/src/views/FaqPage/FaqPanels.vue
+++ frontend/src/views/FaqPage/FaqPanels.vue
@@ -44,7 +44,7 @@ export default {
         {
           title: "Qui peut s’inscrire sur ma-cantine ?",
           answer:
-            "ma cantine est destinée aux responsables légaux et aux gestionnaires publics et privés des services de restauration collective. Concrètement, il s'agit de plusieurs types de peronnes : Élu(e)s de collectivité, Directeur(trice) d’établissement, Gestionnaire administratif, Comptable, Responsable Restauration, Chef(fe) et ou cuisinier. Plusieurs personnes peuvent etre rattachées à une même cantine. Les services de restauration peuvent etre en gestion directe ou en gestion déléguée/concédée.",
+            "ma cantine est destinée aux responsables légaux et aux gestionnaires publics et privés des services de restauration collective. Concrètement, il s'agit de plusieurs types de personnes : Élu(e)s de collectivité, Directeur(trice) d’établissement, Gestionnaire administratif, Comptable, Responsable Restauration, Chef(fe) et ou cuisinier. Plusieurs personnes peuvent etre rattachées à une même cantine. Les services de restauration peuvent etre en gestion directe ou en gestion déléguée/concédée.",
         },
         {
           title: "Comment s’inscrire sur ma-cantine ?",
@@ -64,7 +64,7 @@ export default {
         {
           title: "Le SIRET est-il obligatoire pour créer ma cantine ?",
           answer:
-            "Oui. Il est possible de le trouver ici : https://annuaire-entreprises.data.gouv.fr/. Concernant les cantines scolaires, vous pouvez retrouver le SIRET de l’établissement rattaché à la cantine sur l’Annuaire de l’éducation : https://annuaire-education.fr/.",
+            "Oui. Il est possible de le trouver ici : https://annuaire-entreprises.data.gouv.fr/. Concernant les cantines scolaires, vous pouvez retrouver le SIRET de l’établissement rattaché à la cantine sur l’Annuaire de l’éducation : https://annuaire-education.fr/. S’il existe déjà ou a été créé par une entité, je peux demander à rejoindre ma cantine.",
         },
         {
           title:
@@ -74,14 +74,14 @@ export default {
         },
         {
           title:
-            "Mon prestatiaire ou délégataire refuse de me transmettre mes données d'achats. Quelles obligations pour lui ?",
+            "Mon prestataire ou délégataire refuse de me transmettre mes données d'achats. Quelles obligations pour lui ?",
           answer:
             "L’obligation d’atteinte des taux EGAlim s’impose aux donneurs d’ordre et ils sont responsables d'en faire une information donnée à leurs convives. Les taux EGAlim sont calculés à partir des valeurs d’achats HT des produits alimentaires. D'autre part, les valeurs d’achat sont les informations qui permettront à l’administration de réaliser le bilan annuel qui doit être remis au parlement et rendu public. Cette obligation de transmission d’information peut être reportée dans le contrat qui lie le donneur d’ordre et son prestataire. Une convention de délégation pour la déclaration de ces éléments peut être établie entre le donneur d'ordre et le délégataire. Un modèle de convention est proposé dans la documentation de ma-cantine (https://ma-cantine-1.gitbook.io/ma-cantine-egalim/procedure-campagne-de-remontee-des-donnees-2022).",
         },
         {
           title: "Je suis gestionnaire d'une cuisine centrale, suis-je concerné par l'inscription sur ma-cantine ?",
           answer:
-            "Les gestionnaires de cuisines centrales doivent se créer un compte, creer leur cantine en déclarant chaque restaurants satellites qu’elles livrent. Jusqu'à la campagne 2024 de remontées des données de l'année civile 2023, les informations sur la mise en place des mesures EGAlim peuvent être centralisées au niveau de la cuisine centrale. A moyen terme, les informations devront etre attribuées à chaque cuisines satellites.",
+            "Les gestionnaires de cuisines centrales doivent se créer un compte, créer leur cantine en déclarant chaque restaurant satellite qu’elles livrent. Jusqu'à la campagne 2024 de remontées des données de l'année civile 2023, les informations sur la mise en place des mesures EGAlim peuvent être centralisées au niveau de la cuisine centrale. A moyen terme, les informations devront etre attribuées à chaque cuisines satellites.",
         },
         {
           title: "Je suis gestionnaire d'une cuisine satellite, suis-je concerné par l'inscription sur ma-cantine ?",
@@ -90,9 +90,9 @@ export default {
         },
         {
           title:
-            "Pourquoi, dans mon diagnostic, fournir les valeurs d’achat et pas les parts (en %) des produits durables et de qualité, et de produits bio à la place des valeurs d’achats ?",
+            "Pourquoi, dans mon diagnostic, fournir les valeurs d’achat et pas les parts (en %) des produits durables et de qualité, et de produits bio ?",
           answer:
-            "Le décret du 23 avril 2019, pris en application de la loi EGAlim du 30 octobre 2018 précise que ces parts (taux EGAlim) sont calculées à partir des valeurs d’achats HT des produits alimentaires. Seules les valeurs d’achat permettent de réaliser le bilan statistique annuel.",
+            "Le décret du 23 avril 2019, pris en application de la loi EGAlim du 30 octobre 2018 précise que ces parts (taux EGAlim) sont calculées à partir des valeurs d’achats HT des produits alimentaires. L’arrêté du 14 septembre 2022, indique que les informations à transmettre via ma cantine sont les valeurs d’achat (HT). Ces données permettent de réaliser le bilan statistique annuel.",
         },
         {
           title:
@@ -106,6 +106,48 @@ export default {
           answer:
             "A ce jour, la loi ne prévoit pas de sanctions, ni de contrôles pour ce qui concerne l'atteinte des mesures EGAlim ciblant directement la restauration collective. Cependant la loi EGAlim et plus récemment la Climat et Résilience prévoient un renforcement de la transparence vis-à-vis des convives avec notamment l’obligation d’une information des convives sur leur lieu de repas, de la mise en œuvre des objectifs en matière de produits durables et de qualité entrant dans la composition des repas (Art. L. 230-5-3 du code rural) et d’une information aux citoyens au travers un bilan statistique annuel de l’application de ces objectifs, remis au Parlement par le Gouvernement et rendu public depuis le 1 er janvier 2022 (Article L. 230-5-1 du code rural).",
         },
+        {
+          title:
+            "Je suis gestionnaire d'un collège ou d'un lycée, mon établissement a été créé, suis-je concerné par l'inscription dans ma cantine ?",
+          answer:
+            "Les Départements et les Régions ont respectivement la compétence de la restauration collective pour les collèges et les lycées. Ils sont à ce titre sont responsables du référencement de leurs établissements sur « ma cantine ».  Une convention passée entre l'établissement et la collectivité territoriale de rattachement précise les modalités d'exercice de leurs compétences respectives. Elle comprend un volet relatif à la restauration scolaire, qui vise en particulier à répondre aux objectifs fixés par la loi EGAlim (article L. 421-23 du code de l'éducation). La cantine peut être créée par l'une et/ou l'autre des parties et gérée par les deux.",
+        },        
+        {
+          title:
+            "Les produits Bleu-Blanc-Coeur (BBC) entrent-ils dans les 50 % de produits durables et de qualité ?",
+          answer:
+            "A ce jour, pour les produits issus de la démarche Bleu-Blanc-Coeur ne bénéficiant pas d’un signe ou mention « officiel » listé par la loi EGAlim, il est possible de les comptabiliser, comme pour tout autre produit, dans les objectifs de la loi EGAlim à condition qu’ils soient sélectionnés à l’issue d’un processus de sélection ciblant « les produits acquis selon les modalités prenant en compte les coûts imputés aux externalités environnementales tout au long de son cycle de vie ». Le recours à cette modalité de sélection relève du libre choix et de la responsabilité de l’acheteur, dans le respect du code de la commande publique.",
+        },
+       {
+          title:
+            "Quels sont les produits issus de la « pêche durable » qui entrent dans les 50 % de produits durables et de qualité ?",
+          answer:
+            "La catégorie citée par la loi EGAlim est « Produits issus de la pêche maritime bénéficiant de l’écolabel Pêche durable ». Le label MSC (porté par l’ONG internationale MSC) est différent de la certification par l’écolabel « Pêche Durable ». Dans le cas où un fournisseur propose des produits labellisés « MSC pêche durable » en lieu et place de produits labellisés « écolabel Pêche durable, l’acheteur, sous sa propre responsabilité et sur la base d’éléments de preuve apportés par le fournisseur, peut étudier l’équivalence, dans le respect du code de la commande publique, de produits labellisés avec la catégorie écolabel « Pêche Durable ».",
+        },
+       {
+          title:
+            "Quid des produits locaux ? Sont-ils pris en compte dans les 50% de produits « EGAlim » ?",
+          answer:
+            "Le code de la commande publique ne permet pas de faire mention directement de l’origine locale, ce qui serait contraire aux principes du droit de la concurrence. Ainsi, les produits « locaux » ne sont pas pris en compte dans l’objectif de 50 % de produits durables et de qualité, en tant que tels. Un produit local n’est comptabilisable que s’il entre dans une des catégories citées par la loi. Cependant, les guides d’achats, construits dans le cadre du Conseil National de la Restauration Collective, consultables sur « ma-cantine », présentent des stratégies d’achats permettant de travailler sur des approvisionnements durables et de qualité, dans le cadre d’une démarche territoriale (notamment dans le cadre des projets alimentaires territoriaux).",
+        },
+       {
+          title:
+            "Les produits « circuit court » et « origine France » sont-ils comptabilisables au titre des 50 % ?",
+          answer:
+            "Pour un produit « origine France » ou acquis en « circuit court », il peut être comptabilisé s’il satisfait une des catégories définies dans la loi. Pour la catégorie « produit dont l’acquisition a été fondée principalement sur la base de leurs performances en matière de protection de l’environnement et de développement des approvisionnements directs de produits de l’agriculture », une expertise juridique est encore en cours, pour cadrer l’utilisation de ces critères de sélection. Le suivi des caractéristiques, produit « issu d’un circuit court » ou « d’origine française » est nécessaire pour l’établissement d’un bilan statistique annuel à remettre au Parlement chaque année. ",
+        },
+       {
+          title:
+            "Quel intérêt pour une SRC (société de restauration collective) de saisir les informations des restaurants clients concernant les dispositions de la loi sur la plateforme « ma-cantine » ?",
+          answer:
+            "Les restaurants « clients » doivent transmettre via « ma-cantine » les informations sur leurs données d’achat. La possibilité est donnée à une SRC de pouvoir télétransmettre les données pour l’ensemble de ses restaurants « clients » (avec une possibilité de transmettre « en masse »). L’intérêt pour la SRC est de télétransmettre rapidement et simplement, et de pouvoir montrer, par la publication, les efforts et les résultats de son action. C’est aussi un service amené aux clients de la SRC qui n’auront pas cette action de télédéclaration à faire eux-mêmes. Cette décision de qui télédéclare entre la SRC et le client est à voir entre les deux parties.",
+        },
+        {
+          title:
+            "J'utilise un outil de suivi d'achats (type GPAO). Pourrais-je transmettre les données relatives aux achats automatiquement à partir de mon logiciel de suivi ?",
+          answer:
+            "Une API "ma cantine" est disponible pour les développeurs afin de se pouvoir "s'interconnecter" à la plateforme. Une documentation est en cours d’élaboration mais c'est déjà opérationnel pour les éditeurs de logiciel par ex : https://ma-cantine.agriculture.gouv.fr/, en créant ou en modifiant un profil en cochant l'option "mode développeur".",
+        }
       ],
     }
   },
