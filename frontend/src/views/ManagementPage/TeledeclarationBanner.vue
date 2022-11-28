42b37512ae6b1b04d7ba64154a5ade11d5f3c346
Alejandro MG <alejandro@amguillen.dev>
2022-11-28 10:08:13 +0100

    Smaller TD banner

--- frontend/src/views/ManagementPage/TeledeclarationBanner.vue
+++ frontend/src/views/ManagementPage/TeledeclarationBanner.vue
@@ -1,30 +1,14 @@
 <template>
   <v-card color="primary lighten-4" elevation="1" shaped class="d-flex align-center">
-    <div class="ma-4 ml-8" v-if="$vuetify.breakpoint.smAndUp">
-      <v-img max-width="100" contain src="/static/images/sitting-person-color.png"></v-img>
-    </div>
     <div>
       <v-card-title class="text-body-1 font-weight-bold">
         La campagne de remontée des données 2021 est à présent terminée !
       </v-card-title>
-      <v-card-text class="pb-0">
+      <v-card-text class="pb-6">
         Les données recueillies par télédéclaration seront traitées de manière agrégée pour réaliser le bilan annuel qui
-        sera remis au Parlement et rendu public. Vous pouvez d'ores et déjà compléter vos diagnostics achats 2022 afin
+        sera remis au Parlement et rendu public. Vous pouvez d'ores et déjà compléter vos diagnostics 2022 afin
         d'anticiper la prochaine campagne qui commencera en janvier 2023 !
       </v-card-text>
-      <v-card-actions class="px-4 mt-2">
-        <v-btn :to="{ name: 'PendingActions' }" color="primary">Télédéclarer mes cantines</v-btn>
-        <v-btn
-          text
-          href="https://agriculture.gouv.fr/restauration-collective-top-depart-pour-la-premiere-collecte-dinformations-concernant-les"
-          target="_blank"
-          rel="noopener"
-          class="text-decoration-underline"
-        >
-          Article sur le site du ministère
-          <v-icon small class="ml-2">mdi-open-in-new</v-icon>
-        </v-btn>
-      </v-card-actions>
     </div>
   </v-card>
 </template>
