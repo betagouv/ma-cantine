93933bf5a2e3b9d028a0a9cfbbfbda7e17d64ecf
Helen Root <helenroot1@gmail.com>
2023-01-24 17:23:40 +0100

    Make text less prominent

--- frontend/src/views/PartnersPage/PartnersHome.vue
+++ frontend/src/views/PartnersPage/PartnersHome.vue
@@ -9,19 +9,7 @@
         <p>
           Ces acteurs de la restauration collective au service des gestionnaires sont prêts pour vous aidez à atteindre
           les objectifs de votre cantine
-        </p>
-        <p>
-          <DownloadLink
-            href="/static/documents/charte_de_referencement_Version01_janvier2023.pdf"
-            label="En savoir plus sur les critères de référencement sur la charte ma cantine"
-            sizeStr="191 ko"
-            target="_blank"
-          />
-        </p>
-        <p class="caption">
-          Cette page n’engage pas l’administration ou l'équipe ma cantine ; elle constitue une proposition à l'attention
-          des responsables légaux de restaurants collectifs. Les structures sont seules responsables de la véracité des
-          informations qu’elles communiquent sur la page partenaire.
+          <a href="#en-savoir-plus">en savoir plus</a>
         </p>
       </v-col>
       <v-col cols="0" sm="5" md="4" v-if="$vuetify.breakpoint.smAndUp" class="py-0 pr-8 d-flex">
@@ -173,6 +161,24 @@
         <NewPartnerCard />
       </v-col>
     </v-row>
+
+    <v-divider class="mt-12" id="en-savoir-plus"></v-divider>
+
+    <div class="pt-12">
+      <p>
+        <DownloadLink
+          href="/static/documents/charte_de_referencement_Version01_janvier2023.pdf"
+          label="En savoir plus sur les critères de référencement sur la charte ma cantine"
+          sizeStr="191 ko"
+          target="_blank"
+        />
+      </p>
+      <p class="caption">
+        Cette page n’engage pas l’administration ou l'équipe ma cantine ; elle constitue une proposition à l'attention
+        des responsables légaux de restaurants collectifs. Les structures sont seules responsables de la véracité des
+        informations qu’elles communiquent sur la page partenaire.
+      </p>
+    </div>
 
     <v-divider class="mb-8 mt-12"></v-divider>
     <v-row>
