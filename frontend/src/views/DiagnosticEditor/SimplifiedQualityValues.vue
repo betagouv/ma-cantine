58592ad2c4ba75b32be204381d98a322402fdb56
Alejandro MG <alejandro@amguillen.dev>
2023-02-02 10:56:41 +0100

    Formatting fix

--- frontend/src/views/DiagnosticEditor/SimplifiedQualityValues.vue
+++ frontend/src/views/DiagnosticEditor/SimplifiedQualityValues.vue
@@ -180,14 +180,11 @@
         Zoom sur les familles « viandes et volailles » et « Produits de la mer et aquaculture»
       </h3>
       <p class="text-body-2">
-        Depuis la loi Climat et Résilience, un nouvel objectif pour une alimentation 
-        saine et durable a été ajouté : pour les achats de 2024, au moins 60% de viandes 
-        et poissons de qualité et durables. Ce taux est porté à 100 % pour les restaurants 
-        collectifs gérés par l'Etat, ses établissements publics et les entreprises 
-        publiques nationales. 
-        La réalisation du bilan annuel fait par l’administration implique également 
-        de connaître la part des produits origine France. Pour le moment, 
-        l’information est requise seulement pour la famille des viandes.
+        Depuis la loi Climat et Résilience, un nouvel objectif pour une alimentation saine et durable a été ajouté :
+        pour les achats de 2024, au moins 60% de viandes et poissons de qualité et durables. Ce taux est porté à 100 %
+        pour les restaurants collectifs gérés par l'Etat, ses établissements publics et les entreprises publiques
+        nationales. La réalisation du bilan annuel fait par l’administration implique également de connaître la part des
+        produits origine France. Pour le moment, l’information est requise seulement pour la famille des viandes.
       </p>
     </div>
 
