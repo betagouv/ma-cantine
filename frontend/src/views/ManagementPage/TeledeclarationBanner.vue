ceecb235488a08c94dede1297419b7469a8ac4b8
Helen Root <helenroot1@gmail.com>
2023-02-09 17:57:48 +0100

    Date de la fin de la campagne

--- frontend/src/views/ManagementPage/TeledeclarationBanner.vue
+++ frontend/src/views/ManagementPage/TeledeclarationBanner.vue
@@ -3,7 +3,7 @@
     <div>
       <!-- TODO: update end date -->
       <v-card-title class="text-body-1 font-weight-bold">
-        Du 13 février au XX XXX 2023, participez à la collecte de données 2022 !
+        Du 13 février au 15 mai 2023, participez à la collecte de données 2022 !
       </v-card-title>
       <v-card-text class="pb-0">
         Dans votre espace cantine, remplissez votre diagnostic 2022 et télédéclarez vos données (arrêté ministériel du
