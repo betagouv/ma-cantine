47d2182161ea1516ddae7ee5ce94c571b28ffaf8
Alejandro MG <alejandro@amguillen.dev>
2023-02-23 14:29:22 +0100

    Updates satellites canteens when a central kitchen siret is modified

--- data/signals.py
+++ data/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import User, ManagerInvitation
+from .models import User, ManagerInvitation, Canteen
 
 
 @receiver(post_save, sender=User)
@@ -10,3 +10,16 @@ def update_canteen_managers(sender, instance, created, **kwargs):
         for link in canteen_links:
             link.canteen.managers.add(instance)
             link.delete()
+
+
+@receiver(pre_save, sender=Canteen)
+def update_satellites_siret(sender, instance, raw, using, update_fields, **kwargs):
+    if raw or not instance.is_central_cuisine:
+        return
+    try:
+        obj = sender.objects.get(pk=instance.pk)
+        siret_has_changed = obj.siret != instance.siret
+        if siret_has_changed:
+            Canteen.objects.filter(central_producer_siret=obj.siret).update(central_producer_siret=instance.siret)
+    except sender.DoesNotExist:
+        pass  # Object is new
