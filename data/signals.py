587685f7158fd9890c43eee0f3714cc9f082d86e
Alejandro MG <alejandro@amguillen.dev>
2023-02-28 15:32:35 +0100

    Logs the SIRET change

--- data/signals.py
+++ data/signals.py
@@ -1,6 +1,9 @@
+import logging
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import User, ManagerInvitation, Canteen
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=User)
@@ -20,6 +23,15 @@ def update_satellites_siret(sender, instance, raw, using, update_fields, **kwarg
         obj = sender.objects.get(pk=instance.pk)
         siret_has_changed = obj.siret != instance.siret
         if siret_has_changed:
-            Canteen.objects.filter(central_producer_siret=obj.siret).update(central_producer_siret=instance.siret)
+            logger.info(
+                f"SIRET change. Central kitchen {instance.id} ({instance.name}) changed its SIRET from {obj.siret} to {instance.siret}"
+            )
+            satellites = Canteen.objects.filter(central_producer_siret=obj.siret).only("id")
+            for satellite in satellites:
+                logger.info(
+                    f"SIRET change. Satellite cantine {satellite.id} changed its central_producer_siret from {obj.siret} to {instance.siret}"
+                )
+            satellites.update(central_producer_siret=instance.siret)
+
     except sender.DoesNotExist:
         pass  # Object is new
