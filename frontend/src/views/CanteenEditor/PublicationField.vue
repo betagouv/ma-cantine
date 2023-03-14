cb39e20df8a68970677da7d8f6b44dda95a77f5c
Helen Root <helenroot1@gmail.com>
2023-03-14 17:41:22 +0100

    Make published notice more prominent

--- frontend/src/views/CanteenEditor/PublicationField.vue
+++ frontend/src/views/CanteenEditor/PublicationField.vue
@@ -10,10 +10,7 @@
       <v-col cols="12">
         <v-checkbox hide-details="auto" class="mt-0" color="primary" :input-value="value" @change="updateValue">
           <template v-slot:label>
-            <p
-              class="text-body-2 grey--text text--darken-4 pb-0 my-0 ml-2"
-              :class="{ 'pt-1': !originalCanteenIsPublished }"
-            >
+            <p class="text-body-2 grey--text text--darken-4 pb-0 my-0 ml-2" :class="{ 'pt-1': isDraft }">
               J'accepte que les données relatives aux mesures EGAlim de ma cantine soient visibles sur
               <router-link
                 :to="{
@@ -23,28 +20,7 @@
                 nos cantines
               </router-link>
               <br />
-              <span v-if="originalCanteenIsPublished">
-                Cette cantine est actuellement publiée sur
-                <v-btn
-                  @click.stop
-                  :href="
-                    $router.resolve({
-                      name: 'CanteenPage',
-                      params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(canteen) },
-                    }).href
-                  "
-                  class="text-body-2 pl-0 text-decoration-underline"
-                  id="canteen-page-link"
-                  target="_blank"
-                  small
-                  text
-                  plain
-                >
-                  nos cantines
-                  <v-icon small color="grey darken-4" class="ml-1">mdi-open-in-new</v-icon>
-                </v-btn>
-              </span>
-              <span v-else>
+              <span v-if="isDraft">
                 <v-btn
                   @click.stop="showPreview = true"
                   class="text-body-2 px-0 text-decoration-underline grey--text text--darken-4"
@@ -88,8 +64,8 @@ export default {
     isNewCanteen() {
       return !this.canteen.id
     },
-    originalCanteenIsPublished() {
-      return this.canteen.publicationStatus === "published"
+    isDraft() {
+      return this.canteen.publicationStatus === "draft"
     },
   },
   methods: {
@@ -103,8 +79,5 @@ export default {
 <style scoped>
 .v-btn--plain:not(.v-btn--active):not(.v-btn--loading):not(:focus):not(:hover) >>> .v-btn__content {
   opacity: 1;
-}
-#canteen-page-link {
-  vertical-align: inherit;
 }
 </style>
--- frontend/src/views/CanteenEditor/PublicationStateNotice.vue
+++ frontend/src/views/CanteenEditor/PublicationStateNotice.vue
@@ -1,26 +1,37 @@
 <template>
   <div>
-    <div v-if="!isPublished">
-      <DsfrCallout v-if="readyToPublish" class="mb-1">
-        <span class="grey--text text--darken-2">
-          Vos données d'approvisionnement pour l'année {{ publicationYear }} sont completées, alors nous vous
-          encourageons de publier votre cantine.
-          <router-link v-if="includeLink" :to="{ name: 'PublicationForm', params: { canteenUrlComponent } }">
-            Publier cette cantine.
-          </router-link>
-        </span>
-      </DsfrCallout>
-      <!-- includeLink currently only used by CanteenPage, where we don't want this to show. Bit hacky -->
-      <DsfrCallout color="#C08C65" class="mb-1" v-else-if="!includeLink">
-        <span class="grey--text text--darken-2">
-          Nous vous conseillons de remplir des
-          <router-link :to="{ name: 'DiagnosticList', params: { canteenUrlComponent } }">
-            données d'approvisionnement pour l'année {{ this.publicationYear }}
-          </router-link>
-          avant que vous publiiez vos données.
-        </span>
-      </DsfrCallout>
-    </div>
+    <DsfrCallout v-if="isPublished" icon="$checkbox-circle-fill" color="green">
+      Cette cantine est actuellement publiée sur
+      <router-link
+        :to="{
+          name: 'CanteenPage',
+          params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(canteen) },
+        }"
+        target="_blank"
+      >
+        nos cantines
+        <v-icon small class="ml-1" color="primary">mdi-open-in-new</v-icon>
+      </router-link>
+    </DsfrCallout>
+    <DsfrCallout v-else-if="readyToPublish" class="mb-1">
+      <span class="grey--text text--darken-2">
+        Vos données d'approvisionnement pour l'année {{ publicationYear }} sont completées, alors nous vous encourageons
+        de publier votre cantine.
+        <router-link v-if="includeLink" :to="{ name: 'PublicationForm', params: { canteenUrlComponent } }">
+          Publier cette cantine.
+        </router-link>
+      </span>
+    </DsfrCallout>
+    <!-- includeLink currently only used by CanteenPage, where we don't want this to show. Bit hacky -->
+    <DsfrCallout color="#C08C65" class="mb-1" v-else-if="!includeLink">
+      <span class="grey--text text--darken-2">
+        Nous vous conseillons de remplir des
+        <router-link :to="{ name: 'DiagnosticList', params: { canteenUrlComponent } }">
+          données d'approvisionnement pour l'année {{ this.publicationYear }}
+        </router-link>
+        avant que vous publiiez vos données.
+      </span>
+    </DsfrCallout>
   </div>
 </template>
 
