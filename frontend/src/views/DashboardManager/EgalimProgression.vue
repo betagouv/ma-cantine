035efc679582c18bfbbe611025ee11dc6c6ba410
Alejandro MG <alejandro@amguillen.dev>
2023-05-25 15:28:31 +0200

    Basic structure of progression pages

--- frontend/src/views/DashboardManager/EgalimProgression.vue
+++ frontend/src/views/DashboardManager/EgalimProgression.vue
@@ -2,19 +2,28 @@
   <v-row>
     <v-col cols="12" md="4">
       <v-card outlined>
-        <v-card-title>Qualité des produits</v-card-title>
+        <v-card-title class="text-body-1 font-weight-bold">
+          <v-icon class="mr-2" size="20" color="red">mdi-food-apple</v-icon>
+          Qualité des produits
+        </v-card-title>
       </v-card>
     </v-col>
     <v-col cols="12" md="4">
       <v-row>
         <v-col cols="12">
           <v-card outlined>
-            <v-card-title>Lutte contre le gaspillage</v-card-title>
+            <v-card-title class="text-body-1 font-weight-bold">
+              <v-icon class="mr-2" size="20" color="orange darken-2">mdi-offer</v-icon>
+              Lutte contre le gaspillage
+            </v-card-title>
           </v-card>
         </v-col>
         <v-col cols="12">
           <v-card outlined>
-            <v-card-title>Interdiction du plastique</v-card-title>
+            <v-card-title class="text-body-1 font-weight-bold">
+              <v-icon class="mr-2" size="20" color="blue darken-1">mdi-weather-windy</v-icon>
+              Interdiction du plastique
+            </v-card-title>
           </v-card>
         </v-col>
       </v-row>
@@ -23,12 +32,18 @@
       <v-row>
         <v-col cols="12">
           <v-card outlined>
-            <v-card-title>Diversification des menus</v-card-title>
+            <v-card-title class="text-body-1 font-weight-bold">
+              <v-icon class="mr-2" size="20" color="green darken-2">$leaf-fill</v-icon>
+              Diversification des menus
+            </v-card-title>
           </v-card>
         </v-col>
         <v-col cols="12">
           <v-card outlined>
-            <v-card-title>Information des convives</v-card-title>
+            <v-card-title class="text-body-1 font-weight-bold">
+              <v-icon class="mr-2" size="20" color="amber darken-2">mdi-bullhorn</v-icon>
+              Information des convives
+            </v-card-title>
           </v-card>
         </v-col>
       </v-row>
--- frontend/src/views/DashboardManager/index.vue
+++ frontend/src/views/DashboardManager/index.vue
@@ -43,7 +43,7 @@ export default {
       return this.$store.state.loggedUser
     },
     hasProgression() {
-      return true
+      return false
     },
   },
 }
