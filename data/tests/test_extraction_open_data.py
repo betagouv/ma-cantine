80056aed0fe5654eec6f0582ba8df84ae30d8a2c
qloridant <quentin.loridant@protonmail.com>
2023-06-16 14:48:01 +0200

    [Fail] Test extract dataset td

---
+++ data/tests/test_extraction_open_data.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.core import mail
+from django.test.utils import override_settings
+from django.utils.timezone import now
+from data.factories import DiagnosticFactory, CanteenFactory, UserFactory, TeledeclarationFactory
+from data.models import Teledeclaration
+
+
+class TestExtractionOpenData(TestCase):
+
+    def test_extraction_teledeclaration(self):
+        canteen = CanteenFactory.create()
+        diagnostic = DiagnosticFactory.create(canteen=canteen, year=2021, diagnostic_type=None)
+        applicant = UserFactory.create()
+        TeledeclarationFactory.create(
+            applicant=applicant,
+            year=diagnostic.year,
+            canteen=canteen,
+            canteen_siret=canteen.siret,
+            status=Teledeclaration.TeledeclarationStatus.SUBMITTED,
+            diagnostic=diagnostic,
+            declared_data={
+                "year": diagnostic.year,
+                "canteen": {
+                    "name": "",
+                },
+                "applicant": {
+                    "name": "",
+                },
+                "teledeclaration": {},
+            },
+            teledeclaration_mode='SITE',
+        )
+        td = extract_dataset_teledeclaration(year=diagnostic.year)
+        assert len(td) == 1, "There should be one teledeclaration for 2021"
