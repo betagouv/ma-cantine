99c6472d8c2b748d11f848d075622f2ad91829d4
qloridant <quentin.loridant@protonmail.com>
2023-06-19 10:47:44 +0200

    [Fail] Expect the extraction to be a dataframe with the columns matching the schema

--- data/tests/test_extraction_open_data.py
+++ data/tests/test_extraction_open_data.py
@@ -1,13 +1,11 @@
 from django.test import TestCase
-from django.core import mail
-from django.test.utils import override_settings
-from django.utils.timezone import now
 from data.factories import DiagnosticFactory, CanteenFactory, UserFactory, TeledeclarationFactory
 from data.models import Teledeclaration
 from macantine.tasks import extract_datasets
 
 
 class TestExtractionOpenData(TestCase):
+    td_columns = ["id", "applicant_id", "teledeclaration_mode", "creation_date", "year", "version", "canteen_id", "canteen_siret", "canteen_name", "cantine_central_kitchen_siret", "canteen_department", "canteen_region", "cantine_satellite_canteens_count", "cantine_economic_model", "cantine_management_type", "cantine_production_type", "canteen_sectors", "canteen_line_ministry", "teledeclaration_ratio_bio", "teledeclaration_ratio_egalim_hors_bio"]
 
     def test_extraction_teledeclaration(self):
         canteen = CanteenFactory.create()
@@ -36,3 +34,4 @@ class TestExtractionOpenData(TestCase):
 
         td = extract_datasets(year=diagnostic.year)
         assert len(td) == 1, "There should be one teledeclaration for 2021"
+        assert td.columns == self.td_columns, "There should be one teledeclaration for 2021"
