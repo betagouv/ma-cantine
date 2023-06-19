3cdc300164dc0f283ffafe016e1abe128a6c4803
qloridant <quentin.loridant@protonmail.com>
2023-06-19 09:49:00 +0200

    [Refacto]

--- data/tests/test_extraction_open_data.py
+++ data/tests/test_extraction_open_data.py
@@ -13,43 +13,26 @@ class TestExtractionOpenData(TestCase):
         canteen = CanteenFactory.create()
         diagnostic = DiagnosticFactory.create(canteen=canteen, year=2021, diagnostic_type=None)
         applicant = UserFactory.create()
-        TeledeclarationFactory.create(
-            applicant=applicant,
-            year=diagnostic.year,
-            canteen=canteen,
-            canteen_siret=canteen.siret,
-            status=Teledeclaration.TeledeclarationStatus.SUBMITTED,
-            diagnostic=diagnostic,
-            declared_data={
-                "year": diagnostic.year,
-                "canteen": {
-                    "name": "",
+        for year_td in [2021, 2022]:
+            TeledeclarationFactory.create(
+                applicant=applicant,
+                year=year_td,
+                canteen=canteen,
+                canteen_siret=canteen.siret,
+                status=Teledeclaration.TeledeclarationStatus.SUBMITTED,
+                diagnostic=diagnostic,
+                declared_data={
+                    "year": year_td,
+                    "canteen": {
+                        "name": "",
+                    },
+                    "applicant": {
+                        "name": "",
+                    },
+                    "teledeclaration": {},
                 },
-                "applicant": {
-                    "name": "",
-                },
-                "teledeclaration": {},
-            },
-            teledeclaration_mode='SITE',
-        )
-        TeledeclarationFactory.create(
-            applicant=applicant,
-            year=diagnostic.year + 1,
-            canteen=canteen,
-            canteen_siret=canteen.siret,
-            status=Teledeclaration.TeledeclarationStatus.SUBMITTED,
-            diagnostic=diagnostic,
-            declared_data={
-                "year": diagnostic.year + 1,
-                "canteen": {
-                    "name": "",
-                },
-                "applicant": {
-                    "name": "",
-                },
-                "teledeclaration": {},
-            },
-            teledeclaration_mode='SITE',
-        )
+                teledeclaration_mode='SITE',
+            )
+
         td = extract_datasets(year=diagnostic.year)
         assert len(td) == 1, "There should be one teledeclaration for 2021"
