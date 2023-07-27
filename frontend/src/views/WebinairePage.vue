6261f40098110c9c2477f7fcd315b4c30b4c5adb
Alejandro MG <alejandro@amguillen.dev>
2023-07-27 10:22:54 +0200

    View creation and URL addition in router

---
+++ frontend/src/views/WebinairePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>HELLO WORLD: {{ webinaireUrlComponent }}</div>
+</template>
+
+<script>
+export default {
+  name: "WebinairePage",
+  props: {
+    webinaireUrlComponent: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
