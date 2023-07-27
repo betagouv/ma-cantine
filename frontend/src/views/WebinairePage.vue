7ef0988e457c6f6f6a5084dea00ae846f8fe070d
qloridant <quentin.loridant@protonmail.com>
2023-07-27 10:46:38 +0200

    Methode getter video

--- frontend/src/views/WebinairePage.vue
+++ frontend/src/views/WebinairePage.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>HELLO WORLD: {{ webinaireUrlComponent }}</div>
+  <div>
+    <div>HELLO WORLD: {{ webinaireUrlComponent }}</div>
+    <div>Video url : {{ mainVideo.video }}</div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +12,15 @@ export default {
     webinaireUrlComponent: {
       type: String,
       required: true,
+    },
+  },
+  computed: {
+    videoTutorials() {
+      return this.$store.state.videoTutorials
+    },
+    mainVideo() {
+      const id = parseInt(this.webinaireUrlComponent.split("--")[0])
+      return this.videoTutorials.find((x) => x.id === id)
     },
   },
 }
