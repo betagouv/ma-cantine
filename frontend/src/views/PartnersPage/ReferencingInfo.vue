fce9eed53a4eaebc81794ed282e6d77a53309061
Helen Root <helenroot1@gmail.com>
2023-08-08 17:32:21 +0200

    Update information about how partners are referenced

--- frontend/src/views/PartnersPage/ReferencingInfo.vue
+++ frontend/src/views/PartnersPage/ReferencingInfo.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="pt-12">
-    <p class="caption">
-      Cette page n’engage pas l’administration ou l'équipe ma cantine ; elle constitue une proposition à l'attention des
-      responsables légaux de restaurants collectifs.
+  <div>
+    <p>
+      Ce service est en expérimentation jusqu'à décembre 2023. Les acteurs se sont référencés eux mêmes en respectant
+      notre charte.
     </p>
+    <DownloadLink
+      href="/static/documents/charte-referencement-acteurs-de-l-ecosysteme.pdf"
+      label="Télécharger la charte de référencement"
+      sizeStr="187.3 ko"
+    />
   </div>
 </template>
 
 <script>
+import DownloadLink from "@/components/DownloadLink"
+
 export default {
   name: "ReferencingInfo",
+  components: { DownloadLink },
 }
 </script>
