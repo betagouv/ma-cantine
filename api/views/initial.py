a37f64587c4a01a14f2cec64276a69f64c421582
Alejandro MG <alejandro@amguillen.dev>
2023-08-10 22:21:50 +0200

    Adds canteen previews to backend initial call

--- api/views/initial.py
+++ api/views/initial.py
@@ -1,4 +1,11 @@
-from api.views import LoggedUserView, SectorListView, PartnerTypeListView, CommunityEventsView, VideoTutorialListView
+from api.views import (
+    LoggedUserView,
+    SectorListView,
+    PartnerTypeListView,
+    CommunityEventsView,
+    VideoTutorialListView,
+    UserCanteenPreviews,
+)
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework import status
@@ -6,11 +13,13 @@ from rest_framework import status
 
 class InitialDataView(APIView):
     def get(self, request, *args, **kwargs):
+        is_authenticated = request.user.is_authenticated
         json_content = {
             "loggedUser": LoggedUserView.as_view()(request._request).data,
             "sectors": SectorListView.as_view()(request._request).data,
             "partnerTypes": PartnerTypeListView.as_view()(request._request).data,
             "communityEvents": CommunityEventsView.as_view()(request._request).data,
             "videoTutorials": VideoTutorialListView.as_view()(request._request).data,
+            "canteenPreviews": UserCanteenPreviews.as_view()(request._request).data if is_authenticated else None,
         }
         return JsonResponse(json_content, status=status.HTTP_200_OK)
