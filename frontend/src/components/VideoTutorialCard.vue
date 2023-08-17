30636d31a5a58ea8e07484a8fca3705cfb54ebb0
Alejandro MG <alejandro@amguillen.dev>
2023-08-17 13:06:10 +0200

    Same line icon and text + icon vertical alignment

--- frontend/src/components/VideoTutorialCard.vue
+++ frontend/src/components/VideoTutorialCard.vue
@@ -7,9 +7,9 @@
       params: { webinaireUrlComponent: `${videoTutorial.id}--${videoTutorial.title}` },
     }"
   >
-    <v-card-title class="text-body-1 font-weight-bold">
-      <v-icon color="grey darken-3" class="mr-1">$film-fill</v-icon>
-      <span>{{ videoTutorial.title }}</span>
+    <v-card-title class="text-body-1 font-weight-bold d-block">
+      <v-icon color="grey darken-3" class="mr-1 mt-n1">$film-fill</v-icon>
+      {{ videoTutorial.title }}
     </v-card-title>
     <v-card-text v-if="videoTutorial.description">{{ videoTutorial.description }}</v-card-text>
     <v-spacer></v-spacer>
