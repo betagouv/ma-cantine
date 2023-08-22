5bce53dd2860b10d6341bd42e3c510fc24db7f3a
Helen Root <helenroot1@gmail.com>
2023-08-22 11:55:49 +0200

    Ensure links use anchor tags, and update links to webinaires #2848

--- frontend/src/views/FaqPage/FaqPanels.vue
+++ frontend/src/views/FaqPage/FaqPanels.vue
@@ -29,10 +29,12 @@
 <script>
 import SiretRequired from "./AnswerComponents/SiretRequired"
 import NoSiret from "./AnswerComponents/NoSiret"
+import HowToSignUp from "./AnswerComponents/HowToSignUp"
+import ProviderObligation from "./AnswerComponents/ProviderObligation"
 
 export default {
   name: "FaqPanels",
-  components: { SiretRequired, NoSiret },
+  components: { SiretRequired, NoSiret, HowToSignUp, ProviderObligation },
   data() {
     return {
       sections: [
@@ -61,8 +63,7 @@ export default {
             },
             {
               title: "Comment s’inscrire sur « ma cantine » ?",
-              answer:
-                "Il faut procéder en plusieurs étapes qui sont documentées à ce lien (https://ma-cantine-1.gitbook.io/ma-cantine-egalim/tuto-utilisation-de-la-plateforme-ma-cantine) : se créer un compte sur ma-cantine.agriculture.gouv.fr, créer sa cantine avec son numéro SIRET et créer un diagnostic pour par année civile.",
+              answerComponent: "HowToSignUp",
             },
             {
               title:
@@ -127,8 +128,7 @@ export default {
             {
               title:
                 "Mon prestataire ou délégataire refuse de me transmettre mes données d'achats. Quelles obligations pour lui ?",
-              answer:
-                "L’obligation d’atteinte des taux EGAlim s’impose aux donneurs d’ordre et ils sont responsables d'en faire une information donnée à leurs convives. Les taux EGAlim sont calculés à partir des valeurs d’achats HT des produits alimentaires. D'autre part, les valeurs d’achat sont les informations qui permettront à l’administration de réaliser le bilan annuel qui doit être remis au parlement et rendu public. Cette obligation de transmission d’information peut être reportée dans le contrat qui lie le donneur d’ordre et son prestataire. Une convention de délégation pour la déclaration de ces éléments peut être établie entre le donneur d'ordre et le délégataire. Un modèle de convention est proposé dans la documentation de « ma cantine » (https://ma-cantine-1.gitbook.io/ma-cantine-egalim/procedure-campagne-de-remontee-des-donnees-2022).",
+              answerComponent: "ProviderObligation",
             },
           ],
         },
