fcb104f0b91a6d6294fa8d820b537f85a04d073f
Helen Root <helenroot1@gmail.com>
2023-08-30 14:23:51 +0200

    Application wide SCSS overrides setup

--- frontend-vue3/src/main.js
+++ frontend-vue3/src/main.js
@@ -3,20 +3,10 @@ import "@gouvfr/dsfr/dist/dsfr.min.css" // Import des styles du DSFR
 import "@gouvminint/vue-dsfr/styles" // Import des styles globaux propre à VueDSFR
 import VueDsfr from "@gouvminint/vue-dsfr" // Import (par défaut) de la bibliothèque
 
-// Vuetify
-import "vuetify/styles"
-import { createVuetify } from "vuetify"
-import * as components from "vuetify/components"
-import * as directives from "vuetify/directives"
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
 import App from "./App.vue"
 import { router } from "./router"
 import store from "./store"
+import vuetify from "./plugins/vuetify"
 
 createApp(App)
   .use(vuetify)
---
+++ frontend-vue3/src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import "@/styles/main.scss"
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+
+export default createVuetify({
+  components,
+  directives,
+})
