01927e861e09752ec38e14dacf0b6dd7eb539a5c
Helen Root <helenroot1@gmail.com>
2023-09-08 16:18:58 +0200

    Add DSFR icons

--- frontend-vue3/src/main.js
+++ frontend-vue3/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import "@gouvfr/dsfr/dist/dsfr.min.css" // Import des styles du DSFR
 import "@gouvminint/vue-dsfr/styles" // Import des styles globaux propre à VueDSFR
 import VueDsfr from "@gouvminint/vue-dsfr" // Import (par défaut) de la bibliothèque
+import "@gouvfr/dsfr/dist/utility/icons/icons.min.css"
 
 import App from "./App.vue"
 import { router } from "./router"
--- frontend-vue3/src/views/LandingPage/index.vue
+++ frontend-vue3/src/views/LandingPage/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Test DSFR</h1>
     <DsfrAlert title="Testing" />
+    <span class="fr-icon-pencil-fill" aria-hidden="true"></span>
   </div>
 </template>
 
