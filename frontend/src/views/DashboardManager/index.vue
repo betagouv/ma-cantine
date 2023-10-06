3df4059ab770d8b87ac91d1d96975bed9a617bff
Helen Root <helenroot1@gmail.com>
2023-10-06 12:15:07 +0200

    Refactor purchases

--- frontend/src/views/DashboardManager/index.vue
+++ frontend/src/views/DashboardManager/index.vue
@@ -43,63 +43,8 @@
           Accédez ci-dessous aux différents outils de gestion de votre établissement sur la plateforme « ma cantine ».
         </p>
         <v-row>
-          <v-col cols="12" md="8" id="latest-purchases">
-            <v-card outlined class="fill-height d-flex flex-column pa-4">
-              <v-card-title class="pb-0"><h3 class="fr-h4 mb-0">Mes achats</h3></v-card-title>
-              <v-card-text class="fr-text-xs grey--text text--darken-2 py-0 mt-3">
-                <p v-if="!purchases.length">
-                  Renseignez vos achats pour calculer automatiquement votre progression sur le volet approvisionnements
-                  EGAlim.
-                </p>
-                <p v-else>Source des données : {{ purchaseDataSourceString }}.</p>
-              </v-card-text>
-              <v-card-text class="pt-0">
-                <v-data-table
-                  :items="purchases"
-                  :headers="purchaseHeaders"
-                  :hide-default-footer="true"
-                  :disable-sort="true"
-                  :class="`dsfr-table ${purchases.length && 'table-preview'}`"
-                  dense
-                >
-                  <template v-slot:[`item.characteristics`]="{ item }">
-                    {{ getProductCharacteristicsDisplayValue(item.characteristics) }}
-                  </template>
-                  <template v-slot:[`no-data`]>
-                    <v-card outlined rounded class="my-4 py-4 no-purchases" v-if="!purchasesFetchingError">
-                      <v-card-text class="fr-text-xs primary--text">
-                        Saisissez vos achats manuellement ou connectez votre logiciel de gestion habituel
-                      </v-card-text>
-                      <v-card-actions class="justify-center">
-                        <v-btn
-                          :to="{ name: 'PurchasesHome' }"
-                          color="primary"
-                          class="mx-2 mb-2 fr-text-sm font-weight-medium"
-                        >
-                          Compléter mes achats
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                    <v-alert outlined type="error" v-else>
-                      <p>{{ purchasesFetchingError }}</p>
-                      <v-btn @click="fetchPurchases" outlined color="primary">Essayer à nouveau</v-btn>
-                    </v-alert>
-                  </template>
-                </v-data-table>
-              </v-card-text>
-              <v-spacer></v-spacer>
-              <v-card-actions v-if="purchases.length || purchasesFetchingError">
-                <v-btn :to="{ name: 'NewPurchase' }" outlined color="primary" class="mx-2 mb-2">
-                  Ajouter un achat
-                </v-btn>
-                <v-btn :to="{ name: 'PurchasesImporter' }" outlined color="primary" class="mx-2 mb-2">
-                  Importer des achats
-                </v-btn>
-                <v-btn :to="{ name: 'PurchasesHome' }" outlined color="primary" class="mx-2 mb-2">
-                  Tous mes achats
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+          <v-col cols="12" md="8" id="latest-purchases" fill-height>
+            <PurchasesWidget :canteen="canteen" />
           </v-col>
           <v-col v-if="!canteen.isCentralCuisine" cols="12" md="4" id="publication">
             <v-card outlined class="fill-height d-flex flex-column pa-4">
@@ -353,13 +298,14 @@
 
 <script>
 import EgalimProgression from "./EgalimProgression"
+import PurchasesWidget from "./PurchasesWidget"
 import DsfrAutocomplete from "@/components/DsfrAutocomplete"
-import { toCurrency, capitalise, formatDate, lastYear } from "@/utils"
+import { capitalise, formatDate, lastYear } from "@/utils"
 import Constants from "@/constants"
 
 export default {
   name: "DashboardManager",
-  components: { EgalimProgression, DsfrAutocomplete },
+  components: { EgalimProgression, DsfrAutocomplete, PurchasesWidget },
   data() {
     const canteenId = +this.$route.query.cantine || this.$store.state.userCanteenPreviews[0]?.id
     return {
@@ -375,15 +321,6 @@ export default {
         { text: "Statut", value: "publicationStatus" },
       ],
       satelliteCount: null,
-      // purchases widget
-      purchases: [],
-      purchaseHeaders: [
-        { text: "Date", value: "relativeDate" },
-        { text: "Produit", value: "description" },
-        { text: "Caractéristiques", value: "characteristics" },
-        { text: "Prix HT", value: "priceHt" },
-      ],
-      purchasesFetchingError: null,
       // publication widget
       viewCount: null,
       year: lastYear(),
@@ -459,11 +396,6 @@ export default {
     hasPublicationData() {
       return !!this.diagnostic?.valueTotalHt
     },
-    // purchases widget
-    purchaseDataSourceString() {
-      if (!this.purchases.length) return
-      return this.purchases[0].createdByImport ? "import en masse" : "ajout manuel"
-    },
   },
   methods: {
     fetchCanteenIfNeeded() {
@@ -479,7 +411,6 @@ export default {
           this.canteen = canteen
           this.getCentralKitchen()
           this.updateSatelliteCount()
-          this.fetchPurchases()
           this.getPublishedPageViewCount()
           this.$router.push({ query: { cantine: id } }).catch(() => {})
         })
@@ -512,59 +443,6 @@ export default {
           this.satelliteCount = response.count
           this.satellites = response.results
         })
-    },
-    fetchPurchases() {
-      this.purchasesFetchingError = null
-      const purchaseLimit = 3
-      const query = `limit=${purchaseLimit}&ordering=-date&canteen__id=${this.canteenId}`
-      return fetch(`/api/v1/purchases/?${query}`)
-        .then((response) => {
-          if (response.status < 200 || response.status >= 400) throw new Error(`Error encountered : ${response}`)
-          return response.json()
-        })
-        .then((response) => {
-          this.purchases = response.results
-          this.purchases.forEach((purchase) => {
-            purchase.priceHt = toCurrency(purchase.priceHt)
-            const dateDelta = Date.now() - new Date(purchase.date).getTime()
-            const millisecondsInDay = 1000 * 60 * 60 * 24
-            const daysAgo = Math.floor(dateDelta / millisecondsInDay)
-            const weeksAgo = Math.floor(daysAgo / 7)
-            const monthsAgo = Math.floor(daysAgo / 30)
-            const yearsAgo = Math.floor(daysAgo / 365)
-            if (yearsAgo > 0) {
-              purchase.relativeDate = `il y a ${yearsAgo} an${yearsAgo === 1 ? "" : "s"}`
-            } else if (monthsAgo > 0) {
-              purchase.relativeDate = `il y a ${monthsAgo} mois`
-            } else if (weeksAgo > 0) {
-              purchase.relativeDate = `il y a ${weeksAgo} semaine${weeksAgo === 1 ? "" : "s"}`
-            } else {
-              purchase.relativeDate = `il y a ${daysAgo} jour${daysAgo === 1 ? "" : "s"}`
-            }
-          })
-        })
-        .catch(() => {
-          this.purchases = []
-          this.purchasesFetchingError = "Échec lors du téléchargement des achats"
-        })
-    },
-    getProductCharacteristicsDisplayValue(characteristics) {
-      const priorityOrder = Object.keys(Constants.Characteristics)
-      characteristics = characteristics.filter((c) => priorityOrder.indexOf(c) > -1)
-      characteristics.sort((a, b) => {
-        return priorityOrder.indexOf(a) - priorityOrder.indexOf(b)
-      })
-      const displayCount = 3
-      const remaining = characteristics.length - displayCount
-      characteristics.splice(displayCount)
-      let str = characteristics.map((c) => this.getCharacteristicDisplayValue(c).text).join(", ")
-      if (remaining > 0) str += ` et ${remaining} autre${remaining > 1 ? "s" : ""}`
-      return str
-    },
-    getCharacteristicDisplayValue(characteristic) {
-      if (Object.prototype.hasOwnProperty.call(Constants.Characteristics, characteristic))
-        return Constants.Characteristics[characteristic]
-      return { text: "" }
     },
     getPublishedPageViewCount() {
       if (!this.isPublished) this.viewCount = null
@@ -630,12 +508,5 @@ ul li::before {
 }
 p.publication-detail > span {
   float: right;
-}
-.no-purchases {
-  background-color: #f5f5fe;
-  border: thin dashed #000091;
-}
-.v-application .rounded {
-  border-radius: 8px !important;
 }
 </style>
