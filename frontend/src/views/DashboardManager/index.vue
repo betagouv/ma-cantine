da234b384e99197c16b0b8b3ae23a782040b2713
Helen Root <helenroot1@gmail.com>
2023-10-06 12:39:04 +0200

    Team widget

--- frontend/src/views/DashboardManager/index.vue
+++ frontend/src/views/DashboardManager/index.vue
@@ -55,50 +55,8 @@
           <v-col cols="12" md="8">
             <CanteenInfoWidget :canteen="canteen" />
           </v-col>
-          <v-col cols="12" md="4" id="managers">
-            <v-card outlined class="fill-height d-flex flex-column pa-4">
-              <v-card-title class="fr-h4">
-                Mon équipe
-              </v-card-title>
-              <v-card-text class="fill-height">
-                <div v-if="managers.length > 1" class="fill-height d-flex flex-column">
-                  <p class="fr-text mb-0 grey--text text--darken-3">
-                    {{ managers.length }} personnes (dont vous) peuvent actuellement modifier cet établissement et
-                    ajouter des données.
-                  </p>
-                  <v-spacer></v-spacer>
-                  <ul class="pl-0 fr-text-xs grey--text text--darken-2 mb-n2">
-                    <li v-for="manager in managers" :key="manager.email" class="mb-4">
-                      <v-row class="align-center mx-0">
-                        <v-icon small class="mr-2">
-                          {{ manager.isInvite ? "$user-add-line" : "$user-line" }}
-                        </v-icon>
-                        {{ manager.isInvite ? manager.email : `${manager.firstName} ${manager.lastName}` }}
-                        <span v-if="manager.email === loggedUser.email" class="ml-1">(vous)</span>
-                      </v-row>
-                    </li>
-                  </ul>
-                  <v-spacer></v-spacer>
-                </div>
-                <p class="fr-text grey--text text--darken-3" v-else>
-                  Actuellement, vous êtes la seule personne qui peut modifier cet établissement et ajouter des données.
-                </p>
-              </v-card-text>
-              <v-spacer></v-spacer>
-              <v-card-actions>
-                <v-btn
-                  :to="{
-                    name: 'CanteenManagers',
-                    params: { canteenUrlComponent: this.$store.getters.getCanteenUrlComponent(canteen) },
-                  }"
-                  outlined
-                  color="primary"
-                  class="mx-2 mb-2"
-                >
-                  Modifier
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+          <v-col cols="12" md="4">
+            <TeamWidget :canteen="canteen" />
           </v-col>
         </v-row>
       </div>
@@ -147,6 +105,7 @@ import PurchasesWidget from "./PurchasesWidget"
 import PublicationWidget from "./PublicationWidget"
 import SatellitesWidget from "./SatellitesWidget"
 import CanteenInfoWidget from "./CanteenInfoWidget"
+import TeamWidget from "./TeamWidget"
 import DsfrAutocomplete from "@/components/DsfrAutocomplete"
 
 export default {
@@ -158,6 +117,7 @@ export default {
     PublicationWidget,
     SatellitesWidget,
     CanteenInfoWidget,
+    TeamWidget,
   },
   data() {
     const canteenId = +this.$route.query.cantine || this.$store.state.userCanteenPreviews[0]?.id
@@ -174,22 +134,6 @@ export default {
     },
     canteenPreviews() {
       return this.$store.state.userCanteenPreviews
-    },
-    // team widget
-    managers() {
-      if (!this.canteen) []
-      const managersCopy = [...this.canteen.managers]
-      const loggedUserIndex = managersCopy.findIndex((x) => x.email === this.$store.state.loggedUser.email)
-      managersCopy.splice(0, 0, managersCopy.splice(loggedUserIndex, 1)[0])
-      return managersCopy.concat(this.managerInvitations)
-    },
-    managerInvitations() {
-      return (
-        this.canteen?.managerInvitations.map((i) => {
-          i.isInvite = true
-          return i
-        }) || []
-      )
     },
   },
   methods: {
@@ -240,13 +184,5 @@ export default {
 <style scoped>
 .constrained {
   max-width: 1200px !important;
-}
-
-ul {
-  list-style: none;
-}
-/* https://developer.mozilla.org/en-US/docs/Web/CSS/list-style-type#accessibility_concerns */
-ul li::before {
-  content: "\200B";
 }
 </style>
