cae4cc5d3221d6880759c4277e9250d24f53d01d
Alejandro MG <alejandro@amguillen.dev>
2023-10-17 13:20:17 +0200

    Adds new DSFR Badge component

--- frontend/src/views/DashboardManager/DataInfoChip.vue
+++ frontend/src/views/DashboardManager/DataInfoChip.vue
@@ -1,20 +1,14 @@
 <template>
-  <v-chip
-    color="primary lighten-5"
-    class="blue--text text--darken-4"
-    style="border-radius: 4px !important;"
-    small
-    label
-    v-if="body"
-  >
-    <v-icon small class="mr-2">$information-fill</v-icon>
-    <p class="ma-0 pa-0">{{ body }}</p>
-  </v-chip>
+  <DsfrBadge v-if="body">
+    <p class="ma-0 pa-0 text-uppercase">{{ body }}</p>
+  </DsfrBadge>
   <span v-else></span>
 </template>
 <script>
+import DsfrBadge from "@/components/DsfrBadge"
 export default {
   name: "DataInfoChip",
+  components: { DsfrBadge },
   props: {
     currentYear: {
       default: false,
@@ -27,8 +21,8 @@ export default {
   },
   computed: {
     body() {
-      if (this.currentYear) return "ANNÉE EN COURS"
-      if (this.missingData) return "DONNÉES À COMPLETER"
+      if (this.currentYear) return "Année en cours"
+      if (this.missingData) return "Données à compléter"
       return null
     },
   },
