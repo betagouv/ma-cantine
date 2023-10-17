7ab82d0c61a99739bcef61b7520a054937b9fdfd
Alejandro MG <alejandro@amguillen.dev>
2023-10-17 12:59:16 +0200

    Repositions tags to avoid insufficient spacing

--- frontend/src/views/DashboardManager/DataInfoChip.vue
+++ frontend/src/views/DashboardManager/DataInfoChip.vue
@@ -1,17 +1,16 @@
 <template>
-  <span>
-    <v-chip
-      color="primary lighten-5"
-      class="blue--text text--darken-4"
-      style="border-radius: 4px !important;"
-      small
-      label
-      v-if="body"
-    >
-      <v-icon small class="mr-2">$information-fill</v-icon>
-      {{ body }}
-    </v-chip>
-  </span>
+  <v-chip
+    color="primary lighten-5"
+    class="blue--text text--darken-4"
+    style="border-radius: 4px !important;"
+    small
+    label
+    v-if="body"
+  >
+    <v-icon small class="mr-2">$information-fill</v-icon>
+    <p class="ma-0 pa-0">{{ body }}</p>
+  </v-chip>
+  <span v-else></span>
 </template>
 <script>
 export default {
