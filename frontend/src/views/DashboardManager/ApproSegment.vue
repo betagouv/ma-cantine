20f713b205edb13546f39219715a121e89a0dc97
Helen Root <helenroot1@gmail.com>
2023-10-18 15:44:49 +0200

    Not all satellites have central kitchen in database

--- frontend/src/views/DashboardManager/ApproSegment.vue
+++ frontend/src/views/DashboardManager/ApproSegment.vue
@@ -146,7 +146,7 @@ export default {
     },
     isCentralDiagnostic() {
       if (this.canteen.productionType !== "site_cooked_elsewhere") return false
-      return this.diagnostic && this.diagnostic.canteenId === this.canteen.centralKitchen.id
+      return this.diagnostic && this.diagnostic.canteenId === this.canteen.centralKitchen?.id
     },
     centralKitchenDisplayName() {
       if (this.canteen.centralKitchen?.name) {
