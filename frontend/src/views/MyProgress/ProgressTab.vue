025f8518f203b7ac838c66b156d9b46e3f0b8dd6
Helen Root <helenroot1@gmail.com>
2023-10-19 13:06:41 +0200

    Lightly styled structure for tab pages

--- frontend/src/views/MyProgress/ProgressTab.vue
+++ frontend/src/views/MyProgress/ProgressTab.vue
@@ -1,31 +1,74 @@
 <template>
   <div class="pa-8 pb-4">
-    <h3 class="fr-text font-weight-bold mb-4">
+    <v-row v-if="diagnostic">
+      <v-col cols="12" md="8">
+        <h3 class="fr-h6 font-weight-bold mb-0">
+          {{ keyMeasure.title }}
+        </h3>
+        <v-btn text color="primary" class="px-0" @click="() => (showIntro = !showIntro)">
+          En savoir plus
+        </v-btn>
+      </v-col>
+      <v-col class="fr-text-xs text-right">
+        <p class="mb-0">Votre niveau : EXPERT</p>
+        <p class="grey--text text--darken-2 mb-0">
+          Vous êtes au point, bravo ! Partagez vos meilleures idées pour inspirer d'autres cantines !
+        </p>
+      </v-col>
+    </v-row>
+    <h3 v-else class="fr-h6 font-weight-bold mb-4">
       {{ keyMeasure.title }}
     </h3>
-    <component
-      :is="`${keyMeasure.baseComponent}Info`"
-      :diagnostic="diagnostic"
-      :centralDiagnostic="centralDiagnostic"
-    />
-    <p><i>Sauf mention contraire, toutes les questions sont obligatoires.</i></p>
-    <div v-if="measureId !== establishmentId" class="pt-4">
+    <div v-if="!diagnostic || showIntro">
+      <component
+        :is="`${keyMeasure.baseComponent}Info`"
+        :diagnostic="diagnostic"
+        :centralDiagnostic="centralDiagnostic"
+      />
+      <p><i>Sauf mention contraire, toutes les questions sont obligatoires.</i></p>
       <v-btn
-        v-if="diagnostic && !usesOtherDiagnosticForMeasure(measureId)"
-        outlined
-        color="primary"
-        :to="{ name: 'DiagnosticModification', params: { canteenUrlComponent, year: diagnostic.year } }"
-      >
-        Modifier
-      </v-btn>
-      <v-btn
-        v-else-if="!diagnostic && !usesOtherDiagnosticForMeasure(measureId)"
+        v-if="measureId !== establishmentId && !diagnostic && !usesOtherDiagnosticForMeasure(measureId)"
         color="primary"
         :to="{ name: 'NewDiagnosticForCanteen', params: { canteenUrlComponent }, query: { année: year } }"
+        class="mt-4"
       >
         Commencer
       </v-btn>
     </div>
+    <div v-if="diagnostic">
+      <hr aria-hidden="true" role="presentation" class="mt-4 mb-8" />
+      <v-row class="mb-4">
+        <v-col class="d-flex align-end">
+          <h4 class="fr-text-sm font-weight-bold">SYNTHÈSE</h4>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn
+            v-if="!hasActiveTeledeclaration"
+            outlined
+            small
+            color="primary"
+            class="fr-btn--tertiary px-2"
+            :to="{ name: 'DiagnosticModification', params: { canteenUrlComponent, year: diagnostic.year } }"
+          >
+            <v-icon small class="mr-2">$pencil-line</v-icon>
+            Modifier mes données
+          </v-btn>
+        </v-col>
+      </v-row>
+      <component
+        :is="`${keyMeasure.baseComponent}Info`"
+        :diagnostic="diagnostic"
+        :centralDiagnostic="centralDiagnostic"
+      />
+    </div>
+    <v-row class="mt-6">
+      <v-col cols="6">
+        <router-link :to="{}">Previous tab</router-link>
+      </v-col>
+      <v-col cols="6" class="text-right">
+        <router-link :to="{}">Next tab</router-link>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -68,6 +111,7 @@ export default {
     return {
       approId: "produits-de-qualite",
       establishmentId: "etablissement",
+      showIntro: false,
     }
   },
   computed: {
@@ -79,6 +123,9 @@ export default {
     },
     canteenUrlComponent() {
       return this.$store.getters.getCanteenUrlComponent(this.canteen)
+    },
+    hasActiveTeledeclaration() {
+      return false
     },
   },
   methods: {
@@ -103,3 +150,13 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+hr {
+  border: none;
+  height: 1px;
+  /* Set the hr color */
+  color: #ddd; /* old IE */
+  background-color: #ddd; /* Modern Browsers */
+}
+</style>
