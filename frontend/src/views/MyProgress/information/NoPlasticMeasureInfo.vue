9045fd373572a9321d06b42b9c3444523c4a4b41
Helen Root <helenroot1@gmail.com>
2023-10-20 16:19:24 +0200

    Add plastic info

--- frontend/src/views/MyProgress/information/NoPlasticMeasureInfo.vue
+++ frontend/src/views/MyProgress/information/NoPlasticMeasureInfo.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    Plastic info
+  <div class="fr-text">
+    <p>
+      Votre établissement, quelque soit son secteur d’activité, est déjà soumis à un certain nombre d’obligations pour
+      réduire l'utilisation du plastique. D'autres obligations sont à venir. Pour en savoir plus,
+      <router-link :to="{ name: 'KeyMeasurePage', params: { id: 'interdiction-du-plastique' } }">
+        notre page dédiée.
+      </router-link>
+    </p>
+    <p>
+      Déclarez vos actions entreprises sur ce thème pour évaluer votre situation et bénéficiez de ressources
+      personnalisées : conseils pratiques sous forme d’articles, partenaires agréés, autres établissements dont
+      s’inspirer...
+    </p>
   </div>
 </template>
 
