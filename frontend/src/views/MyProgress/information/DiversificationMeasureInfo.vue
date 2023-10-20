20c7bf55c0604318f6d81b443b8d9cc0373b5663
Helen Root <helenroot1@gmail.com>
2023-10-20 16:10:54 +0200

    Start of info

--- frontend/src/views/MyProgress/information/DiversificationMeasureInfo.vue
+++ frontend/src/views/MyProgress/information/DiversificationMeasureInfo.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    Diversification information
+  <div class="fr-text">
+    <p>
+      Tous les restaurants collectifs scolaires (publics ou privés), de la maternelle au lycée, doivent proposer, au
+      moins une fois par semaine, un menu végétarien (
+      <a>en savoir plus sur la définition du menu végétarien</a>
+      ). Par ailleurs, le menu végétarien doit s'insérer dans un plan alimentaire respectueux des exigences relatives à
+      la qualité nutritionnelle.
+    </p>
+    <p>
+      Tous les gestionnaires de cantine servant plus de 200 couverts par jour doivent élaborer un plan pluriannuel de
+      diversification de protéines incluant des alternatives à base de protéines végétales dans les repas.
+    </p>
+    <p>
+      Depuis 2023, les restaurants collectifs de l'État, les établissements publics et les entreprises publiques
+      nationales doivent proposer une option végétarienne quotidienne, dès lors qu'ils proposent habituellement un choix
+      multiple de menus.
+    </p>
+    <p>
+      Déclarez vos actions entreprises sur ce thème pour évaluer votre situation et bénéficiez de ressources
+      personnalisées : conseils pratiques, recettes, fournisseurs agréés de votre région, autres établissements dont
+      s’inspirer....
+    </p>
   </div>
 </template>
 
