2e96e93f5d3edd81447d3ea2a7ac1e80b3f23641
Alejandro MG <alejandro@amguillen.dev>
2023-10-23 11:18:19 +0200

    Adds graph to appro-summary

--- frontend/src/views/MyProgress/summary/QualityMeasureSummary.vue
+++ frontend/src/views/MyProgress/summary/QualityMeasureSummary.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-    Appro Progress
-    <div v-if="centralDiagnostic">Vos données appros sont fournis par votre cuisine centrale</div>
-    <div v-else-if="diagnostic">Total HT : {{ diagnostic.valueTotalHt }}</div>
-    <div v-else>Pas de diagnostic</div>
+  <div class="fr-text" v-if="hasApproData">
+    <ApproGraph :diagnostic="diagnostic" :canteen="canteen" />
+    <v-row>
+      <v-col cols="12" md="6">
+        <p>Détail du calcul de mes taux EGAlim</p>
+        TODO
+      </v-col>
+      <v-col cols="12" md="6">
+        <p>Par famille de produits</p>
+        TODO
+      </v-col>
+    </v-row>
+    <hr />
+    <div>
+      <p>Données détaillées</p>
+      TODO only if simplified TODO button modifier
+    </div>
+  </div>
+  <div class="fr-text" v-else>
+    TODO
   </div>
 </template>
 
 <script>
+import ApproGraph from "@/views/DashboardManager/ApproGraph"
+import { hasDiagnosticApproData } from "@/utils"
+
 export default {
   name: "QualityMeasureSummary",
-  props: ["diagnostic", "centralDiagnostic"],
+  components: { ApproGraph },
+  props: {
+    diagnostic: {},
+    centralDiagnostic: {},
+    canteen: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    usesCentralDiagnostic() {
+      return this.centralDiagnostic?.centralKitchenDiagnosticMode === "ALL"
+    },
+    displayDiagnostic() {
+      return this.usesCentralDiagnostic ? this.centralDiagnostic : this.diagnostic
+    },
+    hasApproData() {
+      return hasDiagnosticApproData(this.displayDiagnostic)
+    },
+  },
 }
 </script>
