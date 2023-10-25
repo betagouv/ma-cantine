2cafa0310bfffd4f54a94ded15729c5f13d9f2f8
Helen Root <helenroot1@gmail.com>
2023-10-25 17:06:52 +0200

    Basics of EGAlim percentages table

--- frontend/src/views/MyProgress/summary/QualityMeasureSummary.vue
+++ frontend/src/views/MyProgress/summary/QualityMeasureSummary.vue
@@ -3,11 +3,44 @@
     <ApproGraph :diagnostic="diagnostic" :canteen="canteen" />
     <v-row>
       <v-col cols="12" md="6">
-        <p>Détail du calcul de mes taux EGAlim</p>
-        TODO
+        <p class="font-weight-bold">Détail du calcul de mes taux EGAlim</p>
+        <v-row>
+          <v-col cols="6">
+            <p>Produits bio</p>
+          </v-col>
+          <v-col cols="2">
+            <p class="font-weight-bold">{{ bioPercentage }} %</p>
+          </v-col>
+          <v-col cols="4">
+            <p>
+              <i>objectif : {{ applicableRules.bioThreshold }} %</i>
+            </p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6">
+            <p>Produits durables et de qualité (hors bio)</p>
+          </v-col>
+          <v-col cols="2">
+            <p class="font-weight-bold">{{ sustainablePercentage }} %</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6">
+            <p>Produits EGAlim</p>
+          </v-col>
+          <v-col cols="2">
+            <p class="font-weight-bold">{{ egalimPercentage }} %</p>
+          </v-col>
+          <v-col cols="4">
+            <p>
+              <i>objectif : {{ applicableRules.qualityThreshold }} %</i>
+            </p>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="12" md="6">
-        <p>Par famille de produits</p>
+        <p class="font-weight-bold">Par famille de produits</p>
         TODO
       </v-col>
     </v-row>
@@ -24,7 +57,7 @@
 
 <script>
 import ApproGraph from "@/components/ApproGraph"
-import { hasDiagnosticApproData } from "@/utils"
+import { hasDiagnosticApproData, applicableDiagnosticRules, getSustainableTotal, getPercentage } from "@/utils"
 
 export default {
   name: "QualityMeasureSummary",
@@ -46,6 +79,24 @@ export default {
     },
     hasApproData() {
       return hasDiagnosticApproData(this.displayDiagnostic)
+    },
+    applicableRules() {
+      return applicableDiagnosticRules(this.canteen)
+    },
+    bioPercentage() {
+      return (
+        Math.round(this.diagnostic.percentageValueBioHt * 100) ||
+        getPercentage(this.diagnostic.valueBioHt, this.diagnostic.valueTotalHt, true)
+      )
+    },
+    sustainablePercentage() {
+      return (
+        Math.round(this.diagnostic.percentageValueSustainableHt * 100) ||
+        getPercentage(getSustainableTotal(this.diagnostic), this.diagnostic.valueTotalHt, true)
+      )
+    },
+    egalimPercentage() {
+      return this.bioPercentage + this.sustainablePercentage
     },
   },
 }
