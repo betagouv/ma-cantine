ececab8e93d8cf6d46f82244fab16ffc65a3637f
Helen Root <helenroot1@gmail.com>
2023-11-03 14:57:01 +0100

    Refactor to use an intermediary step-managing component for each measure

---
+++ frontend/src/views/DiagnosticTunnel/QualitySteps/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <component :is="step.componentName" :canteen="canteen" :diagnostic="diagnostic" @updatePayload="updatePayload" />
+</template>
+
+<script>
+import QualityMeasureSummary from "@/components/DiagnosticSummary/QualityMeasureSummary"
+import QualityTotalStep from "./QualityTotalStep"
+
+export default {
+  name: "QualitySteps",
+  props: {
+    canteen: {
+      type: Object,
+      required: true,
+    },
+    diagnostic: {
+      type: Object,
+      required: true,
+    },
+    stepUrlSlug: {
+      type: String,
+    },
+  },
+  components: {
+    QualityTotalStep,
+    QualityMeasureSummary,
+  },
+  data() {
+    return {
+      steps: [
+        {
+          title: "Valeurs totales des achats alimentaires",
+          componentName: "QualityTotalStep",
+          urlSlug: "total",
+        },
+        {
+          title: "Synthèse",
+          isSynthesis: true,
+          componentName: "QualityMeasureSummary",
+          urlSlug: "synthèse",
+        },
+      ],
+    }
+  },
+  computed: {
+    step() {
+      const step = this.stepUrlSlug && this.steps.find((step) => step.urlSlug === this.stepUrlSlug)
+      return step || this.steps[0]
+    },
+  },
+  methods: {
+    updatePayload(data) {
+      console.log(data)
+      this.$emit("updatePayload", data)
+    },
+  },
+  mounted() {
+    this.$emit("updateSteps", this.steps)
+  },
+}
+</script>
+
+<style></style>
--- frontend/src/views/DiagnosticTunnel/index.vue
+++ frontend/src/views/DiagnosticTunnel/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="step" class="text-left">
+  <div class="text-left">
     <v-row class="header">
       <v-row class="mx-auto constrained pt-6">
         <v-col cols="9">
@@ -16,7 +16,7 @@
             </router-link>
           </p>
         </v-col>
-        <v-col v-if="!step.isSynthesis" cols="12">
+        <v-col v-if="step && !step.isSynthesis" cols="12">
           <p class="fr-text-sm">Étape {{ stepIdx + 1 }} sur {{ measure.stepTotal }}</p>
           <h1 class="fr-h6">{{ step.title }}</h1>
           <!-- TODO: DSFR stepper component which will include everything else in this column as well -->
@@ -30,8 +30,14 @@
     <div v-if="diagnostic" class="mx-auto constrained pa-10">
       <!-- TODO: padding/centering and sorting out scrolling -->
       <!-- TODO: question OR synthesis (move existing syntheses to /components/ to reuse) -->
-      <!-- TODO: make 6 components for each measure which then handles the question stepping -->
-      <component :is="step.componentName" :canteen="canteen" :diagnostic="diagnostic" @updatePayload="updatePayload" />
+      <component
+        :is="measure.componentName"
+        :canteen="canteen"
+        :diagnostic="diagnostic"
+        :stepUrlSlug="stepUrlSlug"
+        @updatePayload="updatePayload"
+        @updateSteps="updateSteps"
+      />
     </div>
     <v-row class="footer">
       <v-row class="mx-auto constrained">
@@ -44,7 +50,7 @@
         <v-spacer />
         <v-col>
           <v-row class="py-10 align-center justify-end">
-            <p v-if="step.isSynthesis" class="mb-0"><router-link :to="firstStep">Modifier</router-link></p>
+            <p v-if="step && step.isSynthesis" class="mb-0"><router-link :to="firstStep">Modifier</router-link></p>
             <p v-else class="mb-0">
               <!-- TODO: handle previousStep === null properly -->
               <router-link :to="previousStep ? previousStep.to : {}">
@@ -63,24 +69,7 @@
 </template>
 
 <script>
-import QualityTotalStep from "./quality/QualityTotalStep"
-import QualityMeasureSummary from "@/components/DiagnosticSummary/QualityMeasureSummary"
-
-const stepsByMeasure = {
-  "qualite-des-produits": [
-    {
-      title: "Valeurs totales des achats alimentaires",
-      componentName: "QualityTotalStep",
-      urlSlug: "total",
-    },
-    {
-      title: "Synthèse",
-      isSynthesis: true,
-      componentName: "QualityMeasureSummary",
-      urlSlug: "synthèse",
-    },
-  ],
-}
+import QualitySteps from "./QualitySteps"
 
 export default {
   name: "DiagnosticTunnel",
@@ -97,14 +86,14 @@ export default {
       required: true,
     },
   },
-  components: { QualityTotalStep, QualityMeasureSummary },
+  components: { QualitySteps },
   data() {
     return {
       formIsValid: false,
       canteen: null,
       diagnostic: null,
       payload: {},
-      steps: stepsByMeasure[this.measureId],
+      steps: [],
     }
   },
   computed: {
@@ -116,6 +105,7 @@ export default {
         title: "Qualité des approvisionnements",
         stepTotal: 6,
         id: "qualite-des-produits",
+        componentName: "QualitySteps",
       }
     },
     stepUrlSlug() {
@@ -140,9 +130,9 @@ export default {
       return this.stepIdx > 0 ? this.steps[this.stepIdx - 1] : null
     },
     continueActionText() {
-      const returnToTable = this.step.isFinal
+      const returnToTable = this.step?.isFinal
       if (returnToTable) return "Retour au tableau de bord"
-      const nextIsNewMeasure = this.step.isSynthesis
+      const nextIsNewMeasure = this.step?.isSynthesis
       if (nextIsNewMeasure) return "Passer à l'onglet suivant"
       const nextIsSynthesis = this.nextStep?.isSynthesis
       if (nextIsSynthesis) return "Voir la synthèse"
@@ -166,6 +156,9 @@ export default {
     },
   },
   methods: {
+    updateSteps(steps) {
+      this.steps = steps
+    },
     updatePayload({ payload, formIsValid }) {
       this.payload = payload
       this.formIsValid = formIsValid
@@ -188,11 +181,16 @@ export default {
     saveDiagnostic() {
       if (!this.canteen || !this.diagnostic) return Promise.reject()
       this.updateProgress()
-      return this.$store.dispatch("updateDiagnostic", {
-        canteenId: this.canteen.id,
-        id: this.diagnostic.id,
-        payload: this.payload,
-      })
+      return this.$store
+        .dispatch("updateDiagnostic", {
+          canteenId: this.canteen.id,
+          id: this.diagnostic.id,
+          payload: this.payload,
+        })
+        .then(() => {
+          // if the save is successful, make sure we are showing the up to date data
+          Object.assign(this.diagnostic, this.payload)
+        })
       // TODO: error handling
     },
     continueAction() {
