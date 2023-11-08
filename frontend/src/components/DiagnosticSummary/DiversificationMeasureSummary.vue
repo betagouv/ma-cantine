cb98139851134e154aa13d5ef3229649c4805691
Helen Root <helenroot1@gmail.com>
2023-11-08 14:16:28 +0100

    Remove the check in the summary for the XP vege

--- frontend/src/components/DiagnosticSummary/DiversificationMeasureSummary.vue
+++ frontend/src/components/DiagnosticSummary/DiversificationMeasureSummary.vue
@@ -66,19 +66,6 @@
           de protéines végétales
         </div>
       </li>
-
-      <li v-if="canteen.vegetarianExpeParticipant">
-        <v-icon color="primary" class="mr-2">$check-line</v-icon>
-        <div>
-          Je suis volontaire pour l’expérimentation de l’option végétarienne quotidienne
-        </div>
-      </li>
-      <li v-else>
-        <v-icon color="primary" class="mr-2">$close-line</v-icon>
-        <div>
-          Je ne suis pas volontaire pour l’expérimentation de l’option végétarienne quotidienne
-        </div>
-      </li>
     </ul>
   </div>
 </template>
