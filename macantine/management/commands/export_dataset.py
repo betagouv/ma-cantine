dab175977d49a8430d4094d9ad79f8d8f6a65ebb
qloridant <quentin.loridant@protonmail.com>
2023-11-24 17:11:59 +0100

    Refacto- Remove useless logger setting

--- macantine/management/commands/export_dataset.py
+++ macantine/management/commands/export_dataset.py
@@ -2,8 +2,6 @@ import logging
 from django.core.management.base import BaseCommand
 from macantine.tasks import export_datasets
 
-logger = logging.getLogger(__name__)
-
 
 class Command(BaseCommand):
     help = "Export datasets TD and canteens to the 'media/open_data' folder of the configured file system (local or s3)"
