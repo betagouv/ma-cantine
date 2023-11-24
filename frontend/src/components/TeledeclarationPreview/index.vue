5f6cb7f7df6b5d268f9bebfd3fa4e0eafe8a45a0
Alejandro MG <alejandro@amguillen.dev>
2023-11-24 09:49:18 +0100

    Takes into account window.ENABLE_DASHBOARD for teledeclaration modal modification link

--- frontend/src/components/TeledeclarationPreview/index.vue
+++ frontend/src/components/TeledeclarationPreview/index.vue
@@ -190,7 +190,7 @@ export default {
       this.$emit("input", false)
       this.$router
         .push({
-          name: "MyProgress",
+          name: window.ENABLE_DASHBOARD ? "MyProgress" : "DiagnosticModification",
           params: { canteenUrlComponent: this.canteenUrlComponent, year: this.diagnosticForTD.year },
         })
         .catch(() => {})
