72ff48ae067a2cf5dc357fd93f56901e08dd9963
qloridant <quentin.loridant@protonmail.com>
2023-11-28 17:25:16 +0100

    Fix - Adapt test to sector filtering method

--- data/tests/test_extraction_open_data.py
+++ data/tests/test_extraction_open_data.py
@@ -41,7 +41,7 @@ class TestExtractionOpenData(TestCase):
         Teledeclaration.create_from_diagnostic(diagnostic_2022, applicant)
         etl_td.extract_dataset()
         self.assertEqual(
-            etl_td.get_dataset().iloc[0]["canteen_sectors"], list(), "The sectors should be an empty list"
+            etl_td.get_dataset().iloc[0]["canteen_sectors"], [], "The sectors should be an empty list"
         )
 
         canteen = CanteenFactory.create()
@@ -51,8 +51,6 @@ class TestExtractionOpenData(TestCase):
         self.assertGreater(
             etl_td.get_dataset().iloc[0]["teledeclaration_ratio_bio"], 0, "The bio value is aggregated from bio fields and should be greater than 0"
         )
-
-
 
     def test_extraction_canteen(self):
         schema = json.load(open("data/schemas/schema_cantine.json"))
@@ -122,7 +120,7 @@ class TestExtractionOpenData(TestCase):
         canteens = etl_canteen.get_dataset()
         self.assertEqual(etl_canteen.len_dataset(), 0, "There should be one canteen less after hard deletion")
 
-        CanteenFactory.create(sectors=[SectorFactory.create(name="Restaurants des armées/police/gendarmerie")])
+        CanteenFactory.create(sectors=[SectorFactory.create(id=22)])
         etl_canteen.extract_dataset()
         canteens = etl_canteen.get_dataset()
         self.assertEqual(
