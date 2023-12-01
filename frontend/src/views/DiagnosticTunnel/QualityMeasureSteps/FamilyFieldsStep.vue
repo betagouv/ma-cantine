d0e4df174855795dd63658754e7a5e5a2b4b2a65
Helen Root <helenroot1@gmail.com>
2023-12-01 11:33:24 +0100

    Light error handling for errors on detailed input

--- frontend/src/views/DiagnosticTunnel/QualityMeasureSteps/FamilyFieldsStep.vue
+++ frontend/src/views/DiagnosticTunnel/QualityMeasureSteps/FamilyFieldsStep.vue
@@ -16,6 +16,7 @@
     <p v-if="characteristicId === 'LOCAL'">
       Suivant votre propre définition de « local ».
     </p>
+    <FormErrorCallout v-if="hasError" :errorMessages="errorMessages" />
     <v-row>
       <v-col v-for="(family, fId) in families" :key="fId" cols="12" md="6" class="py-2">
         <label :for="fId" class="fr-text">
@@ -31,14 +32,16 @@
           ]"
           solo
           v-model.number="payload[diagnosticKey(fId)]"
-          @blur="populateSimplifiedDiagnostic"
+          @blur="fieldUpdate(diagnosticKey(fId))"
           class="mt-2"
+          :error="fieldHasError(diagnosticKey(fId))"
         />
         <PurchaseHint
           v-if="displayPurchaseHints"
           v-model="payload[diagnosticKey(fId)]"
           :purchaseType="family.shortText + ' pour ce caractéristique'"
           :amount="purchasesSummary[diagnosticKey(fId)]"
+          @autofill="fieldUpdate(diagnosticKey(fId))"
         />
       </v-col>
     </v-row>
@@ -48,9 +51,10 @@
 <script>
 import DsfrCurrencyField from "@/components/DsfrCurrencyField"
 import PurchaseHint from "@/components/KeyMeasureDiagnostic/PurchaseHint"
+import FormErrorCallout from "@/components/FormErrorCallout"
 import Constants from "@/constants"
 import validators from "@/validators"
-import { approTotals } from "@/utils"
+import { approTotals, toCurrency, getCharacteristicFromField } from "@/utils"
 
 export default {
   name: "FamilyFieldsStep",
@@ -78,16 +82,28 @@ export default {
   components: {
     DsfrCurrencyField,
     PurchaseHint,
+    FormErrorCallout,
   },
   data() {
     return {
       families: Constants.ProductFamilies,
       validators,
+      fieldTotalErrorMessage: false,
+      outsideLawErrorMessages: [],
+      erroringFieldName: null,
+      lastUpdatedFieldName: null,
+      errorOnLoad: false,
     }
   },
   computed: {
     displayPurchaseHints() {
       return !!this.purchasesSummary
+    },
+    hasError() {
+      return !!this.fieldTotalErrorMessage || !!this.outsideLawErrorMessages.length
+    },
+    errorMessages() {
+      return [this.fieldTotalErrorMessage, ...this.outsideLawErrorMessages].filter((x) => !!x)
     },
   },
   methods: {
@@ -103,6 +119,7 @@ export default {
       return string
     },
     populateSimplifiedDiagnostic() {
+      if (this.hasError) return
       const { bioTotal, siqoTotal, perfExtTotal, egalimOthersTotal } = approTotals(this.payload)
       this.payload.valueBioHt = bioTotal
       this.payload.valueSustainableHt = siqoTotal
@@ -157,6 +174,75 @@ export default {
       })
       fishEgalim = +fishEgalim.toFixed(2)
       return { fishEgalim }
+    },
+    checkTotal() {
+      this.erroringFieldName = null
+      this.fieldTotalErrorMessage = null
+      this.outsideLawErrorMessages = []
+
+      const groups = Constants.TeledeclarationCharacteristicGroups
+      const sumFields = groups.egalim.fields.concat(groups.nonEgalim.fields)
+      const declaredTotal = +this.payload.valueTotalHt
+      const fieldTotal = this.sum(sumFields)
+      if (fieldTotal > declaredTotal) {
+        this.fieldTotalErrorMessage = `Le total de tous vos achats ${toCurrency(
+          fieldTotal
+        )} doit être inferieur du total saisi ${toCurrency(declaredTotal)}`
+        this.erroringFieldName = this.lastUpdatedFieldName
+        return
+      }
+      const outsideLaw = {
+        FRANCE: "France",
+        SHORT_DISTRIBUTION: "ShortDistribution",
+        LOCAL: "Local",
+      }
+      if (outsideLaw[this.characteristicId]) {
+        // in the outsideLaw group, a product can be counted in more than one category, so we check if any of the
+        // totals for each of the three categories is greater than the main total
+        this.checkTotalForCategory(groups.outsideLaw, outsideLaw[this.characteristicId])
+      }
+
+      if (!this.hasError) {
+        this.errorOnLoad = false
+      }
+    },
+    checkTotalForCategory(group, fieldSuffix) {
+      const fields = group.fields.filter((field) => field.endsWith(fieldSuffix))
+      const total = this.sum(fields)
+      if (total > this.payload.valueTotalHt) {
+        const char = getCharacteristicFromField("_" + fieldSuffix, "_", group)
+        const message = `Le total de vos achats « ${char.text} », ${toCurrency(
+          total
+        )}, doit être inferieur du total saisi ${toCurrency(this.payload.valueTotalHt)}`
+        this.outsideLawErrorMessages.push(message)
+        this.erroringFieldName = this.lastUpdatedFieldName
+      }
+    },
+    fieldUpdate(fieldName) {
+      if (this.diagnostic[fieldName] === this.payload[fieldName]) return
+      this.lastUpdatedFieldName = fieldName
+      this.checkTotal()
+      this.populateSimplifiedDiagnostic()
+    },
+    sum(fields) {
+      return fields.reduce((acc, field) => acc + (this.payload[field] || 0), 0)
+    },
+    checkTotalPageLoad() {
+      this.checkTotal()
+      if (this.hasError) {
+        this.errorOnLoad = true
+      }
+    },
+    fieldHasError(fieldName) {
+      return (this.errorOnLoad && !!this.payload[fieldName]) || fieldName === this.erroringFieldName
+    },
+  },
+  mounted() {
+    this.checkTotalPageLoad()
+  },
+  watch: {
+    $route() {
+      this.checkTotalPageLoad()
     },
   },
 }
