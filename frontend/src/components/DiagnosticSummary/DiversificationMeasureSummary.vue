17129e4c424d1398542a4e6e5188478adc6d1f0a
Helen Root <helenroot1@gmail.com>
2024-01-22 12:31:24 +0100

    Synthesis vege

--- frontend/src/components/DiagnosticSummary/DiversificationMeasureSummary.vue
+++ frontend/src/components/DiagnosticSummary/DiversificationMeasureSummary.vue
@@ -15,7 +15,7 @@
         </div>
       </li>
       <li v-else>
-        <v-icon color="primary" class="mr-2">$close-line</v-icon>
+        <v-icon color="primary" class="mr-2">$question-line</v-icon>
         <div>
           Je n'ai pas renseigné la périodicité du menu végétarien dans ma cantine
         </div>
@@ -30,7 +30,7 @@
           </div>
         </li>
         <li v-else>
-          <v-icon color="primary" class="mr-2">$close-line</v-icon>
+          <v-icon color="primary" class="mr-2">$question-line</v-icon>
           <div>
             Je n'ai pas renseigné le type de menu végétarien servi dans ma cantine
           </div>
@@ -50,31 +50,40 @@
           </div>
         </li>
         <li v-else>
-          <v-icon color="primary" class="mr-2">$close-line</v-icon>
+          <v-icon color="primary" class="mr-2">$question-line</v-icon>
           <div>
             Je n'ai pas renseigné les bases utilisées pour mon menu végétarien
           </div>
         </li>
       </div>
 
-      <li v-if="displayDiversificationPlanSegment && diagnostic.hasDiversificationPlan">
-        <v-icon color="primary" class="mr-2">$check-line</v-icon>
-        <div>
-          J'ai mis en place un plan pluriannuel de diversification des protéines incluant des alternatives à base de
-          protéines végétales
-          <ul role="list" class="mt-2" v-if="appliedDiversificationActions && appliedDiversificationActions.length">
-            <li class="fr-text-xs mb-1" v-for="action in appliedDiversificationActions" :key="action">
-              {{ action }}
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li v-else-if="displayDiversificationPlanSegment">
-        <v-icon color="primary" class="mr-2">$close-line</v-icon>
-        <div>
-          Je n'ai pas mis en place un plan pluriannuel de diversification des protéines incluant des alternatives à base
-          de protéines végétales
-        </div>
+      <li v-if="displayDiversificationPlanSegment">
+        <span v-if="diagnostic.hasDiversificationPlan">
+          <v-icon color="primary" class="mr-1">$check-line</v-icon>
+          <span>
+            J'ai mis en place un plan pluriannuel de diversification des protéines incluant des alternatives à base de
+            protéines végétales
+            <ul role="list" class="mt-2" v-if="appliedDiversificationActions && appliedDiversificationActions.length">
+              <li class="fr-text-xs mb-1" v-for="action in appliedDiversificationActions" :key="action">
+                {{ action }}
+              </li>
+            </ul>
+          </span>
+        </span>
+        <span v-else-if="diagnostic.hasDiversificationPlan === false">
+          <v-icon color="primary" class="mr-1">$close-line</v-icon>
+          <span>
+            Je n'ai pas mis en place un plan pluriannuel de diversification des protéines incluant des alternatives à
+            base de protéines végétales
+          </span>
+        </span>
+        <span v-else>
+          <v-icon color="primary" class="mr-1">$question-line</v-icon>
+          <span>
+            Est-ce que vous avez mis en place un plan pluriannuel de diversification des protéines incluant des
+            alternatives à base de protéines végétales ?
+          </span>
+        </span>
       </li>
     </ul>
   </div>
