04fea22a11de04ecbfbc7cb87cfbecd36f5aa903
Alejandro MG <alejandro@amguillen.dev>
2024-01-23 09:12:11 +0100

    Goes directly to the synthesis when using purchase fill

--- frontend/src/views/DiagnosticTunnel/index.vue
+++ frontend/src/views/DiagnosticTunnel/index.vue
@@ -23,7 +23,7 @@
               class="text-decoration-underline px-0"
               color="primary"
               @click="step.isSynthesis ? quit() : saveAndQuit()"
-              :disabled="!formIsValid"
+              :disabled="!formIsValid && !isSynthesis"
             >
               {{ step.isSynthesis ? "Quitter" : "Sauvegarder et quitter" }}
               <v-icon color="primary" size="1rem" class="ml-0 mb-1">
@@ -79,7 +79,12 @@
       </div>
       <v-spacer v-if="$vuetify.breakpoint.smAndUp" />
       <div class="d-block d-sm-flex align-center flex-row-reverse">
-        <v-btn :disabled="!formIsValid" @click="continueAction" color="primary" class="ml-0 ml-sm-4 mb-4 mb-sm-0">
+        <v-btn
+          :disabled="!formIsValid && !isSynthesis"
+          @click="continueAction"
+          color="primary"
+          class="ml-0 ml-sm-4 mb-4 mb-sm-0"
+        >
           {{ continueActionText }}
         </v-btn>
         <p v-if="step && step.isSynthesis" class="mb-0"><router-link :to="firstStepLink">Modifier</router-link></p>
@@ -363,7 +368,10 @@ export default {
     },
     onFullTunnelAutofill(e) {
       this.updatePayload({ payload: e.payload, formIsValid: true })
-      this.saveAndQuit()
+      const synthesisStep = this.steps.find((x) => x.isSynthesis)
+      return this.saveDiagnostic()
+        .then(this.$nextTick)
+        .then(() => this.$router.push({ query: { étape: synthesisStep.urlSlug } }))
     },
     stepLink(step) {
       return { query: { étape: step.urlSlug } }
