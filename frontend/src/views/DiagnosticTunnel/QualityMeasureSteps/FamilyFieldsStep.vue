834b5eb694b515f6d00ca1e566d8d8bd296cb783
Helen Root <helenroot1@gmail.com>
2024-02-06 16:10:35 +0100

    Fix small bug

--- frontend/src/views/DiagnosticTunnel/QualityMeasureSteps/FamilyFieldsStep.vue
+++ frontend/src/views/DiagnosticTunnel/QualityMeasureSteps/FamilyFieldsStep.vue
@@ -155,7 +155,7 @@ export default {
       ].filter((x) => !!x)
     },
     possibleFamilies() {
-      const exceptions = Constants.CharacteristicFamilyExceptions[this.characteristicId]
+      const exceptions = Constants.CharacteristicFamilyExceptions[this.characteristicId] || []
       return Object.keys(this.families).filter((id) => exceptions.indexOf(id) === -1)
     },
   },
