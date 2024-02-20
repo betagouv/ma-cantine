6d255726593ec543821a103b68c15930833963c1
Helen Root <helenroot1@gmail.com>
2024-02-20 20:44:54 +0100

    Replace the term diagnostic with bilan

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -426,15 +426,15 @@ export default Object.freeze({
     {
       key: "NONE",
       urlSlug: "cantines-seules",
-      title: "Importer des cantines sans diagnostic",
-      label: "Sans diagnostic",
+      title: "Importer des cantines sans bilan",
+      label: "Sans bilan",
       help: "Vous voulez importer des cantines sans données d'approvisionnement",
       icon: "$community-fill",
     },
     {
       key: "SIMPLE",
       urlSlug: "cantines-et-diagnostics-simples",
-      title: "Importer des cantines et diagnostics simples",
+      title: "Importer des cantines et bilans simples",
       label: "Diagnostic simple",
       help: "Vous connaissez les valeurs totales de vos achats bio et de qualité",
       icon: "$bar-chart-box-fill",
@@ -442,7 +442,7 @@ export default Object.freeze({
     {
       key: "COMPLETE",
       urlSlug: "cantines-et-diagnostics-complets",
-      title: "Importer des cantines et diagnostics complets",
+      title: "Importer des cantines et bilans complets",
       label: "Diagnostic complet",
       help: "Vous connaissez les labels et les familles de produits de vos achats",
       icon: "$checkbox-circle-fill",
@@ -452,10 +452,10 @@ export default Object.freeze({
     {
       key: "CC_SIMPLE",
       urlSlug: "cuisine-centrale-diagnostics-simples",
-      title: "Mettre à jour vos satellites et renseigner des diagnostics simples",
+      title: "Mettre à jour vos satellites et renseigner des bilans simples",
       description:
         "Ce type de fichier vous permet de renseigner vos données d'approvisionnement simplifiées au niveau de la cuisine centrale et de lister vos cantines satellites seulement avec leurs données d'établissement. Les données d'approvisionnement restent donc seulement nécessaires pour les cuisines centrales.",
-      label: "Cuisine centrale avec diagnostic simple",
+      label: "Cuisine centrale avec bilan simple",
       help:
         "Vous voulez renseigner vos satellites et vous connaissez les valeurs totaux, bio, et de qualité et durable",
       icon: "$bar-chart-box-fill",
@@ -463,10 +463,10 @@ export default Object.freeze({
     {
       key: "CC_COMPLETE",
       urlSlug: "cuisine-centrale-diagnostics-complets",
-      title: "Mettre à jour vos satellites et renseigner des diagnostics complets",
+      title: "Mettre à jour vos satellites et renseigner des bilans complets",
       description:
         "Ce type de fichier vous permet de renseigner vos données d'approvisionnement complètes au niveau de la cuisine centrale et de lister vos cantines satellites seulement avec leurs données d'établissement. Les données d'approvisionnement restent donc seulement nécessaires pour les cuisines centrales.",
-      label: "Cuisine centrale avec diagnostic complet",
+      label: "Cuisine centrale avec bilan complet",
       help:
         "Vous voulez renseigner vos satellites et vous connaissez les labels et les familles de produits de vos achats",
       icon: "$checkbox-circle-fill",
