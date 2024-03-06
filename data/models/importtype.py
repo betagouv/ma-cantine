6e368f0ef7f5eddd6a0cf79936d17118279a8e52
Alejandro MG <alejandro@amguillen.dev>
2024-03-06 10:09:34 +0100

    Adds french name to importType choices

--- data/models/importtype.py
+++ data/models/importtype.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class ImportType(models.TextChoices):
-    CANTEEN_ONLY_OR_DIAGNOSTIC_SIMPLE = "CANTEEN_ONLY_OR_DIAGNOSTIC_SIMPLE", "Canteen only or simple diagnostic"
-    DIAGNOSTIC_COMPLETE = "DIAGNOSTIC_COMPLETE", "Diagnostic complete"
-    CC_SIMPLE = "CC_SIMPLE", "Central kitchen simple"
-    CC_COMPLETE = "CC_COMPLETE", "Central kitchen complete"
-    PURCHASE = "PURCHASE", "Purchase"
+    CANTEEN_ONLY_OR_DIAGNOSTIC_SIMPLE = (
+        "CANTEEN_ONLY_OR_DIAGNOSTIC_SIMPLE",
+        "Cantines seules ou avec un diagnostic simple",
+    )
+    DIAGNOSTIC_COMPLETE = "DIAGNOSTIC_COMPLETE", "Cantines avec diagnostic complet"
+    CC_SIMPLE = "CC_SIMPLE", "Cuisines centrales avec un diagnostic simple et leurs satellites"
+    CC_COMPLETE = "CC_COMPLETE", "Cuisines centrales avec un diagnostic complet et leurs satellites"
+    PURCHASE = "PURCHASE", "Achats"
