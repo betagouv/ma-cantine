284bef7dbb1f4bd1d39667ca6dc276c66cba6873
Helen Root <helenroot1@gmail.com>
2024-03-11 19:22:37 +0100

    Refactor FAQ to use new component

--- frontend/src/components/DsfrAccordion.vue
+++ frontend/src/components/DsfrAccordion.vue
@@ -1,15 +1,14 @@
 <template>
-  <v-expansion-panels hover accordion tile flat>
+  <v-expansion-panels hover accordion tile flat class="dsfr-accordion">
     <v-expansion-panel v-for="item in items" :key="item.title">
       <v-expansion-panel-header class="px-3 fr-accordion__btn" v-slot="{ open }">
         <component :is="item.titleLevel || 'h3'" class="fr-text" :class="open && 'active-panel'">
           {{ item.title }}
         </component>
       </v-expansion-panel-header>
-      <v-expansion-panel-content class="my-2">
-        <component :is="item.contentComponent" v-if="item.contentComponent" />
-        <p v-else-if="item.content" class="mb-0">{{ item.content }}</p>
-        <slot v-else />
+      <v-expansion-panel-content class="px-3 pt-4 pb-8">
+        <p v-if="item.content" class="mb-0">{{ item.content }}</p>
+        <slot v-else v-bind:item="item" />
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
@@ -20,31 +19,34 @@ export default {
   name: "DsfrAccordion",
   props: {
     // items is an array of objects containing 'title'
-    // and one of 'contentComponent', 'content', or falls back to a slot
+    // and one of 'content' or provide a template using the slot
     items: Array,
   },
 }
 </script>
 
 <style>
-.v-expansion-panel {
+.dsfr-accordion .v-expansion-panel {
   box-shadow: inset 0 1px 0 0 #ddd, 0 1px 0 0 #ddd;
 }
-.v-expansion-panel-header {
+.dsfr-accordion .v-expansion-panel-content__wrap {
+  padding: 0;
+}
+.dsfr-accordion .v-expansion-panel-header {
   color: rgb(0, 0, 145);
 }
-.v-expansion-panel-header__icon > i {
+.dsfr-accordion .v-expansion-panel-header i {
   color: rgb(0, 0, 145) !important;
 }
-.v-expansion-panel--active > .v-expansion-panel-header {
+.dsfr-accordion .v-expansion-panel--active > .v-expansion-panel-header {
   min-height: unset;
 }
-.fr-accordion__btn[aria-expanded="true"] {
+.dsfr-accordion .fr-accordion__btn[aria-expanded="true"] {
   --background-open-blue-france: rgb(227, 227, 253);
   background-color: #e3e3fd;
   background-color: var(--background-open-blue-france);
 }
-.fr-accordion__btn[aria-expanded="true"]:hover {
+.dsfr-accordion .fr-accordion__btn[aria-expanded="true"]:hover {
   --background-open-blue-france-hover: rgb(193, 193, 251);
   background-color: var(--background-open-blue-france-hover);
 }
