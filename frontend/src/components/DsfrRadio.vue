ae3738350db41b892e257dbbd69c5190df1d3c70
Helen Root <helenroot1@gmail.com>
2024-03-18 17:21:40 +0100

    Use DsfrRadio for diversification tunnel; add more config options

--- frontend/src/components/DsfrRadio.vue
+++ frontend/src/components/DsfrRadio.vue
@@ -4,8 +4,20 @@
       <span :class="labelClasses">
         {{ $attrs.label }}
       </span>
+      <span class="fr-hint-text my-2" v-if="optional">Optionnel</span>
     </template>
-    <v-radio v-for="item in $attrs.items" :key="item.value" :label="item.text" :value="item.value"></v-radio>
+    <v-row v-if="optionsRow">
+      <v-col v-for="item in items" :key="item.value">
+        <v-radio :label="item.label || item.text" :value="item.value"></v-radio>
+      </v-col>
+    </v-row>
+    <v-radio
+      v-else
+      v-for="item in items"
+      :key="item.value"
+      :label="item.label || item.text"
+      :value="item.value"
+    ></v-radio>
 
     <!-- For RGAA 8.9 error messages should also be in p tags, by default in vuetify 2 they're in divs -->
     <template v-slot:message="{ key, message }">
@@ -23,6 +35,18 @@ export default {
       required: false,
       default: "mb-2 text-sm-subtitle-1 text-body-2 text-left grey--text text--darken-4",
     },
+    optional: {
+      type: Boolean,
+      default: false,
+    },
+    yesNo: {
+      type: Boolean,
+      default: false,
+    },
+    optionsRow: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return { inputId: null }
@@ -30,6 +54,15 @@ export default {
   computed: {
     value() {
       return this.$refs["radio"].value
+    },
+    items() {
+      if (this.yesNo) {
+        return [
+          { label: "Oui", value: true },
+          { label: "Non", value: false },
+        ]
+      }
+      return this.$attrs["items"]
     },
   },
   methods: {
