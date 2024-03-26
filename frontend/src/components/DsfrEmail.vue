dc040eac00aafc87758a65053a1d71e4bae41bc1
Helen Root <helenroot1@gmail.com>
2024-03-26 13:26:07 +0100

    Create new email component to be reused across forms

---
+++ frontend/src/components/DsfrEmail.vue
@@ -0,0 +1,36 @@
+<template>
+  <DsfrTextField
+    v-bind="$attrs"
+    v-on="$listeners"
+    ref="email"
+    type="email"
+    name="email"
+    autocomplete="email"
+    spellcheck="off"
+    validate-on-blur
+    :rules="[validators.email]"
+  >
+    <template v-slot:label>
+      <span class="fr-label">Adresse électronique</span>
+      <span class="fr-hint-text mt-1">Format attendu : nom@domaine.fr</span>
+    </template>
+  </DsfrTextField>
+</template>
+
+<script>
+import DsfrTextField from "./DsfrTextField"
+import validators from "@/validators"
+
+export default {
+  name: "DsfrEmail",
+  components: { DsfrTextField },
+  computed: {
+    validators() {
+      return validators
+    },
+    rules() {
+      return [validators.email]
+    },
+  },
+}
+</script>
