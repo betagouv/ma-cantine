6ad42b8e25255ef2f856805eb089aa67719df77b
Helen Root <helenroot1@gmail.com>
2024-04-05 18:39:39 +0200

    Make format examples the same in auth and Vue pages

--- web/forms.py
+++ web/forms.py
@@ -47,7 +47,7 @@ class RegisterUserForm(UserCreationForm):
         self.fields["email"].help_text = "Format attendu : nom@domaine.fr"
         self.fields["email"].widget.attrs.update({"placeholder": "agnes.d@example.com", "autocomplete": "email"})
         self.fields["phone_number"].widget.attrs.update({"autocomplete": "tel"})
-        self.fields["phone_number"].help_text = "Format attendu : 07 01 02 03 04"
+        self.fields["phone_number"].help_text = "Format attendu : 01 22 33 44 55"
         self.fields["password1"].widget.attrs.update({"placeholder": "Entrez votre mot de passe"})
         self.fields["password2"].widget.attrs.update({"placeholder": "Confirmez votre mot de passe"})
         self.fields["number_of_managed_cantines"].widget.attrs.update({"class": "cantine-number-input", "min": "0"})
