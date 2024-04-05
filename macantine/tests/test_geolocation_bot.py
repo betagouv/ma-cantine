b9c249958c79f408f82e14fe5ad3e17d615e7f23
Helen Root <helenroot1@gmail.com>
2024-04-05 22:22:00 +0200

    More explicit naming

--- macantine/tests/test_geolocation_bot.py
+++ macantine/tests/test_geolocation_bot.py
@@ -29,7 +29,7 @@ class TestGeolocationBot(TestCase):
         address_api_text += '21340172201787,,11111,00000,11111,Ma ville,"01,Something,Other"\n'
         mock.post(self.api_url, text=address_api_text)
 
-        tasks.fill_missing_geolocation_data()
+        tasks.fill_missing_geolocation_data_using_insee_code_or_postcode()
 
         self.assertEqual(mock.call_count, 2)
 
@@ -43,7 +43,7 @@ class TestGeolocationBot(TestCase):
         address_api_text += f'{canteen.id},,69003,69383,69003,Lyon,"69, Rhône, Auvergne-Rhône-Alpes"\n'
         mock.post(self.api_url, text=address_api_text)
 
-        tasks.fill_missing_geolocation_data()
+        tasks.fill_missing_geolocation_data_using_insee_code_or_postcode()
 
         canteen.refresh_from_db()
         self.assertEqual(canteen.city, "Lyon")
@@ -100,7 +100,7 @@ class TestGeolocationBot(TestCase):
                 postal_code=None,
             ),
         ]
-        result = list(tasks._get_candidate_canteens())
+        result = list(tasks._get_candidate_canteens_for_code_geobot())
         self.assertEqual(len(result), 4)
         for canteen in candidate_canteens:
             match = list(filter(lambda x: x.id == canteen.id, result))
@@ -114,7 +114,7 @@ class TestGeolocationBot(TestCase):
         canteen = CanteenFactory.create(city=None, geolocation_bot_attempts=0, postal_code="69003")
         mock.post(self.api_url, text="", status_code=403)
 
-        tasks.fill_missing_geolocation_data()
+        tasks.fill_missing_geolocation_data_using_insee_code_or_postcode()
 
         canteen.refresh_from_db()
         self.assertEqual(canteen.geolocation_bot_attempts, 1)
@@ -133,7 +133,7 @@ class TestGeolocationWithSiretBot(TestCase):
             CanteenFactory.create(city_insee_code=29890),
             CanteenFactory.create(city_insee_code=None, siret=None),
         ]
-        result = list(tasks._get_candidate_canteens_for_geobot())
+        result = list(tasks._get_candidate_canteens_for_siret_geobot())
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].id, candidate_canteen.id)
 
@@ -166,7 +166,7 @@ class TestGeolocationWithSiretBot(TestCase):
             ),
             status_code=200,
         )
-        response = tasks.get_geo_data(candidate_canteen.siret, token)
+        response = tasks.get_geo_data_from_siret(candidate_canteen.siret, token)
         self.assertEquals(response["city_insee_code"], 29352)
 
     def test_geolocation_with_siret_data_filled(self, mock):
