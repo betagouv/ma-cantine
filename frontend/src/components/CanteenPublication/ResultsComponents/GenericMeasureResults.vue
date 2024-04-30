0434423994eee45f767b78a6db950a5c864bf79f
Helen Root <helenroot1@gmail.com>
2024-04-30 11:25:02 +0200

    Remove synthesis view - not sure at this stage if that is what we want to display

--- frontend/src/components/CanteenPublication/ResultsComponents/GenericMeasureResults.vue
+++ frontend/src/components/CanteenPublication/ResultsComponents/GenericMeasureResults.vue
@@ -2,36 +2,14 @@
   <div>
     <p v-if="badge.earned">{{ badge.subtitle }}</p>
     <p v-else>Cet établissement ne respecte pas encore la loi EGAlim pour cette mesure.</p>
-
-    <component :is="`${badge.baseComponent}Summary`" :canteen="canteen" :diagnostic="diagnostic" />
   </div>
 </template>
 
 <script>
-import { latestCreatedDiagnostic } from "@/utils"
-
-import DiversificationMeasureSummary from "@/components/DiagnosticSummary/DiversificationMeasureSummary"
-import InformationMeasureSummary from "@/components/DiagnosticSummary/InformationMeasureSummary"
-import NoPlasticMeasureSummary from "@/components/DiagnosticSummary/NoPlasticMeasureSummary"
-import WasteMeasureSummary from "@/components/DiagnosticSummary/WasteMeasureSummary"
-
 export default {
   name: "GenericMeasureResults",
   props: {
     badge: Object,
-    canteen: Object,
-    diagnosticSet: Array,
-  },
-  components: {
-    DiversificationMeasureSummary,
-    InformationMeasureSummary,
-    NoPlasticMeasureSummary,
-    WasteMeasureSummary,
-  },
-  computed: {
-    diagnostic() {
-      return latestCreatedDiagnostic(this.diagnosticSet)
-    },
   },
 }
 </script>
