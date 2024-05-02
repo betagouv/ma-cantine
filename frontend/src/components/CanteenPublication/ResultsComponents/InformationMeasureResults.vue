f78c23b2be7855846c7df0983410555abbebf499
Helen Root <helenroot1@gmail.com>
2024-05-02 12:42:33 +0200

    Add back in link to canteen's website if provided

--- frontend/src/components/CanteenPublication/ResultsComponents/InformationMeasureResults.vue
+++ frontend/src/components/CanteenPublication/ResultsComponents/InformationMeasureResults.vue
@@ -1,9 +1,16 @@
 <template>
-  <GenericMeasureResults :badge="badge" :canteen="canteen" :diagnosticSet="diagnosticSet" />
+  <div>
+    <GenericMeasureResults :badge="badge" :canteen="canteen" :diagnosticSet="diagnosticSet" />
+    <p v-if="diagnostic && diagnostic.communicationSupportUrl" class="fr-text">
+      Cette cantine communique aux usagers sur
+      <a :href="diagnostic.communicationSupportUrl">{{ diagnostic.communicationSupportUrl }}</a>
+    </p>
+  </div>
 </template>
 
 <script>
 import GenericMeasureResults from "./GenericMeasureResults"
+import { latestCreatedDiagnostic } from "@/utils"
 
 export default {
   name: "InformationMeasureResults",
@@ -13,5 +20,11 @@ export default {
     diagnosticSet: Array,
   },
   components: { GenericMeasureResults },
+  computed: {
+    diagnostic() {
+      if (!this.diagnosticSet) return
+      return latestCreatedDiagnostic(this.diagnosticSet)
+    },
+  },
 }
 </script>
