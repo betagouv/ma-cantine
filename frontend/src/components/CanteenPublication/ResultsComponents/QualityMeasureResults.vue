49b43226de6ac785dc44a7c1dbf70315cf874b25
Helen Root <helenroot1@gmail.com>
2024-06-03 12:34:10 +0200

    Update wording for appro introduction

--- frontend/src/components/CanteenPublication/ResultsComponents/QualityMeasureResults.vue
+++ frontend/src/components/CanteenPublication/ResultsComponents/QualityMeasureResults.vue
@@ -2,15 +2,14 @@
   <div>
     <CentralKitchenInfo :canteen="canteen" />
 
-    <p v-if="canteen.badges.appro">
+    <p>
+      La loi EGAlim impose {{ applicableRules.qualityThreshold }} % de produits durables et de qualité et durable, dont
+      {{ applicableRules.bioThreshold }} % de bio
       <span v-if="applicableRules.qualityThreshold !== 50">
-        Ce qui est servi dans les assiettes est au moins à {{ applicableRules.qualityThreshold }} % de produits durables
-        et de qualité, dont {{ applicableRules.bioThreshold }} % bio, en respectant
+        - en respectant
         <a href="https://ma-cantine.agriculture.gouv.fr/blog/16">les seuils d'Outre-mer</a>
       </span>
-      <span v-else>{{ badge.subtitle }}</span>
     </p>
-    <p v-else>Cet établissement ne respecte pas encore la loi EGAlim pour cette mesure.</p>
 
     <div v-if="tabs.length" class="mb-8">
       <v-row class="align-end flex-wrap mb-4">
