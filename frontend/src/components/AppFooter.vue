8a472cf63ed4d3e37a717a3e29dee19b977da5db
Helen Root <helenroot1@gmail.com>
2024-06-18 11:49:00 +0200

    contain is not a valid attribute of img

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -10,7 +10,6 @@
         <v-col cols="12" sm="3" md="2" class="d-flex align-center">
           <img
             src="/static/images/Marianne.png"
-            contain
             alt="Ministère de l'Agriculture et la Souveraineté Alimentaire - Liberté Égalité Fraternité"
           />
         </v-col>
