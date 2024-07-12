8f647aeb71584e0058e15e32786638d234424f8b
Helen Root <helenroot1@gmail.com>
2024-07-12 16:43:24 +0200

    Save and quit button styling

--- 2024-frontend/src/views/DiagnosticTunnel/index.vue
+++ 2024-frontend/src/views/DiagnosticTunnel/index.vue
@@ -17,7 +17,9 @@ const steps = [
   },
 ]
 const stepTitles = steps.map((s) => s.title)
-const currentStep = (steps.findIndex((s) => s.id === props.étape) || 0) + 1
+const stepIdx = steps.findIndex((s) => s.id === props.étape)
+const currentStep = stepIdx + 1
+const step = steps[stepIdx]
 
 const tunnels = [
   ...keyMeasures.map((km) => ({
@@ -36,45 +38,45 @@ const tunnels = [
     <div class="fr-container fr-pt-2w">
       <div class="fr-grid-row">
         <!-- TODO: hide on xs -->
-        <div class="fr-col-9 fr-grid-row fr-mx-n2w">
-          <div v-for="tunnel in tunnels" :key="tunnel.id" class="fr-px-2w header-icon fr-grid-row fr-grid-row--middle">
-            <div v-if="tunnel.id === measure.id" class="fr-grid-row fr-grid-row--middle">
-              <!-- TODO: proper colours (originally primary and primary lighten-4) -->
-              <component :is="tunnel.icon" class="fr-mb-1v fr-mr-1v" />
-              <!-- text-uppercase mb-0 grey--text text--darken-2 font-weight-bold -->
-              <p class="tunnel-header-title fr-text--xs fr-mb-0">
-                {{ tunnel.shortTitle }}
-              </p>
-            </div>
-            <div v-else>
-              <component
-                :is="tunnel.icon"
-                fillColor="#ff0000"
-                :title="tunnel.shortTitle"
-                aria-hidden="false"
-                role="img"
-              />
+        <div class="fr-col-9">
+          <div class="fr-grid-row fr-ml-n2w">
+            <div
+              v-for="tunnel in tunnels"
+              :key="tunnel.id"
+              class="fr-px-2w header-icon fr-grid-row fr-grid-row--middle"
+            >
+              <div v-if="tunnel.id === measure.id" class="fr-grid-row fr-grid-row--middle">
+                <!-- TODO: proper colours (originally primary and primary lighten-4) -->
+                <component :is="tunnel.icon" class="fr-mb-1v fr-mr-1v" />
+                <!-- text-uppercase mb-0 grey--text text--darken-2 font-weight-bold -->
+                <p class="tunnel-header-title fr-text--xs fr-mb-0">
+                  {{ tunnel.shortTitle }}
+                </p>
+              </div>
+              <div v-else>
+                <component
+                  :is="tunnel.icon"
+                  fillColor="#ff0000"
+                  :title="tunnel.shortTitle"
+                  aria-hidden="false"
+                  role="img"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <!-- TODO: save and quit -->
-        <!-- <v-col class="text-right py-0" v-if="step">
-          <p class="mb-0">
-            <v-btn
-              text
-              plain
-              class="text-decoration-underline px-0"
-              color="primary"
-              @click="step.isSynthesis ? quit() : saveAndQuit()"
-              :disabled="!formIsValid && !isSynthesis"
-            >
-              {{ step.isSynthesis ? "Quitter" : "Sauvegarder et quitter" }}
-              <v-icon color="primary" size="1rem" class="ml-0 mb-1">
-                $close-line
-              </v-icon>
-            </v-btn>
-          </p>
-        </v-col> -->
+        <!-- TODO: functionality -->
+        <div v-if="step" class="fr-col-3 quit">
+          <DsfrButton
+            :label="step.isSynthesis ? 'Quitter' : 'Sauvegarder et quitter'"
+            icon="fr-icon-close-line"
+            icon-right
+            size="sm"
+            tertiary
+            no-outline
+            class="fr-mr-n1w"
+          />
+        </div>
       </div>
       <DsfrStepper :steps="stepTitles" :currentStep />
     </div>
@@ -132,5 +134,8 @@ const tunnels = [
 .tunnel-header-title {
   text-transform: uppercase;
   font-weight: bold;
+}
+.quit {
+  text-align: right;
 }
 </style>
