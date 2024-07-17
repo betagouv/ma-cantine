9fd3a0ffcf8a206c76bdf1c34a0a17a3b111de62
Paul-Elie VALLEE <paul-elie.vallee@beta.gouv.fr>
2024-07-17 15:08:45 +0200

    add wagtail /api/v2 to routes

--- macantine/urls.py
+++ macantine/urls.py
@@ -8,11 +8,12 @@ from web.views import VueAppDisplayView, Vue3AppDisplayView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
+from cms.api import api_router
 urlpatterns = [
     path("admin/", admin.site.urls),  # if the path of 'admin/' changes, update historical_record_add_auth_method
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path('api/v2/', api_router.urls),
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('pages/', include(wagtail_urls)),
