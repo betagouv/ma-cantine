584df5629921fabba70c2c45ab57b3f1544b153c
Paul-Elie VALLEE <paul-elie.vallee@beta.gouv.fr>
2024-07-19 12:18:24 +0200

    create API viewset to expose wasteactions

--- cms/viewsets/wasteaction.py
+++ cms/viewsets/wasteaction.py
@@ -1,6 +1,7 @@
 from wagtail.snippets.views.snippets import SnippetViewSet
 from wagtail.admin.panels import TabbedInterface, ObjectList, FieldPanel
 from cms.models.wasteaction import WasteAction
+from wagtail.images.api.v2.views import BaseAPIViewSet
 
 
 class WasteActionViewSet(SnippetViewSet):
@@ -32,3 +33,25 @@ class WasteActionViewSet(SnippetViewSet):
         ]
     )
     list_display = ["title", "modification_date"]
+
+
+class WasteActionAPIViewSet(BaseAPIViewSet):
+    """
+    API endpoint that allows WasteActions to be viewed.
+    """
+
+    model = WasteAction
+    api_fields = [
+        "id",
+        "creation_date",
+        "modification_date",
+        "title",
+        "subtitle",
+        "effort",
+        "waste_origin",
+        "description",
+        "lead_image",
+    ]
+    body_fields = BaseAPIViewSet.body_fields + api_fields
+    # If needed we can list all those fields by default. Otherwise we can see them by adding "?fields=*" to URL
+    # listing_default_fields = BaseAPIViewSet.listing_default_fields + api_fields
