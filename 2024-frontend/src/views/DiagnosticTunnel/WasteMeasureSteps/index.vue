8bc00496d4017525d07c52f203ed04029508a880
Helen Root <helenroot1@gmail.com>
2024-07-19 15:19:46 +0200

    Fix variable use in child component

--- 2024-frontend/src/views/DiagnosticTunnel/WasteMeasureSteps/index.vue
+++ 2024-frontend/src/views/DiagnosticTunnel/WasteMeasureSteps/index.vue
@@ -1,3 +1,7 @@
+<script setup>
+const name = defineModel()
+</script>
+
 <template>
   <DsfrInput v-model="name" label="Nom" placeholder="Jean Dupont" label-visible required hint="Indiquez votre nom" />
 </template>
