f857d1390d73a8d209d084169990ebf15ab1d289
Helen Root <helenroot1@gmail.com>
2024-07-19 16:29:20 +0200

    Setup store, fetch logged user, redirect to vue 2 if not logged in

--- 2024-frontend/src/router/index.js
+++ 2024-frontend/src/router/index.js
@@ -1,7 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router"
 import DiagnosticTunnel from "@/views/DiagnosticTunnel"
+import { useRootStore } from "../stores/root"
 
 const routes = [
+  {
+    path: "/",
+    name: "Vue2Home",
+    meta: {
+      redirectToVue2: true,
+    },
+  },
   {
     path: "/diagnostic-tunnel/:canteenUrlComponent/:year/:measureId",
     name: "DiagnosticTunnel",
@@ -22,6 +30,23 @@ routes.forEach((r) => {
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.redirectToVue2) {
+    location.href = location.origin
+    return
+  }
+  const store = useRootStore()
+  if (!store.initialDataLoaded) {
+    store.fetchInitialData().then(() => {
+      if (!store.loggedUser && to.meta.authenticationRequired) {
+        next({ name: "Vue2Home", replace: true })
+      } else {
+        next()
+      }
+    })
+  }
 })
 
 export default router
