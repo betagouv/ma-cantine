e9ca73252beb0403ea800100242d8b4b980c049f
Paul-Elie VALLEE <paul-elie.vallee@beta.gouv.fr>
2024-07-22 16:03:59 +0200

    remove pages endpoint

--- cms/api.py
+++ cms/api.py
@@ -1,4 +1,3 @@
-from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.images.api.v2.views import ImagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
@@ -11,7 +10,6 @@ api_router = WagtailAPIRouter("wagtailapi")
 # The first parameter is the name of the endpoint (such as pages, images). This
 # is used in the URL of the endpoint
 # The second parameter is the endpoint class that handles the requests
-api_router.register_endpoint("pages", PagesAPIViewSet)
 api_router.register_endpoint("images", ImagesAPIViewSet)
 api_router.register_endpoint("documents", DocumentsAPIViewSet)
 api_router.register_endpoint("wasteactions", WasteActionAPIViewSet)
