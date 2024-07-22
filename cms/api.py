dabe5bce3ebd9d3a275dde7072abdaf1a28519e9
Paul-Elie VALLEE <paul-elie.vallee@beta.gouv.fr>
2024-07-22 16:19:31 +0200

    remove comments

--- cms/api.py
+++ cms/api.py
@@ -6,10 +6,6 @@ from .viewsets import WasteActionAPIViewSet
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter("wagtailapi")
 
-# Add the three endpoints using the "register_endpoint" method.
-# The first parameter is the name of the endpoint (such as pages, images). This
-# is used in the URL of the endpoint
-# The second parameter is the endpoint class that handles the requests
 api_router.register_endpoint("images", ImagesAPIViewSet)
 api_router.register_endpoint("documents", DocumentsAPIViewSet)
 api_router.register_endpoint("wasteactions", WasteActionAPIViewSet)
