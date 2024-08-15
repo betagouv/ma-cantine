ff3c4b26566663129c9b3baadc0e464592d8d926
Helen Root <helenroot1@gmail.com>
2024-08-15 13:57:51 +0200

    Simplify text

--- 2024-frontend/src/constants.js
+++ 2024-frontend/src/constants.js
@@ -3,50 +3,38 @@ export default Object.freeze({
     {
       key: "NONE",
       urlSlug: "cantines-seules",
-      title: "Importer des cantines sans bilan",
-      label: "Sans bilan",
-      help: "Vous voulez importer des cantines sans données d'approvisionnement",
-      icon: "$community-fill",
+      title: "Cantines",
+      help: "Vous voulez créer ou mettre à jour des cantines",
     },
     {
       key: "SIMPLE",
       urlSlug: "cantines-et-diagnostics-simples",
-      title: "Importer des cantines et bilans simples",
-      label: "Diagnostic simple",
-      help: "Vous connaissez les valeurs totales de vos achats bio et de qualité",
-      icon: "$bar-chart-box-fill",
+      title: "Bilans simples",
+      help:
+        "Vous voulez créer ou mettre à jour des bilans. Vous connaissez que les valeurs totales de vos achats bio et de qualité",
     },
     {
       key: "COMPLETE",
       urlSlug: "cantines-et-diagnostics-complets",
-      title: "Importer des cantines et bilans complets",
-      label: "Diagnostic complet",
-      help: "Vous connaissez les labels et les familles de produits de vos achats",
-      icon: "$checkbox-circle-fill",
+      title: "Bilans complets",
+      help:
+        "Vous voulez créer ou mettre à jour des bilans. Vous connaissez les labels et les familles de produits de vos achats",
     },
   ],
   CentralKitchenImportLevels: [
     {
       key: "CC_SIMPLE",
       urlSlug: "cuisine-centrale-diagnostics-simples",
-      title: "Mettre à jour vos satellites et renseigner des bilans simples",
-      description:
-        "Ce type de fichier vous permet de renseigner vos données d'approvisionnement simplifiées au niveau de la cuisine centrale et de lister vos cantines satellites seulement avec leurs données d'établissement. Les données d'approvisionnement restent donc seulement nécessaires pour les cuisines centrales.",
-      label: "Cuisine centrale avec bilan simple",
+      title: "Bilans simples pour des livreurs de repas",
       help:
-        "Vous voulez renseigner vos satellites et vous connaissez les valeurs totaux, bio, et de qualité et durable",
-      icon: "$bar-chart-box-fill",
+        "Vous ne connaissez pas les données par cantine satellite, que par livreur de repas. Vous connaissez les valeurs totaux, bio, et de qualité et durable",
     },
     {
       key: "CC_COMPLETE",
       urlSlug: "cuisine-centrale-diagnostics-complets",
-      title: "Mettre à jour vos satellites et renseigner des bilans complets",
-      description:
-        "Ce type de fichier vous permet de renseigner vos données d'approvisionnement complètes au niveau de la cuisine centrale et de lister vos cantines satellites seulement avec leurs données d'établissement. Les données d'approvisionnement restent donc seulement nécessaires pour les cuisines centrales.",
-      label: "Cuisine centrale avec bilan complet",
+      title: "Bilans complets pour des livreurs de repas",
       help:
-        "Vous voulez renseigner vos satellites et vous connaissez les labels et les familles de produits de vos achats",
-      icon: "$checkbox-circle-fill",
+        "Vous ne connaissez pas les données par cantine satellite, que par livreur de repas. Vous connaissez les labels et les familles de produits de vos achats",
     },
   ],
 })
