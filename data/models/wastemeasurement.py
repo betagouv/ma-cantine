f63dc02e63393f82e875663fa61a4d63f100592c
Helen Root <helenroot1@gmail.com>
2024-08-28 12:32:49 +0200

    Fix verbose name to be readable

--- data/models/wastemeasurement.py
+++ data/models/wastemeasurement.py
@@ -1,13 +1,8 @@
-# from decimal import Decimal
-# from django.core.exceptions import ValidationError
-# from django.core.validators import MaxValueValidator
 from django.db import models
 from simple_history.models import HistoricalRecords
 from .canteen import Canteen
 from datetime import datetime
 from django.core.exceptions import ValidationError
-
-# from data.utils import get_diagnostic_lower_limit_year, get_diagnostic_upper_limit_year
 
 
 def validate_before_today(value):
@@ -17,8 +12,8 @@ def validate_before_today(value):
 
 class WasteMeasurement(models.Model):
     class Meta:
-        verbose_name = "waste_measurement"
-        verbose_name_plural = "waste_measurements"
+        verbose_name = "pesage du gaspillage alimentaire"
+        verbose_name_plural = "pesages du gaspillage alimentaire"
 
     creation_date = models.DateTimeField(auto_now_add=True)
     modification_date = models.DateTimeField(auto_now=True)
