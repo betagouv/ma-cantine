04e3ce254c2ce02a90272559d40f46f64887ee09
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-02 18:01:38 +0200

    Add test

--- cms/tests.py
+++ cms/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
+from django.db.utils import IntegrityError
+from .models import WasteAction
 
-# Create your tests here.
+
+class WasteActionModelSaveTest(TransactionTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_waste_action_validation(self):
+        # NOT OK : waste_origins is required
+        self.assertRaises(IntegrityError, WasteAction.objects.create, title="Action 1")
+        # OK
+        WasteAction.objects.create(title="Action 1", subtitle=None, waste_origins=[WasteAction.WasteOrigin.PREP])
