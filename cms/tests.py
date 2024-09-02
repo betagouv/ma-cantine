e0108acc914af7ef4af43af7eb5e62be966a2b13
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-02 18:39:12 +0200

    Validate on save. Improve tests

--- cms/tests.py
+++ cms/tests.py
@@ -1,6 +1,15 @@
 from django.test import TransactionTestCase
-from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
 from .models import WasteAction
+from .factories import WasteActionFactory
+
+
+WASTE_ACTION = {
+    "title": "Action 1",
+    "description": "Description",
+    "effort": WasteAction.Effort.SMALL,
+    "waste_origins": [WasteAction.WasteOrigin.PREP],
+}
 
 
 class WasteActionModelSaveTest(TransactionTestCase):
@@ -9,7 +18,12 @@ class WasteActionModelSaveTest(TransactionTestCase):
         pass
 
     def test_waste_action_validation(self):
-        # NOT OK : waste_origins is required
-        self.assertRaises(IntegrityError, WasteAction.objects.create, title="Action 1")
-        # OK
-        WasteAction.objects.create(title="Action 1", subtitle=None, waste_origins=[WasteAction.WasteOrigin.PREP])
+        # NOT OK: missing required field
+        for REQUIRED_FIELD in ["title", "description", "effort", "waste_origins"]:
+            WASTE_ACTION_WITHOUT_REQUIRED_FIELD = WASTE_ACTION | {REQUIRED_FIELD: None}
+            self.assertRaises(ValidationError, WasteActionFactory, **WASTE_ACTION_WITHOUT_REQUIRED_FIELD)
+        # OK: all required fields passed
+        WasteActionFactory(**WASTE_ACTION)
+        # OK: subtitle can be empty
+        WASTE_ACTION_WITHOUT_SUBTITLE = WASTE_ACTION | {"subtitle": None}
+        WasteActionFactory(**WASTE_ACTION_WITHOUT_SUBTITLE)
