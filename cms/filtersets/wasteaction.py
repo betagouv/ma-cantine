2e345274c5b6cdb2fd1fe3875d1a0edb16ec13f3
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-04 12:26:06 +0200

    add filter on waste_actions. order filter fields

--- cms/filtersets/wasteaction.py
+++ cms/filtersets/wasteaction.py
@@ -1,11 +1,19 @@
+from django import forms
 from wagtail.admin.filters import WagtailFilterSet
-from django_filters import CharFilter
+from django_filters import CharFilter, MultipleChoiceFilter
 from cms.models import WasteAction
 
 
 class WasteActionFilterSet(WagtailFilterSet):
     title = CharFilter(lookup_expr="icontains")
+    effort = MultipleChoiceFilter(choices=WasteAction.Effort.choices, widget=forms.CheckboxSelectMultiple)
+    waste_origins = MultipleChoiceFilter(
+        label=WasteAction._meta.get_field("waste_origins").verbose_name,
+        choices=WasteAction.WasteOrigin.choices,
+        widget=forms.CheckboxSelectMultiple,
+        lookup_expr="icontains",
+    )
 
     class Meta:
         model = WasteAction
-        fields = ["effort"]
+        fields = ["title", "effort", "waste_origins"]
