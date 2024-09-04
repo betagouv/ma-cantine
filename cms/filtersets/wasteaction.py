d7982c82c75210328bb631fab07c70d26e03aa5c
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-04 11:15:19 +0200

    add title icontains search

--- cms/filtersets/wasteaction.py
+++ cms/filtersets/wasteaction.py
@@ -1,8 +1,11 @@
 from wagtail.admin.filters import WagtailFilterSet
+from django_filters import CharFilter
 from cms.models import WasteAction
 
 
 class WasteActionFilterSet(WagtailFilterSet):
+    title = CharFilter(lookup_expr="icontains")
+
     class Meta:
         model = WasteAction
         fields = ["effort"]
