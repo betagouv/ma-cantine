d628e86e42955490e9b6a93f91db2057c5f03aae
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-27 11:30:15 +0200

    Add factory, basic test on save

---
+++ data/tests/test_resource_action.py
@@ -0,0 +1,27 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from data.factories import (
+    CanteenFactory,
+    ResourceActionFactory,
+    UserFactory,
+    WasteActionFactory,
+)
+
+
+class ResourceActionModelSaveTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.waste_action = WasteActionFactory()
+        cls.user = UserFactory()
+        cls.canteen = CanteenFactory()
+        cls.canteen.managers.add(cls.user)
+
+    def test_resource_action_validation(self):
+        # NOT OK: missing required field
+        self.assertRaises(ValidationError, ResourceActionFactory)
+        self.assertRaises(ValidationError, ResourceActionFactory, resource=self.waste_action)
+        # OK: all required fields passed
+        ResourceActionFactory(resource=self.waste_action, user=self.user)
+        ResourceActionFactory(resource=self.waste_action, canteen=self.canteen)
+        ResourceActionFactory(resource=self.waste_action, canteen=self.canteen, user=self.user, is_done=True)
