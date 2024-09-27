411726ce86525d72c547eafd77bf0ce0b1fa9ea8
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-27 15:38:31 +0200

    New ResourceActionSerializer

---
+++ api/serializers/resourceaction.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from data.models import ResourceAction
+
+
+class ResourceActionSerializer(serializers.ModelSerializer):
+    canteen_id = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = ResourceAction
+        fields = (
+            "canteen_id",
+            "is_done",
+            "is_not_interested",
+            "is_favorite",
+        )
