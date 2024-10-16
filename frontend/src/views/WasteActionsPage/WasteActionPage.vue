3780f3f975391982019706f34ca9680957f01414
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-16 12:39:45 +0200

    Add edit button with modal & DsfrAutocomplete. update changes

--- frontend/src/views/WasteActionsPage/WasteActionPage.vue
+++ frontend/src/views/WasteActionsPage/WasteActionPage.vue
@@ -44,15 +44,55 @@
           <p v-else>
             <i>Aucune cantine</i>
           </p>
+          <br />
+          <v-btn small color="primary" @click="showActionModal">
+            <span class="mx-2">
+              Modifier
+            </span>
+          </v-btn>
         </v-col>
       </v-row>
       <v-row class="mt-9">
         <BackLink :to="backLink" text="Retour" :primary="true" />
       </v-row>
+
+      <v-dialog v-model="showModal">
+        <v-card class="pa-6">
+          <div class="mt-n6 mx-n6 mb-4 pa-4 d-flex" style="background-color: #F5F5F5">
+            <v-spacer></v-spacer>
+            <v-btn outlined color="primary" @click="showModal = false">
+              Fermer
+            </v-btn>
+          </div>
+
+          <h2 id="modal-title" class="mb-3" tabindex="-1">Mis en place ?</h2>
+
+          <v-form ref="form" v-model="actionFormIsValid" @submit.prevent>
+            <DsfrAutocomplete
+              v-model="chosenCanteenIds"
+              :items="userCanteens"
+              clearable
+              multiple
+              hide-details
+              id="select-canteen"
+              placeholder="Toutes mes cantines"
+              class="mt-1"
+              no-data-text="Pas de résultats"
+              item-text="name"
+              item-value="id"
+            />
+            <v-row class="mt-2 pa-4">
+              <v-spacer></v-spacer>
+              <v-btn x-large color="primary" @click="saveActionChanges">Valider</v-btn>
+            </v-row>
+          </v-form>
+        </v-card>
+      </v-dialog>
     </div>
   </div>
 </template>
 <script>
+import DsfrAutocomplete from "@/components/DsfrAutocomplete"
 import BreadcrumbsNav from "@/components/BreadcrumbsNav.vue"
 import BackLink from "@/components/BackLink"
 import DsfrTagGroup from "@/components/DsfrTagGroup"
@@ -60,10 +100,13 @@ import DsfrTag from "@/components/DsfrTag"
 import Constants from "@/constants"
 
 export default {
-  components: { BreadcrumbsNav, BackLink, DsfrTagGroup, DsfrTag },
+  components: { DsfrAutocomplete, BreadcrumbsNav, BackLink, DsfrTagGroup, DsfrTag },
   data() {
     return {
       wasteAction: null,
+      actionModal: false,
+      actionFormIsValid: true,
+      chosenCanteenIds: [],
       backLink: { name: "WasteActionsHome" },
     }
   },
@@ -96,10 +139,52 @@ export default {
           })
         })
     },
+    createOrUpdateResourceAction(canteenId, is_done = true) {
+      return this.$store
+        .dispatch("createOrUpdateResourceAction", {
+          resourceId: this.id,
+          payload: { canteenId, is_done },
+        })
+        .catch((e) => this.$store.dispatch("notifyServerError", e))
+    },
+    showActionModal() {
+      // Get the canteens that have already done the action
+      this.chosenCanteenIds = this.userCanteens
+        .filter((canteen) => this.actionsDone.find((actionCanteen) => actionCanteen.id === canteen.id))
+        .map((canteen) => canteen.id)
+      this.actionModal = true
+    },
+    saveActionChanges() {
+      const actionChanges = []
+      // Compare the chosen canteens with the actions done (new & removed)
+      this.chosenCanteenIds.forEach((canteenId) => {
+        if (!this.actionsDone.find((actionCanteen) => actionCanteen.id === canteenId)) {
+          actionChanges.push(this.createOrUpdateResourceAction(canteenId, true))
+        }
+      })
+      this.actionsDone.forEach((actionCanteen) => {
+        if (!this.chosenCanteenIds.includes(actionCanteen.id)) {
+          actionChanges.push(this.createOrUpdateResourceAction(actionCanteen.id, false))
+        }
+      })
+      // close modal and refresh the wasteAction if needed
+      if (actionChanges.length) {
+        Promise.all(actionChanges).then(() => {
+          this.fetchWasteAction()
+          this.actionModal = false
+        })
+      } else {
+        this.actionModal = false
+      }
+    },
   },
   computed: {
     loggedUser() {
       return this.$store.state.loggedUser
+    },
+    userCanteens() {
+      if (!this.loggedUser) return []
+      return this.$store.state.userCanteenPreviews
     },
     effort() {
       return (
@@ -123,7 +208,15 @@ export default {
     canteensDoneAction() {
       return this.wasteAction?.canteenActions
         ?.filter((canteenAction) => canteenAction.isDone)
-        .map((canteenAction) => ({ text: canteenAction.canteen.name }))
+        .map((canteenAction) => ({ id: canteenAction.canteen.id, text: canteenAction.canteen.name }))
+    },
+    showModal: {
+      get() {
+        return !!this.actionModal
+      },
+      set(newValue) {
+        if (!newValue) this.actionModal = null
+      },
     },
   },
   mounted() {
