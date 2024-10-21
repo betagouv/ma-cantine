fa2a3806096055c9025241f097ef74f01adf8f6f
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-21 09:55:34 +0200

    Move to WasteMeasureResults

--- frontend/src/components/CanteenPublication/ResultsComponents/WasteMeasureResults.vue
+++ frontend/src/components/CanteenPublication/ResultsComponents/WasteMeasureResults.vue
@@ -1,9 +1,23 @@
 <template>
-  <GenericMeasureResults :badge="badge" :canteen="canteen" :diagnostics="diagnostics" />
+  <div>
+    <GenericMeasureResults :badge="badge" :canteen="canteen" :diagnostics="diagnostics" />
+    <v-row v-if="canteenResourceActionsDone.length" class="mb-2">
+      <v-col
+        vols="12"
+        sm="6"
+        md="4"
+        v-for="resourceAction in canteenResourceActionsDone"
+        :key="resourceAction.resource.id"
+      >
+        <WasteActionCard :wasteAction="resourceAction.resource" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 import GenericMeasureResults from "./GenericMeasureResults"
+import WasteActionCard from "@/components/WasteActionCard"
 
 export default {
   name: "WasteMeasureResults",
@@ -13,6 +27,11 @@ export default {
     diagnostics: Array,
     editable: Boolean,
   },
-  components: { GenericMeasureResults },
+  components: { GenericMeasureResults, WasteActionCard },
+  computed: {
+    canteenResourceActionsDone() {
+      return this.canteen?.resourceActions ? this.canteen.resourceActions.filter((ra) => ra.isDone) : []
+    },
+  },
 }
 </script>
