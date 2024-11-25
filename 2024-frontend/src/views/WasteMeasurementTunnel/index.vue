b7c9b6e301303fd11e85f364d67f9f3ee12995dc
Helen Root <helenroot1@gmail.com>
2024-11-25 14:08:34 +0100

    Bug fix : return to saved href from synthesis view

--- 2024-frontend/src/views/WasteMeasurementTunnel/index.vue
+++ 2024-frontend/src/views/WasteMeasurementTunnel/index.vue
@@ -74,7 +74,7 @@ const formIsValid = () => {
 
 const continueAction = () => {
   if (step.value.isSynthesis) {
-    router.push({ name: "WasteMeasurements" })
+    quit()
     return
   }
   if (!formIsValid()) {
