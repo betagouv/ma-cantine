cf959e5a93bb777a79db3c93b6460785029c39fd
Helen Root <helenroot1@gmail.com>
2024-11-27 12:19:51 +0100

    Add text

--- 2024-frontend/src/components/WasteMeasurementSummary.vue
+++ 2024-frontend/src/components/WasteMeasurementSummary.vue
@@ -70,7 +70,11 @@ const activeAccordion = ref("")
             </b>
           </p>
         </div>
-        <EmphasiseText :emphasisText="`${formatNumber(wastePerMeal)} g`" contextText="par repas" class="brown" />
+        <EmphasiseText
+          :emphasisText="`${formatNumber(wastePerMeal)} g`"
+          contextText="par repas et par convive"
+          class="brown"
+        />
         <p v-if="editable">
           <router-link :to="measurementTunnel" class="fr-btn fr-btn--secondary fr-btn--sm fr-mr-1w fr-mb-1w">
             Modifier les données
