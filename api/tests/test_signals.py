c88d3ac80de6f73b7c93b24085b913592ee73ed7
qloridant <quentin.loridant@protonmail.com>
2024-12-04 16:31:35 +0100

    Ajout test annulation td bloqué pour appli tierces

--- api/tests/test_signals.py
+++ api/tests/test_signals.py
@@ -4,7 +4,7 @@ from freezegun import freeze_time
 from rest_framework.test import APITestCase
 
 from data.factories import DiagnosticFactory
-from data.models import AuthenticationMethodHistoricalRecords
+from data.models import AuthenticationMethodHistoricalRecords, Teledeclaration
 
 from .utils import authenticate, get_oauth2_token
 
@@ -31,9 +31,9 @@ class TestSignals(APITestCase):
 
     @override_settings(ENABLE_TELEDECLARATION=True)
     @freeze_time("2021-01-20")
-    def test_authentication_method_created_with_api(self):
+    def test_td_creation_api_blocked_for_third_party(self):
         """
-        Teledeclarations created via a third party API should save the corresponding authentication method
+        Teledeclarations cannot be created  via a third party API
         """
         user, token = get_oauth2_token("canteen:write")
         diagnostic = DiagnosticFactory.create(year=2020)
@@ -41,10 +41,25 @@ class TestSignals(APITestCase):
         payload = {"diagnosticId": diagnostic.id}
 
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
-        self.client.post(reverse("teledeclaration_create"), payload)
+        response = self.client.post(reverse("teledeclaration_create"), payload)
+        self.assertEqual(response.status_code, 403)
 
         diagnostic.refresh_from_db()
-        td = diagnostic.teledeclaration_set.first()
-        self.assertEqual(
-            td.history.first().authentication_method, AuthenticationMethodHistoricalRecords.AuthMethodChoices.API
-        )
+        td = diagnostic.teledeclaration_set.all()
+        self.assertEqual(len(td), 0)
+
+    @override_settings(ENABLE_TELEDECLARATION=True)
+    @freeze_time("2021-01-20")
+    def test_td_deletion_api_blocked_for_third_party(self):
+        """
+        Teledeclarations cannot be deleted via a third party API
+        """
+        user, token = get_oauth2_token("canteen:write")
+        diagnostic = DiagnosticFactory.create(year=2020)
+        diagnostic.canteen.managers.add(user)
+        td = Teledeclaration.create_from_diagnostic(diagnostic, user, Teledeclaration.TeledeclarationStatus.CANCELLED)
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+        response = self.client.post(reverse("teledeclaration_cancel", kwargs={"pk": td.id}))
+
+        self.assertEqual(response.status_code, 403)
