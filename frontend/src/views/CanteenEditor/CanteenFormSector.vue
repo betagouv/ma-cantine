77c9026303f66fff3929c44ee5efd02101f4205f
charline-studi <studi@charline-laporte.fr>
2024-12-05 11:45:18 +0100

    Canteen Form : Création et description du comportement du composant

---
+++ frontend/src/views/CanteenEditor/CanteenFormSector.vue
@@ -0,0 +1,8 @@
+/* Comportemnt du composant : 
+- à la création d'une cantine il est vide
+- à la modification d'une cantine il est pré-rempli
+- on sélectionne une secteur
+- à partir de ce secteur on affiche l'activité 
+- si l'activité le demande on affiche l'établissement de tutelle
+- on renvoit bien l'info au parent dès qu'il y a un changement
+*/
