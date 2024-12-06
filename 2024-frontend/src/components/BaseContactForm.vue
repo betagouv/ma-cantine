5db6de3f07df5b6dcd8589a18994317ce68de7ea
Charline Laporte <charline.laporte@beta.gouv.fr>
2024-12-06 19:00:39 +0100

    Contact Form : Supprime la todo de code à refaire

--- 2024-frontend/src/components/BaseContactForm.vue
+++ 2024-frontend/src/components/BaseContactForm.vue
@@ -1,10 +1,4 @@
 <script setup>
-/*
-TODO :
-- Envoyer le demande
-- Meta : à voir si on l'ajoute dans la V1 du formulaire ou pas
-*/
-
 import { reactive } from 'vue'
 import { useRootStore } from '@/stores/root'
 import { useVuelidate } from "@vuelidate/core"
@@ -57,7 +51,7 @@ const validateForm = () => {
 
 /* Send Form */
 const sendInquiry = () => {
-  const { fromEmail, name, message, inquiryType } = form  
+  const { fromEmail, name, message, inquiryType } = form
   const payload = {
     from: fromEmail,
     name: name,
@@ -72,10 +66,9 @@ const sendInquiry = () => {
         status: "success",
         message: "Votre message a bien été envoyé. Nous reviendrons vers vous dans les plus brefs délais.",
       })
-      
-      // console.log($matomo)
+
+      // TODO : Matomo, comment ça fonctionne ?
       // if ($matomo) {
-      //   console.log($matomo)
       //   $matomo.trackEvent("inquiry", "send", inquiryType.value)
       // }
       initFields()
@@ -83,74 +76,6 @@ const sendInquiry = () => {
     })
     .catch((e) => store.notifyServerError(e))
 }
-
-
-// export default {
-//   props: {
-//     // meta is a JSON serialisable object containing data that could help the team help the user
-//     // such as the page the user was on when they sent the message
-//     meta: {
-//       type: Object,
-//       required: false,
-//     },
-//   },
-//   data() {
-//     return {
-//       formIsValid: true,
-//     }
-//   },
-//   computed: {
-//     validators() {
-//       return validators
-//     },
-//   },
-//   methods: {
-//     sendEmail() {
-//       this.$refs.form.validate()
-//       if (!this.formIsValid) {
-//         this.$store.dispatch("notifyRequiredFieldsError")
-//         return
-//       }
-
-//       let meta = this.meta || {}
-//       meta.userId = this.$store.state.loggedUser?.id
-//       meta.userAgent = navigator.userAgent
-
-//       // this text is visible to the team when the inquiry is sent
-//       const inquiryTypeDisplay = {
-//         functionalityQuestion: "fonctionnalité",
-//         bug: "bug",
-//         egalim: "loi",
-//         demo: "demande de démo",
-//         other: "autre",
-//       }
-
-//       const payload = {
-//         from: this.fromEmail,
-//         name: this.name,
-//         message: this.message,
-//         inquiryType: inquiryTypeDisplay[this.inquiryType],
-//         meta,
-//       }
-
-//       this.$store
-//         .dispatch("sendInquiryEmail", payload)
-//         .then(() => {
-//           this.$store.dispatch("notify", {
-//             status: "success",
-//             message: `Votre message a bien été envoyé. Nous reviendrons vers vous dans les plus brefs délais.`,
-//           })
-
-//           if (this.$matomo) {
-//             this.$matomo.trackEvent("inquiry", "send", this.inquiryType)
-//           }
-//           this.$refs.form.reset()
-//           window.scrollTo(0, 0)
-//         })
-//         .catch((e) => this.$store.dispatch("notifyServerError", e))
-//     },
-//   },
-// }
 </script>
 
 <template>
