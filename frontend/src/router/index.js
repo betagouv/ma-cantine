216258cdaf838b8da51d16f1c7e0722e3cea0300
Charline Laporte <charline.laporte@beta.gouv.fr>
2024-12-06 16:06:26 +0100

    Legals Notice : Le titre de la page est géré dans le router vue3

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -608,9 +608,6 @@ const vue3Routes = [
   {
     path: "/mentions-legales",
     name: "LegalNotices",
-    meta: {
-      title: "Mentions légales",
-    },
     sitemapGroup: Constants.SitemapGroups.SITE,
   },
 ]
