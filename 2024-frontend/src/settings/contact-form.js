332e6391a6b64450b044513d5af8ac6a40855371
Charline Laporte <charline.laporte@beta.gouv.fr>
2024-12-06 11:05:46 +0100

    Contact Form : Re-organise le script et commente chaque étape

---
+++ 2024-frontend/src/settings/contact-form.js
@@ -0,0 +1,9 @@
+export default {
+  inquiryOptions: [
+    { text: "Poser une question sur une fonctionnalité de ma cantine ?", value: "functionalityQuestion" },
+    { text: "Demander une démo", value: "demo" },
+    { text: "Signaler un bug", value: "bug" },
+    { text: "Question sur la loi EGAlim", value: "egalim" },
+    { text: "Autre", value: "other" },
+  ]
+}
