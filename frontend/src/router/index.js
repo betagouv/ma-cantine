1633e6e714c3acfeaac85469175c5dafbe725f34
Charline Laporte <charline.laporte@beta.gouv.fr>
2024-12-10 15:48:16 +0100

    Contact Page : Titre non nécessaire dans le router vue 2 après la migration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -604,9 +604,6 @@ const vue3Routes = [
   {
     path: "/contact",
     name: "ContactPage",
-    meta: {
-      title: "Contactez-nous",
-    },
     sitemapGroup: Constants.SitemapGroups.SITE,
   },
 ]
