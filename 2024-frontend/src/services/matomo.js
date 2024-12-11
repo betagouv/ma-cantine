698492ca86e9ad8f9a8237656bcc5b89688d8e5b
Charline Laporte <charline.laporte@beta.gouv.fr>
2024-12-11 18:58:24 +0100

    Contact Form : Corrige les guillemets

--- 2024-frontend/src/services/matomo.js
+++ 2024-frontend/src/services/matomo.js
@@ -2,7 +2,7 @@ export default {
   trackEvent(params) {
     if (window.MATOMO_ID) {
       const { category, action, value} = params
-      window._paq.push(['trackEvent', category, action, value])
+      window._paq.push(["trackEvent", category, action, value])
     }
   }
 }
