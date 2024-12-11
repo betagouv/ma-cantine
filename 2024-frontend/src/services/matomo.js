c4e78b8d9c6956e8d7052b0fa9b9bb713ac04be4
Charline Laporte <charline.laporte@beta.gouv.fr>
2024-12-11 19:12:28 +0100

    Contact Form : Corrige l'export de la méthode

--- 2024-frontend/src/services/matomo.js
+++ 2024-frontend/src/services/matomo.js
@@ -1,8 +1,8 @@
-export default {
-  trackEvent(params) {
-    if (window.MATOMO_ID) {
-      const { category, action, value } = params
-      window._paq.push(["trackEvent", category, action, value])
-    }
-  },
+const trackEvent = (params) => {
+  if (window.MATOMO_ID) {
+    const { category, action, value } = params
+    window._paq.push(["trackEvent", category, action, value])
+  }
 }
+
+export { trackEvent }
