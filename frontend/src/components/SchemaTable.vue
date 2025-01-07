b6709d07df75c428d68d1da93dfc5c0e25bfc0ba
Raphael Odini <raphodn@users.noreply.github.com>
2025-01-07 11:31:01 +0100

    refactor(Import de masse): basculer la logique de documentation (schema -> table) dans un composant dédié

---
+++ frontend/src/components/SchemaTable.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-simple-table class="my-6">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th>Titre</th>
+          <th>Champ</th>
+          <th>Description</th>
+          <th>Type</th>
+          <th>Exemple</th>
+          <th>Obligatoire</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(field, idx) in fieldList" :key="idx">
+          <td>{{ field.name }}</td>
+          <td>{{ field.title }}</td>
+          <td>
+            <p>{{ field.description }}</p>
+            <p v-if="field.constraints && field.constraints.enum">
+              <span>Options acceptées :&#32;</span>
+              <span v-for="(item, idx) in field.constraints.enum" :key="idx">
+                <code>{{ item }}</code>
+                <span v-if="idx < field.constraints.enum.length - 1">,&#32;</span>
+              </span>
+            </p>
+            <p v-if="field.constraints && field.constraints.enum && field.constraints.enum_multiple">
+              Spécifiez plusieurs options en séparant avec un
+              <code>{{ field.constraints.enum_multiple_seperator }}</code>
+              .
+            </p>
+          </td>
+          <td style="min-width: 150px;">{{ getSchemaFieldType(field) }}</td>
+          <td>{{ field.example }}</td>
+          <td class="text-center">{{ field.constraints && field.constraints.required ? "✔" : "✘" }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+import Constants from "@/constants"
+
+export default {
+  props: {
+    schemaUrl: String,
+  },
+  data() {
+    return {
+      fieldList: [],
+    }
+  },
+  mounted() {
+    this.fetchSchema()
+  },
+  methods: {
+    fetchSchema() {
+      fetch(this.schemaUrl)
+        .then((response) => response.json())
+        .then((json) => {
+          this.fieldList = json.fields
+        })
+    },
+    getSchemaFieldType(field) {
+      if (field.name in Constants.SchemaTypes) {
+        return Constants.SchemaTypes[field.name]
+      }
+      if (field.constraints && field.constraints.enum) {
+        if (field.constraints.enum_multiple) {
+          return Constants.SchemaTypes[`${field.type}_enum_multiple`]
+        }
+        return Constants.SchemaTypes[`${field.type}_enum`]
+      }
+      return Constants.SchemaTypes[field.type]
+    },
+  },
+}
+</script>
