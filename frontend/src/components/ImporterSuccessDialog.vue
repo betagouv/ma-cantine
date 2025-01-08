0492caee448f887a2fefc3e9f21d6b354e99a876
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-08 13:15:11 +0100

    ImporterSuccessDialog: Déplace le bouton fermer en haut à droite

--- frontend/src/components/ImporterSuccessDialog.vue
+++ frontend/src/components/ImporterSuccessDialog.vue
@@ -1,6 +1,12 @@
 <template>
   <v-dialog v-model="showDialog" max-width="600">
-    <v-card class="pt-4">
+    <v-card>
+      <div class="pa-4 d-flex align-center" style="background-color: #F5F5F5">
+        <v-spacer></v-spacer>
+        <v-btn color="primary" outlined @click="showDialog = false">
+          Fermer
+        </v-btn>
+      </div>
       <v-card-title class="mb-6">
         <img src="/static/images/picto-dsfr/success.svg" width="50px" />
         <h1 class="fr-h5 mb-0 ml-2">
@@ -15,16 +21,11 @@
             est ouverte.
           </p>
         </v-row>
-        <v-row justify="center" class="mb-2 ml-0 mr-0">
-          <v-btn color="primary" outlined class="px-4 mr-2" @click="showDialog = false">
-            Fermer
+        <router-link :to="{ name: 'ManagementPage' }">
+          <v-btn color="primary" class="px-4">
+            Aller sur mon tableau de bord
           </v-btn>
-          <router-link :to="{ name: 'ManagementPage' }">
-            <v-btn color="primary" class="px-4">
-              Aller sur mon tableau de bord
-            </v-btn>
-          </router-link>
-        </v-row>
+        </router-link>
       </v-card-text>
     </v-card>
   </v-dialog>
