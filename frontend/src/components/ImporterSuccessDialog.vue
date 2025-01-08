65fcae72943bee9febbac74b5d22cf1aef2a4076
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-08 13:18:03 +0100

    ImporterSucessDialog: Aligne le bouton à droite et corrige l'alignement

--- frontend/src/components/ImporterSuccessDialog.vue
+++ frontend/src/components/ImporterSuccessDialog.vue
@@ -7,26 +7,31 @@
           Fermer
         </v-btn>
       </div>
-      <v-card-title class="mb-6">
-        <img src="/static/images/picto-dsfr/success.svg" width="50px" />
-        <h1 class="fr-h5 mb-0 ml-2">
-          Le fichier a été importé avec succès
-        </h1>
-      </v-card-title>
-      <v-card-text class="text-left">
-        <v-row class="mb-4 ml-0 mr-0">
-          <p class="font-weight-bold">{{ description }}</p>
-          <p>
-            Depuis votre tableau de bord, vous devez télédéclarer vos données, lorsque la campagne de télédéclaration
-            est ouverte.
-          </p>
-        </v-row>
-        <router-link :to="{ name: 'ManagementPage' }">
-          <v-btn color="primary" class="px-4">
-            Aller sur mon tableau de bord
-          </v-btn>
-        </router-link>
-      </v-card-text>
+      <div class="pa-6">
+        <v-card-title class="mb-6">
+          <img src="/static/images/picto-dsfr/success.svg" width="50px" />
+          <h1 class="fr-h5 mb-0 ml-2">
+            Le fichier a été importé avec succès
+          </h1>
+        </v-card-title>
+        <v-card-text class="text-left">
+          <v-row class="mb-4 ml-0 mr-0">
+            <p class="font-weight-bold">{{ description }}</p>
+            <p>
+              Depuis votre tableau de bord, vous devez télédéclarer vos données, lorsque la campagne de télédéclaration
+              est ouverte.
+            </p>
+          </v-row>
+          <v-row>
+            <v-spacer></v-spacer>
+            <router-link :to="{ name: 'ManagementPage' }">
+              <v-btn color="primary" class="px-4">
+                Aller sur mon tableau de bord
+              </v-btn>
+            </router-link>
+          </v-row>
+        </v-card-text>
+      </div>
     </v-card>
   </v-dialog>
 </template>
