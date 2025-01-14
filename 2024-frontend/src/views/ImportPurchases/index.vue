c52f3e4403d5447cea668a3f9a43ba0043e027e7
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-14 15:56:24 +0100

    ImportPurchases: Créer la nouvelle page

---
+++ 2024-frontend/src/views/ImportPurchases/index.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <div class="fr-col-7">
+    <h1>Créer des achats</h1>
+    <p>
+      Texte d'introduction à venir. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed
+      consectetur. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+      amet risus. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+  </div>
+</template>
