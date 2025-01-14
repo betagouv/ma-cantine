2b72aee5a4ee1fbdb800022e0d6bb49843178b7d
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-14 16:40:54 +0100

    ImportPurchase: Ajout d'un composant pour l'explication de l'import

---
+++ 2024-frontend/src/components/ImportExplanation.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="fr-grid-row fr-grid-row--bottom">
+    <div class="fr-col-12 fr-col-md-8">
+      <h2 class="fr-h5">Comment ça marche ?</h2>
+      <p>
+        Nous scannons la structure du fichier, puis les valeurs des cellules avant d’importer.
+        <br />
+        <strong>En cas d’erreurs, le fichier est automatiquement rejeté.</strong>
+      </p>
+      <p>Le fichier, les colonnes et les valeurs des cellules doivent impérativement respecter ce format :</p>
+      <ul>
+        <li>
+          Le fichier doit être
+          <strong>au format CSV</strong>
+        </li>
+        <li>
+          Il doit
+          <strong>commencer par une ligne en-tête</strong>
+        </li>
+        <li>
+          Le
+          <strong>nom des colonnes doit être écrit exactement</strong>
+          comme ci-dessous
+        </li>
+        <li>
+          Les
+          <strong>colonnes doivent être dans l'ordre</strong>
+          comme ci-dessous
+        </li>
+        <li>
+          <strong>Une ligne</strong>
+          doit contenir les informations d’
+          <strong>une cantine uniquement</strong>
+        </li>
+        <li>
+          Pour modifier les informations d’une cantine existante,
+          <strong>vous devez avoir un compte utilisateur par cantine inscrite</strong>
+          , autrement le fichier sera rejeté.
+        </li>
+      </ul>
+    </div>
+    <div class="fr-col-offset-md-1"></div>
+    <div class="fr-col-12 fr-col-md-3 fr-text-title--blue-france">
+      <p class="ma-cantine--bold">
+        <span class="fr-icon-star-s-fill" aria-hidden="true"></span>
+        Ressources utiles
+      </p>
+    </div>
+  </section>
+</template>
--- 2024-frontend/src/views/ImportPurchases/index.vue
+++ 2024-frontend/src/views/ImportPurchases/index.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import ImportExplanation from "@/components/ImportExplanation.vue"
+</script>
+
 <template>
-  <div class="fr-col-7">
-    <h1>Créer des achats</h1>
-    <p>
-      Texte d'introduction à venir. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed
-      consectetur. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
-      amet risus. Nullam quis risus eget urna mollis ornare vel eu leo.
-    </p>
-  </div>
+  <h1>Créer des achats</h1>
+  <p class="fr-col-8">
+    Texte d'introduction à venir. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed
+    consectetur. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+    amet risus. Nullam quis risus eget urna mollis ornare vel eu leo.
+  </p>
+  <ImportExplanation />
 </template>
