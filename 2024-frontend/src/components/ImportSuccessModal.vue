0004b54176c63f995c95a6d87bdffb20ed0bfa13
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-15 17:27:04 +0100

    ImportPurchase: Déplace la modal dans un composant réutilisable

---
+++ 2024-frontend/src/components/ImportSuccessModal.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+defineProps(["message", "opened"])
+</script>
+
+<template>
+  <DsfrModal
+    :opened="opened"
+    class="fr-modal--opened"
+    title="Le fichier a été importé avec succès"
+    icon="fr-icon-checkbox-circle-fill"
+    @close="$emit('close')"
+    :actions="[
+      {
+        label: 'Aller sur mon tableau de bord',
+        onClick() {
+          router.push({ name: 'ManagementPage' })
+        },
+      },
+    ]"
+  >
+    <template #default>
+      <p class="ma-cantine--bold">
+        {{ message }}
+      </p>
+      <p>
+        Depuis votre tableau de bord, vous devez télédéclarer vos données, lorsque la campagne de télédéclaration est
+        ouverte.
+      </p>
+    </template>
+  </DsfrModal>
+</template>
