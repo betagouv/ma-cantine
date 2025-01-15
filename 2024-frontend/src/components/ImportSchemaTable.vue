b151931ebf7a7321993e46df53a41b4e189d8763
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-15 10:39:42 +0100

    ImportPurchase: Déplace la fonction dans le service pour alléger le composant

--- 2024-frontend/src/components/ImportSchemaTable.vue
+++ 2024-frontend/src/components/ImportSchemaTable.vue
@@ -9,17 +9,6 @@ const rows = shallowRef([])
 
 /* Schema data */
 const props = defineProps(["url"])
-const schemaTypes = {
-  date: "Date (au format AAAA-MM-JJ)",
-  integer: "Chiffre",
-  number: "Chiffre",
-  siret: "14 chiffres (avec ou sans espaces)",
-  siret_livreur_repas: "14 chiffres (avec ou sans espaces)",
-  string: "Texte (libre)",
-  string_enum: "Texte (choix unique)",
-  string_enum_multiple: "Texte (choix multiples)",
-  year: "Année (AAAA)",
-}
 schemaService.getFields(props.url).then((fields) => {
   rows.value = formatFieldsForRows(fields)
 })
@@ -38,27 +27,13 @@ const formatFieldsForRows = (fields) => {
         multiple: field.constraints.enum_multiple,
         separator: field.constraints.enum_multiple_seperator,
       },
-      getType(field),
+      schemaService.getFieldType(field),
       field.example,
       field.constraints && field.constraints.required ? "Oui" : "Non",
     ]
     cleanedFields.push(row)
   })
   return cleanedFields
-}
-
-/* Clean cells content */
-const getType = (field) => {
-  if (field.name in schemaTypes) {
-    return schemaTypes[field.name]
-  }
-  if (field.constraints && field.constraints.enum) {
-    if (field.constraints.enum_multiple) {
-      return schemaTypes[`${field.type}_enum_multiple`]
-    }
-    return schemaTypes[`${field.type}_enum`]
-  }
-  return schemaTypes[field.type]
 }
 </script>
 
