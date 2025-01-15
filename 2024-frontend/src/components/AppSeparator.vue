b30bbb8e092c0bbd379e6d1a1b88e57ae6fd37cb
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-15 17:55:17 +0100

    ImportPurchase: Composant non présent dans vue-dsfr

---
+++ 2024-frontend/src/components/AppSeparator.vue
@@ -0,0 +1,12 @@
+<template>
+  <i :aria-hidden="true" class="app-separator"></i>
+</template>
+
+<style lang="scss">
+.app-separator {
+  width: 100%;
+  height: 1px;
+  display: block;
+  background-color: var(--border-disabled-grey);
+}
+</style>
