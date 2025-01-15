cc390951ecb4d87065f5de3eef5a20ef6236370a
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-15 10:39:42 +0100

    ImportPurchase: Déplace la fonction dans le service pour alléger le composant

--- 2024-frontend/src/services/schemas.js
+++ 2024-frontend/src/services/schemas.js
@@ -4,4 +4,28 @@ const getFields = async (url) => {
     .then((json) => json.fields)
 }
 
-export default { getFields }
+const types = {
+  date: "Date (au format AAAA-MM-JJ)",
+  integer: "Chiffre",
+  number: "Chiffre",
+  siret: "14 chiffres (avec ou sans espaces)",
+  siret_livreur_repas: "14 chiffres (avec ou sans espaces)",
+  string: "Texte (libre)",
+  string_enum: "Texte (choix unique)",
+  string_enum_multiple: "Texte (choix multiples)",
+  year: "Année (AAAA)",
+}
+const getFieldType = (field) => {
+  if (field.name in types) {
+    return types[field.name]
+  }
+  if (field.constraints && field.constraints.enum) {
+    if (field.constraints.enum_multiple) {
+      return types[`${field.type}_enum_multiple`]
+    }
+    return types[`${field.type}_enum`]
+  }
+  return types[field.type]
+}
+
+export default { getFields, getFieldType }
