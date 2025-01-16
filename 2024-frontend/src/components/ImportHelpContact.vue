084b6c1fee851905cbd827625a55989100ce33af
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-16 15:21:46 +0100

    ImportPurchase: Créer un composant pour le bloc de contact

---
+++ 2024-frontend/src/components/ImportHelpContact.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppLinkMailto from "@/components/AppLinkMailto.vue"
+</script>
+
+<template>
+  <section class="fr-grid-row fr-grid-row--center">
+    <div class="fr-col-8">
+      <h2>Vous n’arrivez pas à importer votre fichier ?</h2>
+      <p class="fr-pr-12w">
+        Si votre fichier comptable agrégé ne ressemble pas du tout à ça, vous pouvez nous l'envoyer à l'adresse
+        <AppLinkMailto />
+      </p>
+    </div>
+  </section>
+</template>
