4033876ecdeffb34ee7c7f9d7b4aec8decce3275
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-16 15:30:16 +0100

    ImportPurchase: Ajoute le séparateur

--- 2024-frontend/src/components/ImportHelpContact.vue
+++ 2024-frontend/src/components/ImportHelpContact.vue
@@ -1,10 +1,15 @@
 <script setup>
 import AppLinkMailto from "@/components/AppLinkMailto.vue"
+import AppSeparator from "@/components/AppSeparator.vue"
 </script>
 
 <template>
   <section class="fr-grid-row fr-grid-row--center">
     <div class="fr-col-8">
+      <div class="import-help-center-separator fr-mb-5w">
+        <AppSeparator />
+        <DsfrBadge class="import-help-center-separator__badge" type="none" label="Besoin d'aide ?" />
+      </div>
       <h2>Vous n’arrivez pas à importer votre fichier ?</h2>
       <p class="fr-pr-12w">
         Si votre fichier comptable agrégé ne ressemble pas du tout à ça, vous pouvez nous l'envoyer à l'adresse
@@ -13,3 +18,15 @@ import AppLinkMailto from "@/components/AppLinkMailto.vue"
     </div>
   </section>
 </template>
+
+<style lang="scss">
+.import-help-center-separator {
+  position: relative;
+
+  &__badge {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
