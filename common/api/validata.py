ceeba0c4353491671fcad60f60b6d586b0c5cb26
Raphael Odini <raphodn@users.noreply.github.com>
2025-01-24 19:41:46 +0100

    _process_validata_errors

--- common/api/validata.py
+++ common/api/validata.py
@@ -1,14 +1,27 @@
 import requests
 
 
-def validate_file_against_schema(self):
+def validate_file_against_schema(file, schema_url):
     # Reset the file pointer to the beginning
-    self.file.seek(0)
+    file.seek(0)
     response = requests.post(
         "https://api.validata.etalab.studio/validate",
         files={
-            "file": ("file.csv", self.file.read(), self.file.content_type),
+            "file": ("file.csv", file.read(), file.content_type),
         },
-        data={"schema": self.schema_url, "header_case": True},
+        data={"schema": schema_url, "header_case": True},
     )
     return response.json()["report"]
+
+
+def process_errors(report):
+    errors = []
+    for error in report["tasks"][0]["errors"]:
+        errors.append(
+            {
+                "row": error["rowNumber"],
+                "status": 400,
+                "message": f"Champ '{error['fieldName']}' : Valeur « {error['cell']} » : {error['message']}",
+            }
+        )
+    return errors
