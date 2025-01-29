8ff0f8d44bf35c7d76692ee75aa7efbc39e7e694
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-01-29 13:44:57 +0100

    Evite la sépération au retour à la ligne

--- 2024-frontend/src/views/ImportCanteens/index.vue
+++ 2024-frontend/src/views/ImportCanteens/index.vue
@@ -6,7 +6,7 @@ console.log("Import cantine")
   <h1>Importer des cantines</h1>
   <p class="fr-col-12 fr-col-md-7">
     Notre outil d’import de masse vous permet de créer vos cantines ou de modifier vos cantines existantes d’un coup.
-    <strong>Il concerne uniquement les gestionnaires qui ont plus de 5 cantines.</strong>
+    <strong>Il concerne uniquement les gestionnaires qui ont plus de 5&nbsp;cantines.</strong>
     Si vous avez moins de 5 cantines vous pouvez créer ou modifier des cantines depuis
     <router-link :to="{ name: 'NewCanteen' }">notre formulaire</router-link>
     .
