ecf1eb5b73bdfdf2405823d41c53a5a34916be52
Charline <charline.laporte@beta.gouv.fr>
2025-02-04 18:41:05 +0100

    ci(Vue3): Change l'organisation du code des views et du router (#5003)

---
+++ 2024-frontend/src/router/vue3.js
@@ -0,0 +1,92 @@
+import WasteMeasurementTunnel from "@/views/WasteMeasurementTunnel.vue"
+import ImportSelection from "@/views/ImportSelection.vue"
+import WasteMeasurements from "@/views/WasteMeasurements.vue"
+import LegalNotices from "@/views/LegalNotices.vue"
+import AccessibilityDeclaration from "@/views/AccessibilityDeclaration.vue"
+import CGU from "@/views/CGU.vue"
+import ContactPage from "@/views/ContactPage.vue"
+import ImportPurchases from "@/views/ImportPurchases.vue"
+
+const vue3routes = [
+  {
+    path: "/evaluation-gaspillage-alimentaire/:canteenUrlComponent/:id?",
+    name: "WasteMeasurementTunnel",
+    component: WasteMeasurementTunnel,
+    props: (route) => ({ ...route.query, ...route.params }),
+    meta: {
+      title: "Évaluation déchets alimentaires",
+      authenticationRequired: true,
+      fullscreen: true,
+    },
+  },
+  {
+    path: "/importer-des-donnees",
+    name: "ImportSelection",
+    component: ImportSelection,
+    meta: {
+      title: "Importer des données",
+      authenticationRequired: true,
+    },
+  },
+  {
+    path: "/gaspillage-alimentaire/:canteenUrlComponent",
+    name: "WasteMeasurements",
+    component: WasteMeasurements,
+    props: (route) => ({ ...route.params }),
+    meta: {
+      title: "Déchets alimentaires",
+      authenticationRequired: true,
+      breadcrumbs: [
+        { to: { name: "ManagementPage" }, title: "Mon tableau de bord" },
+        { to: { name: "DashboardManager" }, useCanteenName: true },
+      ],
+    },
+  },
+  {
+    path: "/mentions-legales",
+    name: "LegalNotices",
+    component: LegalNotices,
+    meta: {
+      title: "Mentions légales",
+    },
+  },
+  {
+    path: "/accessibilite",
+    name: "AccessibilityDeclaration",
+    component: AccessibilityDeclaration,
+    meta: {
+      title: "Déclaration d'accessibilité",
+    },
+  },
+  {
+    path: "/cgu",
+    name: "CGU",
+    component: CGU,
+    meta: {
+      title: "Conditions générales d'utilisation",
+    },
+  },
+  {
+    path: "/contact",
+    name: "ContactPage",
+    component: ContactPage,
+    meta: {
+      title: "Contactez-nous",
+    },
+  },
+  {
+    path: "/importer-des-donnees/achats",
+    name: "ImportPurchases",
+    component: ImportPurchases,
+    meta: {
+      title: "Importer des achats",
+      authenticationRequired: true,
+      breadcrumbs: [
+        { to: { name: "ManagementPage" }, title: "Mon tableau de bord" },
+        { to: { name: "ImportSelection" }, title: "Importer des données" },
+      ],
+    },
+  },
+]
+
+export default vue3routes
