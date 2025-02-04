0fdc3274270a0197419fb256bb3612877f130274
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-02-04 16:42:20 +0100

    Ajoute le mode verbose

--- clevercloud/pre-build-review-app-hook.sh
+++ clevercloud/pre-build-review-app-hook.sh
@@ -6,5 +6,5 @@ cp ./clevercloud/manifest.json build/.vite/
 mkdir 2024-frontend/build
 mkdir 2024-frontend/build/.vite
 cp ./clevercloud/manifest.json 2024-frontend/build/.vite/
-clever service link-addon $1
+clever service link-addon $1 -v
 echo "addon id: $1"
