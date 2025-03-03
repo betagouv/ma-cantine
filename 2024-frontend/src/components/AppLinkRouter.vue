6ffd5c5debb6339ee72174d53b8980b522e6f92b
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-03 12:17:37 +0100

    Créer un composant pour la stylisation des liens internes

---
+++ 2024-frontend/src/components/AppLinkRouter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineProps } from "vue"
+defineProps(["to", "title"])
+</script>
+
+<template>
+  <router-link :to="to">
+    {{ title }}
+    <span class="fr-icon-arrow-right-line ma-cantine--icon-xs" aria-hidden="true"></span>
+  </router-link>
+</template>
