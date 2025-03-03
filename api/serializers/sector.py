d416d7dec97cd34c54a13f18d8e09dcd5686a9b1
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-03 23:52:53 +0100

    Ajoute un champ dans le serializer

--- api/serializers/sector.py
+++ api/serializers/sector.py
@@ -5,11 +5,14 @@ from data.models import Sector
 
 # remember to update TD version if you update this
 class SectorSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="get_category_display")
+
     class Meta:
         model = Sector
         fields = (
             "id",
             "name",
             "category",
+            "category_name",
             "has_line_ministry",
         )
