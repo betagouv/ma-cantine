861ef40c9584077f2f50fb29e3c5c0be02bfedb7
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-03 12:37:29 +0100

    Correction espacements du bloc

--- 2024-frontend/src/components/AppRessources.vue
+++ 2024-frontend/src/components/AppRessources.vue
@@ -1,11 +1,21 @@
 <template>
-  <article class="fr-card fr-col-12 fr-col-md-4 fr-px-7v fr-pt-7v fr-text-title--blue-france">
+  <article class="app-ressources fr-card fr-col-12 fr-col-md-4 fr-p-7v fr-text-title--blue-france">
     <h3 class="ma-cantine--bold fr-mb-1w fr-text--lg ma-cantine--inherit-text-color">
       <span class="fr-icon-question-fill" aria-hidden="true"></span>
       Ressources utiles
     </h3>
-    <ul class="ma-cantine--unstyled-list">
+    <ul class="app-ressources__list ma-cantine--unstyled-list fr-mb-0">
       <slot></slot>
     </ul>
   </article>
 </template>
+
+<style lang="scss">
+.app-ressources {
+  &__list {
+    li:last-child {
+      padding-bottom: 0 !important;
+    }
+  }
+}
+</style>
