d9f3bfc7aa30c6c57d24b91ef853cc350880008d
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-03 12:18:01 +0100

    Créer un composant pour le style de bloc ressources utiles

---
+++ 2024-frontend/src/components/AppRessources.vue
@@ -0,0 +1,11 @@
+<template>
+  <article class="fr-card fr-col-12 fr-col-md-4 fr-px-7v fr-pt-7v fr-text-title--blue-france">
+    <h3 class="ma-cantine--bold fr-mb-1w fr-text--lg ma-cantine--inherit-text-color">
+      <span class="fr-icon-question-fill" aria-hidden="true"></span>
+      Ressources utiles
+    </h3>
+    <ul class="ma-cantine--unstyled-list">
+      <slot></slot>
+    </ul>
+  </article>
+</template>
