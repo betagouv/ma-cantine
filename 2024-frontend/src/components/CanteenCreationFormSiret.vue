c2148f3de6b000fee17e0e23e74776ceb57a5940
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-04 16:04:19 +0100

    Ajoute les informations static du composant

---
+++ 2024-frontend/src/components/CanteenCreationFormSiret.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <p class="fr-mb-0">Mon établissement</p>
+    <p class="fr-hint-text">
+      Nous utilisons le site
+      <a href="https://annuaire-entreprises.data.gouv.fr/" target="_blank">annuaire-des-entreprises</a>
+      afin de retrouver les informations votre établissement
+    </p>
+    <DsfrSearchBar placeholder="Tapez votre n° SIRET" button-text="Rechercher" :large="true" />
+  </div>
+</template>
