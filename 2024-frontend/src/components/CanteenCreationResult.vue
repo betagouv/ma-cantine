0580fe8fa48795cc66da3634f764570403c2b192
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-05 09:37:35 +0100

    Double espace pour la ponctuation en français

--- 2024-frontend/src/components/CanteenCreationResult.vue
+++ 2024-frontend/src/components/CanteenCreationResult.vue
@@ -12,10 +12,10 @@ defineProps(["name", "siret", "city", "department", "status"])
       <div class="fr-col-offset-1"></div>
       <ul class="ma-cantine--unstyled-list fr-my-0 fr-col-6">
         <li>
-          <p class="fr-mb-0 fr-text--xs">SIRET: {{ siret }}</p>
+          <p class="fr-mb-0 fr-text--xs">SIRET : {{ siret }}</p>
         </li>
         <li>
-          <p class="fr-mb-0 fr-text--xs">Ville: {{ city }} ({{ department }})</p>
+          <p class="fr-mb-0 fr-text--xs">Ville : {{ city }} ({{ department }})</p>
         </li>
       </ul>
     </div>
