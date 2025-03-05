eec5b71d50b53720306d984429ac64c834c75134
Charline <charline.laporte@beta.gouv.fr>
2025-03-05 21:30:40 +0100

    Update 2024-frontend/src/components/CanteenCreationSiret.vue
    Co-authored-by: Raphaël Odini <raphodn@users.noreply.github.com>

--- 2024-frontend/src/components/CanteenCreationSiret.vue
+++ 2024-frontend/src/components/CanteenCreationSiret.vue
@@ -99,7 +99,7 @@ const unselectCanteen = () => {
     <p class="fr-hint-text">
       Nous utilisons le site
       <a href="https://annuaire-entreprises.data.gouv.fr/" target="_blank">annuaire-des-entreprises</a>
-      afin de retrouver les informations votre établissement
+      afin de retrouver les informations de votre établissement
     </p>
     <DsfrInputGroup :error-message="error">
       <template #default>
