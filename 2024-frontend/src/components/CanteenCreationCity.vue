0952d74f01d43195aa916c373e7790a069c317bd
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-11 16:32:12 +0100

    Utilise le style du multiselect

--- 2024-frontend/src/components/CanteenCreationCity.vue
+++ 2024-frontend/src/components/CanteenCreationCity.vue
@@ -15,17 +15,24 @@ const findCities = () => {
     .findCities(trimSearch)
     .then((response) => {
       if (response.features.length > 0) displayOptions(response.features)
-      else noResults.value = `Aucune commune trouvée pour « ${trimSearch} »`
+      else displayError(trimSearch)
     })
     .catch((e) => {
       console.log("error", e)
     })
 }
 
+const displayError = (name) => {
+  noResults.value = `Aucune commune trouvée pour « ${name} »`
+  expanded.value = false
+}
+
 /* List of options */
+const expanded = ref(false)
 const citiesOption = ref([])
 const displayOptions = (options) => {
   noResults.value = false
+  expanded.value = true
   const cleanedOptions = options.map((option, index) => {
     return {
       value: index,
@@ -47,24 +54,47 @@ const selectCity = () => {
   citiesOption.value = []
   search.value = city
   citySelected.value = null
+  expanded.value = false
 }
 </script>
 
 <template>
-  <DsfrInputGroup
-    label="Ville *"
-    label-visible
-    v-model="search"
-    placeholder="Tapez les 3 premières lettre de votre ville"
-    @update:modelValue="findCities()"
-    :error-message="noResults"
-  />
-  <DsfrRadioButtonSet
-    v-if="citiesOption.length > 0"
-    legend="Sélectionnez une commune parmis la liste"
-    v-model="citySelected"
-    :options="citiesOption"
-    @update:modelValue="selectCity()"
-    small
-  />
+  <div class="canteen-creation-city fr-select-group">
+    <div class="canteen-creation-city__input fr-multi-select">
+      <DsfrInputGroup
+        label="Ville *"
+        label-visible
+        v-model="search"
+        placeholder="Tapez les 3 premières lettre de votre ville"
+        @update:modelValue="findCities()"
+        :error-message="noResults"
+      />
+    </div>
+    <DsfrRadioButtonSet
+      v-if="citiesOption.length > 0"
+      hint="Sélectionnez une commune parmis la liste"
+      class="canteen-creation-city__options fr-multiselect__collapse fr-collapse"
+      :class="{
+        'fr-collapse--expanded': expanded,
+      }"
+      v-model="citySelected"
+      :options="citiesOption"
+      @update:modelValue="selectCity()"
+      small
+    />
+  </div>
 </template>
+
+<style lang="scss">
+.canteen-creation-city {
+  &__input {
+    .fr-input-group {
+      margin-bottom: 0.25rem !important;
+    }
+  }
+
+  &__options {
+    width: 100% !important;
+  }
+}
+</style>
