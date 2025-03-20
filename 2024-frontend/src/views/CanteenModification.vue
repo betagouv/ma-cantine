42e1f38df4b766b5160883a3afd886c24431ed97
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-20 13:06:29 +0100

    Affichage du chargement avec un composant réutilisable basic

--- 2024-frontend/src/views/CanteenModification.vue
+++ 2024-frontend/src/views/CanteenModification.vue
@@ -4,6 +4,7 @@ import { useRoute } from "vue-router"
 import { useRootStore } from "@/stores/root"
 import canteenService from "@/services/canteens.js"
 import CanteenEstablishmentForm from "@/components/CanteenEstablishmentForm.vue"
+import AppLoader from "@/components/AppLoader.vue"
 
 /* Router and Store */
 const route = useRoute()
@@ -27,7 +28,7 @@ canteenService
   <section class="fr-grid-row fr-grid-row--bottom">
     <h1>{{ route.meta.title }}</h1>
   </section>
-  <p v-if="loading">Chargement en cours...</p>
+  <AppLoader v-if="loading" />
   <p v-else-if="!loading && !canteenData.id">Erreur retourne à la page précédente ?</p>
   <CanteenEstablishmentForm v-else-if="!loading && canteenData.id" :establishment-data="canteenData" />
 </template>
