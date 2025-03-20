d331873e0bf6bd99205619bc84622807d8726c09
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-20 12:52:55 +0100

    Récupère les informations de la cantine à modifier

--- 2024-frontend/src/views/CanteenModification.vue
+++ 2024-frontend/src/views/CanteenModification.vue
@@ -1,19 +1,33 @@
 <script setup>
-import { computed } from "vue"
+import { ref } from "vue"
 import { useRoute } from "vue-router"
+import { useRootStore } from "@/stores/root"
+import canteenService from "@/services/canteens.js"
 import CanteenEstablishmentForm from "@/components/CanteenEstablishmentForm.vue"
 
 /* Router and Store */
 const route = useRoute()
+const store = useRootStore()
 
 /* Get establishemnt infos */
-const canteenId = computed(() => route.params.canteenUrlComponent.split("--")[0])
-console.log("canteenId", canteenId)
+const canteenData = ref({})
+const loading = ref(true)
+const canteenId = route.params.canteenUrlComponent.split("--")[0]
+canteenService
+  .fetchCanteen(canteenId)
+  .then((response) => {
+    loading.value = false
+    if (response.id) canteenData.value = response
+    else store.notifyServerError()
+  })
+  .catch((e) => store.notifyServerError(e))
 </script>
 
 <template>
   <section class="fr-grid-row fr-grid-row--bottom">
     <h1>{{ route.meta.title }}</h1>
   </section>
-  <CanteenEstablishmentForm establishment-data="" />
+  <p v-if="loading">Chargement en cours...</p>
+  <p v-else-if="!loading && !canteenData.id">Erreur retourne à la page précédente ?</p>
+  <CanteenEstablishmentForm v-else-if="!loading && canteenData.id" :establishment-data="canteenData" />
 </template>
