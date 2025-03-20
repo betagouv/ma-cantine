d75cfd200366d9155a474ece12ed53126d6e4495
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-03-20 12:24:52 +0100

    Récupère l'id de la cantine depuis l'url

--- 2024-frontend/src/views/CanteenModification.vue
+++ 2024-frontend/src/views/CanteenModification.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { computed } from "vue"
 import { useRoute } from "vue-router"
 import CanteenEstablishmentForm from "@/components/CanteenEstablishmentForm.vue"
 
+/* Router and Store */
 const route = useRoute()
+
+/* Get establishemnt infos */
+const canteenId = computed(() => route.params.canteenUrlComponent.split("--")[0])
+console.log("canteenId", canteenId)
 </script>
 
 <template>
