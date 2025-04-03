56bd50c5f2a4ade1511b40056e063fb78c8eef5a
Raphael Odini <raphodn@users.noreply.github.com>
2025-04-03 11:38:54 +0200

    Add user. add test

---
+++ data/tests/test_fixtures.py
@@ -0,0 +1,17 @@
+from django.core.management import call_command
+from django.test import TestCase
+
+from data.models import Canteen, Diagnostic, Sector, User
+
+
+class FixturesTest(TestCase):
+    def test_load_fixtures(self):
+        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(Canteen.objects.count(), 0)
+        self.assertEqual(Diagnostic.objects.count(), 0)
+        self.assertEqual(Sector.objects.count(), 0)
+        call_command("loaddata", "initial_data.json")
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(Canteen.objects.count(), 1)
+        self.assertEqual(Diagnostic.objects.count(), 1)
+        self.assertEqual(Sector.objects.count(), 1)
