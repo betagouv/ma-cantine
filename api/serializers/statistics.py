1022d66d2925bd49e60aff0624241913197c7fc8
qloridant <quentin.loridant@protonmail.com>
2025-04-03 19:26:19 +0200

    Remplacer les diagnostics par les teledeclarations dans le serializer. Suppression pour simplification les badges de l API statistique

--- api/serializers/statistics.py
+++ api/serializers/statistics.py
@@ -2,53 +2,19 @@ import logging
 
 from django.db.models import Sum
 from rest_framework import serializers
-
 from common.utils.badges import badges_for_queryset
 from data.models import Sector
 
 logger = logging.getLogger(__name__)
 
 
-class CanteenStatisticsSerializer(serializers.Serializer):
-    canteen_count = serializers.IntegerField()
-    bio_percent = serializers.IntegerField()
-    sustainable_percent = serializers.IntegerField()
-    diagnostics_count = serializers.IntegerField()
-    approPercent = serializers.IntegerField()
-    sector_categories = serializers.DictField()
-    epci_error = serializers.CharField(required=False)
-
-    @staticmethod
-    def calculate_statistics(canteens, diagnostics):
-        data = {"canteen_count": canteens.count(), "diagnostics_count": diagnostics.count()}
-
-        agg = diagnostics.is_filled().aggregate(
-            Sum("value_bio_ht", default=0),
-            Sum("value_total_ht", default=0),
-            Sum("value_sustainable_ht", default=0),
-            Sum("value_externality_performance_ht", default=0),
-            Sum("value_egalim_others_ht", default=0),
-        )
-
-        if agg["value_total_ht__sum"] > 0:
-            data["bio_percent"] = round(100 * agg["value_bio_ht__sum"] / agg["value_total_ht__sum"])
-            data["sustainable_percent"] = round(
-                100
-                * (
-                    agg["value_sustainable_ht__sum"]
-                    + agg["value_externality_performance_ht__sum"]
-                    + agg["value_egalim_others_ht__sum"]
-                )
-                / agg["value_total_ht__sum"]
-            )
-        else:
-            data["bio_percent"] = 0
-            data["sustainable_percent"] = 0
-
-        badge_querysets = badges_for_queryset(diagnostics)
-        total_diag = data["diagnostics_count"]
-        data["approPercent"] = int(badge_querysets["appro"].count() / total_diag * 100) if total_diag else 0
-
+def calculate_statistics_canteens(canteens, data):
+    if canteens:
+        nbre_canteens = canteens.count()
+    else:
+        nbre_canteens = 0
+    data["canteen_count"] = nbre_canteens
+    if canteens:
         sector_categories_data = {}
         for category in Sector.Categories:
             sectors = Sector.objects.filter(category=category)
@@ -56,5 +22,61 @@ class CanteenStatisticsSerializer(serializers.Serializer):
         sectors = Sector.objects.filter(category=None)
         sector_categories_data["inconnu"] = canteens.filter(sectors__in=sectors).count()
         data["sector_categories"] = sector_categories_data
+    return data
 
+
+def calculate_statistics_teledeclarations(teledeclarations, data):
+    if teledeclarations:
+        nbre_teledeclarations = teledeclarations.count()
+    else:
+        nbre_teledeclarations = 0
+    data["teledeclarations_count"] = nbre_teledeclarations
+
+    if nbre_teledeclarations:
+        agg = teledeclarations.aggregate(
+            Sum("value_bio_ht_agg", default=0),
+            Sum("value_total_ht", default=0),
+            Sum("value_sustainable_ht_agg", default=0),
+            Sum("value_externality_performance_ht_agg", default=0),
+            Sum("value_egalim_others_ht_agg", default=0),
+        )
+
+        if agg["value_total_ht__sum"] > 0:
+            data["bio_percent"] = round(100 * agg["value_bio_ht_agg__sum"] / agg["value_total_ht__sum"])
+            data["sustainable_percent"] = round(
+                100
+                * (
+                    agg["value_sustainable_ht_agg__sum"]
+                    + agg["value_externality_performance_ht_agg__sum"]
+                    + agg["value_egalim_others_ht_agg__sum"]
+                )
+                / agg["value_total_ht__sum"]
+            )
+        else:
+            data["bio_percent"] = 0
+            data["sustainable_percent"] = 0
+    else:
+        data["bio_percent"] = 0
+        data["sustainable_percent"] = 0
+
+    badge_querysets = badges_for_queryset(teledeclarations)
+    total_diag = data["diagnostics_count"]
+    data["approPercent"] = int(badge_querysets["appro"].count() / total_diag * 100) if total_diag else 0
+    return data
+
+
+class CanteenStatisticsSerializer(serializers.Serializer):
+    canteen_count = serializers.IntegerField()
+    bio_percent = serializers.IntegerField()
+    sustainable_percent = serializers.IntegerField()
+    teledeclarations_count = serializers.IntegerField()
+    approPercent = serializers.IntegerField()
+    sector_categories = serializers.DictField()
+    epci_error = serializers.CharField(required=False)
+
+    @staticmethod
+    def calculate_statistics(canteens, teledeclarations):
+        data = {}
+        data = calculate_statistics_teledeclarations(teledeclarations, data)
+        data = calculate_statistics_canteens(canteens, data)
         return data
