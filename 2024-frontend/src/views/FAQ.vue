2e43085f96fbb5c6e14b067cf378f7e9faffe0c2
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-04-04 11:00:08 +0200

    Affiche les accordéons dans la page

--- 2024-frontend/src/views/FAQ.vue
+++ 2024-frontend/src/views/FAQ.vue
@@ -1,15 +1,31 @@
 <script setup>
 import { useRoute } from "vue-router"
+import { groups } from "@/constants/faq-accordions.js"
+import AppRawHtml from "@/components/AppRawHtml.vue"
+
 const route = useRoute()
 </script>
 
 <template>
-  <section class="fr-col-12 fr-col-md-7">
+  <section class="fr-col-12 fr-col-md-7 fr-mb-10w">
     <h1>{{ route.meta.title }}</h1>
     <p>
       Vous n'êtes pas familier avec les obligations réglementaires ? Vous vous posez des questions sur votre
       responsabilité ? Vous avez besoin d'aide sur plusieurs aspects techniques ou légaux ? Rassurez-vous ! Vous n'êtes
       pas les seuls à vous poser ces questions.
     </p>
+  </section>
+  <section>
+    <DsfrAccordionsGroup v-for="(group, index) in groups" :key="index" class="fr-mb-5w">
+      <h2 class="fr-h5">{{ group.title }}</h2>
+      <DsfrAccordion
+        v-for="(accordion, index) in group.accordions"
+        :key="index"
+        :title="accordion.question"
+        title-tag="h3"
+      >
+        <AppRawHtml :html="accordion.answer" />
+      </DsfrAccordion>
+    </DsfrAccordionsGroup>
   </section>
 </template>
