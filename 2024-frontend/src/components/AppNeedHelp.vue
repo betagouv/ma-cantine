ac39b6da58c1246c370b9d82757d2133bd635cc8
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-04-04 13:24:53 +0200

    Création d'un composant pour gérer le style des demandes d'aide en bas de page

---
+++ 2024-frontend/src/components/AppNeedHelp.vue
@@ -0,0 +1,33 @@
+<script setup>
+import AppSeparator from "@/components/AppSeparator.vue"
+defineProps(["badge", "title"])
+</script>
+
+<template>
+  <section class="app-need-help fr-mt-8w fr-grid-row fr-grid-row--center">
+    <div class="fr-col-12 fr-col-md-8">
+      <div class="app-need-help__separator fr-mb-5w">
+        <AppSeparator />
+        <DsfrBadge class="app-need-help__badge" type="none" :label="badge" />
+      </div>
+      <div class="fr-pr-0 fr-pr-md-8w">
+        <h2>{{ title }}</h2>
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.app-need-help {
+  &__separator {
+    position: relative;
+  }
+
+  &__badge {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
