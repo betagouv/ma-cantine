fbe63ea4e26ef2353ebc93c8c3134f23a182dd54
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-04-07 12:13:55 +0200

    Mise à jour de la réponse

--- 2024-frontend/src/components/FAQAnswerNoSiret.vue
+++ 2024-frontend/src/components/FAQAnswerNoSiret.vue
@@ -1,47 +1,25 @@
 <template>
   <p>
-    Dans certains cas, il est possible que certaines cantines n’aient pas de SIRET. Le gestionnaire de la cantine doit
-    faire une demande de numéro SIRET en fonction de son statut :
+    Dans certains cas, il est possible que certaines cantines n’aient pas de SIRET. Vous devez alors rattacher votre
+    cantine à son unité legale via le numéro SIREN de l’unité légale.
   </p>
+  <p>Vous pouvez vous rattacher à une unité légale à condition :</p>
   <ul>
     <li>
-      <p>
-        Pour identifier un établissement secondaire rattaché à un organisme public (par exemple une crèche ou école sous
-        une commune), l'organisme peut faire sa demande d'identification au pôle Sirene secteur public à l'adresse
-        suivante :
-        <a href="mailto:sirene-secteurpublic@contact-insee.fr">sirene-secteurpublic@contact-insee.fr</a>
-        . Il faut indiquer le nom de l'établissement, son adresse précise, sa date d'ouverture, l'activité exercée et le
-        numéro siren de l'organisme de rattachement. Hors exception (par exemple un établissement disposant numéro UAI
-        dans la base de l’éducation nationale, d’un numéro Finess dans la base de l’ARS ou d’un budget annexe situé à
-        l’adresse du siège par définition…), il ne peut pas y avoir plusieurs établissements à la même adresse.
+      <p class="fr-mb-0">
+        de ne pas dupliquer un établissement déjà existant sur la plateforme « ma cantine »
       </p>
     </li>
     <li>
-      <p>
-        Pour les associations, vous pouvez joindre le pôle Sirene association :
-        <a href="mailto:sireneasso@contact-insee.fr">sireneasso@contact-insee.fr</a>
-        .
-      </p>
-    </li>
-    <li>
-      <p>
-        Pour toutes les autres entreprises privées, les formalités pour obtenir un numéro siret doivent se faire sur
-        <a href="https://procedures.inpi.fr/" target="_blank">
-          le site du guichet unique de l'Inpi
-        </a>
-        Après avoir créé un compte sur Inpi Connect (identifiant mail et mot de passe requis), les entreprises pourront
-        alors effectuer en ligne toutes les démarches. Les formalités seront directement adressées à l’Insee qui
-        actualisera alors le répertoire Sirene.
+      <p class="fr-mb-0">
+        de se rattacher uniquement à l’unité légale qui exerce la tutelle de votre établissement.
       </p>
     </li>
   </ul>
   <p>
-    Enfin, pour toute question concernant l'Insee et Sirene, vous pouvez
-    <a href="https://www.insee.fr/fr/information/2387873" target="_blank" rel="noopener">
-      contacter l'Insee par formulaire
+    Pour trouver le numéro SIREN de votre unité légale, veuillez utiliser
+    <a href="https://annuaire-entreprises.data.gouv.fr/" target="_blank">
+      l'Annuaire des Entreprises
     </a>
-    ou en téléphonant au
-    <a href="tel:0972724000">09 72 72 40 00</a>
-    .
   </p>
 </template>
