00e0d6142607dc38f78a6c03d26467a05453aa90
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-04-07 12:09:03 +0200

    Mise à jour de la réponse

--- 2024-frontend/src/components/FAQAnswerHowToSignUp.vue
+++ 2024-frontend/src/components/FAQAnswerHowToSignUp.vue
@@ -8,6 +8,6 @@
     <a href="https://ma-cantine.agriculture.gouv.fr/webinaires/2--Cr%C3%A9er%20une%20cantine%20sur%20la%20plateforme">
       créer sa cantine
     </a>
-    avec son numéro SIRET et créer des bilans par année civile.
+    avec le numéro SIRET de son établissement ou le numéro SIREN de son unité légale.
   </p>
 </template>
