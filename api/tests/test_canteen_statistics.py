0e16dd7a1c6dd37da3896cea142b4deed1d71cb6
Raphaël Odini <raphodn@users.noreply.github.com>
2025-04-07 14:25:46 +0200

    refactor(Dates de campagnes): ajout des dates de correction pour la campagne de 2024 (#5229)

--- api/tests/test_canteen_statistics.py
+++ api/tests/test_canteen_statistics.py
@@ -16,8 +16,8 @@ from data.region_choices import Region
 year_data = now().year - 1
 mocked_campaign_dates = {
     year_data: {
-        "start_date": now().replace(month=1, day=1, hour=0, minute=0, second=0),
-        "end_date": now().replace(month=12, day=31, hour=23, minute=59, second=59),
+        "teledeclaration_start_date": now().replace(month=1, day=1, hour=0, minute=0, second=0),
+        "teledeclaration_end_date": now().replace(month=12, day=31, hour=23, minute=59, second=59),
     }
 }
 
