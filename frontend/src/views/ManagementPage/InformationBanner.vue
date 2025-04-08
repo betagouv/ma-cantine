665962a1501687a11669247f0fcfe625736effb0
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-04-08 13:56:27 +0200

    Mise à jour du texte du bandeau de communication

--- frontend/src/views/ManagementPage/InformationBanner.vue
+++ frontend/src/views/ManagementPage/InformationBanner.vue
@@ -1,44 +1,14 @@
 <template>
   <DsfrCallout noIcon class="mt-2">
     <h2 class="fr-text font-weight-bold mb-2 mt-2">
-      INFORMATION IMPORTANTE :
+      <span class="text-uppercase">Droit à l'erreur :</span>
+      du 16 au 30 avril 2025
     </h2>
     <p class="mb-0">
-      La télédéclaration 2025 est exceptionnellement maintenue ouverte sur l'ensemble de la première semaine d'avril.
+      Valable uniquement pour les établissements qui ont validé leur télé-déclaration.
       <br />
-      Les télédéclarations seront possibles jusqu’au dimanche 6 avril 2025 inclus.
+      Depuis votre bilan, vous pourrez corriger vos informations si besoin.
     </p>
-    <v-row class="mt-4 mb-0 mx-0 align-center">
-      <v-btn :to="{ name: 'PendingActions' }" color="primary" class="mb-5 mb-md-2 mr-4">
-        Télédéclarer mes cantines
-      </v-btn>
-      <v-btn :to="{ name: 'CommunityPage', hash: '#evenements' }" color="primary" outlined class="mb-5 mb-md-2 mr-4">
-        Inscrivez-vous à nos derniers webinaires
-      </v-btn>
-      <p class="fr-text-sm mb-5 mb-md-2 mr-4">
-        <a
-          href="https://894795896-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F-MSCF7Mdc8yfeIjMxMZr%2Fuploads%2F85J5bYm9Nd4aFRJk1pvm%2FGuide_prise_en_main_ma_cantine_janv2025.pdf?alt=media"
-          target="_blank"
-          rel="noopener external"
-          title="Le guide de télédéclaration - ouvre une nouvelle fenêtre"
-          class="grey--text text--darken-4"
-        >
-          Le guide de télédéclaration
-          <v-icon small class="ml-2 grey--text text--darken-4">mdi-open-in-new</v-icon>
-        </a>
-      </p>
-      <p class="fr-text-sm mb-5 mb-md-2">
-        <a
-          href="/static/documents/Antiseche_donnees_dachat_ma_cantine_2025.pdf"
-          download
-          title="Comment saisir mes données d'achat"
-          class="grey--text text--darken-4"
-        >
-          Comment saisir mes données d'achat
-          <v-icon class="ml-2 grey--text text--darken-4 icon-small">$download-line</v-icon>
-        </a>
-      </p>
-    </v-row>
   </DsfrCallout>
 </template>
 
@@ -52,13 +22,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.v-sheet--shaped {
-  border-radius: 26px 4px !important;
-}
-
-.icon-small {
-  width: 1rem;
-}
-</style>
