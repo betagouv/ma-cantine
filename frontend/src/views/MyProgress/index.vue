b19c26f9824b6f5e877e5218d68a871eb8b05b0c
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-04-16 15:55:52 +0200

    Simplifie la vérification pour afficher le bouton

--- frontend/src/views/MyProgress/index.vue
+++ frontend/src/views/MyProgress/index.vue
@@ -241,10 +241,7 @@
                     Données télédéclarées
                   </v-btn>
                   <v-btn
-                    v-else-if="
-                      readyToTeledeclare &&
-                        (inTeledeclarationCampaign || (inCorrectionCampaign && hasCancelledTeledeclaration))
-                    "
+                    v-else-if="readyToTeledeclare && actionIsTeledeclare"
                     color="primary"
                     @click="showTeledeclarationPreview = true"
                     class="fr-text font-weight-medium"
@@ -364,11 +361,14 @@ export default {
     mobileSelectItems() {
       return this.tabHeaders.map((x, index) => ({ text: x.text, value: index }))
     },
+    actionIsTeledeclare() {
+      return actionIsTeledeclare(this.canteenAction)
+    },
     hasCancelledTeledeclaration() {
       // During the correction campaign, we allow only canteens with an existing teledeclaration to do corrections
       // BUT the backend does not return CANCELLED teledeclarations
       // instead we look at the canteen's action
-      return actionIsTeledeclare(this.canteenAction)
+      return this.actionIsTeledeclare
     },
     hasActiveTeledeclaration() {
       return this.diagnostic?.teledeclaration?.status === "SUBMITTED"
