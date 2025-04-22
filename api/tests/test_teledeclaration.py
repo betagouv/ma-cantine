5950ef1b09876e9eb537c93fcf5ab150f3ad046d
Raphael Odini <raphodn@users.noreply.github.com>
2025-04-22 12:27:48 +0200

    Improve test

--- api/tests/test_teledeclaration.py
+++ api/tests/test_teledeclaration.py
@@ -273,6 +273,7 @@ class TestTeledeclarationCreateApi(APITestCase):
         payload = {"diagnosticId": diagnostic.id}
         response = self.client.post(reverse("teledeclaration_create"), payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data[0], "Il existe déjà une télédéclaration en cours pour cette année")
 
     @freeze_time("2022-08-30")  # during the 2021 campaign
     @authenticate
