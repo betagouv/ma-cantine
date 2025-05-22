c164127729e0f6a7d5d76e3555b1a7732a52fe46
Raphael Odini <raphodn@users.noreply.github.com>
2025-05-22 11:48:13 +0200

    new get_dataset_resource method

--- common/api/datagouv.py
+++ common/api/datagouv.py
@@ -12,19 +12,26 @@ DATAGOUV_API_URL = "https://www.data.gouv.fr/api/1"
 
 
 def get_dataset(dataset_id):
-    DATAGOUV_API_KEY = os.getenv("DATAGOUV_API_KEY", "")
-    DATAGOUV_API_HEADER = {"X-API-KEY": DATAGOUV_API_KEY}
-
-    if not (dataset_id and DATAGOUV_API_KEY):
-        logger.warning("Datagouv resource update : API key or dataset id incorrect values")
-        return
-
     try:
-        response = requests.get(f"{DATAGOUV_API_URL}/datasets/{dataset_id}", headers=DATAGOUV_API_HEADER)
+        response = requests.get(f"{DATAGOUV_API_URL}/datasets/{dataset_id}")
         response.raise_for_status()
         return response.json()
     except requests.HTTPError as e:
-        logger.error(f"Datagouv resource update : Error while updating dataset : {dataset_id}")
+        logger.error(f"Datagouv dataset get : Error while getting dataset : {dataset_id}")
+        logger.exception(e)
+    except Exception as e:
+        logger.exception(e)
+
+
+def get_dataset_resource(dataset_id, resource_id):
+    try:
+        response = requests.get(f"{DATAGOUV_API_URL}/datasets/{dataset_id}/resources/{resource_id}")
+        response.raise_for_status()
+        return response.json()
+    except requests.HTTPError as e:
+        logger.error(
+            f"Datagouv dataset resource get : Error while getting dataset resource : {dataset_id} / {resource_id}"
+        )
         logger.exception(e)
     except Exception as e:
         logger.exception(e)
