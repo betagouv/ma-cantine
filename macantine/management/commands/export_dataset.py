c366fcf9b06d264caaa64616aa2f6d372d55f3fe
qloridant <quentin.loridant@protonmail.com>
2025-05-23 10:20:22 +0200

    improve(ETL): Affiner le scope des fonctions d'export (#5374)

--- macantine/management/commands/export_dataset.py
+++ macantine/management/commands/export_dataset.py
@@ -1,12 +1,34 @@
 from django.core.management.base import BaseCommand
 
-from macantine.tasks import manual_datasets_export
+from macantine.tasks import (
+    datasets_export_analysis_canteens,
+    datasets_export_analysis_td,
+    datasets_export_opendata_canteens,
+    datasets_export_opendata_td,
+)
 
 
 class Command(BaseCommand):
-    help = (
-        "Export datasets TD and canteens to the 'media/open_data' folder of the configured file system (local or s3)"
-    )
+    help = "Export datasets"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--dataset",
+            dest="export_type",
+            type=str,
+            required=True,
+            help="Choose the type of dataset you want to extract \n 1. Cantines (opendata)\n 2. Cantines (Metabase)\n 3. Ensemble des TD (Metabase)\n 4. TD par années (opendata)",
+        )
 
     def handle(self, *args, **options):
-        manual_datasets_export()
+        export_type = options["export_type"]
+        if export_type == 1:
+            datasets_export_opendata_canteens()
+        elif export_type == 2:
+            datasets_export_analysis_canteens()
+        elif export_type == 3:
+            datasets_export_analysis_td()
+        elif export_type == 4:
+            datasets_export_opendata_td()
+        else:
+            print("Invalid argument. Choose 1,2 or 3")
