b57b6c5879f69c322f6997b110cae3c2ad2c5fd4
qloridant <quentin.loridant@protonmail.com>
2025-05-26 15:49:08 +0200

    Creation seriallizer pour requête TD open data

--- api/serializers/teledeclaration.py
+++ api/serializers/teledeclaration.py
@@ -206,3 +206,30 @@ class TeledeclarationOpenDataSerializer(serializers.ModelSerializer):
             "teledeclaration_mode",
         )
         read_only_fields = fields
+
+
+class TeledeclarationOpenDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teledeclaration
+        fields = (
+            "id",
+            "declared_data",
+            "creation_date",
+            "modification_date",
+            "year",
+            "canteen_siret",
+            "canteen_siren_unite_legale",
+            "value_total_ht",
+            "value_bio_ht_agg",
+            "value_sustainable_ht_agg",
+            "value_externality_performance_ht_agg",
+            "value_egalim_others_ht_agg",
+            "yearly_meal_count",
+            "meal_price",
+            "status",
+            "applicant_id",
+            "canteen_id",
+            "diagnostic_id",
+            "teledeclaration_mode",
+        )
+        read_only_fields = fields
