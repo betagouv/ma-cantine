9e75237e73c2f7ad0093c54a1e4feae3c78d4c65
qloridant <quentin.loridant@protonmail.com>
2025-05-27 10:20:18 +0200

    Transferer la transformation des colonnes caract cantines dans le serializer

--- api/serializers/teledeclaration.py
+++ api/serializers/teledeclaration.py
@@ -198,7 +198,6 @@ class TeledeclarationOpenDataSerializer(serializers.ModelSerializer):
             "value_sustainable_ht_agg",
             "value_externality_performance_ht_agg",
             "value_egalim_others_ht_agg",
-            "yearly_meal_count",
             "meal_price",
             "status",
             "applicant_id",
@@ -207,16 +206,103 @@ class TeledeclarationOpenDataSerializer(serializers.ModelSerializer):
         )
         read_only_fields = fields
 
+    def get_siret(self, obj):
+        return obj.canteen_siret
+
+    def get_name(self, obj):
+        return obj.declared_data["canteen"]["name"]
+
+    def get_daily_meal_count(self, obj):
+        daily_meal_count = obj.declared_data["canteen"]["daily_meal_count"]
+        return int(daily_meal_count) if daily_meal_count else None
+
+    def get_yearly_meal_count(self, obj):
+        yearly_meal_count = obj.declared_data["canteen"]["yearly_meal_count"]
+        return int(yearly_meal_count) if yearly_meal_count else None
+
+    def get_management_type(self, obj):
+        value = obj.declared_data["canteen"]["management_type"]
+        if value == "direct":
+            return "A) directe"
+        elif value == "conceded":
+            return "B) concédée"
+        else:
+            return "C) non renseigné"
+
+    def get_production_type(self, obj):
+        return obj.declared_data["canteen"]["production_type"]
+
+    def get_cuisine_centrale(self, obj):
+        value = obj.declared_data["canteen"]["production_type"]
+        if value in ["site", "site_cooked_elsewhere"]:
+            return "B) non"
+        elif value in ["central", "central_serving"]:
+            return "A) oui"
+        else:
+            return "C) non renseigné"
+
+    def get_central_producer_siret(self, obj):
+        return obj.declared_data["canteen"]["central_producer_siret"]
+
+    def get_modele_economique(self, obj):
+        value = obj.declared_data["canteen"]["economic_model"]
+        if value == "private":
+            return "A) privé"
+        elif value == "public":
+            return "B) public"
+        else:
+            return "C) non renseigné"
+
+    def get_secteur(self, obj):
+        sectors = obj.declared_data["canteen"]["sectors"]
+        if len(sectors) > 1:
+            return "Secteurs multiples"
+        elif len(sectors) == 1:
+            return sectors[0]["name"]
+        else:
+            return None
+
+    def get_categorie(self, obj):
+        sectors = obj.declared_data["canteen"]["sectors"]
+        if len(sectors) > 1:
+            return "Catégories multiples"
+        elif len(sectors) == 1:
+            return sectors[0]["category"]
+        else:
+            return None
+
+    def get_satellite_canteens_count(self, obj):
+        satellite_canteens_count = obj.declared_data["canteen"]["satellite_canteens_count"]
+        return int(satellite_canteens_count) if satellite_canteens_count else None
+
+    def get_departement(self, obj):
+        return obj.declared_data["canteen"]["department"]
+
+    def get_code_insee_commune(self, obj):
+        return obj.declared_data["canteen"]["city_insee_code"]
+
+    def get_lib_departement(self, obj):
+        return Department(obj.declared_data["canteen"]["department"]).label.split(" - ")[1].lstrip()
+
+    def get_region(self, obj):
+        return obj.declared_data["canteen"]["region"]
+
+    def get_lib_region(self, obj):
+        return Region(obj.declared_data["canteen"]["region"]).label.split(" - ")[1].lstrip()
+
+    def get_line_ministry(self, obj):
+        return obj.declared_data["canteen"]["line_ministry"]
+
 
 class TeledeclarationOpenDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teledeclaration
         fields = (
             "id",
-            "declared_data",
-            "creation_date",
-            "modification_date",
             "year",
+            "creation_date",
+            "canteen_id",
+            "declared_data",
             "canteen_siret",
             "canteen_siren_unite_legale",
             "value_total_ht",
@@ -228,7 +314,6 @@ class TeledeclarationOpenDataSerializer(serializers.ModelSerializer):
             "meal_price",
             "status",
             "applicant_id",
-            "canteen_id",
             "diagnostic_id",
             "teledeclaration_mode",
         )
