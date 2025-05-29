16b6f0abf0e226119abd5f4465b53befbc35ed72
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-05-29 18:39:57 +0200

    Stylisation, responsive et accessibilité

--- 2024-frontend/src/views/SiteMap.vue
+++ 2024-frontend/src/views/SiteMap.vue
@@ -70,9 +70,9 @@ const sections = [
 <template>
   <h1>{{ route.meta.title }}</h1>
   <div class="fr-grid-row fr-grid-row--gutters">
-    <section v-for="section in sections" :key="section.title" class="fr-col-6">
+    <section v-for="section in sections" :key="section.title" class="fr-col-12 fr-col-lg-6">
       <h2>{{ section.title }}</h2>
-      <ul>
+      <ul class="ma-cantine--unstyled-list">
         <li v-for="page in section.pages" :key="page.name">
           <AppLinkRouter :title="page.meta.title" :to="page" />
         </li>
