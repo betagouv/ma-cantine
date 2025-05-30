dda245bb6f870d3e80c8ac1df922622ecdb7b988
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-05-30 14:45:40 +0200

    Créer un layout pour les pages simples de text

---
+++ 2024-frontend/src/layouts/LayoutOneColumn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fr-col-12 fr-col-lg-7 layout-one-column">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.layout-one-column {
+  section {
+    margin-top: 2rem;
+  }
+}
+</style>
