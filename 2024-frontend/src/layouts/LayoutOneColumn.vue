559455db9200873478a6ca96ef5c17bdf1e0406f
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-05-30 15:36:01 +0200

    Ajoute un retour en haut de page

--- 2024-frontend/src/layouts/LayoutOneColumn.vue
+++ 2024-frontend/src/layouts/LayoutOneColumn.vue
@@ -1,12 +1,31 @@
 <script setup>
 import { useRoute } from "vue-router"
 const route = useRoute()
+
+const scrollTop = () => {
+  window.scrollTo(0, 0)
+}
 </script>
 
 <template>
-  <div class="fr-col-12 fr-col-lg-7 layout-one-column">
+  <div class="layout-one-column">
     <h1 class="fr-my-8w">{{ route.meta.title }}</h1>
-    <slot></slot>
+    <div class="fr-grid-row">
+      <div class="fr-col-12 fr-col-lg-7">
+        <slot></slot>
+      </div>
+      <div
+        class="ma-cantine--sticky__container fr-hidden fr-unhidden-lg fr-col-lg-5 fr-grid-row fr-grid-row--top fr-grid-row--right"
+      >
+        <DsfrButton
+          class="ma-cantine--sticky__bottom"
+          label="Retour en haut de page"
+          icon="fr-icon-arrow-up-line"
+          secondary
+          @click="scrollTop"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
