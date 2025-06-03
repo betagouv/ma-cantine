44c245a51cb8a13858df37a11f61765951fb04e1
Raphael Odini <raphodn@users.noreply.github.com>
2025-06-03 12:13:27 +0200

    Add test

---
+++ macantine/tests/test_fill_missing_td_canteen_geolocation_data.py
@@ -0,0 +1,77 @@
+from django.core.management import call_command
+from django.test import TestCase
+
+from data.factories import CanteenFactory, DiagnosticFactory, UserFactory
+from data.models import Teledeclaration
+
+
+class FixturesTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.canteen_with_geo_data = CanteenFactory.create(
+            city_insee_code="38185",
+            department="38",
+            department_lib="Isère",
+            region="84",
+            region_lib="Auvergne-Rhône-Alpes",
+            epci="200040715",
+            epci_lib="Grenoble-Alpes-Métropole",
+        )
+        cls.canteen_half_geo_data = CanteenFactory.create(
+            city_insee_code="38185",
+            department="38",
+            department_lib=None,
+            region="84",
+            region_lib=None,
+            epci="200040715",
+            epci_lib=None,
+        )
+        cls.canteen_without_geo_data = CanteenFactory.create(
+            city_insee_code="38185",
+            department=None,
+            department_lib=None,
+            region=None,
+            region_lib=None,
+            epci=None,
+            epci_lib=None,
+        )
+        cls.canteen_without_geo_data_old = CanteenFactory.create(
+            city_insee_code="38185",
+            department=None,
+            department_lib=None,
+            region=None,
+            region_lib=None,
+            epci=None,
+            epci_lib=None,
+        )
+
+    def test_fill_missing_canteen_geolocation_data_command(self):
+        for canteen in [
+            self.canteen_with_geo_data,
+            self.canteen_half_geo_data,
+            self.canteen_without_geo_data,
+            self.canteen_without_geo_data_old,
+        ]:
+            diag = DiagnosticFactory.create(canteen=canteen)
+            td = Teledeclaration.create_from_diagnostic(diag, applicant=UserFactory.create())
+            # Canteen data for 2021 & 2022 TDs did not include department and region
+            if canteen == self.canteen_without_geo_data_old:
+                td_declared_data = td.declared_data.copy()
+                del td_declared_data["canteen"]["department"]
+                del td_declared_data["canteen"]["region"]
+                td.declared_data = td_declared_data
+                td.save()
+
+        self.assertEqual(Teledeclaration.objects.count(), 4)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__city_insee_code=None).count(), 0)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__has_key="department").count(), 3)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__department=None).count(), 1)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__has_key="region").count(), 3)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__region=None).count(), 1)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__has_key="epci").count(), 0)
+        call_command("fill_missing_td_canteen_geolocation_data")
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__has_key="department").count(), 4)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__department=None).count(), 0)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__has_key="region").count(), 4)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__region=None).count(), 0)
+        self.assertEqual(Teledeclaration.objects.filter(declared_data__canteen__has_key="epci").count(), 0)
