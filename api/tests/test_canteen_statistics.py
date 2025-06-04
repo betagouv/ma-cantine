3f2c8ac0e3034bc4e6964afd38b79b56aa51d0d9
Raphael Odini <raphodn@users.noreply.github.com>
2025-06-04 17:37:35 +0200

    move canteen_locations tests to own class

--- api/tests/test_canteen_statistics.py
+++ api/tests/test_canteen_statistics.py
@@ -220,32 +220,6 @@ class TestCanteenStatsApi(APITestCase):
         response = self.client.get(reverse("canteen_statistics"), {"department": "01"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_canteen_locations(self):
-        """
-        Test that the right subset of regions and departments 'in use' by canteens are returned
-        """
-        CanteenFactory.create(department=Department.aisne, department_lib=Department.aisne.label)
-        CanteenFactory.create(department=Department.ain, department_lib=Department.ain.label)
-        # set region directly
-        CanteenFactory.create(region=Region.guadeloupe, region_lib=Region.guadeloupe.label, department=None)
-        # create extra to check that list returned doesn't contain duplicates
-        CanteenFactory.create(department=Department.aisne, department_lib=Department.aisne.label)
-        CanteenFactory.create(department="999")  # not a department, checking None on region
-        CanteenFactory.create(region="", department="")  # checking exlusion of blank strings
-
-        response = self.client.get(reverse("canteen_locations"))
-
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
-        self.assertEqual(len(body["regions"]), 3)
-        self.assertEqual(Region.guadeloupe, body["regions"][0])
-        self.assertEqual(Region.hauts_de_france, body["regions"][1])
-        self.assertEqual(Region.auvergne_rhone_alpes, body["regions"][2])
-        self.assertEqual(len(body["departments"]), 3)
-        self.assertIn(Department.ain, body["departments"][0])
-        self.assertIn(Department.aisne, body["departments"][1])
-        self.assertIn("999", body["departments"])
-
     def test_stats_simple_diagnostic(self):
         """
         The endpoint must take into consideration the simplified diagnostic
@@ -359,3 +333,31 @@ class TestCanteenStatsApi(APITestCase):
 
         body = response.json()
         self.assertEqual(body["canteenCount"], 3)
+
+
+class TestCanteenLocationsApi(APITestCase):
+    def test_canteen_locations(self):
+        """
+        Test that the right subset of regions and departments 'in use' by canteens are returned
+        """
+        CanteenFactory.create(department=Department.aisne, department_lib=Department.aisne.label)
+        CanteenFactory.create(department=Department.ain, department_lib=Department.ain.label)
+        # set region directly
+        CanteenFactory.create(region=Region.guadeloupe, region_lib=Region.guadeloupe.label, department=None)
+        # create extra to check that list returned doesn't contain duplicates
+        CanteenFactory.create(department=Department.aisne, department_lib=Department.aisne.label)
+        CanteenFactory.create(department="999")  # not a department, checking None on region
+        CanteenFactory.create(region="", department="")  # checking exlusion of blank strings
+
+        response = self.client.get(reverse("canteen_locations"))
+
+        self.assertEqual(response.status_code, 200)
+        body = response.json()
+        self.assertEqual(len(body["regions"]), 3)
+        self.assertEqual(Region.guadeloupe, body["regions"][0])
+        self.assertEqual(Region.hauts_de_france, body["regions"][1])
+        self.assertEqual(Region.auvergne_rhone_alpes, body["regions"][2])
+        self.assertEqual(len(body["departments"]), 3)
+        self.assertIn(Department.ain, body["departments"][0])
+        self.assertIn(Department.aisne, body["departments"][1])
+        self.assertIn("999", body["departments"])
