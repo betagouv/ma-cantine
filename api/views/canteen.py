392fe62b53e75f890ed506fe75ec0b62dbe53900
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-06-05 19:24:54 +0200

    Enlève les valeurs vides des sirets des livreurs autorisés

--- api/views/canteen.py
+++ api/views/canteen.py
@@ -249,7 +249,9 @@ def filter_by_diagnostic_params(queryset, query_params):
                 )
             ).distinct()
         canteen_ids = qs_diag.values_list("canteen", flat=True)
-        canteen_sirets = qs_diag.values_list("canteen__siret", flat=True)
+        canteen_sirets = qs_diag.exclude(Q(canteen__siret="") | Q(canteen__siret__isnull=True)).values_list(
+            "canteen__siret", flat=True
+        )
         queryset = queryset.exclude(redacted_appro_years__contains=[publication_year])
         return queryset.filter(Q(id__in=canteen_ids) | Q(central_producer_siret__in=canteen_sirets))
     return queryset
