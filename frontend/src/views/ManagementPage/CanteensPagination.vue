55599f0e9b629cb1e0bbb2c50ea652bacde54cff
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-06-05 12:43:33 +0200

    Ajoute une indication à l'utilisateur si le résultat de sa recherche est vide

--- frontend/src/views/ManagementPage/CanteensPagination.vue
+++ frontend/src/views/ManagementPage/CanteensPagination.vue
@@ -31,6 +31,11 @@
     <v-sheet fluid height="200" v-if="inProgress">
       <v-progress-circular indeterminate style="left: 50%; top: 50%"></v-progress-circular>
     </v-sheet>
+    <v-row v-if="visibleCanteens.length === 0">
+      <v-col cols="12">
+        <p>Aucune cantine trouvée pour la recherche : {{ searchTerm }}</p>
+      </v-col>
+    </v-row>
     <v-row v-else>
       <v-col cols="12" sm="6" md="4" height="100%" v-for="canteen in visibleCanteens" :key="`canteen-${canteen.id}`">
         <CanteenCard :canteen="canteen" class="fill-height" />
