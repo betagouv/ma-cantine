61af0b64ccdeba394541f363945b0f012ca07605
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-06-05 13:02:43 +0200

    Remplace l'image par un format plus léger et transparent

--- 2024-frontend/src/components/AppHeader.vue
+++ 2024-frontend/src/components/AppHeader.vue
@@ -161,7 +161,7 @@ const navItemsForUser = computed(() => {
 <template>
   <DsfrHeader
     :logo-text
-    operatorImgSrc="/static/images/ma-cantine-logo-light.jpg"
+    operatorImgSrc="/static/images/ma-cantine-logo-light.svg"
     operatorImgAlt="ma cantine"
     operatorImgStyle="height: 65px;"
     :quickLinks
