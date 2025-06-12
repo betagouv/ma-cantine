32991f2af82a007a88374f8b1852ff703c1685ce
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-06-12 10:23:43 +0200

    Découpe la liste en un composant pour avoir plusieurs models

---
+++ 2024-frontend/src/components/FAQAccordionsList.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(["group"])
+const model = defineModel()
+</script>
+
+<template>
+  <DsfrAccordionsGroup class="fr-mb-5w" v-model="model">
+    <h2 class="fr-h5">{{ group.title }}</h2>
+    <DsfrAccordion
+      v-for="(accordion, index) in group.accordions"
+      :key="index"
+      :title="accordion.question"
+      title-tag="h3"
+    >
+      <component v-if="accordion.component" :is="accordion.component"></component>
+      <p v-else>{{ accordion.answer }}</p>
+    </DsfrAccordion>
+  </DsfrAccordionsGroup>
+</template>
