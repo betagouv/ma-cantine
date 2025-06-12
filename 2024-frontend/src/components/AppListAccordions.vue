b9564751571161c75d949e49b59a7d4a4ac57366
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-06-12 15:03:19 +0200

    Renomme le composant pour un nom générique

--- 2024-frontend/src/components/AppListAccordions.vue
+++ 2024-frontend/src/components/AppListAccordions.vue
@@ -6,14 +6,9 @@ const model = defineModel()
 <template>
   <DsfrAccordionsGroup class="fr-mb-5w" v-model="model">
     <h2 class="fr-h5">{{ group.title }}</h2>
-    <DsfrAccordion
-      v-for="(accordion, index) in group.accordions"
-      :key="index"
-      :title="accordion.question"
-      title-tag="h3"
-    >
+    <DsfrAccordion v-for="(accordion, index) in group.accordions" :key="index" :title="accordion.title" title-tag="h3">
       <component v-if="accordion.component" :is="accordion.component"></component>
-      <p v-else>{{ accordion.answer }}</p>
+      <p v-else>{{ accordion.content }}</p>
     </DsfrAccordion>
   </DsfrAccordionsGroup>
 </template>
