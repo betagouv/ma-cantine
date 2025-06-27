e0b763d1b973da0a244f183281d8e540f978246c
Charline <charline.laporte@beta.gouv.fr>
2025-06-27 10:10:08 +0200

    Fix typo

--- 2024-frontend/src/components/EdibleChart.vue
+++ 2024-frontend/src/components/EdibleChart.vue
@@ -58,7 +58,7 @@ const displayOption = ref("chart")
           label="Choix d'affichage"
           :options="[
             {
-              label: 'Graph',
+              label: 'Graphe',
               value: 'chart',
             },
             {
