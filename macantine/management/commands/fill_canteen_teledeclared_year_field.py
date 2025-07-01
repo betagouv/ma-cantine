dc679c22c81885512e942ea442192b520a1fca21
Raphael Odini <raphodn@users.noreply.github.com>
2025-07-01 10:02:15 +0200

    Cleanup

--- macantine/management/commands/fill_canteen_teledeclared_year_field.py
+++ macantine/management/commands/fill_canteen_teledeclared_year_field.py
@@ -21,26 +21,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         year = options["year"]
-        logger.info(f"Start task : fill_canteen_teledeclared_year_field for year {year}")
+        logger.info(f"Start task: fill_canteen_teledeclared_year_field for year {year}")
         field_name = f"teledeclared_{year}"
 
-        # Step 1: reset the field for all canteens
+        # Step 1: reset the field for all the canteens
         Canteen.objects.all().update(**{field_name: None})
 
         # Step 2: find the canteens that have a teledeclaration for the specified year
-        # v1: filter only on the canteen_id of Teledeclarations
         teledeclarations = Teledeclaration.objects.submitted_for_year(year)
-        canteens_with_teledeclarations = teledeclarations.values_list("canteen_id", flat=True).distinct()
+        logger.info(f"Found {len(teledeclarations)} teledeclarations for year {year}")
+        # v1: filter only on the canteen_id of Teledeclarations
+        # canteens_with_teledeclarations = teledeclarations.values_list("canteen_id", flat=True).distinct()
         # v2: filter on the canteen_id & declared_data (satellites) of Teledeclarations
-        teledeclarations = Teledeclaration.objects.submitted_for_year(year).values("canteen_id", "declared_data")
         canteens_with_teledeclarations = []
-        for td in teledeclarations:
+        for td in teledeclarations.values("canteen_id", "declared_data"):
             canteens_with_teledeclarations.append(td["canteen_id"])
             if "satellites" in td["declared_data"]:
                 for satellite in td["declared_data"]["satellites"]:
                     canteens_with_teledeclarations.append(satellite["id"])
 
-        # Step 3: update the field
+        # Step 3: update the field for all the canteens
         Canteen.objects.filter(id__in=canteens_with_teledeclarations).update(**{field_name: True})
         Canteen.objects.exclude(**{field_name: True}).update(**{field_name: False})
 
