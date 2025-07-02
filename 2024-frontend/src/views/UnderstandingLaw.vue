f3d2959a5d6611b9ccad1b1e7d882b516cd575b5
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-02 11:01:51 +0200

    Format le code

--- 2024-frontend/src/views/UnderstandingLaw.vue
+++ 2024-frontend/src/views/UnderstandingLaw.vue
@@ -8,10 +8,14 @@ const route = useRoute()
 <template>
   <section class="fr-col-12 fr-col-md-7 fr-mb-5w">
     <h1>{{ route.meta.title }}</h1>
-    <p>Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    <p>
+      Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Vestibulum id
+      ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam
+      id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna.
+    </p>
   </section>
   <ul class="ma-cantine--unstyled-list fr-grid-row fr-grid-row--center fr-grid-row--gutters">
-    <li v-for="tile, index in tiles" :key="index" class="fr-col-4">
+    <li v-for="(tile, index) in tiles" :key="index" class="fr-col-4">
       <DsfrTile
         class="understanding-law__tile"
         :title="tile.title"
