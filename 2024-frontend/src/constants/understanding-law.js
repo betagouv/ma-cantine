abcd6441fbc87f6962a39406955744611723507b
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-02 11:01:51 +0200

    Format le code

--- 2024-frontend/src/constants/understanding-law.js
+++ 2024-frontend/src/constants/understanding-law.js
@@ -3,7 +3,8 @@ const tiles = [
     title: "Plus de produits de qualité et durables dans nos assiettes",
     imgSrc: "/static/images/badges/appro.svg",
     details: "Nulla vitae elit libero, a pharetra augue.",
-    to: "https://ma-cantine.crisp.help/fr/article/les-categories-de-produits-durables-et-de-qualite-loi-egalim-1opzgwy/",
+    to:
+      "https://ma-cantine.crisp.help/fr/article/les-categories-de-produits-durables-et-de-qualite-loi-egalim-1opzgwy/",
   },
   {
     title: "Lutte contre le gaspillage alimentaire et dons alimentaires",
@@ -21,14 +22,14 @@ const tiles = [
     title: "Substitution des plastiques",
     imgSrc: "/static/images/badges/plastic.svg",
     details: "Nulla vitae elit libero, a pharetra augue.",
-    to: "https://ma-cantine.crisp.help/fr/article/la-reduction-du-plastique-dans-les-creches-3dz7y/"
+    to: "https://ma-cantine.crisp.help/fr/article/la-reduction-du-plastique-dans-les-creches-3dz7y/",
   },
   {
     title: "Information des usagers et convives",
     imgSrc: "/static/images/badges/info.svg",
     details: "Nulla vitae elit libero, a pharetra augue.",
-    to: "https://ma-cantine.crisp.help/fr/article/communiquer-aupres-de-ses-convives-loi-egalim-1st93f7/"
-  }
+    to: "https://ma-cantine.crisp.help/fr/article/communiquer-aupres-de-ses-convives-loi-egalim-1st93f7/",
+  },
 ]
 
 export { tiles }
