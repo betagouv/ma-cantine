3ca939ed935f1688d5756b6c5d7a23ea42956f37
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-02 10:59:27 +0200

    Déplace le contenu des tuiles dans un fichier isolé

---
+++ 2024-frontend/src/constants/understanding-law.js
@@ -0,0 +1,34 @@
+const tiles = [
+  {
+    title: "Plus de produits de qualité et durables dans nos assiettes",
+    imgSrc: "/static/images/badges/appro.svg",
+    details: "Nulla vitae elit libero, a pharetra augue.",
+    to: "https://ma-cantine.crisp.help/fr/article/les-categories-de-produits-durables-et-de-qualite-loi-egalim-1opzgwy/",
+  },
+  {
+    title: "Lutte contre le gaspillage alimentaire et dons alimentaires",
+    imgSrc: "/static/images/badges/waste.svg",
+    details: "Nulla vitae elit libero, a pharetra augue.",
+    to: "https://ma-cantine.crisp.help/fr/article/lutte-contre-le-gaspillage-alimentaire-loi-egalim-e1apbo/",
+  },
+  {
+    title: "Diversification des sources de protéines et menus végétariens",
+    imgSrc: "/static/images/badges/diversification.svg",
+    details: "Nulla vitae elit libero, a pharetra augue.",
+    to: "https://ma-cantine.crisp.help/fr/article/menus-vegetariens-et-recettes-loi-egalim-1tvelzv/",
+  },
+  {
+    title: "Substitution des plastiques",
+    imgSrc: "/static/images/badges/plastic.svg",
+    details: "Nulla vitae elit libero, a pharetra augue.",
+    to: "https://ma-cantine.crisp.help/fr/article/la-reduction-du-plastique-dans-les-creches-3dz7y/"
+  },
+  {
+    title: "Information des usagers et convives",
+    imgSrc: "/static/images/badges/info.svg",
+    details: "Nulla vitae elit libero, a pharetra augue.",
+    to: "https://ma-cantine.crisp.help/fr/article/communiquer-aupres-de-ses-convives-loi-egalim-1st93f7/"
+  }
+]
+
+export { tiles }
