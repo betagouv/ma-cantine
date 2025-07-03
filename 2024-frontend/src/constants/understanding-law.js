13eb8bae1ed0e16ef38e60a4b92eb4e6d5665adb
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-03 17:39:06 +0200

    Ajoute les description de tuiles

--- 2024-frontend/src/constants/understanding-law.js
+++ 2024-frontend/src/constants/understanding-law.js
@@ -2,32 +2,37 @@ const tiles = [
   {
     title: "Plus de produits de qualité et durables dans nos assiettes",
     imgSrc: "/static/images/badges/appro.svg",
-    details: "Nulla vitae elit libero, a pharetra augue.",
+    details:
+      "Retrouvez la liste des labels éligibles aux 50% de produits de qualité, aux 20% de produits bio et aux 60% de produits de qualité et durables pour les familles cumulées viandes-volailles-poissons.",
     to:
       "https://ma-cantine.crisp.help/fr/article/les-categories-de-produits-durables-et-de-qualite-loi-egalim-1opzgwy/",
   },
   {
     title: "Lutte contre le gaspillage alimentaire et dons alimentaires",
     imgSrc: "/static/images/badges/waste.svg",
-    details: "Nulla vitae elit libero, a pharetra augue.",
+    details:
+      "La lutte contre le gaspillage alimentaire est un pilier de la transition écologique en restauration collective. Les lois EGalim (2018) et AGEC (2020) imposent des obligations concrètes aux établissements pour limiter les pertes, valoriser les excédents et améliorer la gestion des repas.",
     to: "https://ma-cantine.crisp.help/fr/article/lutte-contre-le-gaspillage-alimentaire-loi-egalim-e1apbo/",
   },
   {
     title: "Diversification des sources de protéines et menus végétariens",
     imgSrc: "/static/images/badges/diversification.svg",
-    details: "Nulla vitae elit libero, a pharetra augue.",
+    details:
+      "La loi EGalim a introduit des évolutions majeures en matière de restauration collective, dont l’obligation de proposer régulièrement des menus végétariens.",
     to: "https://ma-cantine.crisp.help/fr/article/menus-vegetariens-et-recettes-loi-egalim-1tvelzv/",
   },
   {
     title: "Substitution des plastiques",
     imgSrc: "/static/images/badges/plastic.svg",
-    details: "Nulla vitae elit libero, a pharetra augue.",
+    details:
+      "Dans le cadre de la loi EGalim, la réduction du plastique en restauration collective nécessite une adaptation dans les comportements existants.",
     to: "https://ma-cantine.crisp.help/fr/article/la-reduction-du-plastique-dans-les-creches-3dz7y/",
   },
   {
     title: "Information des usagers et convives",
     imgSrc: "/static/images/badges/info.svg",
-    details: "Nulla vitae elit libero, a pharetra augue.",
+    details:
+      "Vous souhaitez valoriser vos actions auprès des convives ? Zoom sur les obligations en matière de communication.",
     to: "https://ma-cantine.crisp.help/fr/article/communiquer-aupres-de-ses-convives-loi-egalim-1st93f7/",
   },
 ]
