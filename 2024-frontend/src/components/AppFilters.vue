f1a6b67862d8211e0f3203326c5c7405315961f1
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-08 18:36:54 +0200

    Ajoute la liste déroulante pour les filtres sur les types de cantines

--- 2024-frontend/src/components/AppFilters.vue
+++ 2024-frontend/src/components/AppFilters.vue
@@ -1,21 +1,50 @@
 <script setup>
 import { ref } from "vue"
-import { getYearsOptions } from "@/services/filters"
+import { getYearsOptions, getCharacteristicsOptions } from "@/services/filters"
 import AppDropdown from "@/components/AppDropdown.vue"
 
 /* Display */
 const props = defineProps(["filters"])
-const displayYearFilter = ref(props.filters.includes("year"))
+const displayYearFilter = ref(props.filters.includes("years"))
+const displayCharacteristicsFilter = ref(props.filters.includes("characteristics"))
 
 /* Otions */
-const yearsOptions = displayYearFilter.value ? ref(getYearsOptions()) : ref()
+const yearsOptions = ref(getYearsOptions())
+const characteristicsOptions = ref(getCharacteristicsOptions())
 
 /* Models */
 const yearModel = ref("")
+const economicModel = ref("")
+const managementType = ref("")
+const productionType = ref([])
 </script>
 
 <template>
   <ul class="ma-cantine--unstyled-list">
+    <li v-if="displayCharacteristicsFilter">
+      <AppDropdown label="Caractéristiques">
+        <DsfrRadioButtonSet
+          legend="Type d’établissement"
+          :modelValue="economicModel"
+          :options="characteristicsOptions.economicModel"
+          small
+          inline
+        />
+        <DsfrRadioButtonSet
+          legend="Mode de gestion"
+          :modelValue="managementType"
+          :options="characteristicsOptions.managementType"
+          small
+          inline
+        />
+        <DsfrCheckboxSet
+          legend="Modes de production"
+          :modelValue="productionType"
+          :options="characteristicsOptions.productionType"
+          small
+        />
+      </AppDropdown>
+    </li>
     <li v-if="displayYearFilter">
       <AppDropdown label="Années">
         <DsfrRadioButtonSet :modelValue="yearModel" :options="yearsOptions" small />
