74fc7fd898ab5c39e5ca533274e0c4e7c127a033
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-08 18:36:54 +0200

    Ajoute la liste déroulante pour les filtres sur les types de cantines

--- 2024-frontend/src/services/filters.js
+++ 2024-frontend/src/services/filters.js
@@ -1,3 +1,5 @@
+import canteenCharacteristics from "@/constants/canteen-establishment-form-options.js"
+
 const getYearsOptions = () => {
   const startYear = 2020
   const endYear = new Date().getFullYear()
@@ -13,4 +15,28 @@ const getYearsOptions = () => {
   return yearsOptions
 }
 
-export { getYearsOptions }
+const getCharacteristicsOptions = () => {
+  const economicModelOptions = canteenCharacteristics.economicModel.map((option) => {
+    option.hint = ""
+    option.name = "economicModel"
+    return option
+  })
+
+  const managementTypeOptions = canteenCharacteristics.managementType.map((option) => {
+    option.name = "managementType"
+    return option
+  })
+
+  const productionTypeOptions = canteenCharacteristics.productionType.map((option) => {
+    option.id = option.value
+    return option
+  })
+
+  const characteristicsOptions = {
+    economicModel: economicModelOptions,
+    managementType: managementTypeOptions,
+    productionType: productionTypeOptions,
+  }
+  return characteristicsOptions
+}
+export { getYearsOptions, getCharacteristicsOptions }
