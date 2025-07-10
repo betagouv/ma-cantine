6f45b4cee0c657def4ba783f53f56e0763a15614
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-10 18:51:48 +0200

    Simplification du composant en détachant les filtres

---
+++ 2024-frontend/src/components/FilterByCities.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue"
+import AppDropdown from "@/components/AppDropdown.vue"
+
+/* Model */
+const selectedCities = ref("")
+
+/* Search */
+const search = ref("")
+</script>
+<template>
+  <AppDropdown label="Communes">
+    <DsfrSearchBar
+      :modelValue="search"
+      placeholder="Rechercher une commune"
+      @update:modelValue="($event) => (search = $event)"
+    />
+    <DsfrCheckboxSet :modelValue="selectedCities" :options="[]" small />
+  </AppDropdown>
+</template>
---
+++ 2024-frontend/src/components/FilterBySectors.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, computed } from "vue"
+import { getSectorsOptions } from "@/services/filters"
+import AppDropdown from "@/components/AppDropdown.vue"
+
+const sectorsSelected = ref([])
+
+/* Get sectors */
+const sectors = ref([])
+getSectorsOptions().then((response) => {
+  sectors.value = response
+})
+
+/* Search */
+const search = ref("")
+const options = computed(() => {
+  if (sectors.value.length === 0) return []
+  if (search.value === "") return sectors.value
+  const searchedSectors = sectors.value.filter((sector) => {
+    const sectorLabel = sector.label.toLowerCase()
+    const stringSearched = search.value.toLowerCase()
+    return sectorLabel.indexOf(stringSearched) >= 0
+  })
+  return searchedSectors
+})
+</script>
+
+<template>
+  <AppDropdown label="Secteurs">
+    <DsfrSearchBar
+      :modelValue="search"
+      placeholder="Rechercher un secteur"
+      @update:modelValue="($event) => (search = $event)"
+    />
+    <DsfrCheckboxSet :modelValue="sectorsSelected" :options="options" small />
+  </AppDropdown>
+</template>
