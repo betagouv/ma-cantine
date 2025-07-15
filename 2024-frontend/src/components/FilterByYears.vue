13aa8f764c1da5a3030d2adf13996d8156924f22
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-15 18:43:13 +0200

    Connecte le filtre par année au store

--- 2024-frontend/src/components/FilterByYears.vue
+++ 2024-frontend/src/components/FilterByYears.vue
@@ -1,14 +1,19 @@
 <script setup>
-import { ref } from "vue"
+import { ref, computed } from "vue"
+import { useFiltersStore } from "@/stores/filters"
 import { getYearsOptions } from "@/services/filters"
 import AppDropdown from "@/components/AppDropdown.vue"
 
-const yearSelected = ref("")
+const filterStore = useFiltersStore()
+const yearSelected = computed(() => filterStore.year)
 const options = ref(getYearsOptions())
+const updateFilter = (value) => {
+  filterStore.update("year", value)
+}
 </script>
 
 <template>
   <AppDropdown label="Années">
-    <DsfrRadioButtonSet :modelValue="yearSelected" :options="options" small />
+    <DsfrRadioButtonSet :modelValue="yearSelected" @update:modelValue="updateFilter" :options="options" small />
   </AppDropdown>
 </template>
