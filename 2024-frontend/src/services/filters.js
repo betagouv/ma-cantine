6d2f71dc7033ffc76bc0ef3eb962fcb16fea726b
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-15 17:02:54 +0200

    Utilise un dataset pour les options de la cantine

--- 2024-frontend/src/services/filters.js
+++ 2024-frontend/src/services/filters.js
@@ -1,18 +1,21 @@
-import canteenCharacteristics from "@/constants/canteen-establishment-form-options.js"
+/* Services */
 import sectorsService from "@/services/sectors"
 import stringsService from "@/services/strings"
+
+/* Dataset */
+import cantines from "@/data/cantines.json"
 import communes from "@/data/communes.json"
 import pats from "@/data/pats.json"
 import epcis from "@/data/epcis.json"
 import departements from "@/data/departements.json"
 import regions from "@/data/regions.json"
-
 const regionsSortedByName = regions.sort((regionBefore, regionAfter) => {
   if (regionBefore.nom < regionAfter.nom) return -1
   else if (regionBefore.nom > regionAfter.nom) return 1
   else return 0
 })
 
+/* Exported functions */
 const getYearsOptions = () => {
   const startYear = 2020
   const endYear = new Date().getFullYear()
@@ -29,24 +32,21 @@ const getYearsOptions = () => {
 }
 
 const getCharacteristicsOptions = () => {
-  const economicModelOptions = canteenCharacteristics.economicModel.map((option) => {
+  const economicModelOptions = cantines.economicModel.map((option) => {
     option.hint = ""
     option.id = option.value
     return option
   })
 
-  const managementTypeOptions = canteenCharacteristics.managementType.map((option) => {
+  const managementTypeOptions = cantines.managementType.map((option) => {
     option.name = "managementType"
     option.id = option.value
     return option
   })
 
-  const productionTypeOptions = [
-    { label: "Site", value: "site", id: "site" },
-    { label: "Satellite", value: "site_cooked_elsewhere", id: "site_cooked_elsewhere" },
-    { label: "Centrale", value: "central", id: "central" },
-    { label: "Centrale et site", value: "central_serving", id: "central_serving" },
-  ]
+  const productionTypeOptions = cantines.productionType.map((option) => {
+    return { label: option.label, value: option.value, id: option.value }
+  })
 
   const characteristicsOptions = {
     economicModel: economicModelOptions,
