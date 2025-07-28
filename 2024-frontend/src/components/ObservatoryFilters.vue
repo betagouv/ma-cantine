eafb5c0fbe13e13c3639c2a1ad6d4bc73c68f2df
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-28 15:40:57 +0200

    Aligne tous les filtres sur une même ligne

--- 2024-frontend/src/components/ObservatoryFilters.vue
+++ 2024-frontend/src/components/ObservatoryFilters.vue
@@ -10,9 +10,9 @@ import FilterByRegions from "@/components/FilterByRegions.vue"
 </script>
 
 <template>
-  <section class="fr-pt-5w">
+  <section class="observatory-filters fr-pt-5w">
     <h2 class="fr-h5">Retrouver les chiffres clés sur votre territoire</h2>
-    <div class="observatory-filters">
+    <div class="observatory-filters__container">
       <p class="observatory-filters__title fr-mb-0">Filtrer par :</p>
       <div class="fr-grid-row">
         <FilterByRegions class="fr-mr-1v fr-mb-1v" />
@@ -20,7 +20,6 @@ import FilterByRegions from "@/components/FilterByRegions.vue"
         <FilterByEPCI class="fr-mr-1v fr-mb-1v" />
         <FilterByPAT class="fr-mr-1v fr-mb-1v" />
         <FilterByCities class="fr-mr-1v fr-mb-1v" />
-        <div class="fr-col-12 fr-hidden fr-unhidden-md"></div>
         <FilterBySectors class="fr-mr-1v fr-mb-1v" />
         <FilterByCharacteristics class="fr-mr-1v fr-mb-1v" />
         <FilterByYears class="fr-mr-1v fr-mb-1v" />
@@ -31,14 +30,16 @@ import FilterByRegions from "@/components/FilterByRegions.vue"
 
 <style lang="scss">
 .observatory-filters {
-  display: flex;
-  justify-content: flex-start;
-  align-items: baseline;
-  flex-direction: column;
-  gap: 1rem;
+  &__container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: baseline;
+    flex-direction: column;
+    gap: 1rem;
 
-  @media (min-width: 768px) {
-    flex-direction: row;
+    @media (min-width: 768px) {
+      flex-direction: row;
+    }
   }
 
   &__title {
