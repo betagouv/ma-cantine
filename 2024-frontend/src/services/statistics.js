c4111f0eaade9d9dbd5452ed2e19989cd0120815
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-28 18:38:24 +0200

    Crée un service pour gérer les appels api à l'url de stats

---
+++ 2024-frontend/src/services/statistics.js
@@ -0,0 +1,31 @@
+import { verifyResponse } from "@/services/api.js"
+
+const formatMultipleValues = (name, list) => {
+  let string = ""
+  list.forEach((item) => {
+    string += `&${name}=${item.value}`
+  })
+  return string
+}
+
+const filtersToQuery = (filters) => {
+  let query = "?"
+  query += `year=${filters.year}`
+  if (filters.sectors.length > 0) query += formatMultipleValues("sectors", filters.sectors)
+  if (filters.regions.length > 0) query += formatMultipleValues("region", filters.regions)
+  if (filters.departments.length >= 0) query += formatMultipleValues("department", filters.departments)
+  if (filters.epcis.length >= 0) query += formatMultipleValues("epci", filters.epcis)
+  if (filters.pats.length >= 0) query += formatMultipleValues("pat", filters.pats)
+  if (filters.cities.length >= 0) query += formatMultipleValues("city", filters.cities)
+  if (filters.economicModel.length >= 0) query += formatMultipleValues("economic_model", filters.economicModel)
+  if (filters.managementType.length >= 0) query += formatMultipleValues("management_type", filters.managementType)
+  if (filters.productionType.length >= 0) query += formatMultipleValues("production_type", filters.productionType)
+  return query
+}
+
+const getStatistics = (filters) => {
+  const query = filtersToQuery(filters)
+  return fetch(`/api/v1/canteenStatistics/${query}`, { method: "GET" }).then(verifyResponse)
+}
+
+export default { getStatistics }
