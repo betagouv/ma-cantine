3cd7b5fd0def138b4af44b69f0c51c8b3f329ea2
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-29 23:06:23 +0200

    Change les filtres sticky en fonction du scroll

--- 2024-frontend/src/components/ObservatoryResultsFilters.vue
+++ 2024-frontend/src/components/ObservatoryResultsFilters.vue
@@ -1,24 +1,48 @@
 <script setup>
-import { computed } from "vue"
+import { ref, computed, onMounted, onUnmounted, useTemplateRef } from "vue"
 import { useStoreFilters } from "@/stores/filters"
 
 const storeFilters = useStoreFilters()
 const filtersList = computed(() => storeFilters.getSelection())
+
+/* Change styles depending on scroll */
+const filtersResults = useTemplateRef("observatory-results-filters")
+const displaySmallFilters = ref(false)
+const scroll = () => {
+  const bounding = filtersResults.value.getBoundingClientRect()
+  const isStickToTop = bounding.top <= 0
+  if (isStickToTop && !displaySmallFilters.value) displaySmallFilters.value = true
+  if (!isStickToTop && displaySmallFilters.value) displaySmallFilters.value = false
+}
+
+/* Scroll watchers */
+onMounted(() => {
+  window.addEventListener("scroll", scroll)
+})
+onUnmounted(() => {
+  window.removeEventListener("scroll", scroll)
+})
 </script>
 
 <template>
-  <div class="observatory-results-filters ma-cantine--sticky__top fr-py-2w fr-background-alt--blue-france">
+  <div
+    ref="observatory-results-filters"
+    class="observatory-results-filters ma-cantine--sticky__top fr-py-2w fr-background-alt--blue-france"
+    :class="{ small: displaySmallFilters }"
+  >
     <p class="observatory-results-filters__title fr-mb-0">Chiffres clés pour la recherche :</p>
-    <ul class="observatory-results-filters__list ma-cantine--unstyled-list fr-my-0">
-      <li v-for="(filter, index) in filtersList" :key="index" class="r-mr-1w">
-        <DsfrTag
-          @click="storeFilters.remove(filter.name, filter.value)"
-          :label="filter.label"
-          class="fr-tag--dismiss"
-          tagName="button"
-        />
-      </li>
-    </ul>
+    <div class="observatory-results-filters__scrollable">
+      <ul class="observatory-results-filters__list ma-cantine--unstyled-list fr-my-0">
+        <li v-for="(filter, index) in filtersList" :key="index" class="observatory-results-filters__item">
+          <DsfrTag
+            @click="storeFilters.remove(filter.name, filter.value)"
+            :label="filter.label"
+            class="fr-tag--dismiss"
+            tagName="button"
+          />
+        </li>
+      </ul>
+    </div>
     <DsfrButton
       class="observatory-results-filters__button"
       tertiary
@@ -45,17 +69,44 @@ const filtersList = computed(() => storeFilters.getSelection())
     flex-shrink: 0;
   }
 
-  &__list {
-    justify-content: flex-start;
+  &__scrollable {
     flex-grow: 1;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.5rem;
     flex-shrink: 1;
   }
 
-  &__button {
+  &__list {
+    justify-content: flex-start;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+  }
+
+  &__item {
     flex-shrink: 0;
+  }
+
+  &__button {
+    display: none !important;
+    flex-shrink: 0;
+  }
+
+  &.small {
+    .observatory-results-filters {
+      &__scrollable {
+        max-height: 2rem;
+        overflow-x: scroll;
+      }
+
+      &__list {
+        flex-wrap: nowrap;
+        width: fit-content;
+      }
+
+      &__button {
+        display: inline-flex !important;
+        flex-shrink: 0;
+      }
+    }
   }
 }
 </style>
