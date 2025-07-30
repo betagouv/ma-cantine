a2c125e0f381157b6bd492a9a083613a82f9dbf9
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-30 16:30:54 +0200

    Crée un composant pour afficher les erreurs dans la section de résultat

---
+++ 2024-frontend/src/components/ObservatoryResultsError.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { computed } from "vue"
+const props = defineProps(["code"])
+
+const description = computed(() => {
+  if (props.code === "noYear") return "vous devez sélectionner une année"
+  if (props.code === "noFilter") return "aucun filtre sélectionné"
+  return "veuillez recharger la page pour renouveller votre recherche, si l'erreur persiste contactez-nous."
+})
+</script>
+<template>
+  <DsfrNotice type="alert" title="Une erreur est survenue lors de l'affichage des résultats :" :desc="description" />
+</template>
