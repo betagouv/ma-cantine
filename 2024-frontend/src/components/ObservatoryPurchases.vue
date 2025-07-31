ad0d45abb13f1e2bf6b5390c8b95ba0e01b913e5
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-07-31 11:57:34 +0200

    Nom plus explicite

--- 2024-frontend/src/components/ObservatoryPurchases.vue
+++ 2024-frontend/src/components/ObservatoryPurchases.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AppLinkRouter from "@/components/AppLinkRouter.vue"
-import ObservatoryCardTitle from "@/components/ObservatoryCardTitle.vue"
+import ObservatoryBadgeTitle from "@/components/ObservatoryBadgeTitle.vue"
 import keyMeasures from "@/data/key-measures.json"
 
 const approBadge = "/static/images/badges/appro.svg"
@@ -9,10 +9,10 @@ const keyMeasureId = keyMeasures[0].id
 
 <template>
   <div class="fr-card fr-p-4w">
-    <ObservatoryCardTitle class="fr-mb-3w" :image="approBadge" color="#f44336" title="Produits durables et de qualité">
+    <ObservatoryBadgeTitle class="fr-mb-3w" :image="approBadge" color="#f44336" title="Produits durables et de qualité">
       Distribuer 50% de produits de qualité dont 20% de produits bio et distribuer au moins 60 % de produits durables et
       de qualité dans la famille de denrées “viandes et poissons“.
       <AppLinkRouter :to="{ name: 'KeyMeasurePage', params: { id: keyMeasureId } }" title="En savoir plus sur la loi" />
-    </ObservatoryCardTitle>
+    </ObservatoryBadgeTitle>
   </div>
 </template>
