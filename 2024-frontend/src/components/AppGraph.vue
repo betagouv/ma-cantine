67580210bdcf64b6577d94cd86f80f8e25b8ec49
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-04 17:28:03 +0200

    Créer un composant pour gérer la description automatiquement pour les graphiques

---
+++ 2024-frontend/src/components/AppGraph.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from "vue"
+const props = defineProps(["valuesToVerify", "description"])
+
+/* Verifications */
+const displayGraph = computed(() => {
+  const incorrectValues = props.valuesToVerify.filter((value) => value === null || value === undefined)
+  return incorrectValues.length === 0
+})
+</script>
+<template>
+  <div>
+    <slot v-if="displayGraph"></slot>
+    <p v-else>
+      Une erreur est survenue lors de l'affichage du graphique, veuillez recharger la page et si l'erreur persiste
+      contactez-nous.
+    </p>
+    <DsfrAccordion v-if="description" title="Description du graphique" titleTag="p">
+      <p class="fr-mb-0">{{ description }}</p>
+    </DsfrAccordion>
+  </div>
+</template>
