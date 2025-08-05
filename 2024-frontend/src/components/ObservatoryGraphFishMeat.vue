88f7ce8dcda622ba0de9a59ac405e120cdc292ab
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 11:08:20 +0200

    Ajoute la description

--- 2024-frontend/src/components/ObservatoryGraphFishMeat.vue
+++ 2024-frontend/src/components/ObservatoryGraphFishMeat.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { reactive } from "vue"
+import { computed, reactive } from "vue"
+import { useStoreFilters } from "@/stores/filters"
 import GraphBase from "@/components/GraphBase.vue"
 import GraphGauge from "@/components/GraphGauge.vue"
 
 const props = defineProps(["meatPercent", "fishPercent"])
+const storeFilters = useStoreFilters()
 const title = "Viandes et volailles + produits d’aquacultures"
 
 /* Graph properties */
@@ -12,10 +14,25 @@ const objectives = [
   { name: "100% pour les restaurants d’état", value: 100 },
 ]
 const stats = reactive([props.meatPercent + props.fishPercent])
+
+/* Function to generate descriptions */
+const getFiltersDescription = () => {
+  const selected = storeFilters.getSelection().map((item) => item.label)
+  const list = selected.join(", ")
+  return `Pour la recherche ${list}`
+}
+
+/* Graph description */
+const description = computed(() => {
+  const filtersDescription = getFiltersDescription()
+  const resultsDescription = `avec un objectif fixé à ${objectives[0].name} et ${objectives[1].name} le résultat est : ${stats[0]}%`
+  return `${filtersDescription}, le résultat du graphique "${title}" ${resultsDescription}.`
+})
 </script>
+
 <template>
   <h3 class="fr-h6 fr-mb-2w">{{ title }}</h3>
-  <GraphBase :valuesToVerify="[props.meatPercent, props.fishPercent]">
+  <GraphBase :valuesToVerify="[props.meatPercent, props.fishPercent]" :description="description">
     <GraphGauge :objectives="objectives" :stats="stats" />
   </GraphBase>
 </template>
