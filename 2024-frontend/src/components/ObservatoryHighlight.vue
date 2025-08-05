7a26311d8cb60332db711e4f59cf85bf7e016ad9
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 17:46:48 +0200

    Créer un composant pour gérer l'affichage du message d'information

---
+++ 2024-frontend/src/components/ObservatoryHighlight.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from "vue"
+const props = defineProps(["reportNotPublished", "campaignNotFound"])
+const text = ref(props.reportNotPublished || props.campaignNotFound)
+</script>
+
+<template>
+  <DsfrHighlight :text="text" class="fr-ml-0" />
+</template>
