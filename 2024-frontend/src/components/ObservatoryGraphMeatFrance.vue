f227848ebbe39cfa4687f23fd5390898bb366a11
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 14:47:45 +0200

    Ajoute la description du graphique

--- 2024-frontend/src/components/ObservatoryGraphMeatFrance.vue
+++ 2024-frontend/src/components/ObservatoryGraphMeatFrance.vue
@@ -1,15 +1,30 @@
 <script setup>
-import { reactive } from "vue"
+import { reactive, computed } from "vue"
+import { useStoreFilters } from "@/stores/filters"
 import GraphBase from "@/components/GraphBase.vue"
 import GraphGauge from "@/components/GraphGauge.vue"
 
 const props = defineProps(["meatFrancePercent"])
+const storeFilters = useStoreFilters()
 const title = "Pourcentage des viandes origine France"
 const stats = reactive([props.meatFrancePercent])
+
+/* Function to generate descriptions */
+const getFiltersDescription = () => {
+  const selected = storeFilters.getSelection().map((item) => item.label)
+  const list = selected.join(", ")
+  return `Pour la recherche ${list}`
+}
+
+/* Graph description */
+const description = computed(() => {
+  const filtersDescription = getFiltersDescription()
+  return `${filtersDescription}, le résultat du graphique "${title}" est : ${stats[0]}%.`
+})
 </script>
 <template>
   <h3 class="fr-h6 fr-mb-2w">{{ title }}</h3>
-  <GraphBase :valuesToVerify="stats">
+  <GraphBase :valuesToVerify="stats" :description="description">
     <GraphGauge :stats="stats" />
   </GraphBase>
 </template>
