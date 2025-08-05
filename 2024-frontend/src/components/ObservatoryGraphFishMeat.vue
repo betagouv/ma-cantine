ae123ffdd29de3dc7ce0d0f1ceaf60a0266f2443
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 10:52:55 +0200

    Utilise la liste pour générer automatiquement les numéros des titres des graphiques

--- 2024-frontend/src/components/ObservatoryGraphFishMeat.vue
+++ 2024-frontend/src/components/ObservatoryGraphFishMeat.vue
@@ -2,5 +2,5 @@
 const title = "Viandes et volailles + produits d’aquacultures"
 </script>
 <template>
-  <h3 class="fr-h6 fr-mb-2w">3. {{ title }}</h3>
+  <h3 class="fr-h6 fr-mb-2w">{{ title }}</h3>
 </template>
--- 2024-frontend/src/components/ObservatoryPurchases.vue
+++ 2024-frontend/src/components/ObservatoryPurchases.vue
@@ -11,18 +11,40 @@ const keyMeasureId = keyMeasures[0].id
 </script>
 
 <template>
-  <div class="fr-card fr-p-4w">
+  <div class="observatory-purcharses fr-card fr-p-4w">
     <ObservatoryBadgeTitle class="fr-mb-3w" :image="approBadge" color="#f44336" title="Produits durables et de qualité">
       Distribuer 50% de produits de qualité dont 20% de produits bio et distribuer au moins 60 % de produits durables et
       de qualité dans la famille de denrées “viandes et poissons“.
       <AppLinkRouter :to="{ name: 'KeyMeasurePage', params: { id: keyMeasureId } }" title="En savoir plus sur la loi" />
     </ObservatoryBadgeTitle>
-    <div>
-      <ObservatoryGraphSustainable :bioPercent="stats.bioPercent" :egalimPercent="stats.egalimPercent" />
-      <ObservatoryGraphFishMeat
-        :meatEgalimPercent="stats.meatEgalimPercent"
-        :fishEgalimPercent="stats.fishEgalimPercent"
-      />
-    </div>
+    <ol class="observatory-purcharses__list-graphic ma-cantine--unstyled-list">
+      <li class="observatory-purcharses__graphic">
+        <ObservatoryGraphSustainable :bioPercent="stats.bioPercent" :egalimPercent="stats.egalimPercent" />
+      </li>
+      <li class="observatory-purcharses__graphic">
+        <ObservatoryGraphFishMeat :meatPercent="stats.meatEgalimPercent" :fishPercent="stats.fishEgalimPercent" />
+      </li>
+    </ol>
   </div>
 </template>
+
+<style lang="scss">
+.observatory-purcharses {
+  &__list-graphic {
+    counter-reset: number;
+  }
+
+  &__graphic {
+    counter-increment: number;
+
+    &::marker {
+      content: initial;
+    }
+
+    h3::before {
+      content: counter(number) ".";
+      margin-right: 0.5rem;
+    }
+  }
+}
+</style>
