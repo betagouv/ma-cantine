e992cc034d63821773f1514b7957de35809fd28b
Charline <charline.laporte@beta.gouv.fr>
2025-08-05 15:19:58 +0200

    feat(Observatoire): ajout du graphique des viandes origine France (#5578)

---
+++ 2024-frontend/src/components/ObservatoryGraphMeatFrance.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive, computed } from "vue"
+import { useStoreFilters } from "@/stores/filters"
+import GraphBase from "@/components/GraphBase.vue"
+import GraphGauge from "@/components/GraphGauge.vue"
+
+const props = defineProps(["meatFrancePercent"])
+const storeFilters = useStoreFilters()
+const title = "Pourcentage des viandes origine France"
+const stats = reactive([props.meatFrancePercent])
+
+/* Function to generate description */
+const getFiltersDescription = () => {
+  const selected = storeFilters.getSelection().map((item) => item.label)
+  const list = selected.join(", ")
+  return `Pour la recherche ${list}`
+}
+
+/* Graph description */
+const description = computed(() => {
+  const filtersDescription = getFiltersDescription()
+  return `${filtersDescription}, le résultat du graphique "${title}" est : ${stats[0]}%.`
+})
+</script>
+<template>
+  <h3 class="fr-h6 fr-mb-2w">{{ title }}</h3>
+  <GraphBase :valuesToVerify="stats" :description="description">
+    <GraphGauge :stats="stats" />
+  </GraphBase>
+</template>
