db2e0ba4d71211a15745c1570b221cc2f4f3bfd8
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 16:00:24 +0200

    Amélioration de stules

--- 2024-frontend/src/components/ObservatoryPurchases.vue
+++ 2024-frontend/src/components/ObservatoryPurchases.vue
@@ -12,8 +12,8 @@ const keyMeasureId = keyMeasures[0].id
 </script>
 
 <template>
-  <div class="observatory-purchases fr-card fr-p-4w">
-    <ObservatoryBadgeTitle class="fr-mb-3w" :image="approBadge" color="#f44336" title="Produits durables et de qualité">
+  <div class="observatory-purchases fr-card fr-p-5w fr-px-md-6w fr-pt-md-8w fr-pb-md-6w">
+    <ObservatoryBadgeTitle class="fr-mb-8w" :image="approBadge" color="#f44336" title="Produits durables et de qualité">
       Distribuer 50% de produits de qualité dont 20% de produits bio et distribuer au moins 60 % de produits durables et
       de qualité dans la famille de denrées “viandes et poissons“.
       <AppLinkRouter :to="{ name: 'KeyMeasurePage', params: { id: keyMeasureId } }" title="En savoir plus sur la loi" />
