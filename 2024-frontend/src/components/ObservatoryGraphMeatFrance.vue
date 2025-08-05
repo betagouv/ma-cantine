a96687a0734492cef6b1eb905c8046f396ad46d9
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 14:45:25 +0200

    Affiche le graphique

--- 2024-frontend/src/components/ObservatoryGraphMeatFrance.vue
+++ 2024-frontend/src/components/ObservatoryGraphMeatFrance.vue
@@ -1,6 +1,15 @@
 <script setup>
+import { reactive } from "vue"
+import GraphBase from "@/components/GraphBase.vue"
+import GraphGauge from "@/components/GraphGauge.vue"
+
+const props = defineProps(["meatFrancePercent"])
 const title = "Pourcentage des viandes origine France"
+const stats = reactive([props.meatFrancePercent])
 </script>
 <template>
   <h3 class="fr-h6 fr-mb-2w">{{ title }}</h3>
+  <GraphBase :valuesToVerify="stats">
+    <GraphGauge :stats="stats" />
+  </GraphBase>
 </template>
