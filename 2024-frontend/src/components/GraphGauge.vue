dfc5b7d65dbc4b8353fa9ede01a2025b146f2204
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-05 16:03:34 +0200

    Diminue l'espacement s'il n'y a pas de légende

--- 2024-frontend/src/components/GraphGauge.vue
+++ 2024-frontend/src/components/GraphGauge.vue
@@ -10,7 +10,7 @@ const checkOutside = (index) => {
 </script>
 
 <template>
-  <div class="graph-gauge fr-mt-7w fr-mb-3w">
+  <div class="graph-gauge fr-mb-3w" :class="{ 'fr-mt-7w': objectives, 'fr-mt-4w': !objectives }">
     <div v-if="objectives" class="graph-gauge__objectives-container">
       <p class="graph-gauge__objectif graph-gauge__objectif--title fr-text--sm ma-cantine--bold">
         Objectif EGalim
