33660c78fd79d8957b66997503b16c34bd554192
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-07 11:59:45 +0200

    Ajout du titre du bloc

--- 2024-frontend/src/components/ObservatoryCanteens.vue
+++ 2024-frontend/src/components/ObservatoryCanteens.vue
@@ -1,6 +1,20 @@
 <script setup>
+import ObservatoryBadgeTitle from "@/components/ObservatoryBadgeTitle.vue"
 defineProps(["stats"])
+
+const canteenBadge = "/static/images/badges/canteen.svg"
 </script>
+
 <template>
-  <p>Cantines</p>
+  <div>
+    <ObservatoryBadgeTitle
+      class="fr-mb-8w"
+      :image="canteenBadge"
+      color="#000090"
+      title="Les restaurants collectifs du territoire"
+    >
+      Tous les restaurants collectifs on l'obligation de s'inscrire sur ma cantine et de télédéclarer, chaque année,
+      leur bilan annuel de l'année précédente.
+    </ObservatoryBadgeTitle>
+  </div>
 </template>
