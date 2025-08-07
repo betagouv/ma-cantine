c11c0e3faaf78a5a1145cd191c5d41c1cc446b4d
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-07 11:56:10 +0200

    Déplace le style réutilisé dans le parent

--- 2024-frontend/src/views/Observatoire.vue
+++ 2024-frontend/src/views/Observatoire.vue
@@ -62,8 +62,8 @@ watchEffect(async () => {
         class="fr-mb-3w"
       />
       <template v-if="stats.egalimPercent !== null">
-        <ObservatoryPurchases :stats="stats" />
-        <ObservatoryCanteens :stats="stats" />
+        <ObservatoryPurchases :stats="stats" class="fr-card fr-mb-4w fr-p-5w fr-px-md-6w fr-pt-md-8w fr-pb-md-6w" />
+        <ObservatoryCanteens :stats="stats" class="fr-card fr-p-5w fr-px-md-6w fr-pt-md-8w fr-pb-md-6w" />
       </template>
       <DsfrHighlight v-else :text="stats.notes.campaignInfo" class="fr-col-12 fr-col-md-8 fr-ml-0" />
     </template>
