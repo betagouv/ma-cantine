e392848953b70497e613f67c3d1747ced674f26a
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-07 11:37:51 +0200

    Ajoute la description

--- 2024-frontend/src/components/ObservatoryGraphEgalim.vue
+++ 2024-frontend/src/components/ObservatoryGraphEgalim.vue
@@ -1,13 +1,21 @@
 <script setup>
+import { computed } from "vue"
+import { useStoreFilters } from "@/stores/filters"
 import GraphBase from "@/components/GraphBase.vue"
 import GraphGauge from "@/components/GraphGauge.vue"
-defineProps(["approPercent"])
+
+const props = defineProps(["approPercent"])
+const storeFilters = useStoreFilters()
 const title = "Télédéclarations qui ont atteint l'objectif EGalim"
+const description = computed(() => {
+  const filters = storeFilters.getSelectionLabels()
+  return `Pour la recherche ${filters}, le pourcentage de "${title}" est : ${props.approPercent}%.`
+})
 </script>
 
 <template>
   <h3 class="fr-h6 fr-mb-2w">{{ title }}</h3>
-  <GraphBase :valuesToVerify="[approPercent]">
+  <GraphBase :valuesToVerify="[approPercent]" :description="description">
     <GraphGauge :stats="[approPercent]" />
   </GraphBase>
 </template>
