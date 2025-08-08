7b0b9e4fd78d590dd44b7a73c840e88513b00fe3
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-08 22:08:13 +0200

    Fix wording

--- 2024-frontend/src/components/ObservatoryShare.vue
+++ 2024-frontend/src/components/ObservatoryShare.vue
@@ -16,9 +16,9 @@ const openModal = () => {
 </script>
 <template>
   <div class="fr-grid-row fr-grid-row--right">
-    <DsfrButton label="Partager ma recherche" secondary :icon="iconShare" @click="openModal" />
+    <DsfrButton label="Copier l’url de la recherche" secondary :icon="iconShare" @click="openModal" />
   </div>
-  <DsfrModal title="Partager la recherche" :opened="opened" :icon="iconShare" @close="opened = false">
+  <DsfrModal title="Copier l’url de la recherche" :opened="opened" :icon="iconShare" @close="opened = false">
     <template #default>
       <p>Pour partager les chiffres clés de votre recherche : {{ selectedFilters }}</p>
       <p class="ma-cantine--bold fr-mb-1w">Copier l'url :</p>
