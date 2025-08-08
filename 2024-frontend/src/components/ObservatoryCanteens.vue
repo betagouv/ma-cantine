3ba7b8f48b5e3beb9a05f2f63b7a925a360f3548
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-08 12:28:59 +0200

    Améliore l'esapcement

--- 2024-frontend/src/components/ObservatoryCanteens.vue
+++ 2024-frontend/src/components/ObservatoryCanteens.vue
@@ -20,13 +20,13 @@ const canteenBadge = "/static/images/badges/canteen.svg"
       leur bilan annuel de l'année précédente.
     </ObservatoryBadgeTitle>
     <ol class="fr-grid-row fr-grid-row--gutters ma-cantine--ordered-list ma-cantine--unstyled-list">
-      <li class="fr-col-12 fr-col-md-6 fr-mb-8w">
+      <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
         <ObservatoryGraphProductionType :productionTypes="stats.productionTypes" :canteensCount="stats.canteenCount" />
       </li>
-      <li class="fr-col-12 fr-col-md-6 fr-mb-8w">
+      <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
         <ObservatoryGraphEconomicModel :economicModels="stats.economicModels" :canteensCount="stats.canteenCount" />
       </li>
-      <li class="fr-col-12 fr-col-md-6 fr-mb-8w">
+      <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
         <ObservatoryGraphManagementType :managementTypes="stats.managementTypes" :canteensCount="stats.canteenCount" />
       </li>
     </ol>
