1e4dd31526557b3eb2b4e49890e37b0f4b9ccd06
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-08 17:24:24 +0200

    Inverse les modes pour être plus homogène dans l'affichaeg en 2 colonnes

--- 2024-frontend/src/components/ObservatoryCanteens.vue
+++ 2024-frontend/src/components/ObservatoryCanteens.vue
@@ -22,13 +22,13 @@ const canteenBadge = "/static/images/badges/canteen.svg"
     </ObservatoryBadgeTitle>
     <ol class="fr-grid-row fr-grid-row--gutters ma-cantine--ordered-list ma-cantine--unstyled-list">
       <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
-        <ObservatoryGraphProductionType :productionTypes="stats.productionTypes" :canteensCount="stats.canteenCount" />
-      </li>
-      <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
         <ObservatoryGraphEconomicModel :economicModels="stats.economicModels" :canteensCount="stats.canteenCount" />
       </li>
       <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
         <ObservatoryGraphManagementType :managementTypes="stats.managementTypes" :canteensCount="stats.canteenCount" />
+      </li>
+      <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
+        <ObservatoryGraphProductionType :productionTypes="stats.productionTypes" :canteensCount="stats.canteenCount" />
       </li>
       <li class="fr-col-12 fr-col-md-6 fr-mb-4w">
         <ObservatoryGraphSectors :sectorCategories="stats.sectorCategories" :canteensCount="stats.canteenCount" />
