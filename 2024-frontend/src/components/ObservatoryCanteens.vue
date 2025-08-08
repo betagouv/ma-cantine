91ef3716cab1544f4eb733282faa9d5bc4f40ea6
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-08 12:20:11 +0200

    Positionne les graphiques en 2 colonnes

--- 2024-frontend/src/components/ObservatoryCanteens.vue
+++ 2024-frontend/src/components/ObservatoryCanteens.vue
@@ -19,14 +19,14 @@ const canteenBadge = "/static/images/badges/canteen.svg"
       Tous les restaurants collectifs ont l'obligation de s'inscrire sur ma cantine et de télédéclarer, chaque année,
       leur bilan annuel de l'année précédente.
     </ObservatoryBadgeTitle>
-    <ol class="ma-cantine--ordered-list ma-cantine--unstyled-list">
-      <li>
+    <ol class="fr-grid-row fr-grid-row--gutters ma-cantine--ordered-list ma-cantine--unstyled-list">
+      <li class="fr-col-12 fr-col-md-6 fr-mb-8w">
         <ObservatoryGraphProductionType :productionTypes="stats.productionTypes" :canteensCount="stats.canteenCount" />
       </li>
-      <li>
+      <li class="fr-col-12 fr-col-md-6 fr-mb-8w">
         <ObservatoryGraphEconomicModel :economicModels="stats.economicModels" :canteensCount="stats.canteenCount" />
       </li>
-      <li>
+      <li class="fr-col-12 fr-col-md-6 fr-mb-8w">
         <ObservatoryGraphManagementType :managementTypes="stats.managementTypes" :canteensCount="stats.canteenCount" />
       </li>
     </ol>
