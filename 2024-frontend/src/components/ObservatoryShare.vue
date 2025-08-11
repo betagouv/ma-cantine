dbbc2a4f7c79f29d4e4a28f79f47b29246dc45f9
Charline <charline.laporte@beta.gouv.fr>
2025-08-11 12:03:42 +0200

    feat(Observatoire): ajout du bouton "Copier l'url" (#5596)

---
+++ 2024-frontend/src/components/ObservatoryShare.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue"
+import { useStoreFilters } from "@/stores/filters.js"
+import AppCode from "@/components/AppCode.vue"
+
+const opened = ref(false)
+const iconShare = "fr-icon-share-line"
+const storeFilters = useStoreFilters()
+const selectedFilters = storeFilters.getSelectionLabels()
+const url = ref()
+
+const openModal = () => {
+  opened.value = true
+  url.value = window.location.href
+}
+</script>
+<template>
+  <div class="fr-grid-row fr-grid-row--right">
+    <DsfrButton label="Copier l’url de la recherche" secondary :icon="iconShare" @click="openModal" />
+  </div>
+  <DsfrModal title="Copier l’url de la recherche" :opened="opened" :icon="iconShare" @close="opened = false">
+    <template #default>
+      <p>Pour partager les chiffres clés de votre recherche : {{ selectedFilters }}</p>
+      <p class="ma-cantine--bold fr-mb-1w">Copier l'url :</p>
+      <AppCode :content="url" />
+    </template>
+  </DsfrModal>
+</template>
