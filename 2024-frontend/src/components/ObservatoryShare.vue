d8cbf2ce239098fcafbcf2ca2f908d51acfe1ae1
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-11 17:31:55 +0200

    Remet le bouton à son état initial au bout de 5 secondes

--- 2024-frontend/src/components/ObservatoryShare.vue
+++ 2024-frontend/src/components/ObservatoryShare.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { ref } from "vue"
 
-const icon = ref("fr-icon-clipboard-line")
-const label = ref("Copier l’url de la recherche")
+const icon = ref()
+const label = ref()
 
 const copyUrl = () => {
-  label.value = "Copié dans le presse-papier"
-  icon.value = "fr-icon-check-line"
   navigator.clipboard.writeText(window.location.href)
+  updateButton("copied")
+  setTimeout(updateButton, 5000)
 }
+
+/* Change button content */
+const updateButton = (state) => {
+  const isCopied = state === "copied"
+  label.value = isCopied ? "Copié dans le presse-papier" : "Copier l’url de la recherche"
+  icon.value = isCopied ? "fr-icon-check-line" : "fr-icon-clipboard-line"
+}
+updateButton()
 </script>
 <template>
   <div class="fr-mt-4w fr-grid-row fr-grid-row--right">
