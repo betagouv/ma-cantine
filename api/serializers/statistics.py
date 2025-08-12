470b6f72469bd75da885771bc069719fadaa918c
Raphael Odini <raphodn@users.noreply.github.com>
2025-08-12 18:00:21 +0200

    improve(API Stats): calcul temporaire du nombre de sites concernés (en attendant 1TD1Site)

--- api/serializers/statistics.py
+++ api/serializers/statistics.py
@@ -44,35 +44,41 @@ def calculate_statistics_canteens(canteens, data):
 
 def calculate_statistics_teledeclarations(teledeclarations, data):
     # aggregate
-    agg = teledeclarations.annotate(
-        value_meat_poultry_ht=Cast(
-            KT("declared_data__teledeclaration__value_meat_poultry_ht"), output_field=FloatField()
-        ),
-        value_meat_poultry_egalim_ht=Cast(
-            KT("declared_data__teledeclaration__value_meat_poultry_egalim_ht"), output_field=FloatField()
-        ),
-        value_meat_poultry_france_ht=Cast(
-            KT("declared_data__teledeclaration__value_meat_poultry_france_ht"), output_field=FloatField()
-        ),
-        value_fish_ht=Cast(KT("declared_data__teledeclaration__value_fish_ht"), output_field=FloatField()),
-        value_fish_egalim_ht=Cast(
-            KT("declared_data__teledeclaration__value_fish_egalim_ht"), output_field=FloatField()
-        ),
-    ).aggregate(
-        Count("id"),
-        Sum("value_bio_ht_agg", default=0),
-        Sum("value_total_ht", default=0),
-        Sum("value_sustainable_ht_agg", default=0),
-        Sum("value_externality_performance_ht_agg", default=0),
-        Sum("value_egalim_others_ht_agg", default=0),
-        Sum("value_meat_poultry_ht", default=0),
-        Sum("value_meat_poultry_egalim_ht", default=0),
-        Sum("value_meat_poultry_france_ht", default=0),
-        Sum("value_fish_ht", default=0),
-        Sum("value_fish_egalim_ht", default=0),
+    agg = (
+        teledeclarations.with_satellites_count()
+        .annotate(
+            value_meat_poultry_ht=Cast(
+                KT("declared_data__teledeclaration__value_meat_poultry_ht"), output_field=FloatField()
+            ),
+            value_meat_poultry_egalim_ht=Cast(
+                KT("declared_data__teledeclaration__value_meat_poultry_egalim_ht"), output_field=FloatField()
+            ),
+            value_meat_poultry_france_ht=Cast(
+                KT("declared_data__teledeclaration__value_meat_poultry_france_ht"), output_field=FloatField()
+            ),
+            value_fish_ht=Cast(KT("declared_data__teledeclaration__value_fish_ht"), output_field=FloatField()),
+            value_fish_egalim_ht=Cast(
+                KT("declared_data__teledeclaration__value_fish_egalim_ht"), output_field=FloatField()
+            ),
+        )
+        .aggregate(
+            Count("id"),
+            Sum("satellites_count", default=0),
+            Sum("value_bio_ht_agg", default=0),
+            Sum("value_total_ht", default=0),
+            Sum("value_sustainable_ht_agg", default=0),
+            Sum("value_externality_performance_ht_agg", default=0),
+            Sum("value_egalim_others_ht_agg", default=0),
+            Sum("value_meat_poultry_ht", default=0),
+            Sum("value_meat_poultry_egalim_ht", default=0),
+            Sum("value_meat_poultry_france_ht", default=0),
+            Sum("value_fish_ht", default=0),
+            Sum("value_fish_egalim_ht", default=0),
+        )
     )
     # count
     data["teledeclarations_count"] = agg["id__count"]
+    data["canteen_teledeclared_count"] = agg["satellites_count__sum"]
     # percent of bio, sustainable & egalim (bio + sustainable)
     if agg["value_total_ht__sum"] > 0:
         data["bio_percent"] = round(100 * agg["value_bio_ht_agg__sum"] / agg["value_total_ht__sum"])
@@ -126,7 +132,10 @@ class CanteenStatisticsSerializer(serializers.Serializer):
         "fish_egalim_percent",
         "appro_percent",
     ]
-    FIELDS_TO_HIDE_IF_CAMPAIGN_NOT_FOUND = ["teledeclarations_count"] + FIELDS_TO_HIDE_IF_REPORT_NOT_PUBLISHED
+    FIELDS_TO_HIDE_IF_CAMPAIGN_NOT_FOUND = [
+        "teledeclarations_count",
+        "canteen_teledeclared_count",
+    ] + FIELDS_TO_HIDE_IF_REPORT_NOT_PUBLISHED
 
     # canteen stats
     canteen_count = serializers.IntegerField()
@@ -135,7 +144,8 @@ class CanteenStatisticsSerializer(serializers.Serializer):
     production_types = serializers.DictField()
     economic_models = serializers.DictField()
     # teledeclaration stats
-    teledeclarations_count = serializers.IntegerField()
+    teledeclarations_count = serializers.IntegerField(label="Nombre de télédéclarations")
+    canteen_teledeclared_count = serializers.IntegerField(label="Nombre de sites concernés")
     bio_percent = serializers.IntegerField(label="Part des achats bio dans les achats alimentaires de l'année")
     sustainable_percent = serializers.IntegerField(
         label="Part des achats durables (hors bio) dans les achats alimentaires de l'année"
