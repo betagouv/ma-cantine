90d01739ef7cfb73b37f28665e92ed34adc1aea5
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-14 15:38:17 +0200

    Fix point de rupture

--- 2024-frontend/src/views/Observatoire.vue
+++ 2024-frontend/src/views/Observatoire.vue
@@ -86,8 +86,8 @@ watchEffect(async () => {
           class="fr-mb-3w"
         />
         <template v-if="stats.egalimPercent !== null">
-          <ObservatoryPurchases :stats="stats" class="fr-card fr-p-4w fr-p-lg-8w fr-mb-4w " />
-          <ObservatoryCanteens :stats="stats" class="fr-card fr-p-4w fr-p-lg-8w" />
+          <ObservatoryPurchases :stats="stats" class="fr-card fr-p-4w fr-p-md-8w fr-mb-4w " />
+          <ObservatoryCanteens :stats="stats" class="fr-card fr-p-4w fr-p-md-8w" />
         </template>
         <DsfrHighlight v-else :text="stats.notes.campaignInfo" class="fr-col-12 fr-col-md-8 fr-ml-0" />
       </template>
