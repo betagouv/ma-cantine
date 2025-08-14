80bd24e2e84a865853461e5c121e882ff7bbaf3b
Raphael Odini <raphodn@users.noreply.github.com>
2025-08-14 11:17:02 +0200

    fix test

--- data/tests/test_diagnostic.py
+++ data/tests/test_diagnostic.py
@@ -4,17 +4,23 @@ from data.factories import CanteenFactory, DiagnosticFactory
 from data.models import Diagnostic
 
 
-class DiagnosticQuerySetTest(TestCase):
+class DiagnosticTeledeclaredQuerySetAndPropertyTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.diagnostic_empty_draft = DiagnosticFactory.create(canteen=CanteenFactory(), value_total_ht=None)
+        cls.diagnostic_filled_draft = DiagnosticFactory.create(canteen=CanteenFactory(), value_total_ht=1000)
         cls.diagnostic_filled_submitted = DiagnosticFactory.create(
             canteen=CanteenFactory(), value_total_ht=1000, status=Diagnostic.DiagnosticStatus.SUBMITTED
         )
 
     def test_teledeclared_queryset(self):
-        self.assertEqual(Diagnostic.objects.all().count(), 2)
+        self.assertEqual(Diagnostic.objects.all().count(), 3)
         self.assertEqual(Diagnostic.objects.teledeclared().count(), 1)
+
+    def test_is_teledeclared_property(self):
+        self.assertFalse(self.diagnostic_empty_draft.is_teledeclared)
+        self.assertFalse(self.diagnostic_filled_draft.is_teledeclared)
+        self.assertTrue(self.diagnostic_filled_submitted.is_teledeclared)
 
 
 class DiagnosticIsFilledQuerySetAndPropertyTest(TestCase):
