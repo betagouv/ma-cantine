384e349e04f5f86317cdcc2b57d584cde1fbff9b
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-14 10:55:59 +0200

    Ré-organisation du composant

--- 2024-frontend/src/components/AppJeDonneMonAvis.vue
+++ 2024-frontend/src/components/AppJeDonneMonAvis.vue
@@ -3,14 +3,17 @@ defineProps(["url", "title"])
 </script>
 
 <template>
-  <section class="fr-background-alt--grey fr-my-5w fr-p-5w fr-grid-row fr-grid-row--center ma-cantine--text-center">
-    <div class="fr-col-8">
-      <h6>Aidez-nous à améliorer ce service !</h6>
-      <slot></slot>
-      <p class="fr-mb-5w ma-cantine--bold">Donnez-nous votre avis, cela ne prend que 3 minutes :</p>
-      <a class="ma-cantine--unstyled-link" :href="url" target="_blank" title="Je donne mon avis - nouvelle fenêtre">
-        <img src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-blanc-clair.svg" alt="Je donne mon avis" />
-      </a>
+  <section class="fr-card fr-my-5w fr-p-5w">
+    <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle">
+      <div class="fr-col-12 fr-col-md-6">
+        <h6 class="fr-mb-1w">{{ title }}</h6>
+        <p class="fr-mb-0">Donnez-nous votre avis, cela ne prend que 2 minutes.</p>
+      </div>
+      <div class="fr-col-12 fr-col-md-6 fr-mt-2w fr-mt-md-0 fr-grid-row fr-grid-row--center">
+        <a class="ma-cantine--unstyled-link" :href="url" target="_blank" title="Je donne mon avis - nouvelle fenêtre">
+          <img src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-bleu-clair.svg" alt="Je donne mon avis" />
+        </a>
+      </div>
     </div>
   </section>
 </template>
