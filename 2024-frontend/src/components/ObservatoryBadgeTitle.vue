0d497b0f1552b8a20936ba1f2d4101737447e3d0
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-14 15:37:37 +0200

    Améliore le responsive

--- 2024-frontend/src/components/ObservatoryBadgeTitle.vue
+++ 2024-frontend/src/components/ObservatoryBadgeTitle.vue
@@ -4,13 +4,13 @@ defineProps(["image", "title", "color"])
 
 <template>
   <div class="observatory-badge-title">
-    <div class="fr-grid-row fr-grid-row--middle fr-mb-2w">
+    <div class="observatory-badge-title__top fr-mb-2w">
       <img :src="image" class="observatory-badge-title__image" alt="" />
-      <h2 class="observatory-badge-title fr-ml-4w fr-h4 fr-mb-0" :style="`color: ${color}`">
+      <h2 class="fr-h4 fr-mt-2w fr-mt-md-0 fr-ml-md-4w fr-mb-0" :style="`color: ${color}`">
         {{ title }}
       </h2>
     </div>
-    <p class="fr-pl-10w fr-col-12 fr-col-md-10 fr-mb-0">
+    <p class="fr-pl-md-10w fr-col-12 fr-col-md-10 fr-mb-0">
       <slot></slot>
     </p>
   </div>
@@ -18,9 +18,21 @@ defineProps(["image", "title", "color"])
 
 <style lang="scss">
 .observatory-badge-title {
+
+  &__top {
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
   &__image {
     width: 3rem;
     height: 3rem;
+    flex-shrink: 0;
   }
 }
 </style>
