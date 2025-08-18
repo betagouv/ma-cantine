5a23200ac18f0b4067a0a8cd53f35f2f49f22f2e
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-08-18 11:47:59 +0200

    Renomme le groupe de champ

--- 2024-frontend/src/components/CanteenEstablishmentForm.vue
+++ 2024-frontend/src/components/CanteenEstablishmentForm.vue
@@ -356,7 +356,7 @@ const validateForm = (action) => {
         />
       </fieldset>
       <fieldset class="fr-mb-4w canteen-establishment-form__reduce-margin-bottom">
-        <legend class="fr-h5 fr-mb-2w">2. SIRET</legend>
+        <legend class="fr-h5 fr-mb-2w">2. Identification</legend>
         <DsfrRadioButtonSet
           v-model="form.hasSiret"
           legend="Avez-vous un numéro SIRET ?"
