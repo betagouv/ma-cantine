d6a1bc2348908f960368aee8954aed2444a95a07
qloridant <quentin.loridant@protonmail.com>
2025-08-19 12:36:15 +0200

    Move migration

--- data/migrations/0204_remove_diagnostic_annual_diagnostic_and_more.py
+++ data/migrations/0204_remove_diagnostic_annual_diagnostic_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("data", "0201_diagnostic_teledeclaration_date_populate"),
+        ("data", "0203_diagnostic_satellites_snapshot_populate"),
     ]
 
     operations = [
