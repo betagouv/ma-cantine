76f64574b94bbd251f23e9211cda6abe6f067f07
Charline <charline.laporte@beta.gouv.fr>
2025-09-04 10:39:00 +0200

    feat(Observatoire): ajout du lien pour comprendre le calcul des chiffres (#5660)

--- 2024-frontend/src/components/ObservatoryHero.vue
+++ 2024-frontend/src/components/ObservatoryHero.vue
@@ -12,7 +12,17 @@ const pictoDocuments = "/static/images/picto-dsfr/documents.svg"
       <h1>{{ route.meta.title }}</h1>
       <p>
         La loi EGalim, complétée par la loi Climat et Résilience, a défini des obligations en ce qui concerne : la
-        qualité des produits entrant dans la composition des repas servis en restauration collective
+        qualité des produits entrant dans la composition des repas servis en restauration collective.
+      </p>
+      <p class="fr-mb-0">
+        Ces chiffres sont rendus publics tous les ans après la publication du rapport parlementaire. Pour comprendre
+        comment sont calculés les chiffres et leurs spécificités,
+        <a
+          href="https://incubateur-masa.notion.site/A-propos-de-l-observatoire-EGalim-en-restauration-collective-262de24614be800d8924eb83901def36"
+          target="_blank"
+        >
+          consulter notre article explicatif
+        </a>
       </p>
     </div>
     <ul class="fr-col-12 fr-col-lg-5 fr-col-offset-lg-1 fr-grid-row fr-grid-row--gutters ma-cantine--unstyled-list">
