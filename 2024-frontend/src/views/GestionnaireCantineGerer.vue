fb92ec7c763e74ea5956a3492f39eefb8f4dbf65
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-09-19 10:18:25 +0200

    Texte d'introduction (à améliorer)

--- 2024-frontend/src/views/GestionnaireCantineGerer.vue
+++ 2024-frontend/src/views/GestionnaireCantineGerer.vue
@@ -4,7 +4,8 @@ const route = useRoute()
 </script>
 
 <template>
-  <section>
+  <section class="fr-col-12 fr-col-md-8">
     <h1>{{ route.meta.title }}</h1>
+    <p>Retrouver toutes les informations de votre établissement.</p>
   </section>
 </template>
