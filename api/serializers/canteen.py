ba7cfd06f235c67c7c0d1712ec4a61b1089c6193
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-09-26 16:29:06 +0200

    Supprime le champ inutilisé

--- api/serializers/canteen.py
+++ api/serializers/canteen.py
@@ -245,7 +245,6 @@ class ElectedCanteenSerializer(serializers.ModelSerializer):
 
 
 class SatelliteCanteenSerializer(serializers.ModelSerializer):
-    sectors = serializers.PrimaryKeyRelatedField(many=True, queryset=Sector.objects.all(), required=False)
     user_can_view = serializers.SerializerMethodField(read_only=True)
     publication_status = serializers.CharField(read_only=True, source="publication_status_display_to_public")
 
@@ -261,7 +260,6 @@ class SatelliteCanteenSerializer(serializers.ModelSerializer):
             "siret",
             "siren_unite_legale",
             "daily_meal_count",
-            "sectors",
             "user_can_view",
             "publication_status",
         )
