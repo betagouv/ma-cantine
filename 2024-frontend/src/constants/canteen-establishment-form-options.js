c986134764182bb5276d7f4d1a79df2079478b71
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-09-29 16:08:32 +0200

    Met à jour la description du type de production

--- 2024-frontend/src/constants/canteen-establishment-form-options.js
+++ 2024-frontend/src/constants/canteen-establishment-form-options.js
@@ -36,7 +36,7 @@ const managementType = [
 const productionType = [
   { label: "Produit sur place les repas qu'il sert à ses convives", value: "site" },
   {
-    label: "Sert des repas préparés par un autre établissement",
+    label: "L'établissement sert des repas préparés par une cuisine centrale (une petite partie peut être réalisée sur place entrée / dessert)",
     value: "site_cooked_elsewhere",
   },
   {
