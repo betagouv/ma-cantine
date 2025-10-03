df5d626f6010499bb62547a9e77b82d512ed0f5d
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-10-03 16:47:02 +0200

    Met à jour la description

--- data/models/canteen.py
+++ data/models/canteen.py
@@ -351,8 +351,8 @@ class Canteen(SoftDeletionModel):
         CONCEDED = "conceded", "Concédée"
 
     class ProductionType(models.TextChoices):
-        CENTRAL = "central", "Cuisine centrale sans lieu de consommation"
-        CENTRAL_SERVING = "central_serving", "Cuisine centrale qui accueille aussi des convives sur place"
+        CENTRAL = "central", "Livre des repas mais n'a pas de lieu de service en propre"
+        CENTRAL_SERVING = "central_serving", "Livre des repas et accueille aussi des convives sur place"
         ON_SITE = "site", "Cantine qui produit les repas sur place"
         ON_SITE_CENTRAL = (
             "site_cooked_elsewhere",
