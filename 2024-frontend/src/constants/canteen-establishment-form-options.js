5d25128c682d684c6976546413fb1e6959ab94bc
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-10-03 16:48:09 +0200

    Met à jour la description dans le formulaire d'inscription

--- 2024-frontend/src/constants/canteen-establishment-form-options.js
+++ 2024-frontend/src/constants/canteen-establishment-form-options.js
@@ -41,12 +41,12 @@ const productionType = [
     value: "site_cooked_elsewhere",
   },
   {
-    label: "L'établissement produit des repas qu'il livre à des lieux de restauration",
+    label: "Livre des repas mais n'a pas de lieu de service en propre",
     value: "central",
     hint: "Vous devrez ajouter vos restaurants satellites depuis votre tableau de bord",
   },
   {
-    label: "L'établissement produit des repas qu'il livre à des lieux de restauration et accueille aussi des convives sur place",
+    label: "Livre des repas et accueille aussi des convives sur place",
     value: "central_serving",
     hint: "Vous devrez ajouter vos restaurants satellites depuis votre tableau de bord",
   },
