d6e5942521c33365a5fe5e8c7b456d07e7f12805
Raphael Odini <raphodn@users.noreply.github.com>
2025-10-07 15:20:58 +0200

    Rename canteen_fill_missing_geolocation_data_using_siret

--- macantine/management/commands/canteen_fill_missing_geolocation_data_using_siret.py
+++ macantine/management/commands/canteen_fill_missing_geolocation_data_using_siret.py
@@ -8,6 +8,11 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    """
+    Usage:
+    - python manage.py canteen_fill_missing_geolocation_data_using_siret
+    """
+
     help = (
         "Export datasets TD and canteens to the 'media/open_data' folder of the configured file system (local or s3)"
     )
