8d357ff1eadf645d809ff850c3f5e63e6cd9c263
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-10-20 16:22:52 +0200

    Intègre l'en-tête de la page

---
+++ 2024-frontend/src/views/GestionnaireTableauDeBord.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from "vue"
+import { useRootStore } from "@/stores/root"
+const store = useRootStore()
+
+const canteenSentence = computed(() => {
+  const count = store.canteenPreviews.length
+  if (count === 0) return "vous n'avez pas encore de cantine"
+  else if (count === 1) return "1 cantine"
+  return `${count} cantines`
+})
+</script>
+
+<template>
+  <div>
+    <h1>Bienvenue dans votre espace, {{ store.loggedUser.firstName }}</h1>
+    <p class="fr-text--lead fr-mb-0">
+      {{ canteenSentence }}
+    </p>
+  </div>
+  <pre>{{ store }}</pre>
+</template>
