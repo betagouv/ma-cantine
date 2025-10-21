f480338412de1f4b977aef62e1efe612f4871a8c
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-10-21 11:51:07 +0200

    Affiche le bandeau sur le nouveau frontend

---
+++ 2024-frontend/src/components/AppHeaderBanner.vue
@@ -0,0 +1,8 @@
+<script setup>
+const show = window.SHOW_INFORMATION_BANNER !== "False"
+const title = window.SHOW_INFORMATION_BANNER
+</script>
+
+<template>
+  <DsfrNotice v-if="show" :title="title"/>
+</template>
