2e0b52cb61cdfacd38c2e2a490728ca7805b5138
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-10-28 12:28:43 +0100

    Récupère et affiche les informations des cantines dans le vue tableau

--- 2024-frontend/src/views/GestionnaireTableauDeBord.vue
+++ 2024-frontend/src/views/GestionnaireTableauDeBord.vue
@@ -1,17 +1,38 @@
 <script setup>
 import { computed } from "vue"
+import { computedAsync } from "@vueuse/core"
 import { useRootStore } from "@/stores/root"
+import canteenService from "@/services/canteens.js"
 import GestionnaireGuides from "@/components/GestionnaireGuides.vue"
 import GestionnaireCanteensCreate from "@/components/GestionnaireCanteensCreate.vue"
 
 const store = useRootStore()
 
+/* Title */
 const canteenSentence = computed(() => {
   const count = store.canteenPreviews.length
   if (count === 0) return "vous n'avez pas encore de cantine"
   else if (count === 1) return "1 cantine"
   return `${count} cantines`
 })
+
+/* Table */
+const header = ["Nom de la cantine", "Siret / Siren", "Commune et code postal", "Type", "Statut", "Actions"]
+const rows = computedAsync(async () => {
+  const canteens = await canteenService.fetchCanteensActions()
+  const rows = []
+  canteens.forEach((canteen) => {
+    rows.push([
+      canteen.name,
+      canteen.siret || canteen.sirenUniteLegale,
+      `${canteen.city} - ${canteen.postalCode}`,
+      canteen.productionType,
+      canteen.action,
+      "",
+    ])
+  })
+  return rows
+}, [])
 </script>
 
 <template>
@@ -21,6 +42,14 @@ const canteenSentence = computed(() => {
   </section>
   <section class="ma-cantine--stick-to-footer">
     <GestionnaireCanteensCreate v-if="store.canteenPreviews.length === 0" />
+    <DsfrDataTable
+      v-else
+      class="gestionnaire-canteens-table"
+      title="Vos cantines"
+      no-caption
+      :headers-row="header"
+      :rows="rows"
+    />
     <GestionnaireGuides />
   </section>
 </template>
