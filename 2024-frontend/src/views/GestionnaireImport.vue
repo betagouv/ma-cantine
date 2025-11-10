14c8f87cfdd11384952e3258c2fdbcab0429639c
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-11-10 18:25:54 +0100

    Intègre les cartes pour afficher les pages enfants

--- 2024-frontend/src/views/GestionnaireImport.vue
+++ 2024-frontend/src/views/GestionnaireImport.vue
@@ -3,18 +3,23 @@ import { ref } from "vue"
 import { useRoute } from "vue-router"
 
 const route = useRoute()
-const tiles = ref([
+const pages = ref([
   {
     title: "Importer des cantines",
-    name: "GestionnaireImportCantines",
+    to: { name: "GestionnaireImportCantines" },
     description: "Vous voulez créer ou mettre à jour des cantines.",
-    tags: ["Excel", "CSV"],
+    badges: ["Excel", "CSV"],
   },
   {
     title: "Importer des achats",
-    name: "GestionnaireImportAchats",
+    to: { name: "GestionnaireImportAchats" },
     description: "Vous voulez importer des données d'achat pour des cantines existantes.",
-    tags: ["CSV"],
+    badges: ["CSV"],
+  },
+  {
+    title: "Importer des bilans",
+    disabled: true,
+    description: "Cette fonctionnalité est en cours de construction et sera disponible prochainement.",
   },
 ])
 </script>
@@ -28,6 +33,14 @@ const tiles = ref([
       <br />
       Pour une expérience optimale, veuillez bien respecter les formats attendus.
     </p>
+    <ul class="ma-cantine--unstyled-list">
+      <li v-for="page in pages" :key="page.name" class="fr-mb-1w">
+        <DsfrCard :title="page.title" :link="page.to" :description="page.description" :horizontal="true">
+          <template #start-details v-if="page.badges">
+            <DsfrBadge v-for="badge in page.badges" :key="badge" :label="badge" class="fr-mr-1v" no-icon />
+          </template>
+        </DsfrCard>
+      </li>
+    </ul>
   </section>
-  <section></section>
 </template>
