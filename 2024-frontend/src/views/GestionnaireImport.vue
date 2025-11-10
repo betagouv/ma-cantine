3d294cdee5f958514ee0dc440a75c81d2ae749ef
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-11-10 16:44:58 +0100

    Affiche les tuiles à partir du router

--- 2024-frontend/src/views/GestionnaireImport.vue
+++ 2024-frontend/src/views/GestionnaireImport.vue
@@ -1,13 +1,34 @@
 <script setup>
+import { computed } from "vue"
 import { useRoute } from "vue-router"
 
 const route = useRoute()
-const pages = []
+
+const tiles = computed(() => {
+  const tiles = []
+  const childrenPages = getChildrenPages()
+
+  childrenPages.forEach((page) => {
+    tiles.push({
+      title: page.meta.title,
+      to: { name: page.name },
+      icon: false,
+    })
+  })
+  return tiles
+})
+
+const getChildrenPages = () => {
+  const currentPage = route.matched[0]
+  const children = currentPage.children
+  const childrenWithouCurrent = children.filter((page) => page.name !== route.name)
+  return childrenWithouCurrent
+}
 </script>
 
 <template>
   <div>
     <h1>{{ route.meta.title }}</h1>
-    <pre>{{ pages }}</pre>
+    <DsfrTiles :tiles="tiles" :horizontal="true" />
   </div>
 </template>
