3aa407f342e8642e99007295e600016908ff6962
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-11-10 17:11:51 +0100

    Génère les tuiles directement dans la page parent pas utile d'automatiser pour l'instant

--- 2024-frontend/src/views/GestionnaireImport.vue
+++ 2024-frontend/src/views/GestionnaireImport.vue
@@ -1,34 +1,35 @@
 <script setup>
-import { computed } from "vue"
+import { ref } from "vue"
 import { useRoute } from "vue-router"
 
 const route = useRoute()
-
-const tiles = computed(() => {
-  const tiles = []
-  const childrenPages = getChildrenPages()
-
-  childrenPages.forEach((page) => {
-    tiles.push({
-      title: page.meta.title,
-      to: { name: page.name },
-      icon: false,
-    })
-  })
-  return tiles
-})
-
-const getChildrenPages = () => {
-  const currentPage = route.matched[0]
-  const children = currentPage.children
-  const childrenWithouCurrent = children.filter((page) => page.name !== route.name)
-  return childrenWithouCurrent
-}
+const tiles = ref([
+  {
+    title: "Importer des cantines",
+    name: "GestionnaireImportCantines",
+    description: "Vous voulez créer ou mettre à jour des cantines.",
+    tags: ["Excel", "CSV"],
+  },
+  {
+    title: "Importer des achats",
+    name: "GestionnaireImportAchats",
+    description: "Vous voulez importer des données d'achat pour des cantines existantes.",
+    tags: ["CSV"],
+  },
+])
 </script>
 
 <template>
   <div>
     <h1>{{ route.meta.title }}</h1>
-    <DsfrTiles :tiles="tiles" :horizontal="true" />
+    <ul class="ma-cantine--unstyled-list fr-grid-row fr-grid-row--gutters">
+      <li v-for="tile in tiles" :key="tile.name" class="fr-col-12 fr-col-md-4">
+        <DsfrTile :title="tile.title" :to="{ name: tile.name }" :description="tile.description">
+          <template #header>
+            <DsfrBadge v-for="tag in tile.tags" :key="tag" :label="tag" class="fr-mr-1v" :no-icon="true" />
+          </template>
+        </DsfrTile>
+      </li>
+    </ul>
   </div>
 </template>
