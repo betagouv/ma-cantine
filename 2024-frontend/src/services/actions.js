fc175396d862ceaaec9c6a1d569169ab61616dd6
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-11-10 12:54:44 +0100

    Affiche uniquement un bouton pour la télédéclaration

--- 2024-frontend/src/services/actions.js
+++ 2024-frontend/src/services/actions.js
@@ -6,51 +6,13 @@ const getBadge = (name) => {
 }
 
 const getButton = (action) => {
-  let noButton = false
-  let label = null
-  let name = null
-  let icon = null
-  let measure = null
+  if (action !== "40_teledeclare") return false
 
-  switch (true) {
-    case action === "10_add_satellites":
-      label = "Ajouter"
-      name = "GestionnaireCantineSatellitesGerer"
-      icon = "fr-icon-add-line"
-      break
-    case action === "20_create_diagnostic" || action === "18_prefill_diagnostic":
-      label = "Créer"
-      name = "MyProgress"
-      icon = "fr-icon-add-line"
-      measure = "qualite-des-produits"
-      break
-    case action === "30_fill_diagnostic":
-      label = "Modifier"
-      name = "MyProgress"
-      icon = "fr-icon-edit-line"
-      measure = "qualite-des-produits"
-      break
-    case action === "35_fill_canteen_data":
-      label = "Modifier"
-      name = "GestionnaireCantineGerer"
-      icon = "fr-icon-edit-line"
-      break
-    case action === "40_teledeclare":
-      label = "Télédéclarer"
-      name = "MyProgress"
-      icon = "fr-icon-send-plane-line"
-      measure = "etablissement"
-      break
-    default:
-      noButton = true
-  }
-
-  if (noButton) return null
   return {
-    label,
-    name,
-    icon,
-    measure,
+    label: "Télédéclarer",
+    name: "MyProgress",
+    icon: "fr-icon-send-plane-line",
+    measure: "etablissement",
   }
 }
 
