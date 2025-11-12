1ca36f7f8b0196812b2c10a8f35ab7913498baf3
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-11-12 15:01:22 +0100

    Mise à jour des articles et homogénéisation des titres

--- 2024-frontend/src/constants/crisp-tiles.js
+++ 2024-frontend/src/constants/crisp-tiles.js
@@ -1,39 +1,40 @@
 const tiles = [
   {
-    title: "Plus de produits de qualité et durables dans nos assiettes",
+    title: "Qualité et durabilité des produits",
     imgSrc: "/static/images/badges/appro.svg",
     details:
       "Retrouvez la liste des labels éligibles aux 50% de produits de qualité, aux 20% de produits bio et aux 60% de produits de qualité et durables pour les familles cumulées viandes-volailles-poissons.",
-    to:
-      "https://ma-cantine.crisp.help/fr/article/les-categories-de-produits-durables-et-de-qualite-loi-egalim-1opzgwy/",
+    to: "https://ma-cantine.crisp.help/fr/article/comprendre-mes-obligations-qualite-et-durabilite-des-produits-w3xg0f",
   },
   {
-    title: "Lutte contre le gaspillage alimentaire et dons alimentaires",
+    title: "Lutte contre le gaspillage alimentaire",
     imgSrc: "/static/images/badges/waste.svg",
     details:
       "La lutte contre le gaspillage alimentaire est un pilier de la transition écologique en restauration collective. Les lois EGalim (2018) et AGEC (2020) imposent des obligations concrètes aux établissements pour limiter les pertes, valoriser les excédents et améliorer la gestion des repas.",
-    to: "https://ma-cantine.crisp.help/fr/article/lutte-contre-le-gaspillage-alimentaire-loi-egalim-e1apbo/",
+    to:
+      "https://ma-cantine.crisp.help/fr/article/comprendre-mes-obligations-lutte-contre-le-gaspillage-alimentaire-5ekzj7",
   },
   {
-    title: "Diversification des sources de protéines et menus végétariens",
+    title: "Diversification des sources de protéines et menu végétarien",
     imgSrc: "/static/images/badges/diversification.svg",
     details:
       "La loi EGalim a introduit des évolutions majeures en matière de restauration collective, dont l’obligation de proposer régulièrement des menus végétariens.",
-    to: "https://ma-cantine.crisp.help/fr/article/menus-vegetariens-et-recettes-loi-egalim-1tvelzv/",
+    to:
+      "https://ma-cantine.crisp.help/fr/article/comprendre-mes-obligations-diversification-des-sources-de-proteines-et-menu-vegetarien-1om8is0",
   },
   {
-    title: "Substitution des plastiques",
+    title: "Interdiction du plastique",
     imgSrc: "/static/images/badges/plastic.svg",
     details:
       "Dans le cadre de la loi EGalim, la réduction du plastique en restauration collective nécessite une adaptation dans les comportements existants.",
-    to: "https://ma-cantine.crisp.help/fr/article/la-reduction-du-plastique-dans-les-creches-3dz7y/",
+    to: "https://ma-cantine.crisp.help/fr/article/comprendre-mes-obligations-interdiction-du-plastique-1q7l42x",
   },
   {
-    title: "Information des usagers et convives",
+    title: "Informations des convives",
     imgSrc: "/static/images/badges/info.svg",
     details:
       "Vous souhaitez valoriser vos actions auprès des convives ? Zoom sur les obligations en matière de communication.",
-    to: "https://ma-cantine.crisp.help/fr/article/communiquer-aupres-de-ses-convives-loi-egalim-1st93f7/",
+    to: "https://ma-cantine.crisp.help/fr/article/comprendre-mes-obligations-informations-des-convives-1st93f7",
   },
 ]
 
