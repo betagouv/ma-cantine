8192eac65096312123ef9e4c813e12ea277cf44a
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-11-14 12:33:24 +0100

    Utilise le même mapping que vue3 pour connaitre quel tag affiché

--- frontend/src/components/DataInfoBadge.vue
+++ frontend/src/components/DataInfoBadge.vue
@@ -7,49 +7,7 @@
 
 <script>
 import DsfrBadge from "@/components/DsfrBadge"
-
-const BADGE_LIST = [
-  {
-    // currentYear
-    body: "Année en cours",
-    mode: "INFO",
-    actions: [],
-  },
-  {
-    // missingData
-    body: "Données à compléter",
-    mode: "ERROR",
-    actions: [
-      "10_add_satellites",
-      "35_fill_canteen_data",
-      "18_prefill_diagnostic",
-      "20_create_diagnostic",
-      "30_fill_diagnostic",
-    ],
-  },
-  {
-    body: "En attente de la télédéclaration de votre cuisine centrale",
-    mode: "WARNING",
-    actions: ["90_nothing_satellite"],
-  },
-  {
-    body: "Bilan non télédéclaré",
-    mode: "NEUTRAL",
-    actions: ["45_did_not_teledeclare"],
-  },
-  {
-    // readyToTeledeclare
-    body: "Bilan à télédéclarer",
-    mode: "ERROR",
-    actions: ["40_teledeclare"],
-  },
-  {
-    // hasActiveTeledeclaration
-    body: "Bilan télédéclaré",
-    mode: "SUCCESS",
-    actions: ["91_nothing_satellite_teledeclared", "95_nothing"],
-  },
-]
+import diagnosticService from "../../../2024-frontend/src/services/diagnostics"
 
 export default {
   name: "DataInfoBadge",
@@ -59,17 +17,13 @@ export default {
       default: false,
       type: Boolean,
     },
-    missingData: {
-      default: false,
+    inTeledeclaration: {
       type: Boolean,
+      default: false,
     },
-    readyToTeledeclare: {
-      default: false,
+    inCorrection: {
       type: Boolean,
-    },
-    hasActiveTeledeclaration: {
       default: false,
-      type: Boolean,
     },
     canteenAction: {
       type: String,
@@ -78,13 +32,17 @@ export default {
   },
   computed: {
     badge() {
-      if (this.currentYear) return BADGE_LIST[0]
-      if (this.canteenAction)
-        return BADGE_LIST.find((badge) => badge.actions && badge.actions.includes(this.canteenAction))
-      if (this.hasActiveTeledeclaration) return BADGE_LIST[3]
-      if (this.missingData) return BADGE_LIST[1]
-      if (this.readyToTeledeclare) return BADGE_LIST[2]
-      return null
+      const campaignDates = {
+        inCorrection: this.inCorrection,
+        inTeledeclaration: this.inTeledeclaration,
+      }
+      const badgeFromAction = diagnosticService.getBadge(this.canteenAction, campaignDates)
+      const mode = this.currentYear ? "INFO" : badgeFromAction.type
+      const body = this.currentYear ? "Année en cours" : badgeFromAction.label
+      return {
+        mode: mode !== null ? mode.toUpperCase() : mode,
+        body,
+      }
     },
   },
 }
