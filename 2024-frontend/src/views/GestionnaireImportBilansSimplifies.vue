e7c7d1f043bb2ebe24f01555467c5a4caf71187d
Charline Laporte <charline.laporte@beta.gouv.fr>
2025-12-22 12:57:19 +0100

    Création de la page

---
+++ 2024-frontend/src/views/GestionnaireImportBilansSimplifies.vue
@@ -0,0 +1,53 @@
+<script setup>
+// import { ref } from "vue"
+import { useRoute } from "vue-router"
+// import ImportExplanation from "@/components/ImportExplanation.vue"
+import ImportHelp from "@/components/ImportHelp.vue"
+// import ImportSchemaTable from "@/components/ImportSchemaTable.vue"
+// import ImportSuccessModal from "@/components/ImportSuccessModal.vue"
+// import ImportFileUpload from "@/components/ImportFileUpload.vue"
+// import ImportFilesExample from "@/components/ImportFilesExample.vue"
+
+/* Router */
+const route = useRoute()
+
+/* Data */
+// const schemaFile = "diagnostics_simple.json"
+// const exampleFile = {
+//   name: "",
+//   size: "",
+// }
+// const filePreviews = {
+//   success: "",
+//   altSuccess: "",
+//   error: "",
+//   altError: "",
+// }
+
+/* Sucess */
+// const showModal = ref(false)
+// const purchaseCount = ref(0)
+
+// const success = (count) => {
+//   purchaseCount.value = count
+//   showModal.value = true
+// }
+</script>
+
+<template>
+  <h1>{{ route.meta.title }}</h1>
+  <p class="fr-col-12 fr-col-md-7">
+    Notre outil d’import de masse vous permet d’ajouter les bilans simplifiés de toutes vos cantines d’un coup.
+  </p>
+  <!--
+  <ImportExplanation :exampleFile/>
+  <ImportFilesExample :filePreviews />
+  <ImportSchemaTable :schemaFile />
+  <ImportFileUpload @success="success" apiUrl="" eventMatomo=""/>
+  <ImportSuccessModal
+    :opened="showModal"
+    :message=""
+    @close="showModal = false"
+  />-->
+  <ImportHelp />
+</template>
