72a40d46e5f7c57ebfb3916013efb1d45a1ee384
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-06 10:26:14 +0100

    Création de la page avec les informations statiques

---
+++ 2024-frontend/src/views/GestionnaireCantineAjouterGroupe.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRoute } from "vue-router"
+import AppRessources from "@/components/AppRessources.vue"
+
+/* Router and Store */
+const route = useRoute()
+</script>
+
+<template>
+  <section class="fr-grid-row fr-grid-row--bottom">
+    <div class="fr-col-12 fr-col-md-6 fr-mb-4w fr-mb-md-0">
+      <h1>{{ route.meta.title }}</h1>
+    </div>
+    <div class="fr-col-offset-md-1"></div>
+    <AppRessources>
+      <li>
+        <a href="https://annuaire-entreprises.data.gouv.fr/" target="_blank">
+          Annuaire des entreprises pour trouver le SIREN
+        </a>
+      </li>
+    </AppRessources>
+  </section>
+</template>
