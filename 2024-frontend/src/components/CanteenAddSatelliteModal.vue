5ac6a5b9ea055ccdc38b38530aaf1da2f45512f5
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-08 00:31:53 +0100

    Ré-initialise le formulaire quand on ferme la modale

--- 2024-frontend/src/components/CanteenAddSatelliteModal.vue
+++ 2024-frontend/src/components/CanteenAddSatelliteModal.vue
@@ -4,9 +4,16 @@ import { useRootStore } from "@/stores/root"
 import canteensService from "@/services/canteens.js"
 
 /* Setup */
-defineEmits(["close"])
 defineProps(["open"])
+const emit = defineEmits(["close"])
 const store = useRootStore()
+
+/* Close */
+const closeModal = () => {
+  emit("close")
+  hasSiret.value = ''
+  resetSearch()
+}
 
 /* Checkboxes */
 const hasSiret = ref('')
@@ -64,7 +71,7 @@ const searchByNumber = () => {
 }
 </script>
 <template>
-  <DsfrModal :opened="open" title="Ajouter un restaurant satellite" @close="$emit('close')" size="lg">
+  <DsfrModal :opened="open" title="Ajouter un restaurant satellite" @close="closeModal()" size="lg">
     <p>Pour ajouter une cantine à votre groupe cette dernière doit : être enregistrée sur la plateforme, être de type "Restaurant satellite", ne doit pas déjà être associée à un groupe.</p>
     <DsfrRadioButtonSet
       v-model="hasSiret"
