1662fc712183559205d1aeafd1ea1d20738a2bad
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-07 19:08:14 +0100

    Ajoute une carte pour créer un groupe

--- 2024-frontend/src/components/GestionnaireCanteensCreate.vue
+++ 2024-frontend/src/components/GestionnaireCanteensCreate.vue
@@ -1,25 +1,27 @@
-<script setup>
-const pictoDocuments = "/static/images/picto-dsfr/documents.svg"
-const pictoDocumentAdd = "/static/images/picto-dsfr/document-add.svg"
-</script>
-
 <template>
   <ul class="gestionnaire-canteens-create ma-cantine--unstyled-list fr-grid-row fr-grid-row--gutters fr-my-4w">
-    <li class="fr-col-12 fr-col-md-6">
+    <li class="fr-col-12 fr-col-md-4">
       <DsfrCard
         class="gestionnaire-canteens-create__card"
         title="Ajouter une cantine via le formulaire"
-        :imgSrc="pictoDocuments"
         :link="{ name: 'GestionnaireCantineRestaurantAjouter' }"
         endDetail="Ajouter votre cantine"
         description="Pour ajouter votre lieu de restauration collective munissez-vous de votre numéro SIRET ou du numéro SIREN de votre unité légale."
       />
     </li>
-    <li class="fr-col-12 fr-col-md-6">
+    <li class="fr-col-12 fr-col-md-4">
+      <DsfrCard
+        class="gestionnaire-canteens-create__card"
+        title="Ajouter un groupe de restaurants satellites"
+        :link="{ name: 'GestionnaireCantineGroupeAjouter' }"
+        endDetail="Ajouter votre groupe"
+        description="Les groupes de restaurants satellites vous permettent de gérer plusieurs établissements ensemble. Vous disposez d’une vue consolidée et pouvez réaliser une déclaration unique."
+      />
+    </li>
+    <li class="fr-col-12 fr-col-md-4">
       <DsfrCard
         class="gestionnaire-canteens-create__card"
         title="Créer plus de 5 cantines via import de fichier"
-        :imgSrc="pictoDocumentAdd"
         :link="{ name: 'GestionnaireImportCantines' }"
         endDetail="Importer toutes vos cantines"
         description="Notre outil d’import de masse vous permet de créer vos cantines ou de modifier vos cantines existantes d’un coup. Il concerne uniquement les gestionnaires qui ont plus de 5 cantines."
