1645df4bc6f7b2da5b48efc077731589c5af32a9
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-07 23:52:01 +0100

    Rend la barre de recherche dynamique

--- 2024-frontend/src/components/CanteenAddSatelliteModal.vue
+++ 2024-frontend/src/components/CanteenAddSatelliteModal.vue
@@ -1,29 +1,42 @@
 <script setup>
-import { ref } from "vue"
+import { ref, computed } from "vue"
 defineProps(["open"])
 defineEmits(["close"])
-const hasSiret = ref(false)
+
+/* Checkboxes */
+const hasSiret = ref('')
 const radioOptions = [
   {
     label: 'Oui',
-    value: '1',
+    value: 'oui',
   },
   {
     label: 'Non',
-    value: '0',
+    value: 'non',
   },
 ]
+
+/* Search */
+const search = ref('')
+const numberName = computed(() => hasSiret.value === 'oui' ? "SIRET" : "SIREN")
 </script>
 <template>
   <DsfrModal :opened="open" title="Ajouter un restaurant satellite" @close="$emit('close')" size="lg">
     <p>Pour ajouter une cantine à votre groupe cette dernière doit : être enregistrée sur la plateforme, être de type "Restaurant satellite", ne doit pas déjà être associée à un groupe.</p>
     <DsfrRadioButtonSet
+      v-model="hasSiret"
       legend="Le restaurant satellite a-t-il un numéro SIRET ?"
-      :modelValue="hasSiret"
       :options="radioOptions"
       name="hasSiret"
       small
       inline
+    />
+    <DsfrSearchBar
+      v-if="hasSiret"
+      v-model="search"
+      button-text="Rechercher"
+      :placeholder="`Tapez le n° ${numberName} du restaurant satellite`"
+      large
     />
   </DsfrModal>
 </template>
