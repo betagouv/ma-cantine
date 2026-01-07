d148a0772962fce6858c8d8ae694e9a51afbd8d8
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-07 23:33:26 +0100

    Création de la modale

---
+++ 2024-frontend/src/components/CanteenAddSatelliteModal.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps(["open"])
+defineEmits(["close"])
+</script>
+<template>
+  <DsfrModal :opened="open" title="Ajouter un restaurant satellite" @close="$emit('close')" size="xl">
+    <p>Pour ajouter une cantine à votre groupe cette dernière doit : être enregistrée sur la plateforme, être de type "Restaurant satellite", ne doit pas déjà être associée à un groupe.</p>
+  </DsfrModal>
+</template>
