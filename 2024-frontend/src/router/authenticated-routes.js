4fbbd8052a64a958b70dfa95b20fbb61eeb1ce0f
Charline <charline.laporte@beta.gouv.fr>
2026-01-07 22:31:06 +0100

    feat(Gérer mes satellites): petites corrections sur la page et affichage du badge des satellites (#6146)

--- 2024-frontend/src/router/authenticated-routes.js
+++ 2024-frontend/src/router/authenticated-routes.js
@@ -155,7 +155,7 @@ const routes = [
         path: "groupe/",
         children: [
           {
-            path: "gerer",
+            path: "satellites",
             name: "GestionnaireCantineGroupeSatellites",
             component: GestionnaireCantineGroupeSatellites,
             meta: {
