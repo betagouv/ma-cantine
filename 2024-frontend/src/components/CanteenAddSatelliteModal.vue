d0b808ff1cc6038bcc0ef321be1f3c5897560bd2
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-07 23:40:45 +0100

    Ajoute une question pour savoir si on fait une recherche par SIREN ou SIRET

--- 2024-frontend/src/components/CanteenAddSatelliteModal.vue
+++ 2024-frontend/src/components/CanteenAddSatelliteModal.vue
@@ -1,9 +1,29 @@
 <script setup>
+import { ref } from "vue"
 defineProps(["open"])
 defineEmits(["close"])
+const hasSiret = ref(false)
+const radioOptions = [
+  {
+    label: 'Oui',
+    value: '1',
+  },
+  {
+    label: 'Non',
+    value: '0',
+  },
+]
 </script>
 <template>
-  <DsfrModal :opened="open" title="Ajouter un restaurant satellite" @close="$emit('close')" size="xl">
+  <DsfrModal :opened="open" title="Ajouter un restaurant satellite" @close="$emit('close')" size="lg">
     <p>Pour ajouter une cantine à votre groupe cette dernière doit : être enregistrée sur la plateforme, être de type "Restaurant satellite", ne doit pas déjà être associée à un groupe.</p>
+    <DsfrRadioButtonSet
+      legend="Le restaurant satellite a-t-il un numéro SIRET ?"
+      :modelValue="hasSiret"
+      :options="radioOptions"
+      name="hasSiret"
+      small
+      inline
+    />
   </DsfrModal>
 </template>
