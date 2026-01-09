6190e44e35cacbfe53b17c39871b640076a41a6c
Raphaël Odini <raphodn@users.noreply.github.com>
2026-01-09 17:00:40 +0100

    refactor(Cantines): enlève le signal post-modification de central (plus besoin) (#6160)

--- data/signals.py
+++ data/signals.py
@@ -1,11 +1,11 @@
 import logging
 
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from simple_history.models import HistoricalRecords
 from simple_history.signals import pre_create_historical_record
 
-from .models import Canteen, ManagerInvitation, User
+from .models import ManagerInvitation, User
 
 logger = logging.getLogger(__name__)
 
@@ -17,30 +17,6 @@ def update_canteen_managers(sender, instance, created, **kwargs):
         for link in canteen_links:
             link.canteen.managers.add(instance)
             link.delete()
-
-
-@receiver(pre_save, sender=Canteen)
-def update_satellites_siret(sender, instance, raw, using, update_fields, **kwargs):
-    if raw or not instance.is_central_cuisine:
-        return
-    try:
-        obj = sender.objects.get(pk=instance.pk)
-        if not obj.siret or not instance.siret:
-            return
-        siret_has_changed = obj.siret != instance.siret
-        if siret_has_changed:
-            logger.info(
-                f"SIRET change. Central kitchen {instance.id} ({instance.name}) changed its SIRET from {obj.siret} to {instance.siret}"
-            )
-            satellites = Canteen.objects.get_satellites(obj.siret).only("id")
-            for satellite in satellites:
-                logger.info(
-                    f"SIRET change. Satellite cantine {satellite.id} had its central_producer_siret changed automatically from {obj.siret} to {instance.siret}"
-                )
-            satellites.update(central_producer_siret=instance.siret)
-
-    except sender.DoesNotExist:
-        pass  # Object is new
 
 
 def historical_record_add_auth_method(history_instance):
