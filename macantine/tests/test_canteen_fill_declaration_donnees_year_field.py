d349c3d4d360af10461776b667398e8dfddd1ff2
Raphael Odini <raphodn@users.noreply.github.com>
2026-01-09 16:35:27 +0100

    improve

--- macantine/tests/test_canteen_fill_declaration_donnees_year_field.py
+++ macantine/tests/test_canteen_fill_declaration_donnees_year_field.py
@@ -9,13 +9,24 @@ from data.models import Canteen, Diagnostic
 class CanteenFillDeclarationDonneesYearFieldCommandTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.canteen_with_diagnostic = CanteenFactory()
-        cls.canteen_diagnostic_not_teledeclared = DiagnosticFactory(
-            canteen=cls.canteen_with_diagnostic,
+        cls.canteen_site_with_diagnostic_not_teledeclared = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE
+        )
+        cls.canteen_site_diagnostic_not_teledeclared = DiagnosticFactory(
+            canteen=cls.canteen_site_with_diagnostic_not_teledeclared,
             diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
             year=2024,
             valeur_totale=1000,
-        )  # filled
+        )  # filled (2024)
+        cls.canteen_satellite_with_diagnostic_teledeclared = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE_CENTRAL
+        )
+        cls.canteen_satellite_diagnostic_teledeclared = DiagnosticFactory(
+            canteen=cls.canteen_satellite_with_diagnostic_teledeclared,
+            diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
+            year=2024,
+            valeur_totale=1000,
+        )  # filled (2024)
         cls.canteen_groupe_with_diagnostic_teledeclared = CanteenFactory(production_type=Canteen.ProductionType.GROUPE)
         cls.canteen_satellite_with_groupe_diagnostic_teledeclared = CanteenFactory(
             production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
@@ -26,26 +37,30 @@ class CanteenFillDeclarationDonneesYearFieldCommandTest(TestCase):
             diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
             year=2024,
             valeur_totale=1000,
-        )  # filled
+        )  # filled (2024)
         with freeze_time("2025-03-30"):  # during the 2024 campaign
+            cls.canteen_satellite_diagnostic_teledeclared.teledeclare(applicant=UserFactory())
             cls.canteen_groupe_diagnostic_teledeclared.teledeclare(applicant=UserFactory())
 
-    def test_canteen_fill_declaration_donnees_year_field(self):
-        self.assertFalse(self.canteen_with_diagnostic.declaration_donnees_2024)
+    def test_command(self):
+        self.assertFalse(self.canteen_site_with_diagnostic_not_teledeclared.declaration_donnees_2024)
+        self.assertFalse(self.canteen_satellite_with_diagnostic_teledeclared.declaration_donnees_2024)
         self.assertFalse(self.canteen_groupe_with_diagnostic_teledeclared.declaration_donnees_2024)
         self.assertFalse(self.canteen_satellite_with_groupe_diagnostic_teledeclared.declaration_donnees_2024)
 
         # run management command & refresh from db
         call_command("canteen_fill_declaration_donnees_year_field", year=2024)
-        self.canteen_with_diagnostic.refresh_from_db()
+        self.canteen_site_with_diagnostic_not_teledeclared.refresh_from_db()
+        self.canteen_satellite_with_diagnostic_teledeclared.refresh_from_db()
         self.canteen_groupe_with_diagnostic_teledeclared.refresh_from_db()
         self.canteen_satellite_with_groupe_diagnostic_teledeclared.refresh_from_db()
 
-        self.assertFalse(self.canteen_with_diagnostic.declaration_donnees_2024)
+        self.assertFalse(self.canteen_site_with_diagnostic_not_teledeclared.declaration_donnees_2024)
+        self.assertTrue(self.canteen_satellite_with_diagnostic_teledeclared.declaration_donnees_2024)
         self.assertTrue(self.canteen_groupe_with_diagnostic_teledeclared.declaration_donnees_2024)
         self.assertTrue(self.canteen_satellite_with_groupe_diagnostic_teledeclared.declaration_donnees_2024)
 
-    def test_canteen_fill_declaration_donnees_year_field_for_canteen_with_deletion_date(self):
+    def test_command_for_canteen_with_deletion_date(self):
         # delete canteen
         self.canteen_satellite_with_groupe_diagnostic_teledeclared.delete()
 
