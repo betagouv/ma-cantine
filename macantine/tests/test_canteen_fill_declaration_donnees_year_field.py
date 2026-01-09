09d0c9c470d2771d7541e2f035576860d4d29815
Raphael Odini <raphodn@users.noreply.github.com>
2026-01-09 16:18:49 +0100

    Update tests

--- macantine/tests/test_canteen_fill_declaration_donnees_year_field.py
+++ macantine/tests/test_canteen_fill_declaration_donnees_year_field.py
@@ -16,41 +16,41 @@ class CanteenFillDeclarationDonneesYearFieldCommandTest(TestCase):
             year=2024,
             valeur_totale=1000,
         )  # filled
-        cls.canteen_groupe_with_diagnostic_submitted = CanteenFactory(production_type=Canteen.ProductionType.GROUPE)
+        cls.canteen_groupe_with_diagnostic_teledeclared = CanteenFactory(production_type=Canteen.ProductionType.GROUPE)
+        cls.canteen_satellite_with_groupe_diagnostic_teledeclared = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
+            groupe=cls.canteen_groupe_with_diagnostic_teledeclared,
+        )
         cls.canteen_groupe_diagnostic_teledeclared = DiagnosticFactory(
-            canteen=cls.canteen_groupe_with_diagnostic_submitted,
+            canteen=cls.canteen_groupe_with_diagnostic_teledeclared,
             diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
             year=2024,
             valeur_totale=1000,
         )  # filled
         with freeze_time("2025-03-30"):  # during the 2024 campaign
             cls.canteen_groupe_diagnostic_teledeclared.teledeclare(applicant=UserFactory())
-        cls.canteen_satellite_with_groupe_diagnostic_submitted = CanteenFactory(
-            production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
-            groupe=cls.canteen_groupe_with_diagnostic_submitted,
-        )
 
     def test_canteen_fill_declaration_donnees_year_field(self):
         self.assertFalse(self.canteen_with_diagnostic.declaration_donnees_2024)
-        self.assertFalse(self.canteen_groupe_diagnostic_teledeclared.declaration_donnees_2024)
-        self.assertFalse(self.canteen_satellite_with_groupe_diagnostic_submitted.declaration_donnees_2024)
+        self.assertFalse(self.canteen_groupe_with_diagnostic_teledeclared.declaration_donnees_2024)
+        self.assertFalse(self.canteen_satellite_with_groupe_diagnostic_teledeclared.declaration_donnees_2024)
 
         # run management command & refresh from db
         call_command("canteen_fill_declaration_donnees_year_field", year=2024)
         self.canteen_with_diagnostic.refresh_from_db()
-        self.canteen_groupe_with_diagnostic_submitted.refresh_from_db()
-        self.canteen_satellite_with_groupe_diagnostic_submitted.refresh_from_db()
+        self.canteen_groupe_with_diagnostic_teledeclared.refresh_from_db()
+        self.canteen_satellite_with_groupe_diagnostic_teledeclared.refresh_from_db()
 
         self.assertFalse(self.canteen_with_diagnostic.declaration_donnees_2024)
-        self.assertTrue(self.canteen_groupe_with_diagnostic_submitted.declaration_donnees_2024)
-        self.assertTrue(self.canteen_satellite_with_groupe_diagnostic_submitted.declaration_donnees_2024)
+        self.assertTrue(self.canteen_groupe_with_diagnostic_teledeclared.declaration_donnees_2024)
+        self.assertTrue(self.canteen_satellite_with_groupe_diagnostic_teledeclared.declaration_donnees_2024)
 
     def test_canteen_fill_declaration_donnees_year_field_for_canteen_with_deletion_date(self):
         # delete canteen
-        self.canteen_satellite_with_groupe_diagnostic_submitted.delete()
+        self.canteen_satellite_with_groupe_diagnostic_teledeclared.delete()
 
         # run management command & refresh from db
         call_command("canteen_fill_declaration_donnees_year_field", year=2024)
-        self.canteen_satellite_with_groupe_diagnostic_submitted.refresh_from_db()
+        self.canteen_satellite_with_groupe_diagnostic_teledeclared.refresh_from_db()
 
-        self.assertTrue(self.canteen_satellite_with_groupe_diagnostic_submitted.declaration_donnees_2024)
+        self.assertTrue(self.canteen_satellite_with_groupe_diagnostic_teledeclared.declaration_donnees_2024)
