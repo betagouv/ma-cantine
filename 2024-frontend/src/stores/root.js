46fa6cd35f46765704f8c1e59300325507911af5
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-12 17:50:16 +0100

    Remplace le code du store pour celui du service

--- 2024-frontend/src/stores/root.js
+++ 2024-frontend/src/stores/root.js
@@ -1,29 +1,11 @@
 import { defineStore } from "pinia"
 import { ref, reactive } from "vue"
 import { useFetch } from "@vueuse/core"
-import { AuthenticationError, BadRequestError } from "../utils"
+import { verifyResponse, AuthenticationError } from "@/services/api.js"
 
 const headers = {
   "X-CSRFToken": window.CSRF_TOKEN || "",
   "Content-Type": "application/json",
-}
-
-const verifyResponse = function(response) {
-  const contentType = response.headers.get("content-type")
-  const hasJSON = contentType && contentType.startsWith("application/json")
-
-  if (response.status < 200 || response.status >= 400) {
-    if (response.status === 403) throw new AuthenticationError()
-    else if (response.status === 400) {
-      if (hasJSON) {
-        throw new BadRequestError(response.json())
-      } else {
-        throw new BadRequestError()
-      }
-    } else throw new Error(`API responded with status of ${response.status}`)
-  }
-
-  return hasJSON ? response.json() : response.text()
 }
 
 export const useRootStore = defineStore("root", () => {
