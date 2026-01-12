883a9b9cdb00ee33358b25ccf86927a388dd4dc7
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-12 18:52:01 +0100

    Simplifie la gestion des erreur renvoyées par l'api

--- 2024-frontend/src/services/api.js
+++ 2024-frontend/src/services/api.js
@@ -1,43 +1,39 @@
-class AuthenticationError extends Error {
-  constructor(...params) {
-    super(...params)
-    // Maintains proper stack trace for where our error was thrown (only available on V8)
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, AuthenticationError)
-    }
-    this.name = "AuthenticationError"
+const getDefaultErrorMessage = () => 'Une erreur est survenue, vous pouvez réessayer plus tard ou nous contacter directement à support-egalim@beta.gouv.fr'
+
+const authenticationError = () => {
+  return {
+    name: "AuthenticationError",
+    message: 'Votre session a expiré. Rechargez la page et reconnectez-vous pour continuer.',
   }
 }
 
-class BadRequestError extends Error {
-  constructor(jsonPromise, ...params) {
-    super(...params)
-    // Maintains proper stack trace for where our error was thrown (only available on V8)
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, BadRequestError)
-    }
-    this.name = "BadRequestError"
-    this.jsonPromise = jsonPromise
+const badRequestError = (error) => {
+  return {
+    name: "BadRequestError",
+    errorsList: error || [],
+    errorDefaultMessage: getDefaultErrorMessage(),
   }
 }
 
-
-const verifyResponse = function(response) {
+const verifyResponse = async (response) => {
   const contentType = response.headers.get("content-type")
   const hasJSON = contentType && contentType.startsWith("application/json")
 
-  if (response.status < 200 || response.status >= 400) {
-    if (response.status === 403) throw new AuthenticationError()
-    else if (response.status === 400) {
-      if (hasJSON) {
-        throw new BadRequestError(response.json())
-      } else {
-        throw new BadRequestError()
-      }
-    } else throw new Error(`API responded with status of ${response.status}`)
+  switch (true) {
+    case response.ok && hasJSON:
+      return response.json()
+    case response.ok && !hasJSON:
+      return response.text()
+    case !response.ok && response.status === 403:
+      throw authenticationError()
+    case !response.ok && response.status === 400:
+      const error = hasJSON ? await response.json() : response.text()
+      throw badRequestError(error)
+    case !response.ok:
+      throw badRequestError()
+    default:
+      console.log("API Service - VerifyResponse : ", response)
   }
-
-  return hasJSON ? response.json() : response.text()
 }
 
-export { verifyResponse, AuthenticationError, BadRequestError }
+export { verifyResponse, getDefaultErrorMessage }
