89900003f07a8b3038368214a902a83dc3b6e054
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-13 11:26:52 +0100

    Ré-organise l'affichage des message d'erreur pour afficher en priorité celui des SAT manquants

--- frontend/src/views/DashboardManager/EgalimProgression.vue
+++ frontend/src/views/DashboardManager/EgalimProgression.vue
@@ -61,28 +61,8 @@
         </v-row>
       </v-col>
     </v-row>
-    <v-card v-if="missingCanteenData" class="pa-6 my-6 fr-text grey--text text--darken-3 text-center cta-block">
-      <p>
-        Certaines données de votre établissement manquent.
-        <span v-if="inTeledeclarationCampaign">
-          Veuillez compléter ces informations avant télédéclarer.
-        </span>
-      </p>
-      <v-card-actions class="px-0 pt-0 pb-0 justify-center">
-        <v-btn
-          :to="{
-            name: 'GestionnaireCantineGerer',
-            params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(canteen) },
-          }"
-          color="primary"
-          class="fr-text font-weight-medium"
-        >
-          Mettre à jour mon établissement
-        </v-btn>
-      </v-card-actions>
-    </v-card>
     <v-card
-      v-else-if="inTeledeclarationCampaign && hasSatelliteInconsistency"
+      v-if="inTeledeclarationCampaign && canteen.satellites.length === 0"
       class="pa-6 my-6 fr-text grey--text text--darken-3 text-center cta-block"
     >
       <p>
@@ -100,6 +80,26 @@
           class="fr-text font-weight-medium"
         >
           Gérer mes restaurants satellites
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card v-else-if="missingCanteenData" class="pa-6 my-6 fr-text grey--text text--darken-3 text-center cta-block">
+      <p>
+        Certaines données de votre établissement manquent.
+        <span v-if="inTeledeclarationCampaign">
+          Veuillez compléter ces informations avant télédéclarer.
+        </span>
+      </p>
+      <v-card-actions class="px-0 pt-0 pb-0 justify-center">
+        <v-btn
+          :to="{
+            name: 'GestionnaireCantineGerer',
+            params: { canteenUrlComponent: $store.getters.getCanteenUrlComponent(canteen) },
+          }"
+          color="primary"
+          class="fr-text font-weight-medium"
+        >
+          Mettre à jour mon établissement
         </v-btn>
       </v-card-actions>
     </v-card>
