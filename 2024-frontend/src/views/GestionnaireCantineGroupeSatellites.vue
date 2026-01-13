ad49f9e9db8dd30845e18147c51df968a23eef45
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-13 17:57:38 +0100

    Renomme le composant

--- 2024-frontend/src/views/GestionnaireCantineGroupeSatellites.vue
+++ 2024-frontend/src/views/GestionnaireCantineGroupeSatellites.vue
@@ -9,7 +9,7 @@ import urlService from "@/services/urls.js"
 import AppLoader from "@/components/AppLoader.vue"
 import CanteenButtonJoin from "@/components/CanteenButtonJoin.vue"
 import CanteenButtonUnlink from "@/components/CanteenButtonUnlink.vue"
-import CanteenAddSatelliteModal from "@/components/CanteenAddSatelliteModal.vue"
+import CanteenModalSatellitesAdd from "@/components/CanteenModalSatellitesAdd.vue"
 
 /* Data */
 const route = useRoute()
@@ -108,7 +108,7 @@ const removeRow = (id) => {
       <div class="fr-col-12 fr-col-md-8 fr-grid-row fr-grid-row--right">
         <DsfrButton primary label="Ajouter un restaurant satellite" icon="fr-icon-add-circle-fill" @click="modalOpened = true" />
       </div>
-      <CanteenAddSatelliteModal :open="modalOpened" :groupId="canteenId" @close="modalOpened = false" @updateSatellites="updateSatellites()" />
+      <CanteenModalSatellitesAdd :open="modalOpened" :groupId="canteenId" @close="modalOpened = false" @updateSatellites="updateSatellites()" />
     </div>
     <AppLoader v-if="loading" />
     <DsfrDataTable
