571edb81ce80185666e8d6b0866e96632e6abc5c
Charline <charline.laporte@beta.gouv.fr>
2026-01-13 14:37:57 +0100

    Update api/views/canteen_groupe.py
    Co-authored-by: Raphaël Odini <raphodn@users.noreply.github.com>

--- api/views/canteen_groupe.py
+++ api/views/canteen_groupe.py
@@ -56,7 +56,7 @@ class CanteenGroupeSatelliteLinkView(APIView):
             if canteen_groupe.has_diagnostic_teledeclared_for_year(timezone.now().year - 1):
                 return JsonResponse(
                     {
-                        "error": "Vous ne pouvez pas ajouter ou retirer de restaurant satellite à votre groupe avec un bilan télédéclaré en période de campagne de télédéclaration. Veuillez annuler la télédéclaration avant de faire les modifications.",
+                        "error": "Vous ne pouvez pas ajouter ou retirer de restaurant satellite à votre groupe, car il possède un bilan télédéclaré (campagne de télédéclaration en cours). Veuillez annuler la télédéclaration pour faire la modification.",
                     },
                     status=status.HTTP_400_BAD_REQUEST,
                 )
