99553c8f11e7acb8d937340e50c9914ae50624e6
Raphael Odini <raphodn@users.noreply.github.com>
2026-01-14 10:36:34 +0100

    refactor(Admin): Echec d'import: améliorations (rendre l'utilisateur cliquable, afficher le message d'erreur)

--- data/admin/importfailure.py
+++ data/admin/importfailure.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from data.models import ImportFailure
 from data.admin.utils import ReadOnlyAdminMixin
@@ -8,17 +10,44 @@ from data.admin.utils import ReadOnlyAdminMixin
 class ImportFailureAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_display = (
         "creation_date",
-        "user",
+        "user_with_link",
         "import_type",
+        "details",
     )
     list_filter = ["creation_date", "import_type"]
     search_fields = (
+        "user__id",
         "user__first_name",
         "user__last_name",
         "user__email",
         "user__username",
         "creation_date",
     )
+    search_help_text = "La recherche est faite sur les champs : utilisateur (ID, nom, prénom, email)"
+
+    fieldsets = (
+        (
+            "",
+            {
+                "fields": (
+                    "user",
+                    "import_type",
+                    "file",
+                    "details",
+                ),
+            },
+        ),
+        (
+            "Metadonnées",
+            {"fields": ("creation_date",)},
+        ),
+    )
 
     def has_delete_permission(self, request, obj=None):
         return True
+
+    def user_with_link(self, obj):
+        url = reverse("admin:data_user_change", args=[obj.user_id])
+        return format_html(f'<a href="{url}">{obj.user}</a>')
+
+    user_with_link.short_description = ImportFailure._meta.get_field("user").verbose_name
