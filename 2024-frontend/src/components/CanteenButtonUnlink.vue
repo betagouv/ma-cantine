f224e5ead1135f072b8001b2952311c345f4e414
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-15 11:57:36 +0100

    Fix typo

--- 2024-frontend/src/components/CanteenButtonUnlink.vue
+++ 2024-frontend/src/components/CanteenButtonUnlink.vue
@@ -70,7 +70,7 @@ const unlinkSatellite = () => {
     <template #default>
       <p class="fr-mb-2w">
         En confirmant cette demande « {{ satellite.name }} » ne fera plus parti des restaurants fournis par votre
-        établissement « {{ canteen.name }} » et donc :
+        groupe « {{ canteen.name }} » et donc :
       </p>
       <ul>
         <li>
