60e409ee0058b493f7e591e034dd767b6a997c6e
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-16 11:19:15 +0100

    création de la nouvelle page vue3 pour supprimer une cantine

---
+++ 2024-frontend/src/views/GestionnaireCantineSupprimer.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useRoute } from "vue-router"
+import urlService from "@/services/urls.js"
+
+/* Router */
+const route = useRoute()
+const canteenId = urlService.getCanteenId(route.params.canteenUrlComponent)
+const canteenName = urlService.getCanteenName(route.params.canteenUrlComponent)
+console.log(canteenId, canteenName)
+</script>
+<template>
+  <div class="fr-col-12 fr-col-md-8 fr-mb-3w">
+    <h1>{{ route.meta.title }} « {{ canteenName }} »</h1>
+  </div>
+</template>
