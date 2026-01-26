9d0f1bf6bad085c8a1b52d4c9c6473d0c623c1db
Raphaël Odini <raphodn@users.noreply.github.com>
2026-01-26 15:09:44 +0100

    refactor(Tests): màj des fixtures (et enlever les SectorM2M) (#6281)

--- data/tests/test_fixtures.py
+++ data/tests/test_fixtures.py
@@ -1,7 +1,7 @@
 from django.core.management import call_command
 from django.test import TestCase
 
-from data.models import Canteen, Diagnostic, SectorM2M, User
+from data.models import Canteen, Diagnostic, User
 
 
 class FixturesTest(TestCase):
@@ -9,9 +9,7 @@ class FixturesTest(TestCase):
         self.assertEqual(User.objects.count(), 0)
         self.assertEqual(Canteen.objects.count(), 0)
         self.assertEqual(Diagnostic.objects.count(), 0)
-        self.assertEqual(SectorM2M.objects.count(), 0)
         call_command("loaddata", "initial_data.json")
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(Canteen.objects.count(), 1)
         self.assertEqual(Diagnostic.objects.count(), 1)
-        self.assertEqual(SectorM2M.objects.count(), 1)
