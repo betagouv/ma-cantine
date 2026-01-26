b7d1fafe2c3e91b7f174b183be750cfee2b4ab67
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-26 16:14:10 +0100

    Mise à jour du texte d'introduction pour faire ressortir les formats

--- 2024-frontend/src/components/ImportExplanation.vue
+++ 2024-frontend/src/components/ImportExplanation.vue
@@ -6,41 +6,35 @@ defineProps(["exampleFile", "links"])
   <section class="fr-grid-row fr-grid-row--bottom">
     <div class="fr-col-12 fr-col-md-7">
       <h2 class="fr-h5">Comment ça marche ?</h2>
-      <p>
-        Nous scannons la structure du fichier, puis les valeurs des cellules avant d’importer.
-        <br />
-        <strong>En cas d’erreurs, le fichier est automatiquement rejeté.</strong>
-      </p>
-      <p>Le fichier, les colonnes et les valeurs des cellules doivent impérativement respecter ce format :</p>
+      <p>Nous scannons la structure du fichier, puis les valeurs des cellules avant d’importer.</p>
+      <h3 class="fr-text--md">Le formattage du fichier d'import doit impérativement respecter les règles suivantes :</h3>
       <ul>
         <li>
-          Le fichier doit être <strong>au format CSV (.csv) ou Excel (.xlsx)</strong>
+          <p class="fr-mb-1v">Le fichier doit être dans un des format acceptés :
+            <DsfrBadge class="fr-mr-1v" type="info" label="Excel" no-icon />
+            <DsfrBadge class="fr-mr-1v" type="info" label="CSV" no-icon />
+            <DsfrBadge type="info" label="TSV" no-icon />
+          </p>
         </li>
         <li>
-          Il doit
-          <strong>commencer par une ligne en-tête</strong>
+          Le fichier doit commencer par une ligne en-tête
         </li>
         <li>
-          Le
-          <strong>nom des colonnes doit être écrit exactement</strong>
-          comme ci-dessous
+          Le nom des colonnes doit être écrit exactement comme ci-dessous
         </li>
         <li>
-          Les
-          <strong>colonnes doivent être dans l'ordre</strong>
-          comme ci-dessous
+          Les colonnes doivent être dans l'ordre comme ci-dessous
         </li>
         <li>
-          <strong>Une ligne</strong>
-          doit contenir les informations d’
-          <strong>une cantine uniquement</strong>
+          Une ligne doit contenir les informations d’une cantine uniquement
         </li>
         <li>
-          Pour modifier les informations d’une cantine existante,
-          <strong>vous devez avoir un compte utilisateur par cantine inscrite</strong>
-          , autrement le fichier sera rejeté.
+          Pour modifier les informations d’une cantine existante, vous devez avoir un compte utilisateur par cantine inscrite
         </li>
       </ul>
+      <p>
+        <strong>En cas d’erreurs, le fichier est automatiquement rejeté.</strong>
+      </p>
     </div>
     <div class="fr-col-offset-md-1"></div>
     <div class="fr-col-12 fr-col-md-4 fr-card fr-px-7v fr-pt-7v fr-text-title--blue-france">
