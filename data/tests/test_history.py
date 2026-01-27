b6fd4ef0dfbd6cc3afea4a9d06b101fc69523dcd
Raphael Odini <raphodn@users.noreply.github.com>
2026-01-27 14:27:13 +0100

    refactor(Historisations): ajout de tests pour expliciter le fonctionnement actuel

--- data/tests/test_history.py
+++ data/tests/test_history.py
@@ -1,14 +1,84 @@
 from datetime import timedelta
 
 from django.test import TestCase
+from django.core.management import call_command
 from django.test.utils import override_settings
 
-from data.factories import CanteenFactory, TeledeclarationFactory
-from data.models import AuthenticationMethodHistoricalRecords
+from data.factories import CanteenFactory
 from macantine import tasks
 
 
-class TestModelHistory(TestCase):
+class HistoryModelTest(TestCase):
+    def test_canteen_history_on_save(self):
+        # create canteen
+        canteen = CanteenFactory(
+            siret="21340172201787",
+            city_insee_code="34172",
+            city=None,
+            department=None,
+            region=None,
+            managers=[],
+            sectors_m2m=[],
+        )
+
+        # why 2 and not 1?
+        # because of the factory's post_generation hooks
+        # see https://factoryboy.readthedocs.io/en/stable/recipes.html
+        self.assertEqual(canteen.history.count(), 1 + 1)
+        self.assertEqual(canteen.history.all().order_by("history_date")[0].history_type, "+")
+        delta = canteen.history.first().diff_against(canteen.history.all().order_by("history_date")[0])
+        self.assertEqual(len(delta.changes), 0)
+
+        # update canteen: adds a new history record
+        canteen.name = "Updated name"
+        canteen.save()
+
+        self.assertEqual(canteen.history.count(), 2 + 1)
+        self.assertEqual(canteen.history.first().history_type, "~")
+
+        # empty save: adds a new history record
+        canteen.save()
+
+        self.assertEqual(canteen.history.count(), 3 + 1)
+        self.assertEqual(canteen.history.first().history_type, "~")
+        delta = canteen.history.first().diff_against(canteen.history.all().order_by("history_date")[2])
+        self.assertEqual(len(delta.changes), 0)
+
+        # change field from None to empty string: adds a new history record
+        canteen.city = ""
+        canteen.save()
+
+        self.assertEqual(canteen.history.count(), 4 + 1)
+        self.assertEqual(canteen.history.first().history_type, "~")
+        delta = canteen.history.first().diff_against(canteen.history.all().order_by("history_date")[3])
+        self.assertEqual(len(delta.changes), 1)
+        self.assertEqual(delta.changes[0].field, "city")
+
+    def test_history_clean_duplicate_command(self):
+        # create canteen
+        canteen = CanteenFactory(
+            siret="21340172201787",
+            city_insee_code="34172",
+            city=None,
+            department=None,
+            region=None,
+            managers=[],
+            sectors_m2m=[],
+        )
+
+        # why 2 and not 1?
+        # because of the factory's post_generation hooks
+        # see https://factoryboy.readthedocs.io/en/stable/recipes.html
+        self.assertEqual(canteen.history.count(), 1 + 1)
+        self.assertEqual(canteen.history.all().order_by("history_date")[0].history_type, "+")
+        delta = canteen.history.first().diff_against(canteen.history.all().order_by("history_date")[0])
+        self.assertEqual(len(delta.changes), 0)
+
+        call_command("clean_duplicate_history", "--auto")
+
+        self.assertEqual(canteen.history.count(), 1)
+        self.assertEqual(canteen.history.first().history_type, "+")
+
     @override_settings(MAX_DAYS_HISTORICAL_RECORDS=1)
     def test_old_history_removal(self):
         canteen = CanteenFactory()
@@ -41,13 +111,3 @@ class TestModelHistory(TestCase):
         # Verify all history items are still there
         tasks.delete_old_historical_records()
         self.assertEqual(canteen.history.count(), history_count)
-
-    def test_authentication_method_created_in_code(self):
-        """
-        Objects created in code should save the corresponding authentication method
-        """
-        td = TeledeclarationFactory()
-
-        self.assertEqual(
-            td.history.first().authentication_method, AuthenticationMethodHistoricalRecords.AuthMethodChoices.AUTO
-        )
