aa9af208312542624f82c093ff268a8ee9344851
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-01-27 15:28:31 +0100

    Renomme la colonne dans les fichiers front

--- 2024-frontend/src/views/GestionnaireImportAchats.vue
+++ 2024-frontend/src/views/GestionnaireImportAchats.vue
@@ -19,9 +19,9 @@ const exampleFile = {
   size: "176 octets",
 }
 const filePreviews = {
-  success: "achats_fichier_exemple_fichier_accepte.jpg?v=3",
+  success: "achats_fichier_exemple_fichier_accepte.jpg?v=4",
   altSuccess: "Exemple de fichier accepté pour importer des achats, qui contient le bon nom de colonnes et les bonnes valeurs",
-  error: "achats_fichier_exemple_fichier_rejete.jpg?v=3",
+  error: "achats_fichier_exemple_fichier_rejete.jpg?v=4",
   altError: "Exemple de fichier rejeté pour importer des achats, qui contient des erreurs dans les colonnes ou les valeurs",
 }
 const links = [
