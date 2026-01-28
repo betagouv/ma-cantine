1cf88768e0ec3a36f9830f51775fa9a810f1606c
Raphael Odini <raphodn@users.noreply.github.com>
2026-01-28 20:39:29 +0100

    extra cleanup

--- macantine/tasks.py
+++ macantine/tasks.py
@@ -8,7 +8,6 @@ from django.db.models import F
 from django.utils import timezone
 
 import macantine.brevo as brevo
-from api.views.utils import update_change_reason
 from common.api.datagouv import (
     fetch_commune_pat_list,
     map_pat_list_to_communes_insee_code,
@@ -234,8 +233,8 @@ def fill_missing_geolocation_data_using_insee_code():
         update = _update_canteen_geo_data_from_insee_code(canteen)
         if update:
             # save
+            canteen._change_reason = "Données de localisation MAJ par bot, via code INSEE"
             canteen.save(skip_validations=True)
-            update_change_reason(canteen, "Données de localisation MAJ par bot, via code INSEE")
             counter += 1
 
     result = f"Updated {counter}/{candidate_canteens.count()} canteens"
