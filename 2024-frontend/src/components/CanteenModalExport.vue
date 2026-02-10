df037f696180c2a7286ecbb3f4108776c9fba8ff
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-10 16:50:20 +0100

    Met à jour la modale pour utiliser un composant de fichier

--- 2024-frontend/src/components/CanteenModalExport.vue
+++ 2024-frontend/src/components/CanteenModalExport.vue
@@ -1,50 +1,42 @@
 <script setup>
-import { ref } from "vue"
-import canteenService from "@/services/canteens"
-import AppLoader from "@/components/AppLoader.vue"
-
-/* PROPS and EMITS */
 defineProps(["opened"])
 const emit = defineEmits(["close"])
-
-/* EXPORT */
-const isGenerating = ref(false)
-const exportSucces = ref(false)
-const exportError = ref(null)
-
-const generateExport = () => {
-  isGenerating.value = true
-  canteenService.exportCanteens()
-    .then(() => exportSucces.value = true)
-    .catch(() => exportError.value = true)
-    .finally(() => isGenerating.value = false)
-}
-
-/* MODAL */
-const closeModal = () => {
-  isGenerating.value = false
-  exportSucces.value = false
-  exportError.value = null
-  emit('close')
-}
 </script>
 
 <template>
   <DsfrModal
+    class="canteen-modal-export"
     :opened="opened"
     title="Export des informations de mes cantines"
-    @close="closeModal"
+    @close="emit('close')"
   >
     <p>
       Cet export contient l'ensemble des données d’information relatives aux cantines dont vous gérez.
       Vous pourrez modifier ce fichier et le ré-utiliser tel quel dans le module d’import “Modifier des cantines” pour mettre à jour vos informations en masse.
     </p>
-    <DsfrHighlight text="Pour rappel, pour modifier les cantines via imports ou créer des bilans, vous devez être gestionnaire de celles-ci." class="fr-ml-0"/>
-    <DsfrAlert v-if="exportSucces" type="success" title="L'export a été généré avec succès." description="Vous pouvez le retrouver dans vos téléchargements." />
-    <DsfrAlert v-else-if="exportError" type="error" title="Une erreur est survenue lors de la génération de l'export." description="Veuillez réessayer plus tard ou contacter le support." />
-    <div v-else class="fr-grid-row fr-grid-row--left fr-grid-row--middle">
-      <DsfrButton primary label="Générer l'export" :disabled="isGenerating" @click="generateExport" />
-      <AppLoader text="Génération de l'export en cours" v-if="isGenerating" class="fr-ml-2w" />
+    <p class="ma-cantine--bold">Pour rappel, pour modifier les cantines via imports ou créer des bilans, vous devez être gestionnaire de celles-ci.</p>
+    <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center fr-grid-row--middle fr-mt-4w">
+      <div class="fr-col-12 fr-col-md-2 fr-h2 ma-cantine--text-center">
+        <VIcon class="canteen-modal-export__icon" name="mdi-microsoft-excel" scale="1.5"/>
+      </div>
+      <div class="fr-col-12 fr-col-md-10">
+        <DsfrFileDownload
+          href="api/v1/export-cantines.xlsx"
+          title="Télécharger l'export de la liste les cantines dont vous êtes gestionnaire sur la plateforme ma cantine"
+          target="_blank"
+          size="Taille du fichier inconnue"
+          format="Excel (.xlsx)"
+          download="export-cantines.xlsx"
+        />
+      </div>
     </div>
   </DsfrModal>
 </template>
+
+<style lang="scss" scoped>
+.canteen-modal-export {
+  &__icon {
+    color: var(--text-action-high-blue-france) !important;
+  }
+}
+</style>
