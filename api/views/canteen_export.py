1ff9341ff6aa4d65a834673162281b53e02d6deb
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-10 18:01:12 +0100

    Filtre les cantines avec SIRET uniquement

--- api/views/canteen_export.py
+++ api/views/canteen_export.py
@@ -15,7 +15,7 @@ class UserCanteenListExportView(ListAPIView, XLSXFileMixin):
 
     def get_queryset(self):
         # similar to UserCanteenActions
-        return self.request.user.canteens.order_by("name")
+        return self.request.user.canteens.has_siret().order_by("name")
 
     # same as data/schemas/imports/cantines.json
     column_header = {
