35bc00dad95a948ef365f63f5a015c33fc430cea
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-16 14:27:52 +0100

    Supprime le contenu mais garde le bandeau si utile plus tard

--- 2024-frontend/src/components/BannerAlert.vue
+++ 2024-frontend/src/components/BannerAlert.vue
@@ -1,5 +1,4 @@
 <template>
-  <DsfrNotice type="alert">
-    Gestionnaires de crèches : rappel de laits infantiles, voir les lots concernés sur le site <a href="https://rappel.conso.gouv.fr/categorie/25" target="_blank">Rappel conso</a>
+  <DsfrNotice v-if="false" type="alert">
   </DsfrNotice>
 </template>
