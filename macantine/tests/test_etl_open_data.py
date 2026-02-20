81aa5de9afb9cfec8da797eed7e16110b5dd7f50
Raphaël Odini <raphodn@users.noreply.github.com>
2026-02-20 17:38:03 +0100

    fix(API Stats): ne pas exclure les TD groupes des calculs TD (en attendant 1TD1Site) (#6399)

--- macantine/tests/test_etl_open_data.py
+++ macantine/tests/test_etl_open_data.py
@@ -119,13 +119,13 @@ class TeledeclarationETLOpenDataTest(TestCase):
         ETLCommonSetUpTestData(cls, with_diagnostics=True)
 
     def test_teledeclaration_extract(self, mock):
-        # 2022: 3 teledeclarations (1 is hidden (groupe))
+        # 2022: 3 teledeclarations (1 groupe) (TODO after 1TD1Site: remove groupe and split data by satellite)
         etl_td_2022 = ETL_OPEN_DATA_TELEDECLARATIONS(2022)
         etl_td_2022.extract_dataset()
 
         self.assertEqual(Diagnostic.objects.filter(year=2022).count(), 3)
         self.assertEqual(Diagnostic.objects.filter(year=2022).teledeclared().count(), 3)
-        self.assertEqual(etl_td_2022.len_dataset(), 2)
+        self.assertEqual(etl_td_2022.len_dataset(), 3)
         self.assertEqual(
             etl_td_2022.df.iloc[0]["id"], self.canteen_site_earlier_diagnostic_2022.teledeclaration_id
         )  # Order by teledeclaration created date ascending
@@ -146,13 +146,13 @@ class TeledeclarationETLOpenDataTest(TestCase):
         self.assertEqual(Diagnostic.objects.filter(year=2024).teledeclared().count(), 2)
         self.assertEqual(etl_td_2024.len_dataset(), 2)
 
-        # 2025: 1 teledeclaration
+        # 2025: 1 teledeclaration (1 groupe) (TODO after 1TD1Site: remove groupe and split data by satellite)
         etl_td_2025 = ETL_OPEN_DATA_TELEDECLARATIONS(2025)
         etl_td_2025.extract_dataset()
 
         self.assertEqual(Diagnostic.objects.filter(year=2025).count(), 1)
         self.assertEqual(Diagnostic.objects.filter(year=2025).teledeclared().count(), 1)
-        self.assertEqual(etl_td_2025.len_dataset(), 0)  # not exported yet
+        self.assertEqual(etl_td_2025.len_dataset(), 1)
 
     def test_teledeclaration_transform(self, mock):
         mock_fetch_communes(mock)
