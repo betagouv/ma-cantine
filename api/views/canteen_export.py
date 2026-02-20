fbb699ffc7fbc4972ecec6f5d6cadade84c420ce
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-20 16:37:26 +0100

    Fait le lien vers le nouveau schema

--- api/views/canteen_export.py
+++ api/views/canteen_export.py
@@ -16,7 +16,7 @@ class UserCanteenListExportView(ListAPIView, XLSXFileMixin):
     def get_queryset(self):
         return self.request.user.canteens.is_serving().order_by("id")
 
-    # same as data/schemas/imports/cantines.json
+    # same as data/schemas/imports/cantines_modifier.json
     column_header = {
         "titles": [
             "id",
