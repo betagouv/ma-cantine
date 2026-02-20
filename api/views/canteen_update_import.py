9cf323611de36486391da183e1ce1a0bffd97f6c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-20 10:43:02 +0100

    Ajoute le code d'import dédié à la modification

--- api/views/canteen_update_import.py
+++ api/views/canteen_update_import.py
@@ -3,10 +3,13 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from django.db import IntegrityError, transaction
+from rest_framework.exceptions import PermissionDenied
+from simple_history.utils import update_change_reason
 
 from api.serializers import FullCanteenSerializer
 from api.views.base_import import BaseImportView
-from data.models import Canteen, ImportType
+from common.utils import utils as utils_utils
+from data.models import Canteen, ImportType, Sector
 
 from .canteen import AddManagerView
 from .utils import camelize
@@ -100,8 +103,87 @@ class CanteensUpdateImportView(BaseImportView):
         row,
         manager_emails,
     ):
-        # TODO: Implement update canteen logic
-        pass
+        id = row[0]
+
+        # ID : check canteen exists
+        canteen = Canteen.objects.get(id=id)
+        if not canteen:
+            raise ValidationError(
+                {"id": f"Aucune cantine avec le numéro identifiant « {id} » trouvée sur la plateforme."}
+            )
+
+        siret = utils_utils.normalize_string(row[1])
+        siren_unite_legale = utils_utils.normalize_string(row[2])
+        name = row[3].strip()
+        city_insee_code = row[4]
+        central_producer_siret = utils_utils.normalize_string(row[5]) if row[5] else None
+        daily_meal_count = row[6]
+        yearly_meal_count = row[7]
+        sector_list = [
+            next(value for value, label in Sector.choices if label == sector.strip().replace("’", "'"))
+            for sector in row[8].strip().split(",")
+        ]
+        production_type = next(
+            (value for value, label in Canteen.ProductionType.choices if row[9].strip() in [label, value]), None
+        )
+        management_type = next(
+            (value for value, label in Canteen.ManagementType.choices if row[10].strip() in [label, value]), None
+        )
+        economic_model = next(
+            (value for value, label in Canteen.EconomicModel.choices if row[11].strip() in [label, value]), None
+        )
+
+        # Group custom validation
+        groupe_id = row[12]
+        groupe = None
+        if groupe_id:
+            try:
+                groupe = Canteen.objects.get(id=groupe_id)
+            except Exception:
+                raise ValidationError(
+                    {"groupe_id": f"Aucun groupe avec le numéro identifiant « {groupe_id} » trouvé sur la plateforme."}
+                )
+
+        line_ministry = (
+            next((value for value, label in Canteen.Ministries.choices if label == row[13].strip()), None)
+            if row[13]
+            else None
+        )
+
+        if canteen and not self._has_canteen_permission(canteen):
+            raise PermissionDenied(detail="Vous n'êtes pas un gestionnaire de cette cantine.")
+
+        canteen.siret = siret
+        canteen.siren_unite_legale = siren_unite_legale
+        if siren_unite_legale and city_insee_code:
+            # Validate city_insee_code
+            # TODO : move to canteen model validators ?
+            code = utils_utils.normalize_string(city_insee_code)
+            if len(code) != 5:
+                raise ValidationError(
+                    {
+                        "city_insee_code": f"Le code INSEE « {city_insee_code} » n'est pas valide, il doit contenir 5 caractères (ex : 75056)."
+                    }
+                )
+            canteen.city_insee_code = city_insee_code
+        canteen.name = name
+        canteen.daily_meal_count = daily_meal_count
+        canteen.yearly_meal_count = yearly_meal_count
+        canteen.sector_list = sector_list
+        canteen.production_type = production_type
+        canteen.management_type = management_type
+        canteen.economic_model = economic_model
+        canteen.central_producer_siret = central_producer_siret
+        canteen.groupe = groupe
+        canteen.line_ministry = line_ministry
+
+        canteen.save()
+        update_change_reason(canteen, f"Mass CSV import. {self.__class__.__name__[:100]}")
+
+        if manager_emails:
+            CanteensUpdateImportView._add_managers_to_canteen(manager_emails, canteen)
+
+        return canteen
 
     def _get_generic_error_message(self):
         return "Une erreur s'est produite en modifiant une cantine pour cette ligne"
