d10356f5140008175a4928b63107f8410250a99c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-25 14:59:33 +0100

    Ajoute un test sur les bilans des satelittes sans centrale

--- macantine/tests/test_diagnostic_generate_csat_from_cc.py
+++ macantine/tests/test_diagnostic_generate_csat_from_cc.py
@@ -42,8 +42,37 @@ class DiagnosticGenerateCsatFromCC2024CommandTest(TestCase):
             Diagnostic.objects.in_year(2024).teledeclared().filter(generated_from_groupe_diagnostic=True).count(), 0
         )
 
+    @authenticate
+    def test_sat_without_central_diagnostic_teledeclared_is_not_modified(self):
+        """
+        Test that the diagnostics of the satellites without central diagnostic are not modified when the command is run.
+        """
+        satellite = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE_CENTRAL, central_producer_siret="19622299600015"
+        )
+        with freeze_time("2025-03-30"):  # during the 2024 campaign
+            satellite_diagnostic = DiagnosticFactory(
+                canteen=satellite,
+                year=2024,
+                diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
+            )
+            satellite_diagnostic.teledeclare(applicant=authenticate.user)
+
+        self.assertEqual(
+            Canteen.objects.filter(siret="19622299600015").count(), 0
+        )  # no central canteen exist with the siret used in sat
+        self.assertEqual(Diagnostic.objects.in_year(2024).count(), 1)
+        self.assertEqual(Diagnostic.objects.in_year(2024).teledeclared().count(), 1)
+
+        self.assertEqual(
+            Diagnostic.objects.in_year(2024).teledeclared().filter(generated_from_groupe_diagnostic=True).count(), 0
+        )
+        call_command("teledeclaration_generate_1td1site", year=2024, apply=True)
+        self.assertEqual(
+            Diagnostic.objects.in_year(2024).teledeclared().filter(generated_from_groupe_diagnostic=True).count(), 0
+        )
+
     # TESTS TO DO :
-    # def test_sat_without_central_diagnostic_teledeclared_is_not_modified(self):
     # def test_central_teledeclare_diagnostic_are_generated(self):
     # def test_central_serving_teledeclare_diagnostic_are_generated(self):
     # def test_command_delete_previous_generated_diagnostics(self):
