dbc400bf31c0a9e0394b60a0314469c8941e3a08
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-02-25 14:52:01 +0100

    Supprime les anciens test pour reprendre cas par cas

--- macantine/tests/test_diagnostic_generate_csat_from_cc.py
+++ macantine/tests/test_diagnostic_generate_csat_from_cc.py
@@ -6,159 +6,18 @@ from data.models import Canteen, Diagnostic
 from api.tests.utils import authenticate
 from data.factories import CanteenFactory, DiagnosticFactory
 
+
 class DiagnosticGenerateCsatFromCC2024CommandTest(TestCase):
-    def setUp(self):
-        # Create a central kitchen
-        self.central_kitchen = Canteen.objects.create(
-            id=1,
-            name="Central Kitchen",
-            siret="11111111111111",
-            yearly_meal_count=1000,
-            production_type=Canteen.ProductionType.CENTRAL,
-        )
-        # Create a central serving kitchen
-        self.central_serving_kitchen = Canteen.objects.create(
-            id=2,
-            name="Central Serving Kitchen",
-            siret="11111111111110",
-            yearly_meal_count=100,
-            production_type=Canteen.ProductionType.CENTRAL_SERVING,
-        )
-        # Create two satellites
-        self.satellite_1 = Canteen.objects.create(
-            id=11,
-            name="Satellite 1",
-            siret="22222222222222",
-            yearly_meal_count=500,
-            production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
-            central_producer_siret=self.central_kitchen.siret,
-        )
-        self.satellite_2 = Canteen.objects.create(
-            id=12,
-            name="Satellite 2",
-            siret="33333333333333",
-            yearly_meal_count=500,
-            production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
-            central_producer_siret=self.central_kitchen.siret,
-        )
-        # Create 2 regular kitchens to check they remain intact
-        self.site_1 = Canteen.objects.create(
-            name="Kitchen",
-            siret="444444444444444",
-            yearly_meal_count=1000,
-            production_type=Canteen.ProductionType.ON_SITE,
-        )
-        self.site_2 = Canteen.objects.create(
-            name="Kitchen 2",
-            siret="444444444444445",
-            yearly_meal_count=1000,
-            production_type=Canteen.ProductionType.ON_SITE,
-        )
-        with freeze_time("2025-03-30"):  # during the 2024 campaign
-            # Create a diagnostic for the central kitchen
-            self.diagnostic_central_kitchen = Diagnostic.objects.create(
-                canteen=self.central_kitchen,
-                diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
-                year=2024,
-                value_total_ht=1000,
-                value_bio_ht=200,
-            )
-            self.diagnostic_central_kitchen.teledeclare(applicant=UserFactory())
-            # Create a diagnostic for the central serving kitchen
-            self.diagnostic_central_serving_kitchen = Diagnostic.objects.create(
-                canteen=self.central_serving_kitchen,
-                diagnostic_type=Diagnostic.DiagnosticType.SIMPLE,
-                year=2024,
-                value_total_ht=1000,
-                value_bio_ht=200,
-            )
-            self.diagnostic_central_serving_kitchen.teledeclare(applicant=UserFactory())
-            # Create a diagnostic for the satellite_1
-            self.diagnostic_satellite_1 = Diagnostic.objects.create(
-                canteen=self.satellite_1,
-                year=2024,
-                value_total_ht=100,
-                value_bio_ht=20,
-            )
-            self.diagnostic_satellite_1.teledeclare(applicant=UserFactory())
-            # Create a diagnostic for the on site canteen
-            self.diagnostic_on_site_kitchen_1 = Diagnostic.objects.create(
-                canteen=self.site_1,
-                year=2024,
-                value_total_ht=100,
-                value_bio_ht=20,
-            )
-            self.diagnostic_on_site_kitchen_1.teledeclare(applicant=UserFactory())
-            # Create a diagnostic for the on site canteen 2 (don't teledeclare it)
-            self.diagnostic_on_site_kitchen_2 = Diagnostic.objects.create(
-                canteen=self.site_2,
-                year=2024,
-                value_total_ht=100,
-                value_bio_ht=20,
-            )
 
-    def test_command_generates_diagnostics_for_satellites(self):
-        self.assertEqual(Canteen.objects.count(), 6)
-        self.assertEqual(Diagnostic.objects.count(), 5)
-        self.assertEqual(Diagnostic.objects.in_year(2024).count(), 5)
-        self.assertEqual(Diagnostic.objects.in_year(2024).filter(status=Diagnostic.DiagnosticStatus.DRAFT).count(), 1)
-        self.assertEqual(Diagnostic.objects.in_year(2024).teledeclared().count(), 4)
-        self.assertEqual(
-            Diagnostic.objects.in_year(2024).teledeclared().filter(generated_from_groupe_diagnostic=False).count(),
-            4,
-        )
-
-        # Run the command
-        call_command("teledeclaration_generate_1td1site", year=2024, apply=True)
-        # There should be 3 new diagnostics: 2 for the satellites, 1 for the central serving kitchen
-        self.assertEqual(Diagnostic.objects.in_year(2024).filter(status=Diagnostic.DiagnosticStatus.DRAFT).count(), 1)
-        self.assertEqual(
-            Diagnostic.objects.in_year(2024)
-            .filter(status=Diagnostic.DiagnosticStatus.SUBMITTED_BUT_OVERRIDDEN_BY_GROUPE)
-            .count(),
-            1,
-        )
-        self.assertEqual(Diagnostic.objects.in_year(2024).teledeclared().count(), 4 - 1 + 3)
-        self.assertEqual(
-            Diagnostic.objects.in_year(2024).teledeclared().filter(generated_from_groupe_diagnostic=True).count(),
-            3,
-        )
-
-    def test_command_creates_satellite_diagnostics_with_split_appro(self):
-        call_command("teledeclaration_generate_1td1site", year=2024, apply=True)
-        # There should be a new diagnostic for each satellite
-        diag_sat1 = Diagnostic.objects.teledeclared().get(
-            canteen=self.satellite_1, year=2024, generated_from_groupe_diagnostic=True
-        )
-        diag_sat2 = Diagnostic.objects.teledeclared().get(
-            canteen=self.satellite_2, year=2024, generated_from_groupe_diagnostic=True
-        )
-        # The satellite canteen data should be set
-        # The appro values should be split equally (1000/2, 200/2)
-        self.assertEqual(diag_sat1.canteen.name, "Satellite 1")
-        self.assertEqual(diag_sat1.value_total_ht, 500)
-        self.assertEqual(diag_sat1.value_bio_ht, 100)
-        self.assertEqual(diag_sat2.canteen.name, "Satellite 2")
-        self.assertEqual(diag_sat2.value_total_ht, 500)
-        self.assertEqual(diag_sat2.value_bio_ht, 100)
-
-    def test_command_does_not_remove_original_cc_diag(self):
-        call_command("teledeclaration_generate_1td1site", year=2024, apply=True)
-        self.assertTrue(Diagnostic.objects.filter(canteen=self.central_kitchen, year=2024).exists())
-
-    def test_command_does_not_edit_on_site_kitchen_diag(self):
-        call_command("teledeclaration_generate_1td1site", year=2024, apply=True)
-        self.assertTrue(Diagnostic.objects.filter(canteen=self.site_1, year=2024).exists())
-
-    def test_command_create_a_central_serving_diag(self):
-        call_command("teledeclaration_generate_1td1site", year=2024, apply=True)
-        self.assertTrue(
-            Diagnostic.objects.filter(
-                canteen=self.central_serving_kitchen, year=2024, generated_from_groupe_diagnostic=True
-            ).exists()
-        )
-        self.assertTrue(
-            Diagnostic.objects.filter(
-                canteen=self.central_serving_kitchen, year=2024, generated_from_groupe_diagnostic=False
-            ).exists()
-        )
+    # TESTS TO DO :
+    # def test_canteen_sites_diagnostics_teledeclared_are_not_modified(self):
+    # def test_sat_without_central_diagnostic_teledeclared_is_not_modified(self):
+    # def test_central_teledeclare_diagnostic_are_generated(self):
+    # def test_central_serving_teledeclare_diagnostic_are_generated(self):
+    # def test_command_delete_previous_generated_diagnostics(self):
+    # def test_command_split_appro_for_satellites_is_correct(self):
+    # def test_other_year_diagnostics_are_not_modified(self):
+    # def test_sat_with_diagnostic_not_teledeclared_is_overwritten(self):
+    # def test_sat_with_diagnostic_teledeclared_is_overwritten(self):
+    # def test_sat_with_two_diagnostics(self):
+    # def test_generated_diagnostics_contains_canteen_data(self):
