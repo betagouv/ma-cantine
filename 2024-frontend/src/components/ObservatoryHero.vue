a398ba8125bc1540a40304d02355bca68132270c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-03-03 10:22:44 +0100

    Met à jour le texte d'introduction

--- 2024-frontend/src/components/ObservatoryHero.vue
+++ 2024-frontend/src/components/ObservatoryHero.vue
@@ -12,18 +12,19 @@ const pictoDocuments = "/static/images/picto-dsfr/documents.svg"
     <div class="fr-col-12 fr-col-lg-6">
       <h1>{{ route.meta.title }}</h1>
       <p>
-        La loi EGalim, complétée par la loi Climat et Résilience, a défini des obligations en ce qui concerne : la
-        qualité des produits entrant dans la composition des repas servis en restauration collective.
+        La loi EGalim, complétée par la loi Climat et Résilience, a défini des obligations de qualité et durabilité des produits entrant dans la composition des repas et de lutte contre le gaspillage alimentaire et d’information des convives. Des obligations en matière de diversification des sources de protéines et de mise en place de menus végétariens sont également en vigueur (selon secteurs et/ou nombre de repas servis).
       </p>
-      <p class="fr-mb-0">
-        Ces chiffres sont rendus publics tous les ans après la publication du rapport parlementaire. Pour comprendre
-        comment sont calculés les chiffres et leurs spécificités,
+      <p>
+        Cette  version de l’Observatoire met à disposition uniquement les données relatives à la qualité et la durabilité des produits. Pour comprendre comment sont calculés les indicateurs, leurs spécificités, et les évolutions à venir :
         <a
           href="https://incubateur-masa.notion.site/A-propos-de-l-observatoire-EGalim-en-restauration-collective-262de24614be800d8924eb83901def36"
           target="_blank"
         >
           consulter notre article explicatif
         </a>
+      </p>
+      <p class="fr-mb-0">
+        Les chiffres de l’année n-1 sont rendus publics en année n, dès publication du bilan statistique annuel (rapport du gouvernement au parlement).
       </p>
     </div>
     <ul class="fr-col-12 fr-col-lg-5 fr-col-offset-lg-1 fr-grid-row fr-grid-row--gutters ma-cantine--unstyled-list">
