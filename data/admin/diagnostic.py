3210ac3a74d83432207cab8017de4e0373a86529
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-03-05 19:15:55 +0100

    Filtre non fonctionnel

--- data/admin/diagnostic.py
+++ data/admin/diagnostic.py
@@ -47,7 +47,7 @@ class DiagnosticAdmin(SimpleHistoryAdmin):
         "creation_date",
         "modification_date",
     )
-    list_filter = ("year", "diagnostic_type", "status", "creation_source", "generated_from_groupe_diagnostic")
+    list_filter = ("year", "diagnostic_type", "status", "creation_source")
     search_fields = (
         "id",
         "canteen__name",
