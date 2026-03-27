9d1b44db2c75174cda7bd92347b527f37a8dc76c
Raphaël Odini <raphodn@users.noreply.github.com>
2026-03-27 13:07:16 +0100

    improve(Bandeau): Coté publique, rajouter une phrase sur la prolongation (#6566)

--- 2024-frontend/src/components/BannerInfo.vue
+++ 2024-frontend/src/components/BannerInfo.vue
@@ -3,5 +3,5 @@ const show = window.SHOW_BANNER
 </script>
 
 <template>
-  <DsfrNotice v-if="show" title="Campagne de télédéclaration 2026 : du 12 janvier au 31 mars 2026." type="info" />
+  <DsfrNotice v-if="show" title="Campagne de télédéclaration du 12 au 31 mars 2026. Une prolongation de quelques jours est prévue - précisions le 31 mars." type="info" />
 </template>
