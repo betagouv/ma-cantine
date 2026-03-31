6f3b3d45f0994e5b758e8739ac870d11fe830941
Raphael Odini <raphodn@users.noreply.github.com>
2026-03-31 15:25:47 +0200

    improve(ETL): dbt: lancer DROP CASCADE si le schéma de la table exportée a changé

--- macantine/etl/data_ware_house.py
+++ macantine/etl/data_ware_house.py
@@ -4,7 +4,7 @@ import time
 
 import pandas as pd
 from dotenv import load_dotenv
-from sqlalchemy import URL, create_engine
+from sqlalchemy import URL, create_engine, text
 
 logger = logging.getLogger(__name__)
 
@@ -25,17 +25,40 @@ class DataWareHouse:
             echo=False,
         )
 
-    def insert_dataframe(self, dataframe, table, dtype=None):
-        start = time.time()
+    def _drop_table(self, table, cascade=False):
+        with self.engine.connect() as connection:
+            sql = text(f'DROP TABLE IF EXISTS "{table}" {"CASCADE" if cascade else ""}')
+            print(f"Executing SQL: {sql}")
+            connection.execute(sql)
+
+    def _insert_dataframe_delete_rows(self, dataframe, table):
         dataframe.to_sql(
             name=table,
             con=self.engine,
             if_exists="delete_rows",
             index=False,
-            dtype=dtype,
             chunksize=1000,
             # method="multi",  # Batch INSERTs for 2-3x speedup
         )
+
+    def _insert_dataframe_replace(self, dataframe, table):
+        dataframe.to_sql(
+            name=table,
+            con=self.engine,
+            if_exists="replace",
+            index=False,
+            chunksize=1000,
+            # method="multi",  # Batch INSERTs for 2-3x speedup
+        )
+
+    def insert_dataframe(self, dataframe, table):
+        start = time.time()
+        try:
+            self._insert_dataframe_delete_rows(dataframe, table)
+        except:  # noqa
+            print("=============== IN ====================")
+            self._drop_table(table, cascade=True)
+            self._insert_dataframe_replace(dataframe, table)
         end = time.time()
         logger.info(f"Inserted {len(dataframe)} rows into table {table} in {end - start:.2f} seconds")
 
