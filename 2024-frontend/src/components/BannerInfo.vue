b480c6b9790c8c847a303b1facbde63c9ead548d
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-04-01 12:27:31 +0200

    Ajoute le CR sur le bandeau du nouveau front

--- 2024-frontend/src/components/BannerInfo.vue
+++ 2024-frontend/src/components/BannerInfo.vue
@@ -10,8 +10,11 @@ const loggedBannerTitle = ""
 const bannerTitle = computed(() => {
   return !!store.loggedUser && loggedBannerTitle ? loggedBannerTitle : publicBannerTitle
 })
+const bannerLink = "https://agriculture.gouv.fr/exemplarite-de-letat-dans-la-restauration-collective-annie-genevard-et-david-amiel-mobilisent-les"
 </script>
 
 <template>
-  <DsfrNotice v-if="show" :title="bannerTitle" type="info" />
+  <DsfrNotice v-if="show" type="info">
+    {{ bannerTitle }} : <a :href="bannerLink" target="_blank">voir le communiqué de presse</a>
+  </DsfrNotice>
 </template>
