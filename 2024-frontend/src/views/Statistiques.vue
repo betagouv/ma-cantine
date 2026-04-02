26fb507a6df7baaa455e48db752adbe1bfdf3fc3
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-04-02 14:56:42 +0200

    Corrige le nom de la plateforme

--- 2024-frontend/src/views/Statistiques.vue
+++ 2024-frontend/src/views/Statistiques.vue
@@ -8,7 +8,7 @@ const metabaseStatsUrl = "https://ma-cantine-metabase.cleverapps.io/public/dashb
     <section class="fr-col-12 fr-col-md-8 fr-mb-2w">
       <h1>Statistiques publiques</h1>
       <p>
-        Cette page présente les chiffres clés de la plateforme ma cantine mis à jour en temps réel.<br/>
+        Cette page présente les chiffres clés de la plateforme <em>ma-cantine</em> mis à jour en temps réel.<br/>
         Pour plus d'informations sur les données télédéclarées, consulter <a :href="documentation.rapportsBilansStatistiquesEgalim" target="_blank">les rapports annuels</a>.
       </p>
     </section>
