45f0c56934d2a11f4d42b15f3e35153f922a9df9
Raphael Odini <raphodn@users.noreply.github.com>
2026-04-08 11:15:35 +0200

    update stats

--- macantine/tests/test_etl_common.py
+++ macantine/tests/test_etl_common.py
@@ -13,81 +13,83 @@ def setUpTestData(cls, with_diagnostics=False):
     cls.canteen_site_manager_1 = UserFactory(email="gestionnaire1@example.com")
     cls.canteen_site_manager_2 = UserFactory(email="gestionnaire2@example.com")
     cls.canteen_groupe_manager = UserFactory()
-    cls.canteen_site = CanteenFactory(
-        name="Cantine",
-        siret="21380185500015",
-        city_insee_code="38185",
-        epci="200040715",
-        epci_lib="Grenoble-Alpes-Métropole",
-        pat_list=["1294", "1295"],
-        pat_lib_list=[
-            "PAT du Département de l'Isère",
-            "Projet Alimentaire inter Territorial de la Grande région grenobloise",
-        ],
-        department="38",
-        department_lib="Isère",
-        region="84",
-        region_lib="Auvergne-Rhône-Alpes",
-        management_type=Canteen.ManagementType.DIRECT,
-        production_type=Canteen.ProductionType.ON_SITE,
-        economic_model=Canteen.EconomicModel.PUBLIC,
-        sector_list=[Sector.SANTE_HOPITAL, Sector.SOCIAL_CRECHE],
-        declaration_donnees_2022=True,
-        declaration_donnees_2023=False,
-        declaration_donnees_2024=True,
-        declaration_donnees_2025=False,
-        managers=[cls.canteen_site_manager_1, cls.canteen_site_manager_2],
-    )
-    cls.canteen_site_without_manager = CanteenFactory(production_type=Canteen.ProductionType.ON_SITE, managers=[])
-    cls.canteen_site_earlier = CanteenFactory(
-        production_type=Canteen.ProductionType.ON_SITE,
-        management_type=Canteen.ManagementType.DIRECT,
-        economic_model=Canteen.EconomicModel.PUBLIC,
-        sector_list=[Sector.EDUCATION_SUPERIEUR_UNIVERSITAIRE],
-        line_ministry=Canteen.Ministries.ENSEIGNEMENT_SUPERIEUR,
-        managers=[cls.canteen_site_manager_2],
-        # creation_date=timezone.now() - timezone.timedelta(days=10),
-    )
-    Canteen.objects.filter(id=cls.canteen_site_earlier.id).update(
-        creation_date=timezone.now() - timezone.timedelta(days=10)
-    )
-    cls.canteen_site_deleted = CanteenFactory(
-        production_type=Canteen.ProductionType.ON_SITE,
-        deletion_date=timezone.now(),
-    )
-    cls.canteen_site_armee = CanteenFactory(
-        production_type=Canteen.ProductionType.ON_SITE,
-        management_type=Canteen.ManagementType.DIRECT,
-        economic_model=Canteen.EconomicModel.PUBLIC,
-        sector_list=[Sector.ADMINISTRATION_PRISON],
-        line_ministry=Canteen.Ministries.ARMEE,
-        managers=[cls.canteen_site_manager_2],
-    )
-    cls.canteen_groupe = CanteenFactory(
-        production_type=Canteen.ProductionType.GROUPE,
-        management_type=Canteen.ManagementType.CONCEDED,
-        managers=[cls.canteen_groupe_manager],
-    )
-    cls.canteen_satellite = CanteenFactory(
-        production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
-        management_type=Canteen.ManagementType.DIRECT,
-        economic_model=Canteen.EconomicModel.PUBLIC,
-        groupe=cls.canteen_groupe,
-        central_producer_siret="22730656663081",
-        city_insee_code="38185",
-        epci="200040715",
-        epci_lib="Grenoble-Alpes-Métropole",
-        pat_list=["1294", "1295"],
-        pat_lib_list=[
-            "PAT du Département de l'Isère",
-            "Projet Alimentaire inter Territorial de la Grande région grenobloise",
-        ],
-        department="38",
-        department_lib="Isère",
-        region="84",
-        region_lib="Auvergne-Rhône-Alpes",
-        sector_list=[Sector.EDUCATION_PRIMAIRE],
-    )
+
+    with freeze_time("2023-05-14"):  # during the 2022 campaign
+        cls.canteen_site = CanteenFactory(
+            name="Cantine",
+            siret="21380185500015",
+            city_insee_code="38185",
+            epci="200040715",
+            epci_lib="Grenoble-Alpes-Métropole",
+            pat_list=["1294", "1295"],
+            pat_lib_list=[
+                "PAT du Département de l'Isère",
+                "Projet Alimentaire inter Territorial de la Grande région grenobloise",
+            ],
+            department="38",
+            department_lib="Isère",
+            region="84",
+            region_lib="Auvergne-Rhône-Alpes",
+            management_type=Canteen.ManagementType.DIRECT,
+            production_type=Canteen.ProductionType.ON_SITE,
+            economic_model=Canteen.EconomicModel.PUBLIC,
+            sector_list=[Sector.SANTE_HOPITAL, Sector.SOCIAL_CRECHE],
+            declaration_donnees_2022=True,
+            declaration_donnees_2023=False,
+            declaration_donnees_2024=True,
+            declaration_donnees_2025=False,
+            managers=[cls.canteen_site_manager_1, cls.canteen_site_manager_2],
+        )
+        cls.canteen_site_without_manager = CanteenFactory(production_type=Canteen.ProductionType.ON_SITE, managers=[])
+        cls.canteen_site_earlier = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE,
+            management_type=Canteen.ManagementType.DIRECT,
+            economic_model=Canteen.EconomicModel.PUBLIC,
+            sector_list=[Sector.EDUCATION_SUPERIEUR_UNIVERSITAIRE],
+            line_ministry=Canteen.Ministries.ENSEIGNEMENT_SUPERIEUR,
+            managers=[cls.canteen_site_manager_2],
+            # creation_date=timezone.now() - timezone.timedelta(days=10),
+        )
+        Canteen.objects.filter(id=cls.canteen_site_earlier.id).update(
+            creation_date=timezone.now() - timezone.timedelta(days=10)
+        )
+        cls.canteen_site_deleted = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE,
+            deletion_date=timezone.now(),
+        )
+        cls.canteen_site_armee = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE,
+            management_type=Canteen.ManagementType.DIRECT,
+            economic_model=Canteen.EconomicModel.PUBLIC,
+            sector_list=[Sector.ADMINISTRATION_PRISON],
+            line_ministry=Canteen.Ministries.ARMEE,
+            managers=[cls.canteen_site_manager_2],
+        )
+        cls.canteen_groupe = CanteenFactory(
+            production_type=Canteen.ProductionType.GROUPE,
+            management_type=Canteen.ManagementType.CONCEDED,
+            managers=[cls.canteen_groupe_manager],
+        )
+        cls.canteen_satellite = CanteenFactory(
+            production_type=Canteen.ProductionType.ON_SITE_CENTRAL,
+            management_type=Canteen.ManagementType.DIRECT,
+            economic_model=Canteen.EconomicModel.PUBLIC,
+            groupe=cls.canteen_groupe,
+            central_producer_siret="22730656663081",
+            city_insee_code="38185",
+            epci="200040715",
+            epci_lib="Grenoble-Alpes-Métropole",
+            pat_list=["1294", "1295"],
+            pat_lib_list=[
+                "PAT du Département de l'Isère",
+                "Projet Alimentaire inter Territorial de la Grande région grenobloise",
+            ],
+            department="38",
+            department_lib="Isère",
+            region="84",
+            region_lib="Auvergne-Rhône-Alpes",
+            sector_list=[Sector.EDUCATION_PRIMAIRE],
+        )
 
     if with_diagnostics:
         with freeze_time("2023-05-14"):  # during the 2022 campaign
