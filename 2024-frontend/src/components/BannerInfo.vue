ba245d2de5f68ae1e29b118a293cabe75c84194d
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-04-10 16:00:00 +0200

    Met à jour le bandeau pour le nouveau frontend

--- 2024-frontend/src/components/BannerInfo.vue
+++ 2024-frontend/src/components/BannerInfo.vue
@@ -1,20 +1,23 @@
 <script setup>
 import { computed } from "vue"
 import { useRootStore } from "@/stores/root"
+import documentation from "@/data/documentation.json"
 
 const store = useRootStore()
 
 const show = window.SHOW_BANNER
-const publicBannerTitle = "La campagne de télédéclaration 2026 est prolongée jusqu'au 15 avril 2026"
+const publicBannerTitle = "DROIT À L’ERREUR : du 16 au 29 avril 2026."
 const loggedBannerTitle = ""
 const bannerTitle = computed(() => {
   return !!store.loggedUser && loggedBannerTitle ? loggedBannerTitle : publicBannerTitle
 })
-const bannerLink = "https://agriculture.gouv.fr/exemplarite-de-letat-dans-la-restauration-collective-annie-genevard-et-david-amiel-mobilisent-les"
 </script>
 
 <template>
-  <DsfrNotice v-if="show" type="info">
-    {{ bannerTitle }} : <a :href="bannerLink" target="_blank">voir le communiqué de presse</a>.
+  <DsfrNotice v-if="show" type="info" :title="bannerTitle">
+    <template #desc>
+      Valable uniquement pour les établissements qui ont validé leur télédéclaration dans les temps. Depuis votre bilan, vous pouvez corriger vos informations si besoin
+      <a :href="documentation.correctionCampaign" target="_blank">voir l’article d’aide pour en savoir plus</a>.
+    </template>
   </DsfrNotice>
 </template>
