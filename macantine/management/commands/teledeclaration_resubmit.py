e36251aa77f722b85f983617333bf84eb3220725
Raphael Odini <raphodn@users.noreply.github.com>
2026-04-15 14:43:05 +0200

    improve(Télédéclarations): améliorer la commande de resubmit pour prendre en entrée une liste d'ID de Diagnostic

--- macantine/management/commands/teledeclaration_resubmit.py
+++ macantine/management/commands/teledeclaration_resubmit.py
@@ -1,21 +1,28 @@
 """
 Why this script?
 We updated/fixed the data of some canteens after they teledeclared.
-To reflect the changes in the Teledeclaration objects, we need to cancel and re-submit these teledeclarations.
+To reflect the changes in the Diagnostic objects, we need to cancel and re-submit these teledeclarations.
 
 How to run?
-python manage.py teledeclaration_resubmit --year 2024 --canteen-siret-list 92341284500011,23456789012345
+python manage.py teledeclaration_resubmit --year 2024 --teledeclaration-id-list 123,456,789
 
-Ran on 2025-04-18 (Campaign for 2024)
+Ran on 2025-04-18 (2024 campaign)
+Ran on 2026-04-15 (last day of 2025 campaign)
 """
 
-from django.core.management.base import BaseCommand
+import logging
 
-from data.models import Canteen, Teledeclaration
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ValidationError
+
+from data.models import Diagnostic
+
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Resubmit teledeclarations for a specified list of canteens"
+    help = "Resubmit teledeclarations for a specified list of diagnostics"
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -26,46 +33,45 @@ class Command(BaseCommand):
             help="Year of the teledeclaration campaign to process",
         )
         parser.add_argument(
-            "--canteen-siret-list",
-            dest="canteen_siret_list",
+            "--teledeclaration-id-list",
+            dest="teledeclaration_id_list",
             type=str,
             required=True,
-            help="Comma-seperated list of canteen SIRETs to process",
+            help="Comma-seperated list of diagnostic IDs to process",
         )
 
     def handle(self, *args, **options):
         # init
-        print("Starting teledeclaration resubmit task")
+        logger.info("Starting task: teledeclaration resubmit")
         teledeclaration_resubmitted_count = 0
         year = options["year"]
-        print("Year in input:", year)
-        canteen_siret_list = options["canteen_siret_list"].split(",")
-        print("SIRET in input list:", len(canteen_siret_list))
+        logger.info(f"Year in input: {year}")
+        teledeclaration_id_list = options["teledeclaration_id_list"].split(",")
+        logger.info(f"Diagnostic IDs in input list: {len(teledeclaration_id_list)}")
 
-        canteen_qs = Canteen.objects.filter(siret__in=canteen_siret_list)
-        print("Canteens found:", canteen_qs.count())
+        # queryset
+        diagnostics_qs = Diagnostic.objects.filter(id__in=teledeclaration_id_list)
+        logger.info(f"Diagnostics (teledeclared) found: {diagnostics_qs.count()}")
 
-        # loop on each canteen
-        for canteen in canteen_qs:
-            # canteen must have submitted a TD during the campaign
-            canteen_td_submitted_for_year_qs = Teledeclaration.objects.filter(canteen=canteen).submitted_for_year(year)
-            if not canteen_td_submitted_for_year_qs.exists():
-                print(f"No teledeclaration for canteen {canteen.siret} for year {year}")
-                continue  # skip canteen
-            canteen_td_submitted_for_year = canteen_td_submitted_for_year_qs.first()
+        # loop on each diagnostic
+        for diagnostic in diagnostics_qs:
+            diagnostic_applicant = diagnostic.applicant
+            # diagnostic must be in the right year
+            if diagnostic.year != year:
+                logger.warning(f"Diagnostic {diagnostic.id} is from year {diagnostic.year}, expected {year}, skipping")
+                continue
+            # diagnostic must be submitted
+            if not diagnostic.is_teledeclared:
+                logger.warning(f"Diagnostic {diagnostic.id} is not teledeclared, skipping")
+                continue
             # cancel TD
-            canteen_td_submitted_for_year.cancel()
+            diagnostic.cancel()
             # recreate TD
             try:
-                Teledeclaration.create_from_diagnostic(
-                    diagnostic=canteen_td_submitted_for_year.diagnostic,
-                    applicant=canteen_td_submitted_for_year.applicant,
-                )
+                diagnostic.teledeclare(applicant=diagnostic_applicant)
                 teledeclaration_resubmitted_count += 1
-            except Exception as e:
-                print(f"Failed to resubmit teledeclaration for canteen {canteen.siret} for year {year}")
-                print(e)
-                continue
+            except ValidationError as e:
+                logger.error(f"Error teledeclaring diagnostic {diagnostic.id}: {e}")
+                continue  # skip to next diagnostic
 
-        print("Done!")
-        print("Teledeclarations resubmitted:", teledeclaration_resubmitted_count)
+        logger.info(f"Done! Teledeclarations resubmitted: {teledeclaration_resubmitted_count}")
