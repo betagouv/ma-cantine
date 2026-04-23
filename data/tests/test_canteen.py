1079dfadf89f99eea8010c23a21255665c6d535f
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-04-23 18:43:08 +0200

    Supprime le serviceDiagnostics des champs renvoyés par l'api

--- data/tests/test_canteen.py
+++ data/tests/test_canteen.py
@@ -1488,7 +1488,7 @@ class CanteenModelPropertiesTest(TestCase):
         )
 
     @freeze_time("2024-01-20")
-    def test_appro_and_service_diagnostics_in_past_ordered_year_desc(self):
+    def test_appro_in_past_ordered_year_desc(self):
         canteen = CanteenFactory()
         DiagnosticFactory(canteen=canteen, year=2024)
         DiagnosticFactory(canteen=canteen, year=2022)
@@ -1497,6 +1497,4 @@ class CanteenModelPropertiesTest(TestCase):
 
         self.assertEqual(canteen.appro_diagnostics.count(), 2)
         self.assertEqual(canteen.appro_diagnostics.first().year, 2023)
-        self.assertEqual(canteen.service_diagnostics.count(), 2)
-        self.assertEqual(canteen.service_diagnostics.first().year, 2023)
         self.assertEqual(canteen.latest_published_year, 2023)
