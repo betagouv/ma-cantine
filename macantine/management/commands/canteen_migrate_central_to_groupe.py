313fd1703654cf35a90dd52a3160300d29ca3c11
Raphael Odini <raphodn@users.noreply.github.com>
2026-04-27 17:19:20 +0200

    logger

--- macantine/management/commands/canteen_migrate_central_to_groupe.py
+++ macantine/management/commands/canteen_migrate_central_to_groupe.py
@@ -90,7 +90,7 @@ class Command(BaseCommand):
         apply = options["apply"]
 
         if not apply:
-            print("Dry run mode, no changes will be applied.")
+            logger.info("Dry run mode, no changes will be applied.")
 
         # stats before
         canteen_central_all_qs = Canteen.all_objects.filter(production_type=Canteen.ProductionType.CENTRAL)
