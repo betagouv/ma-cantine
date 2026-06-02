82b0dc10589df29df85c3aa6d262f6c1c77f573c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-06-02 11:16:08 +0200

    Ajoute le pré-remplissage de l'achat

--- 2024-frontend/src/views/GestionnaireAchatsModifier.vue
+++ 2024-frontend/src/views/GestionnaireAchatsModifier.vue
@@ -1,14 +1,34 @@
 <script setup>
+import { ref } from "vue"
+import { computedAsync } from "@vueuse/core"
 import { useRoute } from "vue-router"
 import documentation from "@/data/documentation.json"
 import urlService from "@/services/urls.js"
+import purchasesService from "@/services/purchases.js"
+import AppLoader from "@/components/AppLoader.vue"
 import AppRessources from "@/components/AppRessources.vue"
+import PurchaseForm from "@/components/PurchaseForm.vue"
 
-/* Router */
+/* Router and store */
 const route = useRoute()
 
 /* Canteen */
 const canteenName = urlService.getCanteenName(route.params.canteenUrlComponent)
+
+/* Purchase */
+const isLoading = ref(true)
+const purchaseId = route.params.id
+
+const purchaseData = computedAsync(async () => {
+  const response = await purchasesService.fetchPurchase(purchaseId)
+  isLoading.value = false
+  return !response?.id ? {} : response
+}, {})
+
+/* Save */
+const savePurchase = async (props) => {
+  console.log(props)
+}
 </script>
 
 <template>
@@ -29,5 +49,20 @@ const canteenName = urlService.getCanteenName(route.params.canteenUrlComponent)
         </a>
       </li>
     </AppRessources>
+  </section>
+  <section
+    class="fr-background-alt--blue-france fr-p-3w fr-mt-4w fr-grid-row fr-grid-row--center"
+  >
+    <AppLoader v-if="isLoading" />
+    <PurchaseForm
+      v-else-if="purchaseData.id"
+      :purchase-data="purchaseData"
+      :showCancelButton="true"
+      @sendForm="(payload) => savePurchase(payload)"
+      @cancel="goToPurchasesList"
+    />
+    <p v-else class="fr-mb-0" >
+      Aucun achat trouvé avec le numéro d'identification « {{ purchaseId }} » pour la cantine « {{ canteenName }} ».
+    </p>
   </section>
 </template>
