70664c9db2ea89315e44252c469fa3c61e5c2d28
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-06-02 10:43:24 +0200

    Ajoute l'en-tête de page comme pour les ajouts d'achats

--- 2024-frontend/src/views/GestionnaireAchatsModifier.vue
+++ 2024-frontend/src/views/GestionnaireAchatsModifier.vue
@@ -1,9 +1,33 @@
 <script setup>
 import { useRoute } from "vue-router"
+import documentation from "@/data/documentation.json"
+import urlService from "@/services/urls.js"
+import AppRessources from "@/components/AppRessources.vue"
 
+/* Router */
 const route = useRoute()
+
+/* Canteen */
+const canteenName = urlService.getCanteenName(route.params.canteenUrlComponent)
 </script>
 
 <template>
-  <h1>{{ route.meta.title }}</h1>
+  <section class="fr-grid-row fr-grid-row--bottom">
+    <div class="fr-col-12 fr-col-md-6 fr-mb-4w fr-mb-md-0">
+      <h1>{{ route.meta.title }} pour la cantine «&nbsp;{{ canteenName }}&nbsp;»</h1>
+    </div>
+    <div class="fr-col-offset-md-1"></div>
+    <AppRessources>
+      <li>
+        <a :href="documentation.ajouterAchat" target="_blank">
+          Tutoriel pour ajouter un achat manuellement
+        </a>
+      </li>
+      <li>
+        <a :href="documentation.critèresQualiteDurabiliteProduits" target="_blank">
+          Comprendre les critères de qualité et durabilité des produits
+        </a>
+      </li>
+    </AppRessources>
+  </section>
 </template>
