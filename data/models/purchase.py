a3f240a741a1c4ecda9d32ca85630bd816797244
Raphaël Odini <raphodn@users.noreply.github.com>
2026-06-10 11:14:56 +0200

    refactor(Achats): Règles métiers: s'assurer qu'EUROPE & FRANCE ne peuvent pas cohabiter sur un même achat (#6804)

--- data/models/purchase.py
+++ data/models/purchase.py
@@ -141,7 +141,10 @@ class Purchase(SoftDeletionModel):
         COMMERCE_EQUITABLE = "COMMERCE_EQUITABLE", "Commerce équitable"
         PERFORMANCE = "PERFORMANCE", "Produits acquis sur la base de leurs performances en matière environnementale"
         EQUIVALENTS = "EQUIVALENTS", "Produits équivalents"  # not used anymore
-        EUROPE = "EUROPE", "Origine Europe"
+        EUROPE = (
+            "EUROPE",
+            "Origine Europe (hors France)",
+        )  # Espace Économique Européen qui inclut l'Union Européenne, l'Islande, le Liechtenstein et la Norvège
         FRANCE = "FRANCE", "Origine France"
         CIRCUIT_COURT = "CIRCUIT_COURT", "Circuit-court"
         LOCAL = "LOCAL", "Produit local"
@@ -266,6 +269,7 @@ class Purchase(SoftDeletionModel):
 
     def clean(self, *args, **kwargs):
         validation_errors = utils_utils.merge_validation_errors(
+            purchase_validators.validate_purchase_caracteristiques(self),
             purchase_validators.validate_purchase_definition_local(self),
         )
         if validation_errors:
