58b3909cd62a12ecdd513da502d9d47f223a476c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-06-23 17:12:20 +0200

    Créer un composant pour répéter le style des blocs de la page tampon

---
+++ 2024-frontend/src/components/ImportCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["title", "description", "icon"])
+</script>
+<template>
+  <div class="fr-card fr-p-4w">
+    <div>
+      <img :src="icon" :alt="`Illustration de ${title}`">
+      <h2 class="fr-h5">{{ title }}</h2>
+      <p>{{ description }}</p>
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
