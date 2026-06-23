4ac9d44c275612868c572a7fe69493feed31727f
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-06-23 17:16:06 +0200

    Change la mise en page en regroupant les imports par entité

--- 2024-frontend/src/views/GestionnaireImport.vue
+++ 2024-frontend/src/views/GestionnaireImport.vue
@@ -1,151 +1,23 @@
 <script setup>
-import { computed } from "vue"
-import { useRoute } from "vue-router"
+import { useRoute, useRouter } from "vue-router"
 import { useRootStore } from "@/stores/root"
+import documentation from "@/data/documentation.json"
+import ImportCard from "@/components/ImportCard.vue"
 
-/* Pages */
-const staffPages = [
-{
-    title: "Ajouter des gestionnaires aux cantines",
-    to: { name: "GestionnaireImportCantinesGestionnaires" },
-    description: "Pour ajouter des gestionnaires à des cantines sans envoyer d'invitation email.",
-    detail: "Pour les utilisateurs administrateurs seulement",
-    badges: [
-      {
-        label: "SIRET",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-]
-const commonPages = [
-  {
-    title: "Créer des cantines",
-    to: { name: "GestionnaireImportCantinesCreer" },
-    description: "Pour créer des cantines en masse avec un numéro SIRET.",
-    badges: [
-      {
-        label: "SIRET",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Modifier des cantines",
-    to: { name: "GestionnaireImportCantinesModifier" },
-    description: "Pour mettre à jour les informations des cantines dont vous êtes gestionnaire. Pour les cantines inscrites avec SIRET ou rattachées à une unité légale.",
-    detail: "Cet import n'est pas utilisable pour les groupes de restaurants satellites",
-    badges: [
-      {
-        label: "ID",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Ajouter des achats (ancien format)",
-    to: { name: "GestionnaireImportAchatsIDOld" },
-    description: "Pour ajouter des achats à des cantines dont vous êtes gestionnaire.",
-    detail: "Pour les cantines et les groupes de restaurants satellites",
-    badges: [
-      {
-        label: "ID",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Ajouter des achats",
-    to: { name: "GestionnaireImportAchatsID" },
-    description: "Pour ajouter des achats à des cantines dont vous êtes gestionnaire.",
-    detail: "Pour les cantines et les groupes de restaurants satellites",
-    badges: [
-      {
-        label: "Nouveau",
-        type: "new",
-      },
-      {
-        label: "ID",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Ajouter des achats via le SIRET de la cantine  (ancien format)",
-    to: { name: "GestionnaireImportAchatsSIRETOld" },
-    description: "Pour ajouter des achat à des cantines dont vous êtes gestionnaire inscrite avec un numéro SIRET.",
-    badges: [
-      {
-        label: "SIRET",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Ajouter des achats via le SIRET de la cantine",
-    to: { name: "GestionnaireImportAchatsSIRET" },
-    description: "Pour ajouter des achat à des cantines dont vous êtes gestionnaire inscrite avec un numéro SIRET.",
-    badges: [
-      {
-        label: "Nouveau",
-        type: "new",
-      },
-      {
-        label: "SIRET",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Créer ou modifier des bilans simples",
-    to: { name: "GestionnaireImportBilansSimples" },
-    description: "Pour créer ou modifier des bilans simples pour des cantines, des groupes de restaurants satellites ou des cantines rattachées à une unité légale.",
-    detail: "Pour les cantines et les groupes de restaurants satellites",
-    badges: [
-      {
-        label: "ID",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Créer ou modifier des bilans simples via le SIRET de la cantine",
-    to: { name: "GestionnaireImportBilansSimplesSIRET" },
-    description: "Pour créer ou modifier des bilans simples pour des cantines dont vous êtes gestionnaire inscrite avec un numéro SIRET.",
-    badges: [
-      {
-        label: "SIRET",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-  {
-    title: "Créer ou modifier des bilans détaillés via le SIRET de la cantine",
-    to: { name: "GestionnaireImportBilansDetailles" },
-    description: "Pour créer ou modifier des bilans détaillés pour des cantines dont vous êtes gestionnaire inscrite avec un numéro SIRET.",
-    badges: [
-      {
-        label: "SIRET",
-        noIcon: true,
-        type: "info",
-      }
-    ],
-  },
-]
+const router = useRouter()
+
+/* Picto */
+const canteenPicto = "/static/images/picto-dsfr/companie.svg"
+const purchasesPicto = "/static/images/picto-dsfr/food.svg"
+const diagnosticsPicto = "/static/images/picto-dsfr/money.svg"
 
 /* Data */
 const route = useRoute()
 const store = useRootStore()
-const pages = computed(() =>  store.loggedUser.isStaff ? [...staffPages, ...commonPages] : commonPages)
+
+const gotTo = (name) => {
+  router.push({ name: name })
+}
 </script>
 
 <template>
@@ -158,28 +30,59 @@ const pages = computed(() =>  store.loggedUser.isStaff ? [...staffPages, ...comm
       Pour une expérience optimale, veuillez bien respecter les formats attendus.
     </p>
   </section>
-  <section>
-    <ul class="ma-cantine--unstyled-list fr-grid-row fr-grid-row--gutters">
-      <li v-for="page in pages" :key="page.name" class="fr-col-12 fr-col-md-4">
-        <DsfrCard
-          class="gestionnaire-import__card"
-          :title="page.title"
-          :link="page.to"
-          :description="page.description"
-          :endDetail="page.detail"
-          :badges="page.badges"
-        />
-      </li>
-    </ul>
+  <section class="fr-grid-row fr-grid-row--gutters fr-mt-4w">
+    <div class="fr-col-12 fr-col-md-4">
+      <ImportCard title="Gérer des cantines" description="Vous pouver créer et/ou modifier les cantines dont vous êtes gestionnaire. Il n'est pas possible d'utiliser ces imports pour les groupes de restaurants satellites." :icon="canteenPicto">
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Créer des cantines avec SIRET" icon="fr-icon-add-line" @click="gotTo('GestionnaireImportCantinesCreer')" />
+        </div>
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Modifier des cantines" icon="fr-icon-edit-fill" @click="gotTo('GestionnaireImportCantinesModifier')" />
+        </div>
+        <div v-if="store.loggedUser.isStaff">
+          <DsfrButton secondary label="Ajouter des gestionnaires" @click="gotTo('GestionnaireImportCantinesGestionnaires')" icon="fr-icon-user-add-fill"/>
+          <p class="fr-text--xs fr-mb-0">Uniquement pour les administrateurs.</p>
+        </div>
+      </ImportCard>
+    </div>
+
+    <div class="fr-col-12 fr-col-md-4">
+      <ImportCard title="Créer des achats" description="Vous pouvez créer des achats uniquement pour les établissements dont vous êtes gestionnaire. Deux formats d'import sont disponibles pour créer des achats en fonction de vos établissements." :icon="purchasesPicto">
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Créer des achats pour des cantines avec SIRET" icon="fr-icon-shopping-cart-2-fill" @click="gotTo('GestionnaireImportCantinesCreer')" />
+        </div>
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Créer des achats pour des groupes ou cantines sans SIRET" icon="fr-icon-shopping-cart-2-fill" @click="gotTo('GestionnaireImportCantinesModifier')" />
+          <p class="fr-text--xs fr-mb-0">Vous aurez besoin du <a :href="documentation.trouverIdCantine" target="_blank">numéro ID</a> de l'établissement.</p>
+        </div>
+        <DsfrCallout class="fr-mb-0">
+          <div>
+            <p class="fr-mb-1w fr-text--bold">Le format d'import des achats a été modifié en 2026.</p>
+            <p class="fr-mb-1w fr-text--sm">La précédente version reste disponible jusqu'à la fin de la campagne de télédéclaration 2027 :</p>
+            <DsfrButton size="sm" tertiary label="Créer des achats pour des cantines avec SIRET (ancien&nbsp;format)" @click="gotTo('GestionnaireImportAchatsIDOld')" />
+            <DsfrButton size="sm" tertiary label="Créer des achats pour des groupes ou cantines sans SIRET (ancien&nbsp;format)"  @click="gotTo('GestionnaireImportAchatsIDOld')" />
+            <p class="fr-mt-2w fr-text--sm">Passé ce délai, il faudra utiliser le format ci-dessus.</p>
+          </div>
+        </DsfrCallout>
+      </ImportCard>
+    </div>
+
+    <div class="fr-col-12 fr-col-md-4">
+      <ImportCard title="Gérer des bilans" description="Vous pouvez créer ou modifier les données d'approvisionnement des bilans pour les établissements dont vous êtes gestionnaire. Deux modes de saisie sont disponibles :" :icon="diagnosticsPicto">
+        <p><span class="fr-text--bold">• Saisie simplifiée :</span> si vous connaissez uniquement les valeurs totales de vos achats bio et de qualité</p>
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Renseigner saisie simplifiée pour des cantines avec SIRET" icon="fr-icon-file-text-line" @click="gotTo('GestionnaireImportBilansSimplesSIRET')" />
+        </div>
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Renseigner saisie simplifiée pour des groupes ou cantines sans SIRET" icon="fr-icon-file-text-line" @click="gotTo('GestionnaireImportBilansSimples')" />
+          <p class="fr-text--xs fr-mb-0">Vous aurez besoin du <a :href="documentation.trouverIdCantine" target="_blank">numéro ID</a> de l'établissement.</p>
+        </div>
+        <p><span class="fr-text--bold">• Saisie détaillée :</span> si vous connaissez les valeurs totales par labels et les familles de produits de vos achats.</p>
+        <div class="fr-mb-2w">
+          <DsfrButton secondary label="Renseigner saisie détaillée" icon="fr-icon-file-text-fill" @click="gotTo('GestionnaireImportBilansDetailles')" />
+          <p class="fr-text--xs fr-mb-0">Uniquement disponible pour les cantines avec un numéro SIRET.</p>
+        </div>
+      </ImportCard>
+    </div>
   </section>
 </template>
-
-<style lang="scss">
-.gestionnaire-import {
-  &__card {
-    .fr-card__content {
-      padding-top: 3rem;
-    }
-  }
-}
-</style>
