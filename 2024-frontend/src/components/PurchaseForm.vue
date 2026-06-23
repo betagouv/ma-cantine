5831b8ef1cdb13f8d4b140074c2df04ed3a024b5
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-06-23 19:32:46 +0200

    Organise le champ facture pour être en 3 colonnes aussi

--- 2024-frontend/src/components/PurchaseForm.vue
+++ 2024-frontend/src/components/PurchaseForm.vue
@@ -244,21 +244,27 @@ const formatPayload = (form) => {
       </div>
     </div>
 
-    <p class="fr-legend-text fr-mb-1w">Facture</p>
-    <p class="fr-hint-text fr-mb-2w">Ce champ n'est pas obligatoire, vous pouvez le laisser vide si vous n'avez pas de facture pour cet achat, ou si vous utilisez un autre moyen pour stocker vos justificatifs.</p>
-    <div class="fr-grid-row fr-grid-row--top fr-grid-row--gutters">
-      <div v-if="form.invoiceUrl" class="fr-col-12 fr-col-md-6">
-        <p class="fr-mb-2w">Vous avez déjà importé une facture pour cet achat, accéder au fichier en <a :href="form.invoiceUrl" target="_blank">cliquant ici</a>.</p>
-      </div>
-      <div class="fr-col-12" :class="{ 'fr-col-md-6': form.invoiceUrl }">
-        <DsfrFileUpload
-          v-model="invoiceFileInputValue"
-          :label="form.invoiceUrl ? 'Télécharger un nouveau fichier' : 'Télécharger un fichier'"
-          hint="PDF ou image (JPEG, PNG) — 10 Mo maximum"
-          accept="image/jpeg,image/png,application/pdf"
-          :error="invoiceFileError"
-          @change="onInvoiceFileChange"
-        />
+    <div class="fr-card fr-p-3w">
+      <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
+        <div class="fr-col-12 fr-col-md-4">
+          <p class="fr-legend-text fr-mb-1w">Facture</p>
+          <p class="fr-hint-text fr-mb-2w">Ce champ n'est pas obligatoire, vous pouvez le laisser vide si vous n'avez pas de facture pour cet achat, ou si vous utilisez un autre moyen pour stocker vos justificatifs.</p>
+        </div>
+        <div class="fr-col-12 fr-col-md-4">
+          <DsfrFileUpload
+            v-model="invoiceFileInputValue"
+            :label="form.invoiceUrl ? 'Télécharger un nouveau fichier' : 'Télécharger un fichier'"
+            hint="PDF ou image (JPEG, PNG) — 10 Mo maximum"
+            accept="image/jpeg,image/png,application/pdf"
+            :error="invoiceFileError"
+            @change="onInvoiceFileChange"
+          />
+        </div>
+        <div class="fr-col-12 fr-col-md-4">
+          <div v-if="form.invoiceUrl" class="fr-col-12 fr-col-md-6">
+            <p class="fr-mb-2w">Vous avez déjà importé une facture pour cet achat, accéder au fichier en <a :href="form.invoiceUrl" target="_blank">cliquant ici</a>.</p>
+          </div>
+       </div>
       </div>
     </div>
 
