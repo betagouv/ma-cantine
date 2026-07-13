3f3c789b28a52f7b35cf6a5d7b21232452932330
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-07-13 14:53:44 +0200

    Rend le menu plus visible

--- 2024-frontend/src/layouts/LayoutSidebarBilan.vue
+++ 2024-frontend/src/layouts/LayoutSidebarBilan.vue
@@ -46,14 +46,14 @@ const menuItems = [
 <template>
   <div>
     <h1>{{ canteenName }}</h1>
-    <div class="ma-cantine--flex-start ma-cantine--flex-gap-1 fr-mb-2w fr-mb-md-0">
+    <div class="ma-cantine--flex-start ma-cantine--flex-gap-1 fr-mb-4w fr-mb-md-0">
       <DsfrBadge v-if="canteenBadgeId" type="neutral" :label="canteenBadgeId" />
       <DsfrBadge v-if="canteenBadgeSiret" type="neutral" :label="canteenBadgeSiret" />
       <DsfrBadge v-if="canteenBadgeSiren" type="neutral" :label="canteenBadgeSiren" />
     </div>
     <div class="fr-grid-row fr-grid-row--top ma-cantine--sticky__container">
       <div class="fr-col-12 fr-col-md-3 ma-cantine--sticky__top fr-background-default--grey">
-        <DsfrSideMenu :menu-items="menuItems"/>
+        <DsfrSideMenu :menu-items="menuItems" buttonLabel="Voir le menu"/>
       </div>
       <section class="fr-col-12 fr-col-md-9 fr-pt-3w">
         <slot></slot>
