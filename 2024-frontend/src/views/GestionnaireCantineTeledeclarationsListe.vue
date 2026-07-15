3de3ae52ad33e74b431c2c97d8b6a904246324cb
Charline <charline.laporte@beta.gouv.fr>
2026-07-15 23:15:26 +0200

    feat(Établissement): ajoute de la page "Toutes mes télédéclarations" (#6907)

---
+++ 2024-frontend/src/views/GestionnaireCantineTeledeclarationsListe.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRoute } from "vue-router"
+import LayoutSidebarCanteen from "@/layouts/LayoutSidebarCanteen.vue"
+import CanteenListTeledeclarations from "@/components/CanteenListTeledeclarations.vue"
+
+const route = useRoute()
+</script>
+
+<template>
+  <LayoutSidebarCanteen>
+    <template #titleName>
+      <h2 class="fr-h3 fr-mb-0">{{ route.meta.title }}</h2>
+    </template>
+    <template #content="{ canteenInformation }">
+      <CanteenListTeledeclarations class="fr-mt-n4w" :diagnostics="canteenInformation?.diagnostics" :groupeDiagnostics="canteenInformation?.centralKitchenDiagnostics" :canteenId="canteenInformation?.id" />
+    </template>
+  </LayoutSidebarCanteen>
+</template>
