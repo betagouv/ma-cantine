40cae2c3b75065673d9b541ed89265385b9f139f
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-07-15 11:38:26 +0200

    Ajoute les sections de pages

--- 2024-frontend/src/views/GestionnaireBilanInformations.vue
+++ 2024-frontend/src/views/GestionnaireBilanInformations.vue
@@ -11,11 +11,31 @@ const goToEdit = (canteenIsGroupe) => {
 </script>
 
 <template>
-  <LayoutSidebarBilan v-slot="{ canteenIsGroupe }">
+  <LayoutSidebarBilan v-slot="{ canteenIsGroupe, canteenInformation }">
     <div class="ma-cantine--flex-between ma-cantine--flex-gap-1">
       <h2 class="fr-mb-0">{{ canteenIsGroupe ? 'Informations du groupe' : 'Mes informations' }}</h2>
       <DsfrButton @click="goToEdit(canteenIsGroupe)" :label="canteenIsGroupe ? 'Modifier les informations du groupe' : 'Modifier mes informations'" icon="ri-pencil-line" />
     </div>
+    <ol class="ma-cantine--ordered-list ma-cantine--unstyled-list">
+      <li>
+        <h3>Caractéristiques</h3>
+      </li>
+      <li>
+        <h3>Identification de l'établissement</h3>
+      </li>
+      <li v-if="!canteenIsGroupe">
+        <h3>Informations générées</h3>
+      </li>
+      <li v-if="!canteenIsGroupe">
+        <h3>Secteurs</h3>
+      </li>
+      <li v-if="canteenInformation.groupe !== null">
+        <h3>Informations de mon groupe</h3>
+      </li>
+      <li v-if="!canteenIsGroupe">
+        <h3>Description</h3>
+      </li>
+    </ol>
     <pre>{{ canteenInformation }}</pre>
   </LayoutSidebarBilan>
 </template>
