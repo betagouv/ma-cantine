e3207fb0bbdb1ee9168b5a8726cb6930e47b0d72
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-07-15 11:56:32 +0200

    Intègre les caractéristiques

---
+++ 2024-frontend/src/components/AppFieldDisplay.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['label', 'value', 'tooltip'])
+</script>
+
+<template>
+  <div class="fr-grid-row fr-grid-row--gutters fr-mb-1w">
+    <div class="fr-col-12 fr-col-md-5">
+      <p class="fr-mb-0 fr-text--bold">{{ label }} :</p>
+    </div>
+    <div class="fr-col-12 fr-col-md-7">
+      <p class="fr-mb-0">
+        {{ value }}
+        <DsfrTooltip v-if="tooltip" :content="tooltip" />
+      </p>
+    </div>
+  </div>
+</template>
--- 2024-frontend/src/views/GestionnaireBilanInformations.vue
+++ 2024-frontend/src/views/GestionnaireBilanInformations.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import LayoutSidebarBilan from '@/layouts/LayoutSidebarBilan.vue'
+import AppFieldDisplay from '@/components/AppFieldDisplay.vue'
+import cantines from '@/data/cantines.json'
 
 /* Edit dynamic button */
 const router = useRouter()
 const goToEdit = (canteenIsGroupe) => {
   const pageName = canteenIsGroupe ? 'GestionnaireCantineGroupeModifier' : 'GestionnaireCantineRestaurantModifier'
   router.push({ name: pageName })
+}
+
+/* Value */
+const getPrettyValue = (name, field) => {
+  return cantines[field].find(model => model.value === name).label
 }
 </script>
 
@@ -17,22 +24,25 @@ const goToEdit = (canteenIsGroupe) => {
       <DsfrButton @click="goToEdit(canteenIsGroupe)" :label="canteenIsGroupe ? 'Modifier les informations du groupe' : 'Modifier mes informations'" icon="ri-pencil-line" />
     </div>
     <ol class="ma-cantine--ordered-list ma-cantine--unstyled-list">
-      <li>
+      <li class="fr-my-3w">
         <h3>Caractéristiques</h3>
+        <AppFieldDisplay :label="cantines.economicModelName" :value="getPrettyValue(canteenInformation.economicModel, 'economicModel')" />
+        <AppFieldDisplay :label="cantines.managementTypeName" :value="getPrettyValue(canteenInformation.managementType, 'managementType')" />
+        <AppFieldDisplay :label="cantines.productionTypeName" :value="getPrettyValue(canteenInformation.productionType, 'productionType')" />
       </li>
-      <li>
+      <li class="fr-my-3w">
         <h3>Identification de l'établissement</h3>
       </li>
-      <li v-if="!canteenIsGroupe">
+      <li v-if="!canteenIsGroupe" class="fr-my-3w">
         <h3>Informations générées</h3>
       </li>
-      <li v-if="!canteenIsGroupe">
+      <li v-if="!canteenIsGroupe" class="fr-my-3w">
         <h3>Secteurs</h3>
       </li>
-      <li v-if="canteenInformation.groupe !== null">
+      <li v-if="canteenInformation.groupe !== null" class="fr-my-3w">
         <h3>Informations de mon groupe</h3>
       </li>
-      <li v-if="!canteenIsGroupe">
+      <li v-if="!canteenIsGroupe" class="fr-my-3w">
         <h3>Description</h3>
       </li>
     </ol>
