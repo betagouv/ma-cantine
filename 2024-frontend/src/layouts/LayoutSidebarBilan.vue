48b178cd306c8a6ff8b2e719b795b59f6b118e08
Charline <charline.laporte@beta.gouv.fr>
2026-07-15 11:06:34 +0200

    feat(Mes bilans): création de la structure de page (#6900)

---
+++ 2024-frontend/src/layouts/LayoutSidebarBilan.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { useRoute } from "vue-router"
+import { computedAsync } from "@vueuse/core"
+import { computed } from "vue"
+import urlService from "@/services/urls.js"
+import canteenService from "@/services/canteens.js"
+
+/* Route */
+const route = useRoute()
+const currentRoute = computed(() => route.name)
+
+/* Header */
+const canteenId = urlService.getCanteenId(route.params.canteenUrlComponent)
+const canteenName = urlService.getCanteenName(route.params.canteenUrlComponent)
+const canteenInformation = computedAsync(async () => await canteenService.fetchCanteen(canteenId), false)
+const canteenIsGroupe = computed(() => canteenInformation.value?.productionType === "groupe")
+const canteenBadgeId = computed(() => canteenId ? `ID : ${canteenId}` : null)
+const canteenBadgeSiret = computed(() => canteenInformation.value?.siret ? `SIRET : ${canteenInformation.value.siret}` : null)
+const canteenBadgeSiren = computed(() => canteenInformation.value?.sirenUniteLegale ? `SIREN : ${canteenInformation.value.sirenUniteLegale}` : null)
+const canteenBadgeGroupe = computed(() => {
+  if (!canteenIsGroupe.value) return null
+  const nbRestaurants = canteenInformation.value?.satellitesCount
+  return `Groupe : ${nbRestaurants} ${nbRestaurants > 1 ? "restaurants" : "restaurant"}`
+})
+
+/* Sidebar links */
+const menuItems = computed(() =>  {
+  const informationActive = currentRoute.value === "GestionnaireBilanInformations"
+  const gestionnairesActive = currentRoute.value === "GestionnaireBilanGestionnaires"
+  const pagePubliqueActive = currentRoute.value === "GestionnaireBilanPagePublique"
+  const toutesTeledeclarationsActive = currentRoute.value === "GestionnaireBilanToutesTeledeclarations"
+  const cantinesGroupeActive = currentRoute.value === "GestionnaireBilanCantinesGroupe"
+
+  const informationPage = {
+    text: canteenIsGroupe.value ? "Informations du groupe" : "Mes informations",
+    to: { name: "GestionnaireBilanInformations" },
+    active: informationActive
+  }
+  const gestionnairesPage = {
+    text: canteenIsGroupe.value ? "Gestionnaires" : "Mes gestionnaires",
+    to: '/',
+    active: gestionnairesActive
+  }
+  const cantinesGroupePage = {
+    text: "Cantines du groupe",
+    to: '/',
+    active: cantinesGroupeActive
+  }
+  const pagePubliquePage = {
+    text: "Ma page publique",
+    to: '/',
+    active: pagePubliqueActive
+  }
+  const teledeclarationsPage =  {
+    text: "Toutes mes télédéclarations",
+    to: { name: "" },
+    active: toutesTeledeclarationsActive
+  }
+
+  // Dynamic links
+  const pages = []
+  pages.push(informationPage)
+  pages.push(gestionnairesPage)
+  if (canteenIsGroupe.value) pages.push(cantinesGroupePage)
+  if (!canteenIsGroupe.value) pages.push(pagePubliquePage)
+  pages.push(teledeclarationsPage)
+
+  return pages
+})
+</script>
+
+<template>
+  <div class="layout-sidebar-bilan">
+    <h1>{{ canteenName }}</h1>
+    <div class="ma-cantine--flex-start ma-cantine--flex-gap-1 fr-mb-4w">
+      <DsfrBadge v-if="canteenBadgeGroupe" type="info" :noIcon="true" :label="canteenBadgeGroupe" />
+      <DsfrBadge v-if="canteenBadgeId" type="neutral" :label="canteenBadgeId" />
+      <DsfrBadge v-if="canteenBadgeSiret" type="neutral" :label="canteenBadgeSiret" />
+      <DsfrBadge v-if="canteenBadgeSiren" type="neutral" :label="canteenBadgeSiren" />
+    </div>
+    <div class="fr-grid-row fr-grid-row--top ma-cantine--sticky__container">
+      <div class="layout-sidebar-bilan__sidebar-container fr-col-12 fr-col-md-3 ma-cantine--sticky__top fr-background-default--grey">
+        <DsfrSideMenu :menu-items="menuItems" buttonLabel="Voir le menu"/>
+      </div>
+      <section class="fr-col-12 fr-col-md-9 fr-pt-1w">
+        <slot></slot>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.layout-sidebar-bilan {
+  &__sidebar-container {
+    .fr-sidemenu__title {
+      display: none !important;
+    }
+    .fr-sidemenu__inner {
+      padding-right: 0 !important;
+    }
+  }
+}
+</style>
---
+++ 2024-frontend/src/views/GestionnaireBilanInformations.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LayoutSidebarBilan from '@/layouts/LayoutSidebarBilan.vue';
+</script>
+
+<template>
+  <LayoutSidebarBilan>
+    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+  </LayoutSidebarBilan>
+</template>
