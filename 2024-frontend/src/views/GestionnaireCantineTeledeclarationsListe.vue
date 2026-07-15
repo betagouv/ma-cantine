6f5fa5ca013805efe9dea10ded6db4958246b70c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-07-15 17:39:08 +0200

    Crée la page

---
+++ 2024-frontend/src/views/GestionnaireCantineTeledeclarationsListe.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useRoute } from "vue-router"
+import LayoutSidebarCanteen from "@/layouts/LayoutSidebarCanteen.vue"
+
+const route = useRoute()
+</script>
+
+<template>
+  <LayoutSidebarCanteen>
+    <template #titleName>
+      <h2 class="fr-h3 fr-mb-0">{{ route.meta.title }}</h2>
+    </template>
+  </LayoutSidebarCanteen>
+</template>
