5b6b54a87851416910100e24f494fa5655f99eba
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-07-15 22:38:45 +0200

    Nom des composants en anglais

--- 2024-frontend/src/components/CanteenListTeledeclarations.vue
+++ 2024-frontend/src/components/CanteenListTeledeclarations.vue
@@ -62,12 +62,12 @@ const teledeclarations = computed(() => {
 </script>
 
 <template>
-  <ul class="teledeclarations-liste ma-cantine--unstyled-list">
-    <li v-for="teledeclaration in teledeclarations" :key="teledeclaration.year" class="teledeclarations-liste__item fr-grid-row fr-grid-row--gutters fr-grid-row--top fr-pb-1w fr-my-1w">
+  <ul class="canteen-list-telesdeclarations ma-cantine--unstyled-list">
+    <li v-for="teledeclaration in teledeclarations" :key="teledeclaration.year" class="canteen-list-telesdeclarations__item fr-grid-row fr-grid-row--gutters fr-grid-row--top fr-pb-1w fr-my-1w">
       <div class="fr-col-12 fr-col-md-3">
         <p class="fr-mb-0 fr-text--bold">Ma télédéclaration {{ teledeclaration.year }}</p>
       </div>
-      <div class="teledeclarations-liste__justificatif fr-col-12 fr-col-md-9">
+      <div class="canteen-list-telesdeclarations__justificatif fr-col-12 fr-col-md-9">
         <a v-if="teledeclaration.url" :href="teledeclaration.url" target="_self" download class="fr-text-title--blue-france">
           <span class="fr-icon-file-download-fill ma-cantine--icon-xs" aria-hidden="true"></span>
           Télécharger mon justificatif
@@ -79,7 +79,7 @@ const teledeclarations = computed(() => {
 </template>
 
 <style lang="scss">
-.teledeclarations-liste {
+.canteen-list-telesdeclarations {
   &__item {
     border-bottom: solid 1px var(--border-disabled-grey);
   }
--- 2024-frontend/src/views/GestionnaireCantineTeledeclarationsListe.vue
+++ 2024-frontend/src/views/GestionnaireCantineTeledeclarationsListe.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRoute } from "vue-router"
 import LayoutSidebarCanteen from "@/layouts/LayoutSidebarCanteen.vue"
-import TeledeclarationsListe from "@/components/TeledeclarationsListe.vue"
+import CanteenListTeledeclarations from "@/components/CanteenListTeledeclarations.vue"
 
 const route = useRoute()
 </script>
@@ -12,7 +12,7 @@ const route = useRoute()
       <h2 class="fr-h3 fr-mb-0">{{ route.meta.title }}</h2>
     </template>
     <template #content="{ canteenInformation }">
-      <TeledeclarationsListe class="fr-mt-n4w" :diagnostics="canteenInformation?.diagnostics" :groupeDiagnostics="canteenInformation?.centralKitchenDiagnostics" :canteenId="canteenInformation?.id" />
+      <CanteenListTeledeclarations class="fr-mt-n4w" :diagnostics="canteenInformation?.diagnostics" :groupeDiagnostics="canteenInformation?.centralKitchenDiagnostics" :canteenId="canteenInformation?.id" />
     </template>
   </LayoutSidebarCanteen>
 </template>
