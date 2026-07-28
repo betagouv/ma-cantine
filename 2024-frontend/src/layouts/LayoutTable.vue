58c74755127b345814184bde2dbae78ac35e9b71
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-07-28 15:47:02 +0200

    Fix css

--- 2024-frontend/src/layouts/LayoutTable.vue
+++ 2024-frontend/src/layouts/LayoutTable.vue
@@ -14,22 +14,13 @@
     overflow: scroll;
   }
 
-  th:nth-child(1) {
-    width: 35% !important;
-  }
-
-  td:nth-child(1),
-  td:nth-child(4) {
-    white-space: pre-wrap !important;
-  }
-
   .fr-table__container {
     overflow: initial !important;
   }
 
   .fr-table {
-    min-width: 55rem;
-    width: 100%;
+    min-width: 55rem !important;
+    width: 100% !important;
   }
 
   .fr-select {
