4d354f22b48b26c629f83e54cffc55d9884fba6e
Charline <charline.laporte@beta.gouv.fr>
2026-07-29 11:32:03 +0200

    improve(Établissement): utilise les enpoints dédiés pour mettre à jour les images de la cantine (#6954)

--- 2024-frontend/src/components/CanteenFormImages.vue
+++ 2024-frontend/src/components/CanteenFormImages.vue
@@ -1,57 +1,60 @@
 <script setup>
-import { computed, ref, watch } from "vue"
+import { ref, onMounted } from "vue"
 import { toBase64 } from "@/utils.js"
 import { useRootStore } from "@/stores/root"
 import canteenService from "@/services/canteens.js"
 import AppFormImage from "@/components/AppFormImage.vue"
 
-const props = defineProps(["canteenId", "images"])
+const props = defineProps(["canteenId"])
 const store = useRootStore()
-const displayImages = ref(props.images || [])
 const isSaving = ref(false)
-const maxImages = 3
-const canAddImage = computed(() => displayImages.value.length < maxImages)
+const canteenImages = ref([])
 
-/* Images change */
-watch(() => props.images, (newImages) => {
-  displayImages.value = newImages || []
-})
+/* Display images */
+const setImages = async () => {
+  const response = await canteenService.fetchCanteenImages(props.canteenId)
+  canteenImages.value = response
+}
+onMounted(setImages)
 
 /* Actions */
-const saveImage = async (file) => {
-  if (!file || !canAddImage.value) return
+const addImage = async (file) => {
+  if (!file) return
   const base64 = await toBase64(file)
-  await updateImages([...displayImages.value, { image: base64 }])
-}
-
-const deleteImage = (imageToDelete) => {
-  const nextImages = displayImages.value.filter((image) => image.image !== imageToDelete.image)
-  updateImages(nextImages)
-}
-
-const saveAlt = (imageToSave, altText) => {
-  const imageIndex = displayImages.value.findIndex((image) => image.image === imageToSave.image)
-  displayImages.value[imageIndex].altText = altText
-  updateImages(displayImages.value)
-}
-
-const updateImages = async (value) => {
   isSaving.value = true
-  try {
-    const response = await canteenService.updateCanteen({ images: value }, props.canteenId)
-    if (!response?.id) store.notifyServerError(response)
-    else successImages(response)
-  } catch (error) {
-    store.notifyServerError(error)
-  } finally {
-    isSaving.value = false
-  }
+  canteenService.addCanteenImage(props.canteenId, base64)
+    .then((response) => {
+      if (!response?.id) store.notifyServerError(response)
+      else successImages("L'image a été ajoutée")
+    })
+    .catch((error) => store.notifyServerError(error))
+    .finally(() => { isSaving.value = false })
 }
 
-const successImages = (response) => {
-  displayImages.value = response.images || []
+const deleteImage = (imageId) => {
+  isSaving.value = true
+  canteenService.deleteCanteenImage(props.canteenId, imageId)
+    .then(() => successImages("L'image a été supprimée"))
+    .catch((error) => store.notifyServerError(error))
+    .finally(() => { isSaving.value = false })
+}
+
+const updateAltImage = (imageId, altText) => {
+  isSaving.value = true
+  canteenService.updateCanteenImage(props.canteenId, imageId, { altText })
+    .then((response) => {
+      if (!response?.id) store.notifyServerError(response)
+      else successImages("La description de l'image a été mise à jour")
+    })
+    .catch((error) => store.notifyServerError(error))
+    .finally(() => { isSaving.value = false })
+}
+
+const successImages = (message) => {
+  setImages()
+  store.removeNotifications()
   store.notify({
-    title: "L'image a été mise à jour",
+    title: message,
     status: "success",
   })
 }
@@ -60,25 +63,22 @@ const successImages = (response) => {
 <template>
   <li class="canteen-form-images">
     <h4 class="fr-h6 fr-mb-1w">Images de l'établissement</h4>
-    <p class="fr-text--sm fr-mb-2w">Vous pouvez ajouter jusqu'à 3 images.</p>
-
     <div class="fr-grid-row fr-grid-row--gutters">
       <AppFormImage
-        v-for="image in displayImages"
-        :key="image.image"
+        v-for="image in canteenImages"
+        :key="image.id"
         :src="image.image"
         :alt="image.altText"
         :disabled="isSaving"
         :show-alt="true"
-        @delete="deleteImage(image)"
-        @save-alt="saveAlt(image, $event)"
+        @delete="deleteImage(image.id)"
+        @save-alt="updateAltImage(image.id, $event)"
         class="fr-col-12 fr-col-md-4"
       />
       <AppFormImage
-        v-if="canAddImage"
         :disabled="isSaving"
         class="fr-col-12 fr-col-md-4"
-        @save-file="saveImage"
+        @save-file="addImage"
       />
     </div>
   </li>
