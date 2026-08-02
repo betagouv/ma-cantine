f045417f4de1ac74dfb616f0c03ca0e466dbb8ea
Raphael Odini <raphodn@users.noreply.github.com>
2026-08-02 22:16:24 +0200

    improve(Bilans): API: nouvel endpoint pour récupérer la liste de ses bilans

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -24,7 +24,11 @@ from .canteen_groupe import (  # noqa: F401
     CanteenGroupeSatellitesListView,
     CanteenGroupeSatelliteUnlinkView,
 )  # noqa: F401
-from .canteen_images import UserCanteenLogoView, UserCanteenImagesListView, UserCanteenImagesRetrieveUpdateDestroyView  # noqa: F401
+from .canteen_images import (  # noqa: F401
+    UserCanteenImagesListView,
+    UserCanteenImagesRetrieveUpdateDestroyView,
+    UserCanteenLogoView,
+)
 from .canteen_managers import (  # noqa: F401
     AddManagerView,
     ClaimCanteenView,
@@ -38,7 +42,7 @@ from .canteen_managers_import import CanteensManagersImportView  # noqa: F401
 from .canteen_update_import import CanteensUpdateImportView  # noqa: F401
 from .communityevent import CommunityEventsView  # noqa: F401
 from .diagnostic import (  # noqa: F401
-    DiagnosticCreateView,
+    DiagnosticListCreateView,
     DiagnosticsToTeledeclareListView,
     DiagnosticUpdateView,
     EmailDiagnosticImportFileView,
