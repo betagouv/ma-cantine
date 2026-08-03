7c0eed61fcc1f8ceeaa4b80a168c759ba854d41a
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-03 17:19:25 +0200

    Ajoute le nouvel endpoint

--- 2024-frontend/src/services/diagnostics.js
+++ 2024-frontend/src/services/diagnostics.js
@@ -1,3 +1,17 @@
+import { verifyResponse } from "@/services/api.js"
+
+const fetchDiagnosticsRecap = (canteenId) => {
+  return fetch(`/api/v1/canteens/${canteenId}/diagnostics/recap`, {
+    method: "GET",
+    headers: {
+      "X-CSRFToken": window.CSRF_TOKEN || "",
+    },
+  })
+    .then(verifyResponse)
+    .then((response) => response)
+    .catch((e) => e)
+}
+
 const toCompleteActions = [
   "10_add_satellites",
   "35_fill_canteen_data",
@@ -76,4 +90,4 @@ const getCompleteButton = (action) => {
   }
 }
 
-export default { getBadge, getTeledeclareButton, getCompleteButton }
+export default { fetchDiagnosticsRecap, getBadge, getTeledeclareButton, getCompleteButton }
