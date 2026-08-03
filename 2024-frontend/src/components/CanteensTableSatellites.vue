cb8c15ef443cb1d72432ee4c07c63df51b283d48
Charline <charline.laporte@beta.gouv.fr>
2026-08-03 19:03:09 +0200

    improve(Tableau): rend l'affichage de la pagination dynamique (#6950)

--- 2024-frontend/src/components/CanteensTableSatellites.vue
+++ 2024-frontend/src/components/CanteensTableSatellites.vue
@@ -10,13 +10,14 @@ import canteensTableService from "@/services/canteensTable.js"
 import urlService from "@/services/urls.js"
 import AppDropdownMenu from "@/components/AppDropdownMenu.vue"
 import AppRawHTML from "@/components/AppRawHTML.vue"
-import LayoutBigTable from "@/layouts/LayoutBigTable.vue"
 
 /* Settings */
+const minPagination = 50
 const props = defineProps(["satellites", "groupe"])
 const emit = defineEmits(["showModalRemoveSatellite", "updateSatellites"])
 const lastYear = new Date().getFullYear() - 1
 const store = useRootStore()
+const showPagination = computed(() => props.satellites.length > minPagination)
 
 /* Campaign */
 const campaign = computedAsync(async () => {
@@ -149,63 +150,45 @@ const claimCanteen = (canteen) => {
 </script>
 
 <template>
-  <LayoutBigTable>
-    <DsfrDataTable
-      title="Vos restaurants satellites"
-      no-caption
-      :headers-row="tableHeaders"
-      :rows="tableRows"
-      :sortable-rows="['name', 'diagnostic']"
-      :pagination="true"
-      :pagination-options="[50, 100, 200]"
-      :rows-per-page="50"
-      pagination-wrapper-class="canteens-table-satellites__pagination fr-mt-4w"
-      class="canteens-table-satellites"
-    >
-      <template #header="{ label }">
-        <AppRawHTML :html="label" />
+  <DsfrDataTable
+    title="Vos restaurants satellites"
+    no-caption
+    :headers-row="tableHeaders"
+    :rows="tableRows"
+    :sortable-rows="['name', 'diagnostic']"
+    :pagination="showPagination"
+    :pagination-options="[minPagination, 100, 200]"
+    :rows-per-page="minPagination"
+    pagination-wrapper-class="ma-cantine--table-pagination fr-mt-4w"
+  >
+    <template #header="{ label }">
+      <AppRawHTML :html="label" />
+    </template>
+    <template #cell="{ colKey, cell }">
+      <template v-if="colKey === 'name'">
+        <p class="fr-text-title--blue-france fr-text--bold">
+          <router-link
+            v-if="cell.isManagedByUser"
+            :to="{ name: 'GestionnaireCantine', params: { canteenUrlComponent: cell.url } }"
+          >
+            {{ cell.canteen }}
+          </router-link>
+          <span v-else>
+            {{ cell.canteen }}
+          </span>
+        </p>
       </template>
-      <template #cell="{ colKey, cell }">
-        <template v-if="colKey === 'name'">
-          <p class="fr-text-title--blue-france fr-text--bold">
-            <router-link
-              v-if="cell.isManagedByUser"
-              :to="{ name: 'GestionnaireCantine', params: { canteenUrlComponent: cell.url } }"
-            >
-              {{ cell.canteen }}
-            </router-link>
-            <span v-else>
-              {{ cell.canteen }}
-            </span>
-          </p>
-        </template>
-        <template v-else-if="colKey === 'diagnostic'">
-          <DsfrBadge small :label="cell.label" :type="cell.type" no-icon />
-        </template>
-        <template v-else-if="colKey === 'actions'">
-          <div class="fr-grid-row fr-grid-row--right">
-            <AppDropdownMenu label="Actions" :links="cell.links" size="small" @click="(event) => clickAction(event, cell.canteen)" />
-          </div>
-        </template>
-        <template v-else>
-          {{ cell }}
-        </template>
+      <template v-else-if="colKey === 'diagnostic'">
+        <DsfrBadge small :label="cell.label" :type="cell.type" no-icon />
       </template>
-    </DsfrDataTable>
-  </LayoutBigTable>
+      <template v-else-if="colKey === 'actions'">
+        <div class="fr-grid-row fr-grid-row--right">
+          <AppDropdownMenu label="Actions" :links="cell.links" size="small" @click="(event) => clickAction(event, cell.canteen)" />
+        </div>
+      </template>
+      <template v-else>
+        {{ cell }}
+      </template>
+    </template>
+  </DsfrDataTable>
 </template>
-
-<style lang="scss">
-.canteens-table-satellites {
-
-  &__pagination {
-    .fr-select {
-      max-width: 7rem !important;
-    }
-  }
-
-  .fr-table__container {
-    overflow: initial !important;
-  }
-}
-</style>
