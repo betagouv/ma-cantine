d1b3e1492cbe7e3406427113d4708d6177e46cdb
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-14 16:28:12 +0200

    Mise à jour du texte d'introduction des gestionnaires

--- 2024-frontend/src/views/GestionnaireCantineGestionnaires.vue
+++ 2024-frontend/src/views/GestionnaireCantineGestionnaires.vue
@@ -35,9 +35,11 @@ const closeRemoveModal = () => {
       @click="addModalOpened = true"
     />
   </CanteenSidebarTitle>
-  <p>
-    Tous les gestionnaires peuvent modifier et supprimer une cantine, ainsi qu'ajouter et enlever des autres
-    gestionnaires.
+  <p v-if="canteenInformations.isGroupe">
+    Tous les gestionnaires du groupe peuvent : modifier les informations du groupe, archiver le groupe, rattacher ou retirer des cantines du groupe, ajouter des achats au fil de l’eau (via l’outil de suivi des achats), compléter et télédéclarer le bilan groupé de l’année n-1 (uniquement lors de la campagne de télédéclaration de l’année n), ajouter ou retirer des gestionnaires, consulter et télécharger les justificatifs de télédéclaration.
+  </p>
+  <p v-else>
+    Tous les gestionnaires de la cantine peuvent : modifier les informations de la cantine, archiver la cantine, ajouter des achats au fil de l’eau (via l’outil de suivi des achats), compléter et télédéclarer le bilan de l’année précédente (uniquement lors de la campagne de télédéclaration), ajouter ou retirer des gestionnaires, consulter et télécharger les justificatifs de télédéclaration.
   </p>
   <CanteenTableManagers
     v-if="canteenInformations.id"
