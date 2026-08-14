45710ea319434875bcd33a1988f817da000dee1f
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-14 16:35:02 +0200

    Ré-organisation de la page publique

--- 2024-frontend/src/views/GestionnaireCantinePagePublique.vue
+++ 2024-frontend/src/views/GestionnaireCantinePagePublique.vue
@@ -31,52 +31,57 @@ const goToPrintPage = () => {
   <div class="fr-mb-5w">
     <div class="ma-cantine--flex-between ma-cantine--flex-gap-1 fr-mb-4w">
       <h3 class="fr-h5 fr-mb-0">L’obligation d’informer vos convives</h3>
-      <DsfrButton
-        label="Imprimer mon affiche"
-        icon="fr-icon-printer-line"
-        @click="goToPrintPage"
-      />
     </div>
     <p>
-      Conformément à l'article L.230-5-1 du Code rural et de la pêche maritime, <strong>les gestionnaires de restauration
-      collective ont l'obligation d'informer les convives,</strong> au moins une fois par an, sur la part des produits
-      durables et de qualité entrant dans la composition des repas. Cette information doit être diffusée par voie
-      d'affichage, de manière visible et lisible, ainsi que par communication électronique.
+      Conformément à l'article L.230-5-1 du Code rural et de la pêche maritime,
+      <strong>les gestionnaires de restauration collective ont l'obligation d'informer les convives,</strong>
+      au moins une fois par an, sur la part des produits durables et de qualité entrant dans la composition des repas.
+      Cette information doit être diffusée par voie d'affichage, de manière visible et lisible, ainsi que par
+      communication électronique.
     </p>
     <p>
-      Retrouvez et partagez vos résultats sur votre <strong>page publique</strong> pour valoriser vos initiatives auprès des
-      convives et des acteurs de votre territoire. Vous pouvez <strong>imprimer votre affiche</strong> directement à partir des
-      informations de votre page publique, pour une communication visible et accessible sur place.
+      Retrouvez et partagez vos résultats sur votre
+      <strong>page publique</strong>
+      pour valoriser vos initiatives auprès des convives et des acteurs de votre territoire. Vous pouvez
+      <strong>imprimer votre affiche</strong>
+      directement à partir des informations de votre page publique, pour une communication visible et accessible sur
+      place.
     </p>
   </div>
 
-  <AppSeparator class="fr-mt-3w fr-mb-5w" />
+  <AppSeparator class="fr-my-5w" />
 
   <div>
     <div class="ma-cantine--flex-between ma-cantine--flex-gap-1 fr-mb-4w">
-      <h3 class="fr-h5 fr-mb-0">Partagez votre page publique</h3>
-      <DsfrButton
-        label="Voir ma page en ligne"
-        icon="ri-global-line"
-        @click="goToOnlinePage(canteenInformations)"
-      />
+      <h3 class="fr-h5 fr-mb-0">Votre page publique</h3>
+      <DsfrButton label="Voir ma page en ligne" icon="ri-global-line" @click="goToOnlinePage(canteenInformations)" />
     </div>
     <p>
-      <strong>Personnalisez votre espace public</strong> et donnez de la visibilité à vos actions auprès des convives, des
-      collectivités et du territoire, dans un espace dédié aux initiatives durables et inspirantes.
+      <strong>Personnalisez votre espace public</strong>
+      et donnez de la visibilité à vos actions auprès des convives, des collectivités et du territoire, dans un espace
+      dédié aux initiatives durables et inspirantes.
     </p>
 
     <ol class="ma-cantine--ordered-list ma-cantine--unstyled-list">
-      <CanteenFormLogo
-        v-if="canteenInformations.id"
-        :canteen-id="canteenInformations.id"
-        class="fr-mb-3w"
-      />
-      <CanteenFormImages
-        v-if="canteenInformations.id"
-        :canteen-id="canteenInformations.id"
-        class="fr-mb-3w"
-      />
+      <CanteenFormLogo v-if="canteenInformations.id" :canteen-id="canteenInformations.id" class="fr-mb-3w" />
+      <CanteenFormImages v-if="canteenInformations.id" :canteen-id="canteenInformations.id" class="fr-mb-3w" />
     </ol>
+  </div>
+
+  <AppSeparator class="fr-my-5w" />
+
+  <div class="fr-mb-5w">
+    <div class="ma-cantine--flex-between ma-cantine--flex-gap-1 fr-mb-4w">
+      <h3 class="fr-h5 fr-mb-0">Générer et imprimer votre affiche</h3>
+      <DsfrButton label="Imprimer mon affiche" icon="fr-icon-printer-line" @click="goToPrintPage" />
+    </div>
+    <p>
+      Vous pourrez générer automatiquement
+      <strong>un PDF à afficher ou à envoyer par mail</strong>
+      à vos convives pour les informer sur la part de produits de qualité et durables et de la part des produits issus
+      de projets alimentaires territoriaux entrant dans la composition des repas servis. Les informations, officielles,
+      <strong>sont issues de votre dernière télédéclaration</strong>
+      et des informations complémentaires renseignées sur votre page publique.
+    </p>
   </div>
 </template>
