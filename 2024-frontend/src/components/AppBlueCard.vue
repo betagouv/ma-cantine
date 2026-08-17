8b0650c2bb5ce138966e43774dc849f88ae83570
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-17 11:41:53 +0200

    Crée un composant de carte

---
+++ 2024-frontend/src/components/AppBlueCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router'
+defineProps(['title', 'button', 'alert'])
+
+const router = useRouter()
+const goTo = (to) => {
+  router.push({ name: to })
+}
+</script>
+
+<template>
+  <div class="app-blue-card fr-background-alt--blue-france fr-p-4w">
+    <p class="fr-h6">{{ title }}</p>
+    <DsfrAlert :description="alert" small class="fr-mb-2w" />
+    <slot />
+    <DsfrButton :label="button.label" @click="goTo(button.to)" secondary />
+  </div>
+</template>
