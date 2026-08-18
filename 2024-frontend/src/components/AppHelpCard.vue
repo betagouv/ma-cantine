c3a1082132b1bb11df20232f8aed2d74a20d1893
Charline <charline.laporte@beta.gouv.fr>
2026-08-18 16:49:03 +0200

    feat(Établissement): ajout de la page "Télédéclaration de l'année" (#7020)

---
+++ 2024-frontend/src/components/AppHelpCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps(["title", "content"])
+</script>
+
+<template>
+  <div class="app-help-card fr-card fr-p-7v">
+    <div class="app-help-card__content">
+      <span class="fr-icon-question-fill fr-text-title--blue-france" aria-hidden="true"></span>
+      <div>
+        <p class="ma-cantine--bold fr-mb-1w fr-text--lg">
+          {{ title }}
+        </p>
+        <p>{{ content }}</p>
+        <div>
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.app-help-card {
+
+  &__content {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    align-items: baseline;
+  }
+}
+</style>
