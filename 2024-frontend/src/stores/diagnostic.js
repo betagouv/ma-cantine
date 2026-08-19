18cf46e10077f74a3aa4704799d53d6aac69a363
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-19 12:35:36 +0200

    Créé un store pour stocker les bilans de la cantine

---
+++ 2024-frontend/src/stores/diagnostic.js
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia"
+import { computed, ref } from "vue"
+import diagnosticService from "@/services/diagnostics.js"
+
+const useStoreDiagnostic = defineStore("diagnostic", () => {
+  const diagnostics = ref({})
+  const lastYear = new Date().getFullYear() - 1
+
+  /* Diagnostic of the current campaign */
+  const diagnosticCurrentCampaign = computed(() => diagnostics.value[lastYear])
+
+  /* Init store with diagnostics of all the years */
+  async function initStore(canteenId) {
+    const response = await diagnosticService.fetchDiagnostics(canteenId)
+    for (let i = 0; i < response["results"].length; i++) {
+      const resultYear = response["results"][i].year
+      diagnostics.value[resultYear] = response["results"][i]
+    }
+  }
+
+  /* Empty store */
+  function deleteStore() {
+    diagnostics.value = {}
+  }
+
+  /* Check if has diagnostic for the current campaign */
+  function hasDiagnosticCurrentCampaign() {
+    return diagnosticCurrentCampaign.value !== undefined
+  }
+
+  return {
+    diagnostics,
+    diagnosticCurrentCampaign,
+    initStore,
+    deleteStore,
+    hasDiagnosticCurrentCampaign,
+  }
+})
+
+export { useStoreDiagnostic }
