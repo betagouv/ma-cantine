11ebcae33d76524854d4d21812e3243329903cd3
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-19 12:14:01 +0200

    Ajoute la fonction pour créer un bilan depuis le nouveau frontend

--- 2024-frontend/src/services/diagnostics.js
+++ 2024-frontend/src/services/diagnostics.js
@@ -1,5 +1,20 @@
 import { verifyResponse } from "@/services/api.js"
 
+const createDiagnostic = (canteenId, payload) => {
+  payload["creationSource"] = "APP"
+  return fetch(`/api/v1/canteens/${canteenId}/diagnostics/`, {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": window.CSRF_TOKEN || "",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(verifyResponse)
+    .then((response) => response)
+    .catch((e) => e)
+}
+
 const fetchDiagnosticsRecap = (canteenId) => {
   return fetch(`/api/v1/canteens/${canteenId}/diagnostics/recap`, {
     method: "GET",
@@ -12,4 +27,4 @@ const fetchDiagnosticsRecap = (canteenId) => {
     .catch((e) => e)
 }
 
-export default { fetchDiagnosticsRecap }
+export default { createDiagnostic, fetchDiagnosticsRecap }
