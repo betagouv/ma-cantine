893ce5f4d464998d55fa5ce34907904c1db95810
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-26 17:03:22 +0200

    Enlève le selecteur radio des composants générique du tunnel car il a trop de code personnalisé

--- 2024-frontend/src/components/TunnelTeledeclarationField.vue
+++ 2024-frontend/src/components/TunnelTeledeclarationField.vue
@@ -3,44 +3,23 @@ import { ref, computed, onMounted } from "vue"
 import { useStoreDiagnostic } from "@/stores/diagnostic"
 import diagnosticsFieldsService from "@/services/diagnosticsFields"
 
-const props = defineProps(["name", "customSelectOptions"])
+const props = defineProps(["name"])
 const storeDiagnostic = useStoreDiagnostic()
 const data = computed(() => diagnosticsFieldsService.getField(props.name))
 const field = ref()
 
 /* Informations */
-const type = computed(() => data.value.type)
+const isNumber = computed(() => data.value.type === "number")
 const isRequired = computed(() => data.value.required)
 const label = computed(() => data.value.label)
-const options = computed(() => props.customSelectOptions || data.value.options)
 const errorMessage = computed(() => diagnosticsFieldsService.getFieldError(props.name, storeDiagnostic.diagnosticCurrentCampaignErrors))
 
 /* Actions */
-const fieldChange = () => { storeDiagnostic.setDiagnosticCurrentCampaign(props.name, field.value) }
-const prefillField = () => { field.value = storeDiagnostic.diagnosticCurrentCampaign[props.name] }
+const fieldChange = () =>  storeDiagnostic.setDiagnosticCurrentCampaign(props.name, field.value)
+const prefillField = () => field.value = storeDiagnostic.diagnosticCurrentCampaign[props.name]
 onMounted(prefillField)
 </script>
 <template>
-  <DsfrInputGroup
-    v-if="type === 'number'"
-    type="number"
-    v-model="field"
-    :label="label"
-    :label-visible="true"
-    :name="props.name"
-    :required="isRequired"
-    @change="fieldChange"
-    :error-message="errorMessage"
-  />
-  <DsfrRadioButtonSet
-    v-else-if="type === 'radioRiche'"
-    v-model="field"
-    :small="true"
-    :required="isRequired"
-    :legend="label"
-    :options="options"
-    @change="fieldChange"
-    :error-message="errorMessage"
-  />
-  <pre v-else>{{ data }}</pre>
+  <DsfrInputGroup v-if="isNumber" v-model="field" :label="label" :label-visible="true" :name="props.name" type="number" :required="isRequired" @change="fieldChange" :error-message="errorMessage" />
+  <pre v-else>{{ field }}</pre>
 </template>
