73b35f7cccd1096f0f38021edf919f9795aa880c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-26 11:40:36 +0200

    Ajoute les liens externes pour le router link

--- 2024-frontend/src/components/AppLinkRouter.vue
+++ 2024-frontend/src/components/AppLinkRouter.vue
@@ -1,11 +1,14 @@
 <script setup>
-defineProps(["to", "title", "icon"])
+import { computed } from "vue"
+const props = defineProps(["to", "title", "icon", "target"])
+const displayArrowIcon = computed(() => !props.icon && props.target !== '_blank')
+const displayIcon = computed(() => props.icon !== undefined && props.icon !== null)
 </script>
 
 <template>
-  <router-link :to="to">
-    <span v-if="icon" :class="`${icon} ma-cantine--icon-xs`" aria-hidden="true"></span>
+  <router-link :to="props.to" :target="props.target || '_self'">
+    <span v-if="displayIcon" :class="`${props.icon} ma-cantine--icon-xs`" aria-hidden="true"></span>
     {{ title }}
-    <span v-if="!icon" class="fr-icon-arrow-right-line ma-cantine--icon-xs" aria-hidden="true"></span>
+    <span v-if="displayArrowIcon" class="fr-icon-arrow-right-line ma-cantine--icon-xs" aria-hidden="true"></span>
   </router-link>
 </template>
