86659eb357496c473983a28dd5f6e35a25d7c048
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-27 08:57:33 +0200

    Ajout du champ d'aide

--- 2024-frontend/src/views/GestionnaireTunnelEgalim.vue
+++ 2024-frontend/src/views/GestionnaireTunnelEgalim.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { computed } from "vue"
+import { useStoreDiagnostic } from "@/stores/diagnostic"
+import { storeToRefs } from "pinia"
+import documentation from "@/data/documentation.json"
+import AppHelpCard from "@/components/AppHelpCard.vue"
+
+const storeDiagnostic = useStoreDiagnostic()
+const { diagnosticCurrentCampaign } = storeToRefs(storeDiagnostic)
+const diagIsSimple = computed(() => diagnosticCurrentCampaign.value.diagnosticType === "SIMPLE")
+</script>
 <template>
-  <h2>Tunnel EGalim</h2>
+  <h2 class="fr-h6">1 - Total des approvisionnements toutes familles de produits confondus :</h2>
+  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top">
+    <div class="fr-col-12 fr-col-md-7">
+
+    </div>
+    <div class="fr-col-12 fr-col-md-5">
+      <AppHelpCard title="Comment comptabiliser les produits ayant plusieurs labels ?">
+        <a :href="documentation.teledeclaration" target="_blank">Je consulte l'aide</a>
+      </AppHelpCard>
+    </div>
+  </div>
+  <pre>{{ diagIsSimple }}</pre>
 </template>
