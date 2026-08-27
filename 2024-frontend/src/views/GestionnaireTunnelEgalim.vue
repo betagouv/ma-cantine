e79ef9f6794c1723a33b0b061c1efb1a7b4a47a6
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-27 18:53:57 +0200

    Ajoute les champs de la TD simplifiée EGalim

--- 2024-frontend/src/views/GestionnaireTunnelEgalim.vue
+++ 2024-frontend/src/views/GestionnaireTunnelEgalim.vue
@@ -5,14 +5,15 @@ import { storeToRefs } from "pinia"
 import documentation from "@/data/documentation.json"
 import AppHelpCard from "@/components/AppHelpCard.vue"
 import TunnelTeledeclarationField from "@/components/TunnelTeledeclarationField.vue"
+import DiagnosticEgalimSimple from "@/components/DiagnosticEgalimSimple.vue"
 
 const storeDiagnostic = useStoreDiagnostic()
 const { diagnosticCurrentCampaign } = storeToRefs(storeDiagnostic)
 const diagIsSimple = computed(() => diagnosticCurrentCampaign.value.diagnosticType === "SIMPLE")
 </script>
 <template>
-  <h2 class="fr-h6">1 - Total des approvisionnements toutes familles de produits confondus :</h2>
-  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top">
+  <h2 class="fr-h6">1. Total des approvisionnements toutes familles de produits confondus :</h2>
+  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top fr-mb-4w">
     <div class="fr-col-12 fr-col-md-7">
       <TunnelTeledeclarationField name="valeurTotale" />
     </div>
@@ -22,6 +23,6 @@ const diagIsSimple = computed(() => diagnosticCurrentCampaign.value.diagnosticTy
       </AppHelpCard>
     </div>
   </div>
+  <DiagnosticEgalimSimple v-if="diagIsSimple" />
   <pre>{{ diagnosticCurrentCampaign }}</pre>
-  <pre>{{ diagIsSimple }}</pre>
 </template>
