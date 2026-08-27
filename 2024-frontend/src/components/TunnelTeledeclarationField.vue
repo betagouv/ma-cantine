798a2014f1beaeb7fe4198d82ff0cd8b2eb9645e
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-27 10:07:50 +0200

    Ajoute en description le cout détecté

--- 2024-frontend/src/components/TunnelTeledeclarationField.vue
+++ 2024-frontend/src/components/TunnelTeledeclarationField.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { ref, computed, onMounted } from "vue"
 import { useStoreDiagnostic } from "@/stores/diagnostic"
+import { useStorePurchaseSummary } from "@/stores/purchaseSummary"
+import { storeToRefs } from "pinia"
 import diagnosticsFieldsService from "@/services/diagnosticsFields"
 
 const props = defineProps(["name"])
 const storeDiagnostic = useStoreDiagnostic()
+const storePurchaseSummary = useStorePurchaseSummary()
+const { diagnosticCurrentCampaign } = storeToRefs(storeDiagnostic)
+const { purchaseSummary } = storeToRefs(storePurchaseSummary)
 const data = computed(() => diagnosticsFieldsService.getField(props.name))
 const field = ref()
 
@@ -13,6 +18,10 @@ const isNumber = computed(() => data.value.type === "number")
 const isRequired = computed(() => data.value.required)
 const label = computed(() => data.value.label)
 const errorMessage = computed(() => diagnosticsFieldsService.getFieldError(props.name, storeDiagnostic.diagnosticCurrentCampaignErrors))
+const hint = computed(() => {
+  const enablePurchaseSummary = data.value.enablePurchaseSummary
+  return enablePurchaseSummary ? `Vous avez ${purchaseSummary.value[diagnosticCurrentCampaign.value.year][props.name]}€ renseignés dans l'Outil de Suivi des Achats` : data.value.hint
+})
 
 /* Actions */
 const fieldChange = () =>  storeDiagnostic.setDiagnosticCurrentCampaign(props.name, field.value)
@@ -20,6 +29,6 @@ const prefillField = () => field.value = storeDiagnostic.diagnosticCurrentCampai
 onMounted(prefillField)
 </script>
 <template>
-  <DsfrInputGroup v-if="isNumber" v-model="field" :label="label" :label-visible="true" :name="props.name" type="number" :required="isRequired" @change="fieldChange" :error-message="errorMessage" />
+  <DsfrInputGroup v-if="isNumber" v-model="field" :label="label" :label-visible="true" :name="props.name" type="number" :required="isRequired" @change="fieldChange" :error-message="errorMessage" :hint="hint" />
   <pre v-else>{{ field }}</pre>
 </template>
