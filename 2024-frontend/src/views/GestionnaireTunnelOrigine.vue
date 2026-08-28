cc3d9b527cb820a86c51eba7838ef833bee16e92
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-29 00:39:24 +0200

    Fix typo

--- 2024-frontend/src/views/GestionnaireTunnelOrigine.vue
+++ 2024-frontend/src/views/GestionnaireTunnelOrigine.vue
@@ -10,7 +10,7 @@ import documentation from '@/data/documentation.json'
       <p>Ces données ne sont pas obligatoires pour valider votre déclaration. Ici, vous pouvez affecter le produit dans plusieurs caractéristiques. Par exemple, un produit à la fois biologique et local pourra être comptabilisé dans les deux champs “bio” et “local”.</p>
     </div>
     <div class="fr-col-12 fr-col-md-5">
-      <AppHelpCard title="A propos de l’origine France">
+      <AppHelpCard title="À propos de l’origine France">
         <a :href="documentation.origineFrance" target="_blank">Consultez la documentation</a>
       </AppHelpCard>
     </div>
