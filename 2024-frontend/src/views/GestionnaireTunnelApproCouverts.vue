523995e94123ce2846a81ff167d87cb6f0ad1a3c
Charline <charline.laporte@beta.gouv.fr>
2026-08-31 18:09:05 +0200

    improve(Télédéclaration): réorganise les pages et les étapes du tunnel pour le volet approvisionnement (#7057)

--- 2024-frontend/src/views/GestionnaireTunnelApproCouverts.vue
+++ 2024-frontend/src/views/GestionnaireTunnelApproCouverts.vue
@@ -1,21 +1,14 @@
 <script setup>
-import { computed } from "vue"
-import { useRoute } from "vue-router"
-import diagnosticsFieldsService from "@/services/diagnosticsFields"
 import documentation from "@/data/documentation.json"
 import AppHelpCard from "@/components/AppHelpCard.vue"
 import TunnelTeledeclarationField from "@/components/TunnelTeledeclarationField.vue"
-
-const route = useRoute()
-const pageName = route.name
-const fields = computed(() => diagnosticsFieldsService.getPageFields(pageName))
 </script>
 
 <template>
   <div class="fr-grid-row fr-grid-row--gutters">
     <div class="fr-col-12 fr-col-md-7">
-      <h2>Estimer mon nombre de couverts annuel</h2>
       <p>Ce chiffre doit refléter la réalité des approvisionnements déclarés sur l’année, en tenant compte des repas complets et des repas partiels (petit-déjeuner, collation…).</p>
+      <TunnelTeledeclarationField name="nombreRepasAn" size="full"/>
     </div>
     <div class="fr-col-12 fr-col-md-5">
       <AppHelpCard title="Bien estimer son nombre de couverts" content="Pour toute question sur le calcul du nombre de couverts : ">
@@ -23,5 +16,4 @@ const fields = computed(() => diagnosticsFieldsService.getPageFields(pageName))
       </AppHelpCard>
     </div>
   </div>
-  <TunnelTeledeclarationField v-for="field in fields" :key="field" :name="field" size="half"/>
 </template>
--- 2024-frontend/src/views/GestionnaireTunnelRecapitulatif.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <p>On TD ici ou on affiche le récapitulatif de la TD</p>
-  <RouterLink :to="{name: 'GestionnaireTunnelConvives'}">Aller aux convives</RouterLink>
-</template>
