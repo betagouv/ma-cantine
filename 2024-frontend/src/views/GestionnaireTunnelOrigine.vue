a39fbf3d0ad10c5068de7398a17490680d230027
Charline <charline.laporte@beta.gouv.fr>
2026-08-31 09:18:18 +0200

    feat(Télédéclaration): ajoute l'étape origine (#7055)

---
+++ 2024-frontend/src/views/GestionnaireTunnelOrigine.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AppHelpCard from '@/components/AppHelpCard.vue'
+import documentation from '@/data/documentation.json'
+import TunnelTeledeclarationField from '@/components/TunnelTeledeclarationField.vue'
+
+const fieldsFR = ["valeurFrance", "valeurViandesVolaillesFrance", "valeurProduitsDeLaMerFrance", "valeurFruitsEtLegumesFrance", "valeurCharcuterieFrance", "valeurProduitsLaitiersFrance", "valeurBoulangerieFrance", "valeurBoissonsFrance", "valeurAutresFrance"]
+const fieldsEU = ["valeurEurope", "valeurViandesVolaillesEurope", "valeurProduitsDeLaMerEurope", "valeurFruitsEtLegumesEurope", "valeurCharcuterieEurope", "valeurProduitsLaitiersEurope", "valeurBoulangerieEurope", "valeurBoissonsEurope", "valeurAutresEurope"]
+</script>
+<template>
+  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top fr-mb-4w">
+    <div class="fr-col-12 fr-col-md-7">
+      <h2>Origine France et UE</h2>
+      <p class="fr-mb-0">Ces données ne sont pas obligatoires pour valider votre déclaration. Ici, vous pouvez affecter le produit dans plusieurs caractéristiques. Par exemple, un produit à la fois biologique et local pourra être comptabilisé dans les deux champs “bio” et “local”.</p>
+    </div>
+    <div class="fr-col-12 fr-col-md-5">
+      <AppHelpCard title="À propos de l’origine France">
+        <a :href="documentation.origineFrance" target="_blank">Consultez la documentation</a>
+      </AppHelpCard>
+    </div>
+  </div>
+  <h3 class="fr-h6">1. Valeurs totales des achats origine France</h3>
+  <div class="fr-mb-4w fr-grid-row fr-grid-row--gutters">
+    <TunnelTeledeclarationField v-for="field in fieldsFR" :key="field" :name="field" size="inline" />
+  </div>
+  <h3 class="fr-h6">2. Valeurs totales des achats origine UE (hors France)</h3>
+  <div class="fr-mb-4w fr-grid-row fr-grid-row--gutters">
+    <TunnelTeledeclarationField v-for="field in fieldsEU" :key="field" :name="field" size="inline" />
+  </div>
+</template>
