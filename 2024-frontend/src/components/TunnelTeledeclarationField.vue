2635ae39c911897505efdb4605979e19549bd18c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-08-31 12:44:20 +0200

    Ajoute le type de champ select dans les types

--- 2024-frontend/src/components/TunnelTeledeclarationField.vue
+++ 2024-frontend/src/components/TunnelTeledeclarationField.vue
@@ -18,6 +18,7 @@ const { purchaseSummary } = storeToRefs(storePurchaseSummary)
 const field = ref()
 const data = computed(() => diagnosticsFieldsService.getField(props.name))
 const isNumber = computed(() => data.value?.type === "number")
+const isSelect = computed(() => data.value?.type === "select")
 const isRequired = computed(() => data.value.required)
 const label = computed(() => data.value.label)
 const tooltip = computed(() => data.value.tooltip)
@@ -32,6 +33,7 @@ const img = computed(() => data.value.img)
 const imgAlt = computed(() => data.value.imgAlt)
 const modale = computed(() => data.value.modaleDocLink )
 const opened = ref(false)
+const options = computed(() => data.value.options)
 
 const getPurchaseSummaryHint = (fieldName) => {
   const fieldValue = purchaseSummary.value[diagnosticCurrentCampaign.value.year][fieldName]
@@ -52,10 +54,11 @@ onMounted(prefillField)
 </script>
 <template>
   <div class="fr-grid-row fr-mb-2w" :class="{ 'fr-col-6' : displayInline }">
-    <div class="fr-grid-row" :class="{ 'fr-col-12': displayFull, 'fr-col-7': displayHalf }">
+    <div class="fr-grid-row" :class="{ 'fr-col-12': displayFull || displayInline, 'fr-col-7': displayHalf }">
       <div v-if="isRelated" class="tunnel-teledeclaration-field__related fr-col-1"></div>
       <div class="tunnel-teledeclaration-field__input" :class="{ 'fr-col-11': isRelated, 'fr-col-12': !isRelated }">
         <DsfrInputGroup v-if="isNumber" v-model="field" :label="label" :label-visible="true" :name="props.name" type="number" :required="isRequired" @change="fieldChange" :error-message="errorMessage" :hint="hint" />
+        <DsfrSelect v-if="isSelect" v-model="field" :label="label" :label-visible="true" :name="props.name" :required="isRequired" :options="options" @change="fieldChange" :error-message="errorMessage" :hint="hint" />
       </div>
     </div>
     <div v-if="displayHalf" class="fr-col-5 fr-pl-1v fr-grid-row fr-grid-row--bottom">
