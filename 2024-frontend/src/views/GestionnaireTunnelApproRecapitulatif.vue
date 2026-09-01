bc5431807e527c81469a5ee5e9930d929cbb7730
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-01 11:26:37 +0200

    Ajoute le contenu du bloc de télédéclaration

--- 2024-frontend/src/views/GestionnaireTunnelApproRecapitulatif.vue
+++ 2024-frontend/src/views/GestionnaireTunnelApproRecapitulatif.vue
@@ -1,4 +1,50 @@
+<script setup>
+import { computed } from 'vue'
+import { computedAsync } from '@vueuse/core'
+import { storeToRefs } from 'pinia'
+import { useStoreCanteen } from '@/stores/canteen'
+import canteenService from '@/services/canteens'
+import AppHelpCard from '@/components/AppHelpCard.vue'
+
+/* Stores */
+const canteenStore = useStoreCanteen()
+const { canteenInformations } = storeToRefs(canteenStore)
+
+/* Canteen action */
+const lastYear = new Date().getFullYear() - 1
+const canteenAction = computedAsync(async () => {
+  const actions = await canteenService.fetchCanteensActions(lastYear)
+  const currentCanteen = actions.find(canteen => canteen.id === canteenInformations.value.id)
+  return currentCanteen.action
+})
+
+/* Bloc TD */
+const canTeledeclare = computed(() => canteenAction.value === "40_teledeclare")
+const sentence = computed(() => canTeledeclare.value ? "Je valide ma déclaration et la publication des données sur mon espace vitrine." : "Vous devez corriger votre télédéclaration avant de déclarer.")
+const icon = computed(() => canTeledeclare.value ? "fr-icon-checkbox-circle-fill" : "fr-icon-checkbox-line")
+
+const submitDeclaration = () => {
+  console.log("submitDeclaration")
+}
+</script>
 <template>
-  <h2 class="fr-h6">Votre télédéclaration vous semble t’elle cohérente ?</h2>
-  <p>Toutes vos données d’approvisionnement sont saisies, vous pouvez faire une relecture avant de soumettre votre télédéclaration.</p>
+  <div class="fr-grid-row fr-grid-row--gutters">
+    <div class="fr-col-12 fr-col-md-7">
+      <h2 class="fr-h5">Votre télédéclaration vous semble t’elle cohérente ?</h2>
+      <p>Toutes vos données d’approvisionnement sont saisies, vous pouvez faire une relecture avant de soumettre votre télédéclaration.</p>
+    </div>
+    <div class="fr-col-12 fr-col-md-5">
+      <AppHelpCard
+        :title="sentence"
+        :icon="icon"
+      >
+        <DsfrButton
+          label="Télédéclarer"
+          icon="ri-send-plane-line"
+          :disabled="!canTeledeclare"
+          @click="submitDeclaration"
+        />
+      </AppHelpCard>
+    </div>
+  </div>
 </template>
