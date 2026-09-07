a0a9f87a09906be29421c1b515e944098218b8ae
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-07 17:28:19 +0200

    Refactor

--- 2024-frontend/src/components/AppErrorList.vue
+++ 2024-frontend/src/components/AppErrorList.vue
@@ -1,13 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 
-const props = defineProps({
-  errors: {
-    type: Array,
-    required: true,
-  },
-})
-
+const props = defineProps(["errors"])
 const badge = computed(() => {
   const count = props.errors.length
   const sentence = count > 1 ? 'erreurs détectées' : 'erreur détectée'
@@ -16,14 +10,12 @@ const badge = computed(() => {
 </script>
 
 <template>
-  <div>
-    <DsfrBadge :label="badge" type="error" />
-    <ul>
-      <li v-for="field in errors" :key="field" class="fr-text-default--error">
-        <p class="fr-mb-0">
-          erreur sur le champ <span class="ma-cantine--bold">« {{ field }} »</span>
-        </p>
-      </li>
-    </ul>
-  </div>
+  <DsfrBadge :label="badge" type="error" />
+  <ul>
+    <li v-for="field in errors" :key="field" class="fr-text-default--error">
+      <p class="fr-mb-0">
+        erreur sur le champ <span class="ma-cantine--bold">« {{ field }} »</span>
+      </p>
+    </li>
+  </ul>
 </template>
