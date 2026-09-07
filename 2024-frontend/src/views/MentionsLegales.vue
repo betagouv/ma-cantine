ad32bfa04972aa5e7494fc7f0ccdaa16a4578de2
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-07 12:02:51 +0200

    Supprime la redirection vers la page d'accueil vue2 qui redirige

--- 2024-frontend/src/views/MentionsLegales.vue
+++ 2024-frontend/src/views/MentionsLegales.vue
@@ -8,7 +8,7 @@ import LayoutOneColumn from "@/layouts/LayoutOneColumn.vue"
       <h2>Éditeur de la Plateforme</h2>
       <p>
         La Plateforme
-        <router-link :to="{ name: 'Vue2Home' }">https://ma-cantine.agriculture.gouv.fr/</router-link>
+        <router-link :to="{ name: 'LandingPage' }">https://ma-cantine.agriculture.gouv.fr/</router-link>
         est éditée par la Direction générale de l’alimentation du ministère de l’Agriculture, de l’Agro-alimentaire et
         de la Souveraineté alimentaire.
       </p>
