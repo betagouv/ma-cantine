4578966d44a6f376d82e5520a5ccf4d7c9142b56
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-08 17:02:21 +0200

    Met en avant le bouton pour modifier les informations

--- 2024-frontend/src/views/GestionnaireTunnelApproInformations.vue
+++ 2024-frontend/src/views/GestionnaireTunnelApproInformations.vue
@@ -1,19 +1,34 @@
 <script setup>
+import { computed } from "vue"
 import { useStoreCanteen } from "@/stores/canteen.js"
 import { storeToRefs } from "pinia"
-import { useRoute } from "vue-router"
+import { useRouter, useRoute } from "vue-router"
 import CanteenDisplayInformations from "@/components/CanteenDisplayInformations.vue"
-import AppLinkRouter from "@/components/AppLinkRouter.vue"
 
+const router = useRouter()
 const route = useRoute()
 const canteenStore = useStoreCanteen()
 const { canteenInformations } = storeToRefs(canteenStore)
+
+/* Button */
+const label = computed(() => canteenInformations.isGroupe ? 'Modifier les informations de mon groupe' : 'Modifier mes informations')
+const gotTo = () => {
+  router.push({name: canteenInformations.isGroupe ? 'GestionnaireCantineGroupeModifier' : 'GestionnaireCantineRestaurantModifier', query: { redirection: route.fullPath }})
+}
 </script>
 <template>
-  <p class="fr-mb-4w">
-    <span v-if="canteenInformations.isGroupe">C’est le moment de vérifier les informations de votre établissement et en tant qu’entité de gestion d’un groupe.</span>
-    <span v-else>C’est le moment de vérifier les informations de votre cantine (nom, gestionnaire, effectifs, mode de gestion, etc.).</span>
-    Si vous remarquez des erreurs, corriger-les avant de passer à l'étape suivante <AppLinkRouter title="en cliquant ici" :to="{name: canteenInformations.isGroupe ? 'GestionnaireCantineGroupeModifier' : 'GestionnaireCantineRestaurantModifier', query: { redirection: route.fullPath }}" />
-  </p>
+  <div class="ma-cantine--flex-between ma-cantine--flex-gap-2 ma-cantine--flex-top fr-mb-4w">
+    <p class="fr-mb-0">
+      <span v-if="canteenInformations.isGroupe">C’est le moment de vérifier les informations de votre établissement et en tant qu’entité de gestion d’un groupe.</span>
+      <span v-else>C’est le moment de vérifier les informations de votre cantine (nom, gestionnaire, effectifs, mode de gestion, etc.).</span>
+      Si vous remarquez des erreurs, corriger-les avant de passer à l'étape suivante.
+    </p>
+    <DsfrButton
+      class="ma-cantine--flex-shrink-0"
+      :label="label"
+      icon="fr-icon-edit-line"
+      @click="gotTo"
+    />
+  </div>
   <CanteenDisplayInformations :canteen-information="canteenInformations" :canteen-is-groupe="canteenInformations.isGroupe" />
 </template>
