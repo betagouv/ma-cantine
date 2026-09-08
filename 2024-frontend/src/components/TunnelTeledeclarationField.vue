4d5195f6dc96c63a1fdb23285c5f729299052b75
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-08 16:26:31 +0200

    Met à jour le placeholder

--- 2024-frontend/src/components/TunnelTeledeclarationField.vue
+++ 2024-frontend/src/components/TunnelTeledeclarationField.vue
@@ -23,6 +23,7 @@ const isRequired = computed(() => data.value.required)
 const label = computed(() => data.value.label)
 const tooltip = computed(() => data.value.tooltip)
 const isRelated = computed(() => data.value?.isRelatedField)
+const placeholder = computed(() => data.value?.placeholder)
 const errorMessage = computed(() => diagnosticsFieldsService.getFieldError(props.name, storeDiagnostic.diagnosticCurrentCampaignErrors))
 const hint = computed(() => {
   const enablePurchaseSummary = data.value.enablePurchaseSummary
@@ -57,7 +58,7 @@ onMounted(prefillField)
     <div class="fr-grid-row" :class="{ 'fr-col-12': displayFull || displayInline, 'fr-col-7': displayHalf }">
       <div v-if="isRelated" class="tunnel-teledeclaration-field__related fr-col-1"></div>
       <div class="tunnel-teledeclaration-field__input" :class="{ 'fr-col-11': isRelated, 'fr-col-12': !isRelated }">
-        <DsfrInputGroup v-if="isNumber" v-model="field" :label="label" :label-visible="true" :name="props.name" type="number" :required="isRequired" @change="fieldChange" :error-message="errorMessage" :hint="hint" />
+        <DsfrInputGroup v-if="isNumber" v-model="field" :label="label" :label-visible="true" :name="props.name" type="number" :required="isRequired" @change="fieldChange" :error-message="errorMessage" :hint="hint" :placeholder="placeholder" />
         <DsfrSelect v-if="isSelect" v-model="field" :label="label" :label-visible="true" :name="props.name" :required="isRequired" :options="options" @change="fieldChange" :error-message="errorMessage" :hint="hint" />
       </div>
     </div>
