407d86038e9be9b503d46ec8d3567025aa68a9d9
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-14 10:03:58 +0200

    Supprime la page accueil gestionnaire

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import store from "@/store/index"
 import LandingPage from "@/views/LandingPage"
-import ManagerLanding from "@/views/ManagerLanding"
 import DiagnosticPage from "@/views/DiagnosticPage"
 import KeyMeasuresHome from "@/views/KeyMeasuresPage/KeyMeasuresHome"
 import GeneratePosterPage from "@/views/GeneratePosterPage"
@@ -41,14 +40,6 @@ const routes = [
     path: "/",
     name: "LandingPage",
     component: LandingPage,
-  },
-  {
-    path: "/accueil-gestionnaire",
-    name: "ManagerLanding",
-    component: ManagerLanding,
-    beforeEnter: (_to, _from, next) => {
-      store.state.loggedUser ? next({ name: "GestionnaireTableauDeBord" }) : next()
-    },
   },
   {
     path: "/mon-compte",
@@ -467,6 +458,11 @@ routes.push({
 
 routes.push({
   path: "/accueil",
+  redirect: { name: "LandingPage" },
+})
+
+routes.push({
+  path: "/accueil-gestionnaire",
   redirect: { name: "LandingPage" },
 })
 
