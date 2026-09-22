d65e6d92cdfeea563a18b4301355eb862df067f9
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-22 15:40:41 +0200

    Redirige vers la page de toutes les TD quand la campagne est terminée

--- 2024-frontend/src/router/canteen-routes.js
+++ 2024-frontend/src/router/canteen-routes.js
@@ -5,8 +5,8 @@ import GestionnaireCantineTeledeclarationEnCours from "@/views/GestionnaireCanti
 import GestionnaireCantineTeledeclarations from "@/views/GestionnaireCantineTeledeclarations.vue"
 import GestionnaireCantineGestionnaires from "@/views/GestionnaireCantineGestionnaires.vue"
 import GestionnaireCantinePagePublique from "@/views/GestionnaireCantinePagePublique.vue"
-
 import LayoutSidebarCanteen from "@/layouts/LayoutSidebarCanteen.vue"
+import campaignService from "@/services/campaigns.js"
 
 /* Route */
 const currentYear = window.TELEDECLARATION_YEAR
@@ -48,6 +48,11 @@ const canteenRoutes = {
           { to: { name: "GestionnaireTableauDeBord" }, title: "Mon tableau de bord" },
           { to: { name: "GestionnaireCantine" }, useCanteenName: true },
         ],
+      },
+      beforeEnter: async (to) => {
+        const currentCampaign = await campaignService.getCampaignDates(currentYear)
+        if (currentCampaign.inTeledeclaration || currentCampaign.inCorrection) return true
+        return { name: "GestionnaireCantineTeledeclarations", params: to.params }
       },
     },
     {
