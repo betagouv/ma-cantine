bf62661b2784190090804123e6a4b1b47645275f
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-22 18:29:54 +0200

    Ajoute un commentaire pour ne pas oublier de le corriger

--- 2024-frontend/src/stores/teledeclaration.js
+++ 2024-frontend/src/stores/teledeclaration.js
@@ -63,6 +63,7 @@ const useStoreTeledeclaration = defineStore("teledeclaration", () => {
     for (let i = 0; i < errorsKeys.length; i++) {
       errorList.push({ field: errorsKeys[i], message: errorsValues[i] })
     }
+    // DOES NOT WIRK : Errors duplicated with "/check" errors
     diagnosticErrors.value = [...diagnosticErrors.value, ...errorList]
   }
 
