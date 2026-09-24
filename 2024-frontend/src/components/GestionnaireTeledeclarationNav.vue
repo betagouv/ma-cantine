5d7ede3e5a046e0144af761f396a3ff557470045
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-24 16:02:29 +0200

    Ajoute les badges en non-rempli pour l'isntant

--- 2024-frontend/src/components/GestionnaireTeledeclarationNav.vue
+++ 2024-frontend/src/components/GestionnaireTeledeclarationNav.vue
@@ -28,6 +28,19 @@ const currentCanteenAction = computed(() => {
   return canteen?.action || null
 })
 const diagnosticBadge = computed(() => diagnosticsBadgeService.getBadge(currentCanteenAction.value, currentCampaignInformations.value))
+
+/* Check */
+// TODO : make it dynamic with the check
+const approIconLink = computed(() => '/static/images/badges/badge-appro-disabled.svg')
+const infoIconLink = computed(() => '/static/images/badges/badge-info-disabled.svg')
+const wasteIconLink = computed(() => '/static/images/badges/badge-waste-disabled.svg')
+const diversificationIconLink = computed(() => '/static/images/badges/badge-diversification-disabled.svg')
+const plasticIconLink = computed(() => '/static/images/badges/badge-plastic-disabled.svg')
+const approIconAlt = computed(() => 'Volet approvisionnement non complété')
+const infoIconAlt = computed(() => 'Volet information convive non complété')
+const wasteIconAlt = computed(() => 'Volet gestion du gaspillage alimentaire non complété')
+const diversificationIconAlt = computed(() => 'Volet diversification des protéines non complété')
+const plasticIconAlt = computed(() => 'Volet réductions du plastique non complété')
 </script>
 <template>
   <div
@@ -37,7 +50,14 @@ const diagnosticBadge = computed(() => diagnosticsBadgeService.getBadge(currentC
   >
     <router-link :to="{ name: 'GestionnaireCantineTeledeclarationEnCours' }" class="gestionnaire-teledeclaration-nav__bloc fr-sidemenu__link">
       <DsfrBadge :label="diagnosticBadge.label" :type="diagnosticBadge.type" />
-      {{ isGroupe ? `Télédéclaration ${currentYear}` : `Ma télédéclaration ${currentYear}` }}
+      <span>{{ isGroupe ? `Télédéclaration ${currentYear}` : `Ma télédéclaration ${currentYear}` }}</span>
+      <span class="gestionnaire-teledeclaration-nav__macarons-container">
+        <img :src="approIconLink" :alt="approIconAlt">
+        <img :src="infoIconLink" :alt="infoIconAlt">
+        <img :src="wasteIconLink" :alt="wasteIconAlt">
+        <img :src="diversificationIconLink" :alt="diversificationIconAlt">
+        <img :src="plasticIconLink" :alt="plasticIconAlt">
+      </span>
     </router-link>
   </div>
 </template>
@@ -53,6 +73,22 @@ const diagnosticBadge = computed(() => diagnosticsBadgeService.getBadge(currentC
     flex-direction: column !important;
     align-items: flex-start !important;
     gap: 0.5rem;
+  }
+
+  &__macarons-container {
+    display: flex;
+    gap: 0.5rem;
+    border: 1px solid var(--border-disabled-grey);
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    width: 70%;
+
+    img {
+      flex: 1 1 0;
+      min-width: 0;
+      width: 100%;
+      height: auto;
+    }
   }
 }
 </style>
