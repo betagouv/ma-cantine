d10529b24b5231b40a6d585b1a076c7c1d8ba0b4
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-24 15:45:33 +0200

    Ajoute le badge de la TD dans le lien

--- 2024-frontend/src/components/GestionnaireTeledeclarationNav.vue
+++ 2024-frontend/src/components/GestionnaireTeledeclarationNav.vue
@@ -1,17 +1,15 @@
 <script setup>
 import { computed } from "vue"
+import { computedAsync } from "@vueuse/core"
 import { useRoute } from "vue-router"
 import { storeToRefs } from "pinia"
 import { useStoreCampaignDates } from "@/stores/campaignDates.js"
+import diagnosticsBadgeService from "@/services/diagnosticsBadge.js"
+import canteensService from "@/services/canteens.js"
 
 const props = defineProps(["canteen"])
-
-/* Route */
 const route = useRoute()
 const teledeclarationEnCoursActive = computed(() => route.name === "GestionnaireCantineTeledeclarationEnCours")
-
-/* Canteen */
-const isGroupe = computed(() => props.canteen?.isGroupe)
 
 /* Campaign dates */
 const campaignDatesStore = useStoreCampaignDates()
@@ -19,15 +17,26 @@ const { currentCampaignInformations } = storeToRefs(campaignDatesStore)
 const currentYear = window.TELEDECLARATION_YEAR
 const isInTeledeclaration = computed(() => currentCampaignInformations.value.inTeledeclaration || false)
 const isInCorrection = computed(() => currentCampaignInformations.value.inCorrection || false)
-</script>
 
+/* Canteen */
+const tdYear = window.TELEDECLARATION_YEAR
+const isGroupe = computed(() => props.canteen?.isGroupe)
+const allCanteens = computedAsync(async () => await canteensService.fetchCanteensActions(tdYear), [])
+const currentCanteenAction = computed(() => {
+  if (!allCanteens.value) return null
+  const canteen = allCanteens.value.find((canteen) => canteen.id === props.canteen.id)
+  return canteen?.action || null
+})
+const diagnosticBadge = computed(() => diagnosticsBadgeService.getBadge(currentCanteenAction.value, currentCampaignInformations.value))
+</script>
 <template>
   <div
     v-if="isInTeledeclaration || isInCorrection"
     :class="{ 'fr-sidemenu__item--active': teledeclarationEnCoursActive }"
     class="gestionnaire-teledeclaration-nav fr-sidemenu__item"
   >
-    <router-link :to="{ name: 'GestionnaireCantineTeledeclarationEnCours' }" class="fr-sidemenu__link">
+    <router-link :to="{ name: 'GestionnaireCantineTeledeclarationEnCours' }" class="gestionnaire-teledeclaration-nav__bloc fr-sidemenu__link">
+      <DsfrBadge :label="diagnosticBadge.label" :type="diagnosticBadge.type" />
       {{ isGroupe ? `Télédéclaration ${currentYear}` : `Ma télédéclaration ${currentYear}` }}
     </router-link>
   </div>
@@ -37,6 +46,13 @@ const isInCorrection = computed(() => currentCampaignInformations.value.inCorrec
 .gestionnaire-teledeclaration-nav {
   [href] {
     background: none !important;
+  }
+
+  &__bloc {
+    display: flex;
+    flex-direction: column !important;
+    align-items: flex-start !important;
+    gap: 0.5rem;
   }
 }
 </style>
