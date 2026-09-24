750d68646338cbc74d2df01a6952a6d05ac49047
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-24 06:49:00 +0200

    Affiche le bloc pour le cout repas

--- 2024-frontend/src/views/GestionnaireTunnelApproEgalim.vue
+++ 2024-frontend/src/views/GestionnaireTunnelApproEgalim.vue
@@ -11,12 +11,16 @@ import DiagnosticEgalimComplete from "@/components/DiagnosticEgalimComplete.vue"
 const storeTeledeclaration = useStoreTeledeclaration()
 const { diagnostic } = storeToRefs(storeTeledeclaration)
 const diagIsSimple = computed(() => diagnostic.value.diagnosticType === "SIMPLE")
+const coutRepas = computed(() => diagnostic.value.coutRepas || '-')
 </script>
 <template>
   <h2 class="fr-h5">1. Total des approvisionnements toutes familles de produits confondus :</h2>
   <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top fr-mb-4w">
     <div class="fr-col-12 fr-col-md-7">
       <TunnelTeledeclarationField name="valeurTotale" size="full" />
+      <DsfrCallout>
+        Estimation du coût moyen par repas servi : <span class="fr-text--bold">{{ coutRepas }} €</span>
+      </DsfrCallout>
     </div>
     <div class="fr-col-12 fr-col-md-5">
       <AppHelpCard title="Comment comptabiliser les produits ayant plusieurs labels ?">
