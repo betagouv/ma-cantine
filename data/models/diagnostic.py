f2f47e03e91654f98d6cd2b6c42a902a2abe6686
Raphael Odini <raphodn@users.noreply.github.com>
2026-09-24 15:40:31 +0200

    add comment

--- data/models/diagnostic.py
+++ data/models/diagnostic.py
@@ -587,6 +587,17 @@ class Diagnostic(models.Model):
         "valeur_egalim_agg",
     ]
 
+    SIMPLIFIED_DIAGNOSTIC_FIELDS = [
+        "valeur_bio",
+        "valeur_bio_dont_commerce_equitable",
+        "valeur_siqo",
+        "valeur_externalites_performance",
+        "valeur_egalim_autres",
+        "valeur_egalim_autres_dont_commerce_equitable",
+        "valeur_viandes_volailles_egalim",
+        "valeur_produits_de_la_mer_egalim",
+    ]
+
     APPRO_FAMILY_FIELDS = [
         "valeur_viandes_volailles",
         "valeur_produits_de_la_mer",
@@ -1983,6 +1994,7 @@ class Diagnostic(models.Model):
                 setattr(self, field_name, 0)
 
     def populate_simplified_diagnostic_values(self):
+        # NOTE: SIMPLIFIED_DIAGNOSTIC_FIELDS
         self.valeur_bio = self.label_group_sum("bio")
         self.valeur_bio_dont_commerce_equitable = self.label_sum("bio_dont_commerce_equitable")
         self.valeur_siqo = self.label_group_sum("siqo")
@@ -2009,6 +2021,7 @@ class Diagnostic(models.Model):
         self.valeur_produits_de_la_mer_egalim = total_fish_egalim
 
     def populate_aggregated_values(self):
+        # NOTE: AGGREGATED_DIAGNOSTIC_FIELDS
         self.valeur_bio_agg = self.label_group_sum("bio")
         self.valeur_siqo_agg = self.label_group_sum("siqo")
         self.valeur_externalites_performance_agg = self.label_group_sum("externalites_performance")
@@ -2018,6 +2031,7 @@ class Diagnostic(models.Model):
         self.valeur_egalim_agg = self.label_group_group_sum("egalim")
 
     def populate_egalim_stats(self):
+        # NOTE: EGALIM_STATS_FIELDS
         self.pourcentage_bio = self.compute_pourcentage_bio()
         self.pourcentage_egalim = self.compute_pourcentage_egalim()
         self.pourcentage_egalim_hors_bio = self.compute_pourcentage_egalim_hors_bio()
