ff02648be70a18e79985f924ad5ade10a14e369c
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-09-24 07:26:34 +0200

    Met à jour le cout repas au changement de la valeur totale

--- 2024-frontend/src/views/GestionnaireTunnelApproEgalim.vue
+++ 2024-frontend/src/views/GestionnaireTunnelApproEgalim.vue
@@ -7,17 +7,26 @@ import AppHelpCard from "@/components/AppHelpCard.vue"
 import TunnelTeledeclarationField from "@/components/TunnelTeledeclarationField.vue"
 import DiagnosticEgalimSimple from "@/components/DiagnosticEgalimSimple.vue"
 import DiagnosticEgalimComplete from "@/components/DiagnosticEgalimComplete.vue"
+import teledeclarationFields from "@/data/teledeclaration.json"
 
+/* Fields names */
+const valeurTotalFieldName = teledeclarationFields.groups["valeurTotale"][0]
+const coutRepasFieldName = teledeclarationFields.groups["coutRepas"][0]
+
+/* Teledeclaration */
 const storeTeledeclaration = useStoreTeledeclaration()
 const { diagnostic } = storeToRefs(storeTeledeclaration)
 const diagIsSimple = computed(() => diagnostic.value.diagnosticType === "SIMPLE")
-const coutRepas = computed(() => diagnostic.value.coutRepas || '-')
+
+/* Meal count */
+const coutRepas = computed(() => diagnostic.value[coutRepasFieldName] || '-')
+const updateCoutRepas = async () => await storeTeledeclaration.updateMealCount()
 </script>
 <template>
   <h2 class="fr-h5">1. Total des approvisionnements toutes familles de produits confondus :</h2>
   <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top fr-mb-4w">
     <div class="fr-col-12 fr-col-md-7">
-      <TunnelTeledeclarationField name="valeurTotale" size="full" />
+      <TunnelTeledeclarationField :name="valeurTotalFieldName" size="full" @change="updateCoutRepas" />
       <DsfrCallout>
         Estimation du coût moyen par repas servi : <span class="fr-text--bold">{{ coutRepas }} €</span>
       </DsfrCallout>
